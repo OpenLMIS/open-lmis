3f0b516c4523f6fb698341a03acc9fab00aea066
Manan <manan.bharara@gmail.com>
2012-10-17 14:44:52 +0530

    #123 | Manan, Sandeep | Completed Setup of Spring MVC for combined web app

---
+++ modules/openlmis-web/src/main/java/com/thoughtworks/openlmis_core/controller/HomeController.java
@@ -0,0 +1,24 @@
+package com.thoughtworks.openlmis_core.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Manan
+ * Date: 10/16/12
+ * Time: 12:44 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "*", method = RequestMethod.GET)
+    public ModelAndView loadHelloWorld(){
+      //  System.out.println("in controller");
+           return new ModelAndView("openlmis");
+    }
+}
---
+++ modules/openlmis-web/src/main/webapp/WEB-INF/views/openlmis.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World!-Open Lmis</h2>
+</body>
+</html>
