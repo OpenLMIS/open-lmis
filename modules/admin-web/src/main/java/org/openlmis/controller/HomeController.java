7f4392d627bc28cc36fab832b30d47dfad2f1339
shruthi <shruthd@thoughtworks.com>
2012-10-18 11:17:23 +0530

    Balvinder, Shruthi | #0 | Refactoring package structure to use org/openlmis

--- modules/admin-web/src/main/java/org/openlmis/controller/HomeController.java
+++ modules/admin-web/src/main/java/org/openlmis/controller/HomeController.java
@@ -1,28 +1,22 @@
-package com.thoughtworks.openlmis_admin.controller;
+package org.openlmis.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Manan
- * Date: 10/17/12
- * Time: 11:05 AM
- * To change this template use File | Settings | File Templates.
- */
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "home", method = RequestMethod.GET)
-    public ModelAndView loadHelloWorld(){
+    public ModelAndView loadHelloWorld() {
         //  System.out.println("in controller");
         return new ModelAndView("admin-hello");
     }
 
-    @RequestMapping(value = "",method = RequestMethod.GET)
-    public ModelAndView loadHomePage(){
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ModelAndView loadHomePage() {
         return new ModelAndView("redirect:home");
     }
+
 }
--- modules/openlmis-web/src/main/java/org/openlmis/controller/HomeController.java
+++ modules/openlmis-web/src/main/java/org/openlmis/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.thoughtworks.openlmis_core.controller;
+package org.openlmis.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
