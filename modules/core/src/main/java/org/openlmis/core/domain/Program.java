724bfb62281bae04dbd2cba85207bd31640a8552
shruthi <shruthd@thoughtworks.com>
2012-10-18 17:53:45 +0530

    Balvinder, Shruthi | #229 | Setup the initial db migration framework using flyway.
    gradle setupDB - to setup migration schema(recreates database)
    gradle migrateDB - to run migrations

---
+++ modules/core/src/main/java/org/openlmis/core/domain/Program.java
@@ -0,0 +1,9 @@
+package org.openlmis.core.domain;
+
+public class Program {
+
+    String id;
+    String name;
+    String description;
+
+}
---
+++ modules/core/src/main/resources/db/migration/V1_1__Insert_Program.sql
@@ -0,0 +1,2 @@
+insert into program(name, description) values('ARV', 'some ARV program');
+insert into program(name, description) values('HIV', 'some HIV program');
