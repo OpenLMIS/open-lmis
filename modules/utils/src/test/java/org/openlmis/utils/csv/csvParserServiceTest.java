8bc57679dbd9f8cab906eac87e150a90ed74637f
Balvinder Khurana <balvindk@thoughtworks.com>
2012-10-30 10:09:33 +0530

    Balvinder, Manan | #122 | Fixed test for upload

--- modules/utils/src/test/java/org/openlmis/utils/csv/csvParserServiceTest.java
+++ modules/utils/src/test/java/org/openlmis/utils/csv/csvParserServiceTest.java
@@ -2,9 +2,11 @@ package org.openlmis.utils.csv;
 
 import org.junit.Test;
 import org.openlmis.core.domain.Product;
+import org.springframework.remoting.support.UrlBasedRemoteAccessor;
 import org.supercsv.cellprocessor.ift.CellProcessor;
 
 import java.io.File;
+import java.net.URL;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -15,7 +17,7 @@ public class CsvParserServiceTest {
     @Test
     public void shouldGetListOfListOfObjectsForCsvFile() throws Exception {
 
-        File csvFile = new File("/Users/Manan/Documents/open-lmis/modules/utils/src/test/java/org/openlmis/utils/resources/BulkLoadProducts.csv");
+        File csvFile = new File(this.getClass().getResource("/BulkLoadProducts.csv").getFile());
         CsvParserService csvParserService = new CsvParserService();
         CellProcessor[] csvCellProcessor = CsvCellProcessor.getProductProcessors();
 
