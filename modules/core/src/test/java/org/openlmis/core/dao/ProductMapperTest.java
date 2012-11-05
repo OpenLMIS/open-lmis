f9ef0fbb7ae5166cee00f54aeb8641f791b59976
Rahul Joshi <rahulj@thoughtworks.com>
2012-11-05 11:26:05 +0530

    |#122| Manan & Rahul added migration script for product table

--- modules/core/src/test/java/org/openlmis/core/dao/ProductMapperTest.java
+++ modules/core/src/test/java/org/openlmis/core/dao/ProductMapperTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath*:applicationContext-requisition.xml")
+@ContextConfiguration(locations = "classpath*:applicationContext-core.xml")
 public class ProductMapperTest {
 
     @Test
