e58d75f4029a67644cf7ac49269bde1254b15bc5
Manan <manan.bharara@gmail.com>
2012-11-07 14:48:52 +0530

    #78 | Manan, Shruthi | Created a route to select facility  on create rnr page

--- modules/openlmis-web/src/main/webapp/public/js/module/rnr.js
+++ modules/openlmis-web/src/main/webapp/public/js/module/rnr.js
@@ -1,4 +1,7 @@
 'use strict';
 angular.module('rnr', ['openlmis']).
     config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/select-facility', {templateUrl:'select-facility.html'}).
+        otherwise({redirectTo:'/select-facility'});
 }]);
