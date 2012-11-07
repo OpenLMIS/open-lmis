52d53889ab3ea37c91579fb7a9e7cd510a9fd062
Manan <manan.bharara@gmail.com>
2012-11-07 17:17:44 +0530

    #78| Shruthi & manan | Navigating to page for selecting facilities and from there navigating to selecting a program

--- modules/openlmis-web/src/main/webapp/public/js/module/rnr.js
+++ modules/openlmis-web/src/main/webapp/public/js/module/rnr.js
@@ -1,7 +1,9 @@
 'use strict';
-angular.module('rnr', ['openlmis']).
+angular.module('rnr', ['openlmis', 'openlmis.services']).
     config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
         when('/select-facility', {templateUrl:'select-facility.html'}).
+        when('/select-program', {templateUrl:'select-program.html'}).
+        when('/rnr-header', {templateUrl:'rnr-header.html'}).
         otherwise({redirectTo:'/select-facility'});
 }]);
