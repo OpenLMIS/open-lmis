a3f15f8585e71b7a1d2683797d84d3b187164685
Manjyot <manjyots@thoughtworks.com>
2012-11-12 14:41:53 +0530

    |Manjyot, Lokesh|#61| Added code to run functional tests on Public QA IP|

--- functional-tests/src/main/java/org/openlmis/UiUtils/DriverFactory.java
+++ functional-tests/src/main/java/org/openlmis/UiUtils/DriverFactory.java
@@ -35,9 +35,10 @@ public class DriverFactory {
 
         if (driverType.equalsIgnoreCase("Firefox")) {
             return createFirefoxDriver(enableJavascript);
-        } else if (driverType.equalsIgnoreCase("IE")) {
-            return new InternetExplorerDriver();
         }
+//        } else if (driverType.equalsIgnoreCase("IE")) {
+//            return new InternetExplorerDriver();
+//        }
 //        else if (driverType.equals("<path_to_chromedriver> /Users/Manjot/Downloads/chromedriver"))
 //        {
 //                return new ChromeDriver();
