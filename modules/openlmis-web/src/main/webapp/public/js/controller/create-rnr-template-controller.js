67b0e56dd9835118f5576c2d37de61ded4ef15fb
Manan <manan.bharara@gmail.com>
2012-11-19 11:37:53 +0530

    |#00|+Rahul| redirect to create r&r template page on error

--- modules/openlmis-web/src/main/webapp/public/js/controller/create-rnr-template-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/controller/create-rnr-template-controller.js
@@ -1,16 +1,19 @@
 function CreateRnrTemplateController($scope, Program) {
-    Program.get({}, function (data) {   //success
-        $scope.programs = data.programList;
-    }, {});
+  Program.get({}, function (data) {   //success
+    $scope.programs = data.programList;
+  }, {});
 }
 
-function SaveRnrTemplateController($scope, RnRColumnList, $http) {
-    RnRColumnList.get({programId:$scope.program.id}, function (data) {   //success
-        $scope.rnrColumnsList = data.rnrColumnList;
-    }, {});
+function SaveRnrTemplateController($scope, RnRColumnList, $http, $location) {
+  var id = ($scope.program? $scope.program.id: "");
+  RnRColumnList.get({programId:id}, function (data) {   //success
+    $scope.rnrColumnsList = data.rnrColumnList;
+  }, function () {
+    $location.path('select-program');
+  });
 
-    $scope.createProgramRnrTemplate = function () {
-        $http.post('/admin/rnr/' + $scope.program.id + '/columns.json', $scope.rnrColumnsList);
-        $scope.message = "saved successfully";
-    }
+  $scope.createProgramRnrTemplate = function () {
+    $http.post('/admin/rnr/' + $scope.program.id + '/columns.json', $scope.rnrColumnsList);
+    $scope.message = "saved successfully";
+  }
 }
