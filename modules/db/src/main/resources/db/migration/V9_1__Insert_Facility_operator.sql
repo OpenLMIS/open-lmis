20582e8ef31440ebdb3a54c24bbf103c3461255f
Sandeep <sandeeb@thoughtworks.com>
2012-11-20 14:48:55 +0530

    manan, sandeep| #0 | removing id from facility operator table

--- modules/db/src/main/resources/db/migration/V9_1__Insert_Facility_operator.sql
+++ modules/db/src/main/resources/db/migration/V9_1__Insert_Facility_operator.sql
@@ -1,6 +1,6 @@
 INSERT INTO facility_operator
-(id,code, text, display_order) VALUES
-(1,'MoH','MoH',1),
-(2,'NGO','NGO',2),
-(3,'FBO','FBO',3),
-(4,'Private','Private',4);
+(code, text, display_order) VALUES
+('MoH','MoH',1),
+('NGO','NGO',2),
+('FBO','FBO',3),
+('Private','Private',4);
--- modules/db/src/main/resources/db/migration/V9__Create_Table_Facility_Operator.sql
+++ modules/db/src/main/resources/db/migration/V9__Create_Table_Facility_Operator.sql
@@ -1,7 +1,6 @@
 DROP TABLE IF EXISTS facility_operator;
 CREATE TABLE facility_operator   (
-   id INTEGER PRIMARY KEY,
-   code varchar UNIQUE,
+   code varchar PRIMARY KEY,
    text varchar(20),
    display_order INTEGER
 );
