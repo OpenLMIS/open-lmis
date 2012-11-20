62cf74d08b3faadcddf35463dbf4c63a2bdb2717
Rahul Joshi <rahulj@thoughtworks.com>
2012-11-20 15:48:32 +0530

    |#000|+Shruthi| added user friendly headers for facility upload

--- modules/core/src/main/java/org/openlmis/core/domain/Facility.java
+++ modules/core/src/main/java/org/openlmis/core/domain/Facility.java
@@ -13,66 +13,97 @@ import java.util.Date;
 @NoArgsConstructor
 public class Facility implements Importable {
 
-    @ImportField(mandatory = true, name="FacilityCode")
+    @ImportField(mandatory = true, name = "Facility Code")
     private String code;
-    @ImportField(mandatory = true, name="FacilityName")
+
+    @ImportField(mandatory = true, name = "Facility Name")
     private String name;
-    @ImportField(name = "FacilityDescription")
+
+    @ImportField(name = "Facility Description")
     private String description;
-    @ImportField
+
+    @ImportField(name = "GLN")
     private String GLN;
-    @ImportField(name = "FacilityMainPhone")
+
+    @ImportField(name = "Facility Main Phone")
     private String mainPhone;
-    @ImportField(name = "FacilityFax")
+
+    @ImportField(name = "Facility Fax")
     private String fax;
-    @ImportField(name = "FacilityAddress1")
+
+    @ImportField(name = "Facility Address1")
     private String address1;
-    @ImportField(name = "FacilityAddress2")
+
+    @ImportField(name = "Facility Address2")
     private String address2;
-    @ImportField(mandatory = true, type = "long", name="GeographicZoneID")
+
+    @ImportField(mandatory = true, type = "long", name = "Geographic Zone ID")
     private long geographicZone;
-    @ImportField(mandatory = true, type = "long", name = "FacilityTypeID")
+
+    @ImportField(mandatory = true, type = "long", name = "Facility Type ID")
     private long type;
-    @ImportField(type = "long" )
+
+    @ImportField(type = "long", name = "Catchment Population")
     private long catchmentPopulation;
-    @ImportField(type = "double", name = "FacilityLAT")
+
+    @ImportField(type = "double", name = "Facility LAT")
     private double latitude;
-    @ImportField(type = "double", name = "FacilityLONG")
+
+    @ImportField(type = "double", name = "Facility LONG")
     private double longitude;
-    @ImportField(type = "double", name = "FacilityAltitude")
+
+    @ImportField(type = "double", name = "Facility Altitude")
     private double altitude;
-    @ImportField(type = "String", name = "FacilityOperatedBy")
+
+    @ImportField(type = "String", name = "Facility Operated By")
     private String operatedBy;
-    @ImportField (type = "double")
+
+    @ImportField(type = "double", name = "Cold Storage Gross Capacity")
     private double coldStorageGrossCapacity;
-    @ImportField (type = "double")
+
+    @ImportField(type = "double", name = "Cold Storage Net Capacity")
     private double coldStorageNetCapacity;
-    @ImportField(type = "boolean", name = "FacilitySuppliesOthers")
+
+    @ImportField(type = "boolean", name = "Facility Supplies Others")
     private boolean suppliesOthers;
-    @ImportField(type = "boolean", mandatory = true, name="FacilityIsSDP")
+
+    @ImportField(type = "boolean", mandatory = true, name = "Facility Is SDP")
     private boolean sdp;
-    @ImportField(type = "boolean", name="FacilityIsOnline")
-    private boolean online;
-    @ImportField(type = "boolean", name="FacilityHasElectricity")
+
+    @ImportField(type = "boolean", name = "Facility Has Electricity")
     private boolean hasElectricity;
-    @ImportField(type = "boolean", name="FacilityHasElectronicSCC")
+
+    @ImportField(type = "boolean", name = "Facility Is Online")
+    private boolean online;
+
+    @ImportField(type = "boolean", name = "Facility Has Electronic SCC")
     private boolean hasElectronicScc;
-    @ImportField(type = "boolean", name="FacilityHasElectronicDAR")
+
+    @ImportField(type = "boolean", name = "Facility Has Electronic DAR")
     private boolean hasElectronicDar;
-    @ImportField(type = "boolean", mandatory = true, name="IsActive")
+
+    @ImportField(type = "boolean", mandatory = true, name = "Is Active")
     private boolean active;
-    @ImportField(type = "Date", mandatory = true, name="FacilityGoLiveDate")
+
+    @ImportField(type = "Date", mandatory = true, name = "Facility Go Live Date")
     private Date goLiveDate;
-    @ImportField(type = "Date", name = "FacilityGoDownDate")
+
+    @ImportField(type = "Date", name = "Facility Go Down Date")
     private Date goDownDate;
-    @ImportField(type = "boolean", name = "IsSatelliteFacility")
+
+    @ImportField(type = "boolean", name = "Is Satellite Facility")
     private boolean satellite;
-    @ImportField(name = "SatelliteParentCode")
+
+    @ImportField(name = "Satellite Parent ID")
     private String satelliteParentCode;
-    @ImportField(name = "FacilityComments")
+
+    @ImportField(name = "Facility Comments")
     private String comment;
-    @ImportField(type = "boolean")
+
+    @ImportField(type = "boolean", name = "Do Not Display")
     private boolean doNotDisplay;
+
     private String modifiedBy;
+
     private Date modifiedDate;
 }
