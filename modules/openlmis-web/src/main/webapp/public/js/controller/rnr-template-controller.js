f7ba6dda61bb3d680e765bb8909e142e6a023eac
Jaideep Singh <jaideep@thoughtworks.com>
2012-12-03 13:03:16 +0530

    Jaideep | UI tweaks and fixes

--- modules/openlmis-web/src/main/webapp/public/js/controller/rnr-template-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/controller/rnr-template-controller.js
@@ -4,56 +4,6 @@ function CreateRnrTemplateController($scope, Program) {
     }, {});
 }
 
-angular.module('ui.directives').directive('uiSortable', [
-  'ui.config', function(uiConfig) {
-    var options;
-    options = {};
-    if (uiConfig.sortable != null) {
-      angular.extend(options, uiConfig.sortable);
-    }
-    return {
-      require: '?ngModel',
-      link: function(scope, element, attrs, ngModel) {
-        var onStart, onUpdate, opts, _start, _update;
-        opts = angular.extend({}, options, scope.$eval(attrs.uiSortable));
-        if (ngModel != null) {
-          onStart = function(e, ui) {
-            return ui.item.data('ui-sortable-start', ui.item.index());
-          };
-          onUpdate = function(e, ui) {
-            var end, start;
-            start = ui.item.data('ui-sortable-start');
-            end = ui.item.index();
-            ngModel.$modelValue.splice(end, 0, ngModel.$modelValue.splice(start, 1)[0]);
-            return scope.$apply();
-          };
-          if (opts.start != null) {
-            _start = opts.start;
-            opts.start = function(e, ui) {
-              onStart(e, ui);
-              _start(e, ui);
-              return scope.$apply();
-            };
-          } else {
-            opts.start = onStart;
-          }
-          if (opts.update != null) {
-            _update = opts.update;
-            opts.update = function(e, ui) {
-              onUpdate(e, ui);
-              _update(e, ui);
-              return scope.$apply();
-            };
-          } else {
-            opts.update = onUpdate;
-          }
-        }
-        return element.sortable(opts);
-      }
-    };
-  }
-]);
-
 function SaveRnrTemplateController($scope, RnRColumnList, $http, $location) {
 
     var code = ($scope.program ? $scope.program.code : "");
