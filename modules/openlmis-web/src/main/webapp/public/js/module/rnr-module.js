1dddb6cef2a753639234869d0062855793840e1b
Balvinder Khurana <balvindk@thoughtworks.com>
2012-12-06 17:10:00 +0530

    |#323| +Rahul| indentation

--- modules/openlmis-web/src/main/webapp/public/js/module/rnr-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/module/rnr-module.js
@@ -1,27 +1,27 @@
 angular.module('rnr', ['openlmis']).config(['$routeProvider', function ($routeProvider) {
-          $routeProvider.
-              when('/init-rnr', {controller:InitiateRnrController, templateUrl:'partials/init.html'}).
-              when('/create-rnr', {controller:CreateRnrController, templateUrl:'partials/create.html'}).
-              otherwise({redirectTo:'/init-rnr'});
-      }]).directive('rnrValidator', function() {
-                   return {
-                       require: '?ngModel',
-                       link: function(scope, element, attrs, ctrl) {
-                           var validationFunction = scope[attrs.rnrValidator];
-                           ctrl.$parsers.unshift(function(viewValue) {
-                               if (validationFunction(viewValue, element.attr('name'))) {
-                                   ctrl.$setValidity(element.attr('name'), true);
-                                   ctrl.$setValidity('rnrError', true);
-                                   if(viewValue =="") viewValue = undefined;
-                                   return viewValue;
-                               } else {
-                                   ctrl.$setValidity(element.attr('name'), false);
-                                   ctrl.$setValidity('rnrError', false);
-                                   return undefined;
-                               }
-                           });
-                       }
-                   };
-         });
+    $routeProvider.
+        when('/init-rnr', {controller:InitiateRnrController, templateUrl:'partials/init.html'}).
+        when('/create-rnr', {controller:CreateRnrController, templateUrl:'partials/create.html'}).
+        otherwise({redirectTo:'/init-rnr'});
+}]).directive('rnrValidator', function () {
+        return {
+            require:'?ngModel',
+            link:function (scope, element, attrs, ctrl) {
+                var validationFunction = scope[attrs.rnrValidator];
+                ctrl.$parsers.unshift(function (viewValue) {
+                    if (validationFunction(viewValue, element.attr('name'))) {
+                        ctrl.$setValidity(element.attr('name'), true);
+                        ctrl.$setValidity('rnrError', true);
+                        if (viewValue == "") viewValue = undefined;
+                        return viewValue;
+                    } else {
+                        ctrl.$setValidity(element.attr('name'), false);
+                        ctrl.$setValidity('rnrError', false);
+                        return undefined;
+                    }
+                });
+            }
+        };
+    });
 
 
