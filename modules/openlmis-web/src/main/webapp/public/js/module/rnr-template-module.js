7a4e5e33b191b4ed1a9303cf2befe80738edfe15
Rahul Joshi <rahulj@thoughtworks.com>
2012-12-07 12:31:51 +0530

    |#0| fixing the angular controller route

--- modules/openlmis-web/src/main/webapp/public/js/module/rnr-template-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/module/rnr-template-module.js
@@ -3,7 +3,6 @@ angular.module('createRnRTemplate', ['openlmis']).
     config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
         when('/select-program', {controller:ConfigureRnRTemplateController, templateUrl:'../select-program.html'}).
-        when('/create-rnr-template', {controller:ConfigureRnRTemplateController, templateUrl:'partials/form.html'}).
-        when('/save-rnr-template', {controller:SaveRnrTemplateController, templateUrl:'partials/form.html'}).
+        when('/create-rnr-template', {controller:SaveRnrTemplateController, templateUrl:'partials/form.html'}).
         otherwise({redirectTo:'/select-program'});
 }]);
