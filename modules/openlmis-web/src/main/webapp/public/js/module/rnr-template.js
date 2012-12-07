3acfef7189196087eb8d1485bec0d557ca48ee44
Rahul Joshi <rahulj@thoughtworks.com>
2012-12-07 10:44:40 +0530

    |#0|  renamed rnr-template.js to rnr-template-module.js

--- modules/openlmis-web/src/main/webapp/public/js/module/rnr-template.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('createRnRTemplate', ['openlmis']).
-  config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.
-    when('/select-program', {controller:ConfigureRnRTemplateController, templateUrl:'../select-program.html'}).
-    when('/create-rnr-template', {controller:SaveRnrTemplateController, templateUrl:'partials/form.html'}).
-    otherwise({redirectTo:'/select-program'});
-}]);
