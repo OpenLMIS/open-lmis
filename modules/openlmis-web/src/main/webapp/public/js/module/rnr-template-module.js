3775ff38c470e303ce518e34a89ccc8cd326ca78
Mridul Khanal <mridulk@thoughtworks.com>
2012-12-07 11:34:55 +0530

    Manan |#000| fixed rnr template page bug

--- modules/openlmis-web/src/main/webapp/public/js/module/rnr-template-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/module/rnr-template-module.js
@@ -3,7 +3,7 @@ angular.module('createRnRTemplate', ['openlmis']).
     config(['$routeProvider', function ($routeProvider) {
     $routeProvider.
         when('/select-program', {controller:ConfigureRnRTemplateController, templateUrl:'../select-program.html'}).
-        when('/create-rnr-template', {controller:CreateRnrTemplateController, templateUrl:'partials/form.html'}).
-        when('/save-rnr-template', {controller:SaveRnRTemplateController, templateUrl:'partials/form.html'}).
+        when('/create-rnr-template', {controller:ConfigureRnRTemplateController, templateUrl:'partials/form.html'}).
+        when('/save-rnr-template', {controller:SaveRnrTemplateController, templateUrl:'partials/form.html'}).
         otherwise({redirectTo:'/select-program'});
 }]);
