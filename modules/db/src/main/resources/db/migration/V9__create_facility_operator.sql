76dd6643495f9d2fe5c3bc14ec04bc7e106bca3f
Rahul Joshi <rahulj@thoughtworks.com>
2012-12-15 00:08:22 +0530

    |#000| renamed facility_operator.display_order to displayOrder

--- modules/db/src/main/resources/db/migration/V9__create_facility_operator.sql
+++ modules/db/src/main/resources/db/migration/V9__create_facility_operator.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS facility_operator;
 CREATE TABLE facility_operator   (
    id  SERIAL PRIMARY KEY,
-   code varchar NOT NULL UNIQUE,
+   code varchar NOT NULL,
    text varchar(20),
-   display_order INTEGER
+   DisplayOrder INTEGER
 );
+
+CREATE UNIQUE INDEX uc_facility_operator ON facility_operator(LOWER(code));
--- seed/facility_operators.sql
+++ seed/facility_operators.sql
@@ -1,6 +1,6 @@
 delete from facility_operator;
 INSERT INTO facility_operator
-(code, text, display_order) VALUES
+(code, text, displayOrder) VALUES
 ('MoH','MoH',1),
 ('NGO','NGO',2),
 ('FBO','FBO',3),
