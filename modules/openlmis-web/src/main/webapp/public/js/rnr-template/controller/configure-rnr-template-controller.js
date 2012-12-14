509dd5ee59042d8a46b074df19e3d7e8e7fab5cf
Rahul Joshi <rahulj@thoughtworks.com>
2012-12-14 23:46:20 +0530

    |#000| adding angular resolve to configure rnr template controller

--- modules/openlmis-web/src/main/webapp/public/js/rnr-template/controller/configure-rnr-template-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/rnr-template/controller/configure-rnr-template-controller.js
@@ -1,17 +1,32 @@
-function ConfigureRnRTemplateController($scope, Program, $location) {
-    Program.get({}, function (data) {   //success
-        $scope.programs = data.programList;
-    }, {});
+function ConfigureRnRTemplateController($scope, programs, $location) {
 
-    $scope.createRnrTemplate = function () {
-        if ($scope.$parent.program != undefined) {
-            $scope.error = "";
-            $location.path('/create-rnr-template/'+$scope.$parent.program.code);
-        }
-        else {
-            $scope.error = "Please select a program";
-        }
-    };
+  $scope.programs = programs;
+
+  $scope.createRnrTemplate = function () {
+    if ($scope.$parent.program != undefined) {
+      $scope.error = "";
+      $location.path('/create-rnr-template/' + $scope.$parent.program.code);
+    } else {
+      $scope.error = "Please select a program";
+    }
+  };
 }
+
+ConfigureRnRTemplateController.resolve = {
+  programs:function ($q, Program, $location, $route, $timeout) {
+    var deferred = $q.defer();
+
+    $timeout(function () {
+      Program.get({}, function (data) { //success
+        deferred.resolve(data.programList);
+      }, function () {
+        location.path('/select-program');
+      });
+    }, 100);
+
+    return deferred.promise;
+  }
+};
+
 
 
--- modules/openlmis-web/src/main/webapp/public/js/rnr-template/module/rnr-template-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/rnr-template/module/rnr-template-module.js
@@ -1,9 +1,15 @@
 'use strict';
-angular.module('createRnRTemplate', ['openlmis']).
-    config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.
-        when('/select-program', {controller:ConfigureRnRTemplateController, templateUrl:'../select-program.html'}).
-        when('/create-rnr-template/:programCode', {controller:SaveRnrTemplateController, templateUrl:'partials/form.html', resolve:
-             SaveRnrTemplateController.resolve }).
-        otherwise({redirectTo:'/select-program'});
+angular.module('createRnRTemplate', ['openlmis']).config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.
+    when('/select-program', {
+      controller:ConfigureRnRTemplateController,
+      templateUrl:'../select-program.html',
+      resolve:ConfigureRnRTemplateController.resolve }).
+
+    when('/create-rnr-template/:programCode', {
+      controller:SaveRnrTemplateController,
+      templateUrl:'partials/form.html',
+      resolve:SaveRnrTemplateController.resolve }).
+
+    otherwise({redirectTo:'/select-program'});
 }]);
