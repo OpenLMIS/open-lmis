789620beb49ea9151170c6943f926943bf9dd9ca
Rahul Joshi <rahulj@thoughtworks.com>
2012-12-17 12:55:43 +0530

    |#000| refactored master_rnr_template

--- modules/db/src/main/resources/db/migration/V3__create_master_template.sql
+++ modules/db/src/main/resources/db/migration/V3__create_master_template.sql
@@ -1,15 +1,15 @@
 Drop TABLE IF EXISTS master_rnr_template;
 CREATE TABLE master_rnr_template (
     id serial PRIMARY KEY,
-    column_name varchar(200) NOT NULL UNIQUE,
-    column_position integer  NOT NULL,
+    name varchar(200) NOT NULL UNIQUE,
+    position integer  NOT NULL,
     source VARCHAR(1) NOT NULL,
-    is_source_configurable boolean NOT NULL,
-    column_label varchar(200),
+    sourceConfigurable boolean NOT NULL,
+    label varchar(200),
     formula varchar(200),
-    column_indicator varchar(3) not null,
-    is_used boolean NOT NULL,
-    is_visible boolean NOT NULL,
-    is_mandatory boolean NOT NULL,
+    indicator varchar(3) not null,
+    used boolean NOT NULL,
+    visible boolean NOT NULL,
+    mandatory boolean NOT NULL,
     description varchar(250)
 );
--- modules/requisition/src/main/java/org/openlmis/rnr/repository/mapper/ProgramRnrColumnMapper.java
+++ modules/requisition/src/main/java/org/openlmis/rnr/repository/mapper/ProgramRnrColumnMapper.java
@@ -21,33 +21,15 @@ public interface ProgramRnrColumnMapper {
   @Select("select 0<(select count(id) as count from program_rnr_template where program_code=#{programCode})")
   boolean isRnrTemplateDefined(@Param("programCode") String programCode);
 
-  @Results(value = {
-      @Result(property = "id", column = "id"),
-      @Result(property = "name", column = "name"),
-      @Result(property = "description", column = "description"),
-      @Result(property = "position", column = "position"),
-      @Result(property = "label", column = "label"),
-      @Result(property = "sourceConfigurable", column = "is_source_configurable"),
-      @Result(property = "formula", column = "formula"),
-      @Result(property = "indicator", column = "indicator"),
-      @Result(property = "used", column = "used"),
-      @Result(property = "visible", column = "visible"),
-      @Result(property = "mandatory", column = "mandatory"),
-      @Result(property = "sourceString", column = "source")
-
-  })
-  @Select("select m.id as id, m.column_name as name, m.description description," +
-      " p.position as position, p.label as label," +
-      " m.formula as formula, m.column_indicator as indicator," +
-      " p.is_visible as visible, m.is_used as used, m.is_mandatory as mandatory," +
-      " p.source as source, m.is_source_configurable" +
-      " from program_rnr_template p INNER JOIN master_rnr_template m" +
-      " ON p.column_id = m.id" +
-      " where p.program_code=#{programCode}" +
-      " ORDER BY visible desc,position")
+  @Select("select m.id, m.name, m.description, m.formula, m.indicator, m.used, m.mandatory, m.sourceConfigurable, " +
+      " p.position, p.label, p.is_visible AS visible, p.source as sourceString" +
+      " FROM program_rnr_template p INNER JOIN master_rnr_template m " +
+      " ON p.column_id = m.id " +
+      " WHERE p.program_code = #{programCode} " +
+      " ORDER BY visible DESC, position")
   List<RnrColumn> getAllRnrColumnsForProgram(String programCode);
 
-  @Update("UPDATE Program_RnR_Template SET " +
+  @Update("UPDATE program_rnr_template SET " +
       "is_visible = #{rnrColumn.visible}, " +
       "label = #{rnrColumn.label}, " +
       "position = #{rnrColumn.position}, " +
@@ -55,46 +37,17 @@ public interface ProgramRnrColumnMapper {
       "WHERE program_code = #{programCode} AND column_id = #{rnrColumn.id}")
   void update(@Param("programCode") String programCode, @Param("rnrColumn") RnrColumn rnrColumn);
 
-  @Results(value = {
-      @Result(property = "id", column = "id"),
-      @Result(property = "name", column = "name"),
-      @Result(property = "description", column = "description"),
-      @Result(property = "position", column = "position"),
-      @Result(property = "label", column = "label"),
-      @Result(property = "formula", column = "formula"),
-      @Result(property = "indicator", column = "indicator"),
-      @Result(property = "used", column = "used"),
-      @Result(property = "visible", column = "visible"),
-      @Result(property = "mandatory", column = "mandatory"),
-      @Result(property = "sourceString", column = "source")
-  })
-  @Select("SELECT m.id AS id, m.column_name AS name, m.description description," +
-      " p.position AS position, p.label AS label," +
-      " m.formula AS formula, m.column_indicator AS indicator," +
-      " p.is_visible AS visible, m.is_used as used, m.is_mandatory AS mandatory," +
-      " p.source AS source" +
+  @Select("SELECT m.id, m.name, m.description, m.used, m.mandatory, m.formula, m.indicator, " +
+      " p.position, p.label, p.is_visible AS visible, p.source as sourceString" +
       " FROM program_rnr_template p INNER JOIN master_rnr_template m" +
       " ON p.column_id = m.id" +
-      " WHERE p.program_code=#{programCode} AND p.is_visible = 'true'" +
+      " WHERE p.program_code = #{programCode} AND p.is_visible = 'true'" +
       " ORDER BY visible desc, position")
   List<RnrColumn> getVisibleProgramRnrColumns(String programCode);
 
 
-  @Select(value = "SELECT master.* FROM master_rnr_template as master ")
-  @Results(value = {
-      @Result(property = "id", column = "id"),
-      @Result(property = "name", column = "column_name"),
-      @Result(property = "description", column = "description"),
-      @Result(property = "position", column = "column_position"),
-      @Result(property = "label", column = "column_label"),
-      @Result(property = "sourceString", column = "source"),
-      @Result(property = "sourceConfigurable", column = "is_source_configurable"),
-      @Result(property = "formula", column = "formula"),
-      @Result(property = "indicator", column = "column_indicator"),
-      @Result(property = "used", column = "is_used"),
-      @Result(property = "visible", column = "is_visible"),
-      @Result(property = "mandatory", column = "is_mandatory")
-  })
+  @Select(value = "SELECT * FROM master_rnr_template")
+  @Results(value = { @Result(property = "sourceString", column = "source")})
   List<RnrColumn> fetchAllMasterRnRColumns();
 
 
