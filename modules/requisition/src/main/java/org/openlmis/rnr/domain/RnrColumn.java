5b8106fe4f1b005633df083a486f8454b39e9be1
Manan <manan.bharara@gmail.com>
2012-12-18 16:22:19 +0530

    Setting default arithmetic validation flag value to true

--- modules/requisition/src/main/java/org/openlmis/rnr/domain/RnrColumn.java
+++ modules/requisition/src/main/java/org/openlmis/rnr/domain/RnrColumn.java
@@ -19,7 +19,7 @@ public class RnrColumn {
     private boolean visible;
     private boolean mandatory;
     private String description;
-    private Boolean validated;
+    private boolean validated = true;
 
     @SuppressWarnings(value = "unused")
     public void setSourceString(String sourceString) {
