0fe42d9c5a775b47d36b07cd7bdc0184a732d66a
Sanchit Bahal <sanchit.bahal@live.com>
2012-12-22 16:35:04 +0530

    | #000 | Moved seed data into db module.

---
+++ modules/db/src/main/resources/seed/seed.sql
@@ -0,0 +1,8 @@
+\i src/main/resources/seed/dosage_units.sql
+\i src/main/resources/seed/facility_operators.sql
+\i src/main/resources/seed/facility_types.sql
+\i src/main/resources/seed/geopolitical_levels.sql
+\i src/main/resources/seed/geographic_zones.sql
+\i src/main/resources/seed/product_forms.sql
+\i src/main/resources/seed/programs.sql
+\i src/main/resources/seed/schedules.sql
--- seed/seed.sql
+++
@@ -1,8 +0,0 @@
-\i ../../seed/dosage_units.sql
-\i ../../seed/facility_operators.sql
-\i ../../seed/facility_types.sql
-\i ../../seed/geopolitical_levels.sql
-\i ../../seed/geographic_zones.sql
-\i ../../seed/product_forms.sql
-\i ../../seed/programs.sql
-\i ../../seed/schedules.sql
