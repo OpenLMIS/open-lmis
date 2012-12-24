0d95dfa84877e22badcb2279777103f82960c699
shibha <shibha20@gmail.com>
2012-12-24 16:56:44 +0530

    |#337| +Sanchit | Added supply lines upload option on UI. Added validations for supply line save.

--- modules/core/src/main/java/org/openlmis/core/repository/SupplyLineRepository.java
+++ modules/core/src/main/java/org/openlmis/core/repository/SupplyLineRepository.java
@@ -1,8 +1,12 @@
 package org.openlmis.core.repository;
 
 import lombok.NoArgsConstructor;
+import org.openlmis.core.domain.SupervisoryNode;
 import org.openlmis.core.domain.SupplyLine;
 import org.openlmis.core.exception.DataException;
+import org.openlmis.core.repository.mapper.FacilityMapper;
+import org.openlmis.core.repository.mapper.ProgramMapper;
+import org.openlmis.core.repository.mapper.SupervisoryNodeMapper;
 import org.openlmis.core.repository.mapper.SupplyLineMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
@@ -14,13 +18,44 @@ public class SupplyLineRepository {
 
     private SupplyLineMapper supplyLineMapper;
 
+    private ProgramMapper programMapper;
+
+    private FacilityMapper facilityMapper;
+
+    private SupervisoryNodeMapper supervisoryNodeMapper;
+
     @Autowired
-    public SupplyLineRepository(SupplyLineMapper supplyLineMapper) {
+    public SupplyLineRepository(SupplyLineMapper supplyLineMapper, SupervisoryNodeMapper supervisoryNodeMapper, ProgramMapper programMapper, FacilityMapper facilityMapper) {
         this.supplyLineMapper = supplyLineMapper;
+        this.programMapper = programMapper;
+        this.supervisoryNodeMapper = supervisoryNodeMapper;
+        this.facilityMapper = facilityMapper;
     }
 
     public void insert(SupplyLine supplyLine) {
         try {
+            supplyLine.getProgram().setId(programMapper.getIdByCode(supplyLine.getProgram().getCode()));
+            supplyLine.getSupplyingFacility().setId(facilityMapper.getIdForCode(supplyLine.getSupplyingFacility().getCode()));
+            supplyLine.getSupervisoryNode().setId(supervisoryNodeMapper.getIdForCode(supplyLine.getSupervisoryNode().getCode()));
+
+            if (supplyLine.getProgram().getId() == null) {
+                throw new DataException("Program Code does not exist");
+            }
+
+            if (supplyLine.getSupplyingFacility().getId() == null) {
+                throw new DataException("Facility Code does not exist");
+            }
+
+            if (supplyLine.getSupervisoryNode().getId() == null) {
+                throw new DataException("Supervising Node does not exist");
+            } else {
+                SupervisoryNode supervisoryNode = supervisoryNodeMapper.getSupervisoryNode(supplyLine.getSupervisoryNode().getId());
+
+                if (supervisoryNode != null && supervisoryNode.getParent() != null) {
+                    throw new DataException("Supervising Node is not the Top node");
+                }
+            }
+
             supplyLineMapper.insert(supplyLine);
         } catch (DuplicateKeyException ex) {
             throw new DataException("Duplicate entry for Supply Line found.");
--- modules/core/src/test/java/org/openlmis/core/repository/SupplyLineRepositoryTest.java
+++ modules/core/src/test/java/org/openlmis/core/repository/SupplyLineRepositoryTest.java
@@ -6,13 +6,20 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.openlmis.core.builder.SupplyLineBuilder;
+import org.openlmis.core.domain.Program;
+import org.openlmis.core.domain.SupervisoryNode;
 import org.openlmis.core.domain.SupplyLine;
 import org.openlmis.core.exception.DataException;
+import org.openlmis.core.repository.mapper.FacilityMapper;
+import org.openlmis.core.repository.mapper.ProgramMapper;
+import org.openlmis.core.repository.mapper.SupervisoryNodeMapper;
 import org.openlmis.core.repository.mapper.SupplyLineMapper;
 import org.springframework.dao.DuplicateKeyException;
 
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.verify;
+import static com.natpryce.makeiteasy.MakeItEasy.a;
+import static com.natpryce.makeiteasy.MakeItEasy.make;
+import static org.mockito.Mockito.*;
 
 @RunWith(org.mockito.runners.MockitoJUnitRunner.class)
 public class SupplyLineRepositoryTest {
@@ -21,29 +28,87 @@ public class SupplyLineRepositoryTest {
 
     @Mock
     SupplyLineMapper supplyLineMapper;
+    @Mock
+    SupervisoryNodeMapper supervisoryNodeMapper;
+    @Mock
+    ProgramMapper programMapper;
+    @Mock
+    FacilityMapper facilityMapper;
+
+    SupplyLine supplyLine;
 
     @Rule
     public ExpectedException expectedEx = ExpectedException.none();
 
     @Before
     public void setUp() {
-        supplyLineRepository = new SupplyLineRepository(supplyLineMapper);
+        supplyLineRepository = new SupplyLineRepository(supplyLineMapper, supervisoryNodeMapper, programMapper, facilityMapper);
+        supplyLine = make(a(SupplyLineBuilder.defaultSupplyLine));
     }
 
     @Test
     public void shouldInsertSupplyLine() {
-        SupplyLine supplyLine = new SupplyLine();
-
         supplyLineRepository.insert(supplyLine);
         verify(supplyLineMapper).insert(supplyLine);
     }
 
     @Test
     public void shouldThrowExceptionForDuplicateSupplyLines() {
-        SupplyLine supplyLine = new SupplyLine();
         expectedEx.expect(DataException.class);
         expectedEx.expectMessage("Duplicate entry for Supply Line found.");
         doThrow(new DuplicateKeyException("Duplicate entry for Supply Line found.")).when(supplyLineMapper).insert(supplyLine);
+        supplyLineRepository.insert(supplyLine);
+    }
+
+    @Test
+    public void shouldThrowErrorIfProgramDoesNotExist() {
+
+        supplyLine.setProgram(new Program());
+        when(programMapper.getIdByCode(supplyLine.getProgram().getCode())).thenReturn(null);
+
+        expectedEx.expect(DataException.class);
+        expectedEx.expectMessage("Program Code does not exist");
+        supplyLineRepository.insert(supplyLine);
+    }
+
+    @Test
+    public void shouldThrowErrorIfFacilityDoesNotExist() {
+
+        supplyLine.setProgram(new Program());
+        when(programMapper.getIdByCode(supplyLine.getProgram().getCode())).thenReturn(1);
+        when(facilityMapper.getIdForCode(supplyLine.getSupplyingFacility().getCode())).thenReturn(null);
+
+        expectedEx.expect(DataException.class);
+        expectedEx.expectMessage("Facility Code does not exist");
+        supplyLineRepository.insert(supplyLine);
+    }
+
+    @Test
+    public void shouldThrowErrorIfSupervisoryNodeDoesNotExist() {
+
+        supplyLine.setProgram(new Program());
+        when(programMapper.getIdByCode(supplyLine.getProgram().getCode())).thenReturn(1);
+        when(facilityMapper.getIdForCode(supplyLine.getSupplyingFacility().getCode())).thenReturn(1);
+        when(supervisoryNodeMapper.getIdForCode(supplyLine.getSupplyingFacility().getCode())).thenReturn(null);
+
+        expectedEx.expect(DataException.class);
+        expectedEx.expectMessage("Supervising Node does not exist");
+        supplyLineRepository.insert(supplyLine);
+    }
+
+    @Test
+    public void shouldThrowErrorIfSupervisoryNodeIsNotTheParentNode() {
+        when(programMapper.getIdByCode(supplyLine.getProgram().getCode())).thenReturn(1);
+        when(facilityMapper.getIdForCode(supplyLine.getSupplyingFacility().getCode())).thenReturn(1);
+
+        SupervisoryNode supervisoryNode = supplyLine.getSupervisoryNode();
+        supervisoryNode.setId(1);
+        supervisoryNode.setParent(new SupervisoryNode());
+        when(supervisoryNodeMapper.getIdForCode(supervisoryNode.getCode())).thenReturn(1);
+        when(supervisoryNodeMapper.getSupervisoryNode(supervisoryNode.getId())).thenReturn(supervisoryNode);
+
+        expectedEx.expect(DataException.class);
+        expectedEx.expectMessage("Supervising Node is not the Top node");
         supplyLineRepository.insert(supplyLine);
     }
 }
