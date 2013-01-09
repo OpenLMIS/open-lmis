12c0eb237c21aea6c2c948765b9c8dcd3e27d5fa
Mridul Khanal <mridulk@thoughtworks.com>
2013-01-09 11:25:43 +0530

    #140 | Mridul | Adding Tests for retracting to old values on edit cancel for each schedule

--- modules/openlmis-web/src/test/javascript/specs/schedules/schedule-controller-test.js
+++ modules/openlmis-web/src/test/javascript/specs/schedules/schedule-controller-test.js
@@ -5,71 +5,87 @@ describe("Schedule", function () {
 
     var scope, $httpBackend, ctrl, routeParams, facility, location;
     var existingSchedule = {"id":1, "name":"name", "code":"code", "description":"description"};
-    var editScheduleForm = {$invalid : false};
+    var editScheduleForm = {$invalid:false};
 
-    beforeEach(inject(function ($rootScope, _$httpBackend_, $controller, $location, $routeParams) {
-      scope = $rootScope.$new();
-      routeParams = $routeParams;
-      $httpBackend = _$httpBackend_;
-      location = $location;
+    beforeEach(
+        inject(function ($rootScope, _$httpBackend_, $controller, $location, $routeParams) {
+          scope = $rootScope.$new();
+          routeParams = $routeParams;
+          $httpBackend = _$httpBackend_;
+          location = $location;
 
-      $httpBackend.expectGET('/schedules.json').respond(200, {"schedules":[existingSchedule]});
-      ctrl = $controller(ScheduleController, {$scope:scope, $routeParams:routeParams});
-    }));
+          $httpBackend.expectGET('/schedules.json').respond(200, {"schedules":[existingSchedule]});
+          ctrl = $controller(ScheduleController, {$scope:scope, $routeParams:routeParams});
+        })
+    );
 
     it('should fetch all schedules', function () {
       $httpBackend.flush();
+      var expectedScheduleBackupMap = [];
+      expectedScheduleBackupMap[1] = {code:existingSchedule.code, name:existingSchedule.name, description:existingSchedule.description};
       expect(scope.schedules).toEqual([existingSchedule]);
+      expect(scope.schedulesBackupMap).toEqual(expectedScheduleBackupMap);
     });
 
-    it('should create a new schedule', function() {
-      var newSchedule = {"name":"newName", "code":"newCode", "description":"newDescription"};
+    it('should create a new schedule', function () {
+      scope.newSchedule = {"name":"newName", "code":"newCode", "description":"newDescription"};
       var newScheduleWithId = {"id":2, "name":"newName", "code":"newCode", "description":"newDescription"};
+      var expectedScheduleBackupMap = [];
+      expectedScheduleBackupMap[1] = {code:existingSchedule.code, name:existingSchedule.name, description:existingSchedule.description};
+      expectedScheduleBackupMap[2] = {"name":"newName", "code":"newCode", "description":"newDescription"};
+
       $httpBackend.expectPOST('/schedules.json').respond(200, {"schedule":newScheduleWithId, "success":"success message"});
-      scope.createScheduleForm = {$invalid : false};
-      scope.createSchedule(newSchedule);
+      scope.createScheduleForm = {$invalid:false};
+      scope.createSchedule();
       $httpBackend.flush();
       expect(scope.schedules.length).toEqual(2);
+
+      expect(scope.schedulesBackupMap).toEqual(expectedScheduleBackupMap);
+
       expect(scope.schedules).toEqual([newScheduleWithId, existingSchedule]);
       expect(scope.message).toEqual("success message");
     });
 
-    it('should show error on failure of creation of a new schedule', function() {
+    it('should show error on failure of creation of a new schedule', function () {
       var schedule = {"name":"newName", "code":"newCode", "description":"newDescription"};
+      scope.createScheduleForm = {$invalid:false};
       $httpBackend.expectPOST('/schedules.json').respond(400, {"error":"errorMsg"});
-      scope.createScheduleForm = {$invalid : false};
 
       scope.createSchedule(schedule);
       $httpBackend.flush();
-
       expect(scope.message).toEqual("");
       expect(scope.newSchedule.error).toEqual("errorMsg");
     });
 
-    it('should update an existing schedule', function() {
+    it('should update an existing schedule', function () {
       var updatedSchedule = {"id":1, "name":"newName", "code":"newCode", "description":"newDescription", "modifiedBy":"", "modifiedDate":"12345"};
-      $httpBackend.expectPUT('/schedules/1.json').respond(200,{"schedule":updatedSchedule, "success":"success message"});
+      var expectedScheduleBackupMap = [];
+      expectedScheduleBackupMap[1] = {"name":"newName", "code":"newCode", "description":"newDescription"};
+      $httpBackend.expectPUT('/schedules/1.json').respond(200, {"schedule":updatedSchedule, "success":"success message"});
 
       scope.updateSchedule(existingSchedule, editScheduleForm);
       $httpBackend.flush();
 
       expect(scope.schedules.length).toEqual(1);
-      expect(scope.schedules).toEqual([{"id":1, "name":"newName", "code":"newCode", "description":"newDescription", "modifiedBy":"", "modifiedDate":"12345"}]);
+      expect(scope.schedules).toEqual([
+        {"id":1, "name":"newName", "code":"newCode", "description":"newDescription", "modifiedBy":"", "modifiedDate":"12345"}
+      ]);
+      expect(scope.schedulesBackupMap).toEqual(expectedScheduleBackupMap);
       expect(scope.message).toEqual("success message");
     });
 
-    it('should show failure error on updating an existing schedule', function() {
+    it('should show failure error on updating an existing schedule', function () {
       var updatedSchedule = {"id":1, "name":"newName", "code":"newCode", "description":"newDescription"};
       $httpBackend.expectPUT('/schedules/1.json').respond(400, {"error":"errorMsg"});
 
-      var editScheduleForm = {$invalid : false};
+      var editScheduleForm = {$invalid:false};
       scope.updateSchedule(updatedSchedule, editScheduleForm);
       $httpBackend.flush();
       expect(scope.message).toEqual("");
       expect(scope.error).toEqual("errorMsg");
     });
 
-    it('should set correct schedule in the scope and navigate to period', function() {
+    it('should set correct schedule in the scope and navigate to period', function () {
       var selectedSchedule = {"id":1, "name":"newName", "code":"newCode", "description":"newDescription"};
       scope.navigateToPeriodFor(selectedSchedule);
       expect(location.path()).toEqual("/manage-period/1");
