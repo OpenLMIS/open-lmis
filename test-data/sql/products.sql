c4179d86a9364531d37ce7c1cbb54e7bc77d1835
Rahul Joshi <rahulj@thoughtworks.com>
2013-01-18 15:08:39 +0530

    |#000|+Manan| test seed lookup based on product code

--- test-data/sql/products.sql
+++ test-data/sql/products.sql
@@ -1,8 +1,8 @@
-delete from facility_approved_products;
-delete from program_products;
-delete from products;
+DELETE FROM facility_approved_products;
+DELETE FROM program_products;
+DELETE FROM products;
 
-insert into products
+INSERT INTO products
 (code,    alternateItemCode,  manufacturer,       manufacturerCode,  manufacturerBarcode,   mohBarcode,   gtin,   type,         primaryName,    fullName,       genericName,    alternateName,    description,      strength,    formId,  dosageUnitId, dispensingUnit,  dosesPerDispensingUnit,  packSize,  alternatePackSize,  storeRefrigerated,   storeRoomTemperature,   hazardous,  flammable,   controlledSubstance,  lightSensitive,  approvedByWho,  contraceptiveCyp,  packLength,  packWidth, packHeight,  packWeight,  packsPerCarton, cartonLength,  cartonWidth,   cartonHeight, cartonsPerPallet,  expectedShelfLife,  specialStorageInstructions, specialTransportInstructions, active,  fullSupply, tracer,   packRoundingThreshold,  roundToZero,  archived) values
 ('P10',  'a',                'Glaxo and Smith',  'a',              'a',                    'a',          'a',    'antibiotic', 'antibiotic',   'TDF/FTC/EFV',  'TDF/FTC/EFV',  'TDF/FTC/EFV',    'TDF/FTC/EFV',  '300/200/600',  2,        1,            'Strip',           10,                     10,        30,                   TRUE,                  TRUE,                TRUE,       TRUE,         TRUE,                 TRUE,             TRUE,               1,          2.2,            2,          2,            2,            2,            2,              2,              2,              2,                    2,                    'a',                          'a',          TRUE,     TRUE,       TRUE,         1,                    FALSE,      TRUE),
 ('P11',  'a',                'Glaxo and Smith',  'a',              'a',                    'a',          'a',    'antibiotic', 'antibiotic',   'TDF/FTC/EFV',  'TDF/FTC/EFV',  'TDF/FTC/EFV',    'TDF/FTC/EFV',  '300/200/600',  2,        1,            'Strip',           10,                     10,        30,                   TRUE,                  TRUE,                TRUE,       TRUE,         TRUE,                 TRUE,             TRUE,               1,          2.2,            2,          2,            2,            2,            2,              2,              2,              2,                    2,                    'a',                          'a',          TRUE,     TRUE,       TRUE,         1,                    FALSE,      TRUE),
@@ -13,7 +13,7 @@ INSERT INTO program_products(programId, productId, dosesPerMonth, currentPrice, 
 (1, (SELECT id from products WHERE code = 'P11'), 30, 50, true),
 (1, (SELECT id from products WHERE code = 'P12'), 30, 0, true);
 
-insert into facility_approved_products(facilityTypeId, programProductId, maxMonthsOfStock) values
-(1, (select id from program_products where programId=1 and productId=1), 3),
-(1, (select id from program_products where programId=1 and productId=2), 3),
-(1, (select id from program_products where programId=1 and productId=3), 3);
+INSERT INTO facility_approved_products(facilityTypeId, programProductId, maxMonthsOfStock) VALUES
+(1, (SELECT id FROM program_products WHERE programId=1 AND productId=(SELECT id FROM products WHERE  code='P10')), 3),
+(1, (SELECT id FROM program_products WHERE programId=1 AND productId=(SELECT id FROM products WHERE  code='P11')), 3),
+(1, (SELECT id FROM program_products WHERE programId=1 AND productId=(SELECT id FROM products WHERE  code='P12')), 3);
