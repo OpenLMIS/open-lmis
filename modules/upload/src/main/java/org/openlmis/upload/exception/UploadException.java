fe080fbad9f9f68267f51ad0adfcb26be8128c06
Mridul Khanal <mridulk@thoughtworks.com>
2013-01-22 15:43:50 +0530

    #0 | +Anshul | Fixing warning message for equals and hashcode for the one generated by Lombok's @data annotation

--- modules/upload/src/main/java/org/openlmis/upload/exception/UploadException.java
+++ modules/upload/src/main/java/org/openlmis/upload/exception/UploadException.java
@@ -1,8 +1,10 @@
 package org.openlmis.upload.exception;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class UploadException extends RuntimeException {
 
   public UploadException(String message) {
