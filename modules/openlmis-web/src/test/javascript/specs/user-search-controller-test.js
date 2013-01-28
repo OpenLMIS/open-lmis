a8762285f9ea6a94d7d342fe978b3253a334b310
Balvinder Khurana <balvindk@thoughtworks.com>
2013-01-28 11:18:40 +0530

    |#438| Anshul, Balvinder| Fixed JasmineTests for Changes in user search

--- modules/openlmis-web/src/test/javascript/specs/user-search-controller-test.js
+++ modules/openlmis-web/src/test/javascript/specs/user-search-controller-test.js
@@ -2,11 +2,13 @@ describe("User Search Controller", function () {
 
   var scope, $httpBackend, ctrl;
   beforeEach(module('openlmis.services'));
+  var searchTextId = 'searchTextId';
 
   beforeEach(inject(function ($rootScope, _$httpBackend_, $controller) {
     scope = $rootScope.$new();
     $httpBackend = _$httpBackend_;
     scope.query = "joh";
+
 
     ctrl = $controller('UserSearchController', {$scope:scope});
   }));
@@ -14,8 +16,8 @@ describe("User Search Controller", function () {
   it('should get all users depending on search criteria when three characters are entered in search', function () {
     var userResponse = {"userList":[{"id":1,"firstName":"john","lastName":"Doe","email":"john_doe@gmail.com"}]};
     $httpBackend.when('GET', '/admin/search-user.json?userSearchParam=' + scope.query).respond(userResponse);
-
-    scope.showUserSearchResults(scope.query);
+    spyOn(document, 'getElementById').andReturn({value : scope.query});
+    scope.showUserSearchResults(searchTextId);
     $httpBackend.flush();
 
     expect(scope.userList).toEqual([{"id":1,"firstName":"john","lastName":"Doe","email":"john_doe@gmail.com"}]);
@@ -23,8 +25,9 @@ describe("User Search Controller", function () {
 
   it('should filter users when more than 3 characters are entered for search', function (){
     scope.query="john";
+    spyOn(document, 'getElementById').andReturn({value : scope.query});
     scope.userList  = [{"id":1,"firstName":"john","lastName":"Doe","email":"john_doe@gmail.com"}];
-    scope.showUserSearchResults(scope.query);
+    scope.showUserSearchResults(searchTextId);
     expect(scope.filteredUsers).toEqual([{"id":1,"firstName":"john","lastName":"Doe","email":"john_doe@gmail.com"}]);
   });
 });
