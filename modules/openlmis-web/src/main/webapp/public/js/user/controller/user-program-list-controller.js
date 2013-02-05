7ae33c613f6256ce67f1ac8c18b61c2d7a4f6129
Rahul Joshi <rahulj@thoughtworks.com>
2013-02-05 12:31:22 +0530

    |#412| +Jaideep| movin role assignment out of grid

--- modules/openlmis-web/src/main/webapp/public/js/user/controller/user-program-list-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/user/controller/user-program-list-controller.js
@@ -1,38 +1,4 @@
 function UserProgramRoleListController($scope) {
-  $scope.$parent.gridOptions = { data:'user.roleAssignments',
-    headerRowHeight:30,
-    rowHeight:50,
-    sortable:false,
-    showColumnMenu:false,
-    displaySelectionCheckbox:false,
-    showFilter:false,
-    footerVisible:false,
-    enableSorting:false,
-    canSelectRows:false,
-    columnDefs:[
-      { field:'supportedPrograms', displayName:'Programs(s)', cellTemplate:program(), width:'30%' },
-      { field:'roles', displayName:'Role', cellTemplate:selectRole(), width:'60%' },
-      { field:'', displayName:'', cellTemplate:deleteRow(), width:'10%'}
-    ]
-  };
-
-  function deleteRow() {
-    return '<div><a value="delete" ng-click="deleteCurrentRow($parent.$index)">delete</a></div>';
-  }
-
-  function program() {
-    return '<label ng-bind="getProgramName(user.roleAssignments[$parent.$index].programId)"></label>';
-  }
-
-  function selectRole() {
-    return '<select ui-select2 ng-model="user.roleAssignments[$parent.$index].roleIds" placeholder="+ Add Role"' +
-      ' multiple="multiple" style="width:400px" name="roles" id="roles"> ' +
-      ' <option ng-repeat="role in allRoles" value="{{role.id}}">{{role.name}}</option>' +
-      '</select>' +
-      '<span ng-show="user.roleAssignments[$parent.$index].roleIds.length == 0" class="field-error">' +
-      ' Please Fill In this value' +
-      '</span>';
-  }
 
   $scope.deleteCurrentRow = function (rowNum) {
     $scope.user.roleAssignments.splice(rowNum, 1);
