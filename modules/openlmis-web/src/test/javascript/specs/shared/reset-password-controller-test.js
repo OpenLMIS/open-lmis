0f66d102ab085ab1d72fc34651ef5f708219e075
Balvinder Khurana <balvindk@thoughtworks.com>
2013-02-05 12:59:43 +0530

    |#163|  Anshul, Balvinder|Adding jasmine tests for reset password

---
+++ modules/openlmis-web/src/test/javascript/specs/shared/reset-password-controller-test.js
@@ -0,0 +1,45 @@
+describe("ResetPasswordController", function () {
+
+
+  beforeEach(module('openlmis.services'));
+  beforeEach(inject(function ($rootScope, _$httpBackend_, $controller, $route) {
+    scope = $rootScope.$new();
+    $httpBackend = _$httpBackend_;
+    route = $route;
+    route.current = {params : {token : "token"}};
+    controller = $controller(ResetPasswordController, {$scope:scope, tokenValid:true, route:route});
+  }));
+
+  it("Should give error message if password is less than 8 characters", function () {
+    scope.password1 = "1234567";
+    scope.resetPassword();
+    expect(scope.error).toEqual("Password is invalid. Password must be between 8 to 16 characters, should not contain spaces and contain at least 1 number.");
+  });
+
+  it("Should give error message if password does not contain a digit", function () {
+    scope.password1 = "abcdefghi";
+    scope.resetPassword();
+    expect(scope.error).toEqual("Password is invalid. Password must be between 8 to 16 characters, should not contain spaces and contain at least 1 number.");
+  });
+
+  it("Should give error message if password contains spaces", function () {
+    scope.password1 = "abcd efghi4  ";
+    scope.resetPassword();
+    expect(scope.error).toEqual("Password is invalid. Password must be between 8 to 16 characters, should not contain spaces and contain at least 1 number.");
+  });
+
+  it("Should give error message passwords do not match", function () {
+    scope.password1 = "abcdefghi4";
+    scope.password2 = "abcdefghi5";
+    scope.resetPassword();
+    expect(scope.error).toEqual("Passwords do not match");
+  });
+
+  it("Should update password if it is valid and both passwords match", function () {
+    $httpBackend.expectPUT('/user/resetPassword/token.json').respond(200);
+    scope.password1 = "abcdefghi4";
+    scope.password2 = "abcdefghi4";
+    scope.resetPassword();
+    $httpBackend.flush();
+  });
+});
