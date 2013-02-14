97d0624e1f258493d8f2244b036793fbc5b839c0
shibha <shibha20@gmail.com>
2013-02-14 17:56:58 +0530

    |#477++Vinay, Making facility search server side

--- modules/openlmis-web/src/main/webapp/public/js/facility/controller/facility-search-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/facility/controller/facility-search-controller.js
@@ -1,31 +1,68 @@
 function FacilitySearchController($scope, AllFacilities, $location) {
 
-    AllFacilities.get({}, function (data) {
-        $scope.facilityList = data.facilityList;
-    }, {});
+  $scope.previousQuery = '';
 
-    $scope.editFacility = function (id) {
-        $location.path('edit/' + id);
-    };
+  $scope.editFacility = function (id) {
+    $location.path('edit/' + id);
+  };
 
 
-    $scope.filterFacilitiesByNameOrCode = function (query) {
-        var filteredFacilities = [];
-        query = query || "";
+  $scope.filterFacilitiesByNameOrCode = function (query) {
+    var filteredFacilities = [];
+    query = query || "";
 
-        angular.forEach($scope.facilityList, function (facility) {
-            if (facility.name.toLowerCase().indexOf(query.toLowerCase()) >= 0 || facility.code.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
-                filteredFacilities.push(facility);
-            }
-        });
-        $scope.resultCount = filteredFacilities.length;
-        return filteredFacilities;
-    };
+    angular.forEach($scope.facilityList, function (facility) {
+      if (facility.name.toLowerCase().indexOf(query.toLowerCase()) >= 0 || facility.code.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
+        filteredFacilities.push(facility);
+      }
+    });
+    $scope.resultCount = filteredFacilities.length;
+    return filteredFacilities;
+  };
 
-    $scope.clearSearch = function () {
-        $scope.query = "";
-        $scope.resultCount = 0;
-        angular.element("#searchFacility").focus();
-    };
+  $scope.clearSearch = function () {
+    $scope.query = "";
+    $scope.resultCount = 0;
+    angular.element("#searchFacility").focus();
+  };
+
+  $scope.updateFilteredQueryList = function () {
+
+    $scope.query = $scope.query.trim();
+    var queryLength = $scope.query.length;
+    if (queryLength >= 3) {
+      if (compareQuery()) {
+        AllFacilities.get({"searchParam":$scope.query.substring(0, 3)}, function (data) {
+          $scope.filteredFacilities = data.facilityList;
+          $scope.facilityList = $scope.filteredFacilities;
+          $scope.previousQuery = $scope.query;
+          if (queryLength > 3) {
+            filterFacilities();
+          }
+          $scope.resultCount = $scope.facilityList.length;
+        }, {});
+      }
+      else{
+          filterFacilities();
+          $scope.resultCount = $scope.facilityList.length;
+      }
+    }
+  }
+
+  var compareQuery = function() {
+    if ($scope.previousQuery.substring(0, 3) != $scope.query.substring(0, 3)) {
+      return true;
+    }
+  }
+
+  var filterFacilities = function() {
+    $scope.facilityList = [];
+    angular.forEach($scope.filteredFacilities, function (facility) {
+      var searchString = $scope.query.toLowerCase();
+      if (facility.code.toLowerCase().indexOf(searchString) >= 0 || facility.name.toLowerCase().indexOf(searchString) >= 0) {
+        $scope.facilityList.push(facility);
+      }
+    });
+  }
 }
 
--- modules/openlmis-web/src/test/javascript/specs/facility-search-controller-test.js
+++ modules/openlmis-web/src/test/javascript/specs/facility-search-controller-test.js
@@ -1,20 +1,67 @@
 describe("Facility Search Controller", function () {
 
-  var scope,$httpBackend, ctrl;
+  var scope, $httpBackend, ctrl;
   beforeEach(module('openlmis.services'));
 
   beforeEach(inject(function ($rootScope, _$httpBackend_, $controller) {
     scope = $rootScope.$new();
     $httpBackend = _$httpBackend_;
-    var facilityResponse = {"facilityList":[{"id":1}]};
-    $httpBackend.when('GET', '/facilities.json').respond(facilityResponse);
-
-    ctrl = $controller('FacilitySearchController',{$scope:scope});
+    ctrl = $controller('FacilitySearchController', {$scope:scope});
   }));
 
-  it('should get all facilities',function() {
+//  it('should get all facilities',function() {
+//    $httpBackend.flush();
+//    expect(scope.facilityList).toEqual([{"id":1}]);
+//  });
+
+  it("should get filtered facility list when three characters are entered for search", function () {
+    scope.query = "lok";
+    var facilityResponse = {"facilityList":[
+      {"id":1,"code":"F11","name":"lokesh"}
+    ]};
+    $httpBackend.when('GET', '/facilities.json?searchParam=' +scope.query.substring(0,3)).respond(facilityResponse);
+    scope.updateFilteredQueryList();
     $httpBackend.flush();
-    expect(scope.facilityList).toEqual([{"id":1}]);
+    expect(scope.facilityList).toEqual([{"id":1,"code":"F11","name":"lokesh"}]);
   });
+
+  it("should filter facilities when more than three characters are entered for search and first three characters of previous query are same as current query",function(){
+    scope.previousQuery='lok';
+    scope.query = "loke";
+    scope.filteredFacilities=[ {"id":1,"code":"F11","name":"lokesh"},
+      {"id":2,"code":"F10","name":"lokash"},
+      {"id":3,"code":"F12","name":"LOKERE"}];
+    scope.facilityList=[ {"id":1,"code":"F11","name":"lokesh"},
+      {"id":2,"code":"F10","name":"lokash"},
+      {"id":3,"code":"F12","name":"LOKERE"}];
+    scope.updateFilteredQueryList();
+    expect(scope.facilityList).toEqual([{"id":1,"code":"F11","name":"lokesh"},{"id":3,"code":"F12","name":"LOKERE"}]);
+    expect(scope.facilityList.length).toEqual(2);
+  })
+
+  it("should filter facilities when more than three characters are entered for search and first three characters of previous query are NOT same as current query",function(){
+    scope.previousQuery='abc';
+    scope.query = "loke";
+    var facilityResponse = {"facilityList":[
+      {"id":1,"code":"F11","name":"lokesh"},
+      {"id":2,"code":"F10","name":"lokash"}
+    ]};
+    $httpBackend.when('GET', '/facilities.json?searchParam=' +scope.query.substring(0,3)).respond(facilityResponse);
+    scope.updateFilteredQueryList();
+    $httpBackend.flush();
+    expect(scope.facilityList).toEqual([{"id":1,"code":"F11","name":"lokesh"}]);
+    expect(scope.facilityList.length).toEqual(1);
+  })
+
+  it("should return filtered facilities when query contains space",function(){
+    scope.query="lok ";
+    var facilityResponse = {"facilityList":[
+      {"id":1,"code":"F11","name":"lokesh"}
+    ]};
+    $httpBackend.when('GET', '/facilities.json?searchParam=' +scope.query.substring(0,3)).respond(facilityResponse);
+    scope.updateFilteredQueryList();
+    $httpBackend.flush();
+    expect(scope.facilityList).toEqual([{"id":1,"code":"F11","name":"lokesh"}]);
+  })
 
 });
