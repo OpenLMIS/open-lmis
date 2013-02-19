df8874dfa0a9842d93d0ed4c4dfcf9c00772bc3c
Manjyot <manjyots@thoughtworks.com>
2013-02-19 13:14:07 +0530

    Fixing FTs

--- test-modules/functional-tests/src/main/java/org/openlmis/pageobjects/PeriodsPage.java
+++ test-modules/functional-tests/src/main/java/org/openlmis/pageobjects/PeriodsPage.java
@@ -16,202 +16,199 @@ import java.util.Date;
 
 public class PeriodsPage extends Page {
 
-    @FindBy(how = How.ID, using = "name")
-    private static WebElement namePeriod;
+  @FindBy(how = How.ID, using = "name")
+  private static WebElement namePeriod;
 
-    @FindBy(how = How.ID, using = "description")
-    private static WebElement descriptionPeriod;
+  @FindBy(how = How.ID, using = "description")
+  private static WebElement descriptionPeriod;
 
-    @FindBy(how = How.ID, using = "startDate")
-    private static WebElement startDatePeriod;
+  @FindBy(how = How.ID, using = "startDate")
+  private static WebElement startDatePeriod;
 
-    @FindBy(how = How.ID, using = "endDate")
-    private static WebElement endDatePeriod;
+  @FindBy(how = How.ID, using = "endDate")
+  private static WebElement endDatePeriod;
 
-    @FindBy(how = How.XPATH, using = "//span[contains(text(),'Prev')]")
-    private static WebElement prevCalender;
+  @FindBy(how = How.XPATH, using = "//span[contains(text(),'Prev')]")
+  private static WebElement prevCalender;
 
-    @FindBy(how = How.XPATH, using = "//span[contains(text(),'Next')]")
-    private static WebElement nextCalender;
+  @FindBy(how = How.XPATH, using = "//span[contains(text(),'Next')]")
+  private static WebElement nextCalender;
 
-    @FindBy(how = How.XPATH, using = "//a[@class='ui-state-default' and contains(text(),'1')]")
-    private static WebElement startDateCalender;
+  @FindBy(how = How.XPATH, using = "//a[@class='ui-state-default' and contains(text(),'1')]")
+  private static WebElement startDateCalender;
 
-    @FindBy(how = How.XPATH, using = "//a[@class='ui-state-default' and contains(text(),'2')]")
-    private static WebElement endDateCalender;
+  @FindBy(how = How.XPATH, using = "//a[@class='ui-state-default' and contains(text(),'2')]")
+  private static WebElement endDateCalender;
 
-    @FindBy(how = How.XPATH, using = "//a[@class='ui-state-default' and contains(text(),'26')]")
-    private static WebElement endDateSecondCalender;
+  @FindBy(how = How.XPATH, using = "//a[@class='ui-state-default' and contains(text(),'26')]")
+  private static WebElement endDateSecondCalender;
 
-    @FindBy(how = How.XPATH, using = "//a[contains(text(),'28')]")
-    private static WebElement endDateThirdCalender;
+  @FindBy(how = How.XPATH, using = "//a[contains(text(),'28')]")
+  private static WebElement endDateThirdCalender;
 
-    @FindBy(how = How.XPATH, using = "//span[@ng-show='calculateDays(newPeriod.startDate, newPeriod.endDate)+1']")
-    private static WebElement totalDaysPeriods;
+  @FindBy(how = How.XPATH, using = "//span[@ng-show='calculateDays(newPeriod.startDate, newPeriod.endDate)+1']")
+  private static WebElement totalDaysPeriods;
 
-    @FindBy(how = How.XPATH, using = "//span[@ng-model='newPeriod.numberOfMonths']")
-    private static WebElement numberOfMonthsPeriods;
+  @FindBy(how = How.XPATH, using = "//span[@ng-model='newPeriod.numberOfMonths']")
+  private static WebElement numberOfMonthsPeriods;
 
-    @FindBy(how = How.XPATH, using = "//input[@value='Add']")
-    private static WebElement addButton;
-
-
-    @FindBy(how = How.ID, using = "saveSuccessMsgDiv")
-    private static WebElement saveSuccessMsgDiv;
-
-    @FindBy(how = How.XPATH, using = "//input[@value='Delete']")
-    private static WebElement deleteButton;
-
-    @FindBy(how = How.XPATH, using = "(//div[@class='row-fluid schedule-row ng-scope']/div[@class='span2 ng-binding'])[2]")
-    private static WebElement startDateList;
-
-    @FindBy(how = How.XPATH, using = "(//div[@class='row-fluid schedule-row ng-scope']/div[@class='span2 ng-binding'])[3]")
-    private static WebElement endDateList;
+  @FindBy(how = How.XPATH, using = "//input[@value='Add']")
+  private static WebElement addButton;
 
 
-    public PeriodsPage(TestWebDriver driver) throws IOException {
-        super(driver);
-        PageFactory.initElements(new AjaxElementLocatorFactory(testWebDriver.getDriver(), 10), this);
-        testWebDriver.setImplicitWait(25);
+  @FindBy(how = How.ID, using = "saveSuccessMsgDiv")
+  private static WebElement saveSuccessMsgDiv;
 
+  @FindBy(how = How.XPATH, using = "//input[@value='Delete']")
+  private static WebElement deleteButton;
+
+  @FindBy(how = How.XPATH, using = "(//div[@class='row-fluid schedule-row ng-scope']/div[@class='span2 ng-binding'])[2]")
+  private static WebElement startDateList;
+
+  @FindBy(how = How.XPATH, using = "(//div[@class='row-fluid schedule-row ng-scope']/div[@class='span2 ng-binding'])[3]")
+  private static WebElement endDateList;
+
+
+  public PeriodsPage(TestWebDriver driver) throws IOException {
+    super(driver);
+    PageFactory.initElements(new AjaxElementLocatorFactory(testWebDriver.getDriver(), 10), this);
+    testWebDriver.setImplicitWait(25);
+
+  }
+
+
+  public void createAndVerifyPeriods() throws IOException {
+    testWebDriver.waitForElementToAppear(namePeriod);
+    enterAndVerifyPeriodDetails("Period1", "first period", "1", 1);
+    enterAndVerifyPeriodDetails("Period2", "second period", "1", 2);
+    enterAndVerifyPeriodDetails("Period3", "third period", "1", 3);
+  }
+
+  public void deleteAndVerifyPeriods() throws IOException {
+    testWebDriver.waitForElementToAppear(startDateList);
+    String actualStartDateListValue = startDateList.getText().trim();
+
+    int flag = compareDateWithToday(actualStartDateListValue);
+
+    if (flag == 1) {
+      SeleneseTestNgHelper.assertTrue("delete button is not getting displayed", deleteButton.isDisplayed());
+      deleteButton.click();
+      testWebDriver.sleep(1000);
+      testWebDriver.waitForElementToAppear(saveSuccessMsgDiv);
+      SeleneseTestNgHelper.assertTrue("Period deleted successfully message is not getting displayed", saveSuccessMsgDiv.isDisplayed());
+      testWebDriver.waitForElementToAppear(endDateList);
+    }
+    String actualEndDateList = endDateList.getText().trim();
+    String actualStartDateCalender = testWebDriver.getAttribute(startDatePeriod, "value");
+    int diffInDays = compareTwoDates(actualStartDateCalender, actualEndDateList);
+    SeleneseTestNgHelper.assertEquals(String.valueOf(diffInDays), "1");
+
+  }
+
+
+  public void enterAndVerifyPeriodDetails(String name, String desc, String totalMonths, int indicator) {
+    namePeriod.clear();
+    namePeriod.sendKeys(name);
+    descriptionPeriod.clear();
+    descriptionPeriod.sendKeys(desc);
+    if (indicator == 1) {
+      startDatePeriod.click();
+      testWebDriver.sleep(100);
+      prevCalender.click();
+      testWebDriver.sleep(100);
+      startDateCalender.click();
+      testWebDriver.sleep(100);
+      endDatePeriod.click();
+      testWebDriver.sleep(100);
+      nextCalender.click();
+      testWebDriver.sleep(100);
+      endDateCalender.click();
+    } else if (indicator == 2) {
+      testWebDriver.sleep(100);
+      endDatePeriod.click();
+      testWebDriver.sleep(100);
+      nextCalender.click();
+      testWebDriver.sleep(100);
+      endDateSecondCalender.click();
+      testWebDriver.sleep(100);
+    } else {
+      testWebDriver.sleep(100);
+      endDatePeriod.click();
+      testWebDriver.sleep(100);
+      nextCalender.click();
+      testWebDriver.sleep(100);
+      endDateThirdCalender.click();
+      testWebDriver.sleep(100);
     }
 
 
-    public void createAndVerifyPeriods() throws IOException {
-        testWebDriver.waitForElementToAppear(namePeriod);
-        enterAndVerifyPeriodDetails("Period1", "first period", "1", 1);
-        enterAndVerifyPeriodDetails("Period2", "second period", "1", 2);
-        enterAndVerifyPeriodDetails("Period3", "third period", "1", 3);
+    String actualNumberOfMonthsPeriods = numberOfMonthsPeriods.getText().trim();
+    SeleneseTestNgHelper.seleniumEquals(actualNumberOfMonthsPeriods, totalMonths);
+
+    addButton.click();
+    testWebDriver.sleep(500);
+    testWebDriver.waitForElementToAppear(saveSuccessMsgDiv);
+    SeleneseTestNgHelper.assertTrue("Message Period added successfully is not getting displayed", saveSuccessMsgDiv.isDisplayed());
+
+    testWebDriver.waitForElementToAppear(startDateList);
+    String actualStartDateListValue = startDateList.getText().trim();
+
+    int flag = compareDateWithToday(actualStartDateListValue);
+
+    if (flag == 1)
+      SeleneseTestNgHelper.assertTrue("delete button is not getting displayed", deleteButton.isDisplayed());
+  }
+
+  public int compareTwoDates(String newerDateString, String olderDateString) {
+    int diffInDays = 0;
+    try {
+
+      java.text.SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+      java.util.Date newerDate = sdf.parse(newerDateString);
+
+      java.util.Date olderDate = sdf.parse(olderDateString);
+
+      diffInDays = (int) ((newerDate.getTime() - olderDate.getTime())
+        / (1000 * 60 * 60 * 24));
+
+    } catch (java.text.ParseException e) {
+      e.printStackTrace();
+    } finally {
+      return diffInDays;
     }
+  }
 
-    public void deleteAndVerifyPeriods() throws IOException {
-        testWebDriver.waitForElementToAppear(startDateList);
-        String actualStartDateListValue = startDateList.getText().trim();
+  public int compareDateWithToday(String dateToCompare) {
 
-        int flag = compareDateWithToday(actualStartDateListValue);
+    Date todayDate = null;
+    java.text.SimpleDateFormat sdf;
+    Date datePeriods = null;
+    int flag = 0;
+    try {
 
-        if (flag == 1) {
-            SeleneseTestNgHelper.assertTrue("delete button is not getting displayed", deleteButton.isDisplayed());
-            deleteButton.click();
-            testWebDriver.waitForElementToAppear(saveSuccessMsgDiv);
-            SeleneseTestNgHelper.assertTrue("Period deleted successfully message is not getting displayed",saveSuccessMsgDiv.isDisplayed());
-            testWebDriver.waitForElementToAppear(endDateList);
-        }
-        String actualEndDateList=endDateList.getText().trim();
-        String actualStartDateCalender=testWebDriver.getAttribute(startDatePeriod,"value");
-        int diffInDays=compareTwoDates(actualStartDateCalender, actualEndDateList);
-        SeleneseTestNgHelper.assertEquals(String.valueOf(diffInDays),"1");
+      todayDate = new Date();
+      sdf = new java.text.SimpleDateFormat("dd/MM/yyyy");
+      String todayDateString = sdf.format(todayDate);
+      todayDate = sdf.parse(todayDateString);
 
+      datePeriods = sdf.parse(dateToCompare);
+
+      if (datePeriods.compareTo(todayDate) > 0) {
+        System.out.println("DatePeriods is after TodayDate");
+        flag = 1;
+      } else if (datePeriods.compareTo(todayDate) < 0) {
+        System.out.println("DatePeriods is before TodayDate");
+        flag = 2;
+
+      } else if (datePeriods.compareTo(todayDate) == 0) {
+        System.out.println("DatePeriods is equal to TodayDate");
+
+      }
+
+    } catch (Exception ex) {
+      ex.printStackTrace();
+    } finally {
+      return flag;
     }
-
-
-    public void enterAndVerifyPeriodDetails(String name, String desc, String totalMonths, int indicator) {
-        namePeriod.clear();
-        namePeriod.sendKeys(name);
-        descriptionPeriod.clear();
-        descriptionPeriod.sendKeys(desc);
-        if (indicator == 1) {
-            startDatePeriod.click();
-            testWebDriver.sleep(100);
-            prevCalender.click();
-            testWebDriver.sleep(100);
-            startDateCalender.click();
-            testWebDriver.sleep(100);
-            endDatePeriod.click();
-            testWebDriver.sleep(100);
-            nextCalender.click();
-            testWebDriver.sleep(100);
-            endDateCalender.click();
-        } else if(indicator == 2) {
-            testWebDriver.sleep(100);
-            endDatePeriod.click();
-            testWebDriver.sleep(100);
-            nextCalender.click();
-            testWebDriver.sleep(100);
-            endDateSecondCalender.click();
-            testWebDriver.sleep(100);
-        }
-        else {
-            testWebDriver.sleep(100);
-            endDatePeriod.click();
-            testWebDriver.sleep(100);
-            nextCalender.click();
-            testWebDriver.sleep(100);
-            endDateThirdCalender.click();
-            testWebDriver.sleep(100);
-        }
-
-
-        String actualNumberOfMonthsPeriods = numberOfMonthsPeriods.getText().trim();
-        SeleneseTestNgHelper.seleniumEquals(actualNumberOfMonthsPeriods, totalMonths);
-
-        addButton.click();
-        testWebDriver.sleep(500);
-        testWebDriver.waitForElementToAppear(saveSuccessMsgDiv);
-        SeleneseTestNgHelper.assertTrue("Message Period added successfully is not getting displayed", saveSuccessMsgDiv.isDisplayed());
-
-        testWebDriver.waitForElementToAppear(startDateList);
-        String actualStartDateListValue = startDateList.getText().trim();
-
-        int flag = compareDateWithToday(actualStartDateListValue);
-
-        if (flag == 1)
-            SeleneseTestNgHelper.assertTrue("delete button is not getting displayed", deleteButton.isDisplayed());
-    }
-
-    public int compareTwoDates(String newerDateString, String olderDateString) {
-        int diffInDays=0;
-        try{
-
-            java.text.SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
-            java.util.Date newerDate = sdf.parse(newerDateString);
-
-            java.util.Date olderDate = sdf.parse(olderDateString);
-
-            diffInDays = (int)( (newerDate.getTime() - olderDate.getTime())
-                    / (1000 * 60 * 60 * 24) );
-
-        }catch(java.text.ParseException e)
-        {
-             e.printStackTrace();
-        }
-        finally
-        {
-            return diffInDays;
-        }
-    }
-
-    public int compareDateWithToday(String dateToCompare) {
-
-        Date todayDate = null;
-        java.text.SimpleDateFormat sdf;
-        Date datePeriods = null;
-        int flag = 0;
-        try {
-
-            todayDate = new Date();
-            sdf = new java.text.SimpleDateFormat("dd/MM/yyyy");
-            String todayDateString = sdf.format(todayDate);
-            todayDate = sdf.parse(todayDateString);
-
-            datePeriods = sdf.parse(dateToCompare);
-
-            if (datePeriods.compareTo(todayDate) > 0) {
-                System.out.println("DatePeriods is after TodayDate");
-                flag = 1;
-            } else if (datePeriods.compareTo(todayDate) < 0) {
-                System.out.println("DatePeriods is before TodayDate");
-                flag = 2;
-
-            } else if (datePeriods.compareTo(todayDate) == 0) {
-                System.out.println("DatePeriods is equal to TodayDate");
-
-            }
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
-            return flag;
-        }
-    }
+  }
 }
--- test-modules/functional-tests/src/main/java/org/openlmis/pageobjects/TemplateConfigPage.java
+++ test-modules/functional-tests/src/main/java/org/openlmis/pageobjects/TemplateConfigPage.java
@@ -14,69 +14,65 @@ import org.openqa.selenium.support.ui.Select;
 public class TemplateConfigPage extends Page {
 
 
-    @FindBy(how = How.LINK_TEXT, using = "Logout")
-    private static WebElement logoutLink;
+  @FindBy(how = How.LINK_TEXT, using = "Logout")
+  private static WebElement logoutLink;
 
-    @FindBy(how = How.XPATH, using = "//input[@value='Save']")
-    private static WebElement SaveButton;
+  @FindBy(how = How.XPATH, using = "//input[@value='Save']")
+  private static WebElement SaveButton;
 
-    @FindBy(how = How.XPATH, using = "//div[@id='saveSuccessMsgDiv' and @ng-show='message']")
-    private static WebElement saveSuccessMsg;
+  @FindBy(how = How.XPATH, using = "//div[@id='saveSuccessMsgDiv' and @ng-show='message']")
+  private static WebElement saveSuccessMsg;
 
-    @FindBy(how = How.XPATH, using = "//div[@id='saveSuccessMsgDiv' and @ng-show='error']")
-    private static WebElement saveErrorMsgDiv;
+  @FindBy(how = How.XPATH, using = "//div[@id='saveSuccessMsgDiv' and @ng-show='error']")
+  private static WebElement saveErrorMsgDiv;
 
-    @FindBy(how = How.XPATH, using = "//li[@id='E']/span[@class='tpl-source']/span/select")
-    private static WebElement stockInHandDropDown;
+  @FindBy(how = How.XPATH, using = "//li[@id='E']/span[@class='tpl-source']/span/select")
+  private static WebElement stockInHandDropDown;
 
-    @FindBy(how = How.XPATH, using = "//li[@id='A']/span/span/span/span/input[@type='checkbox']")
-    private static WebElement checkboxBeginningBalance;
+  @FindBy(how = How.XPATH, using = "//li[@id='A']/span/span/span/span/input[@type='checkbox']")
+  private static WebElement checkboxBeginningBalance;
 
-    @FindBy(how = How.XPATH, using = "//li[@id='C']/span[@class='tpl-source']/span/select")
-    private static WebElement dropDownTotalConsumedQuantity;
+  @FindBy(how = How.XPATH, using = "//li[@id='C']/span[@class='tpl-source']/span/select")
+  private static WebElement dropDownTotalConsumedQuantity;
 
-    private String TEMPLATE_SUCCESS_MESSAGE = "Template saved successfully!";
+  private String TEMPLATE_SUCCESS_MESSAGE = "Template saved successfully!";
 
 
+  public TemplateConfigPage(TestWebDriver driver) {
+    super(driver);
+    PageFactory.initElements(new AjaxElementLocatorFactory(testWebDriver.getDriver(), 10), this);
+    testWebDriver.setImplicitWait(25);
+  }
 
-    public TemplateConfigPage(TestWebDriver driver) {
-        super(driver);
-        PageFactory.initElements(new AjaxElementLocatorFactory(testWebDriver.getDriver(), 10), this);
-        testWebDriver.setImplicitWait(25);
+
+  public void verifySourceForTotalConsumedQuantity() {
+    Select select = new Select(dropDownTotalConsumedQuantity);
+    SeleneseTestNgHelper.assertEquals(select.getFirstSelectedOption().getText(), "User Input");
+  }
+
+  public void verifySourceForStockOnHand() {
+    Select select = new Select(stockInHandDropDown);
+    SeleneseTestNgHelper.assertEquals(select.getFirstSelectedOption().getText(), "User Input");
+  }
+
+  public void configureTemplate() {
+    String message = null;
+
+    testWebDriver.waitForElementToAppear(SaveButton);
+    verifySourceForTotalConsumedQuantity();
+    verifySourceForStockOnHand();
+    testWebDriver.selectByVisibleText(stockInHandDropDown, "Calculated");
+    testWebDriver.sleep(1500);
+    SaveButton.click();
+
+    testWebDriver.sleep(2000);
+    if (saveSuccessMsg.isDisplayed()) {
+      message = testWebDriver.getText(saveSuccessMsg);
+    } else {
+      message = testWebDriver.getText(saveErrorMsgDiv);
     }
-
-
-    public void verifySourceForTotalConsumedQuantity(){
-        Select select = new Select(dropDownTotalConsumedQuantity);
-        SeleneseTestNgHelper.assertEquals(select.getFirstSelectedOption().getText(), "User Input");
-    }
-
-    public void verifySourceForStockOnHand(){
-        Select select = new Select(stockInHandDropDown);
-        SeleneseTestNgHelper.assertEquals(select.getFirstSelectedOption().getText(), "User Input");
-    }
-
-    public void configureTemplate(){
-        String message=null;
-
-        testWebDriver.waitForElementToAppear(SaveButton);
-        verifySourceForTotalConsumedQuantity();
-        verifySourceForStockOnHand();
-        testWebDriver.selectByVisibleText(stockInHandDropDown,"Calculated");
-        testWebDriver.sleep(1500);
-        SaveButton.click();
-
-        testWebDriver.sleep(2000);
-        if(saveSuccessMsg.isDisplayed())
-        {
-            message= testWebDriver.getText(saveSuccessMsg);
-        }
-        else
-        {
-            message= testWebDriver.getText(saveErrorMsgDiv);
-        }
 
     SeleneseTestNgHelper.assertEquals(message, TEMPLATE_SUCCESS_MESSAGE);
 
-    }
+  }
 }
