95ddafc083cce601d592e01ea8cd143663973929
Manjyot <manjyots@thoughtworks.com>
2013-03-01 12:16:02 +0530

    Manjyot, Lokesh | Fixing FTs i.e. changes pertaining to facility w.r.t upload geo zones story

--- test-data/sql/facilities.sql
+++ test-data/sql/facilities.sql
@@ -2,9 +2,9 @@ DELETE FROM programs_supported;
 DELETE FROM facilities;
 
 INSERT INTO facilities
-(code, name, description, gln, mainPhone, fax, address1, address2, geographicZoneId, typeId, catchmentPopulation, latitude, longitude, altitude, operatedById, coldStorageGrossCapacity, coldStorageNetCapacity, suppliesOthers, sdp, hasElectricity, online, hasElectronicScc, hasElectronicDar, active, goLiveDate, goDownDate, satellite, comment, dataReportable) values
-('F10','Village Dispensary','IT department','G7645',9876234981,'fax','A','B',1,1,333,22.1,1.2,3.3,2,9.9,6.6,'TRUE','TRUE','TRUE','TRUE','TRUE','TRUE','TRUE','11/11/12','11/11/1887','TRUE','fc','TRUE'),
-('F11','Central Hospital','IT department','G7646',9876234981,'fax','A','B',1,2,333,22.3,1.2,3.3,3,9.9,6.6,'TRUE','TRUE','TRUE','TRUE','TRUE','TRUE','TRUE','11/11/12','11/11/2012','TRUE','fc','TRUE');
+(code, name, description, gln, mainPhone, fax, address1, address2, geographicZoneCode, typeId, catchmentPopulation, latitude, longitude, altitude, operatedById, coldStorageGrossCapacity, coldStorageNetCapacity, suppliesOthers, sdp, hasElectricity, online, hasElectronicScc, hasElectronicDar, active, goLiveDate, goDownDate, satellite, comment, dataReportable) values
+('F10','Village Dispensary','IT department','G7645',9876234981,'fax','A','B','Arusha',1,333,22.1,1.2,3.3,2,9.9,6.6,'TRUE','TRUE','TRUE','TRUE','TRUE','TRUE','TRUE','11/11/12','11/11/1887','TRUE','fc','TRUE'),
+('F11','Central Hospital','IT department','G7646',9876234981,'fax','A','B','Dodoma',2,333,22.3,1.2,3.3,3,9.9,6.6,'TRUE','TRUE','TRUE','TRUE','TRUE','TRUE','TRUE','11/11/12','11/11/2012','TRUE','fc','TRUE');
 
 INSERT INTO programs_supported(facilityId, programId, startDate, active, modifiedBy) VALUES
 ((SELECT id FROM facilities WHERE code = 'F10'), 1, '11/11/12', true, 1),
