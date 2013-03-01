10f34ab78d90dbf48d6b825ebe7280ed1518cf58
Manjyot <manjyots@thoughtworks.com>
2013-03-01 11:12:46 +0530

    Manjyot, Lokesh| Changing test data file to incorporate changes pertaining to showcase

--- test-data/sql/users.sql
+++ test-data/sql/users.sql
@@ -1,7 +1,7 @@
 INSERT INTO roles
  (name, description) VALUES
  ('Store In-Charge', ''),
- ('District Pharmacist', ''),
+ ('LMU', ''),
  ('FacilityHead', ''),
  ('Medical-Officer', '');
 
@@ -9,19 +9,23 @@ INSERT INTO role_rights
   (roleId, rightName) VALUES
   ((select id from roles where name='Store In-Charge'), 'VIEW_REQUISITION'),
   ((select id from roles where name='Store In-Charge'), 'CREATE_REQUISITION'),
-  ((select id from roles where name='Store In-Charge'), 'AUTHORIZE_REQUISITION'),
-  ((select id from roles where name='Store In-Charge'), 'APPROVE_REQUISITION'),
   ((select id from roles where name='Medical-Officer'), 'VIEW_REQUISITION'),
-  ((select id from roles where name='Medical-Officer'), 'CONVERT_TO_ORDER'),
-  ((select id from roles where name='FacilityHead'), 'APPROVE_REQUISITION'),
-  ((select id from roles where name='District Pharmacist'), 'VIEW_REQUISITION');
+  ((select id from roles where name='Medical-Officer'), 'APPROVE_REQUISITION'),
+  ((select id from roles where name='FacilityHead'), 'AUTHORIZE_REQUISITION'),
+  ((select id from roles where name='FacilityHead'), 'VIEW_REQUISITION'),
+  ((select id from roles where name='District Pharmacist'), 'VIEW_REQUISITION'),
+  ((select id from roles where name='LMU'), 'VIEW_REQUISITION'),
+  ((select id from roles where name='LMU'), 'APPROVE_REQUISITION'),
+  ((select id from roles where name='LMU'), 'CONVERT_TO_ORDER');
+
 
 
 INSERT INTO users
   (id, userName, password, facilityId, firstName, lastName, email, active) VALUES
   (200, 'StoreInCharge', 'Agismyf1Whs0fxr1FFfK8cs3qisVJ1qMs3yuMLDTeEcZEGzstjiswaaUsQNQTIKk1U5JRzrDbPLCzCO1isvB5YGaEQieie', (SELECT id FROM facilities WHERE code = 'F10'), 'Fatima', 'Doe', 'Fatima_Doe@openlmis.com', true),
   (300, 'FacilityHead', 'Agismyf1Whs0fxr1FFfK8cs3qisVJ1qMs3yuMLDTeEcZEGzstjiswaaUsQNQTIKk1U5JRzrDbPLCzCO1isvB5YGaEQieie', (SELECT id FROM facilities WHERE code = 'F10'), 'Jane', 'Doe', 'Jane_Doe@openlmis.com', true),
-  (400, 'MedicalOfficer', 'Agismyf1Whs0fxr1FFfK8cs3qisVJ1qMs3yuMLDTeEcZEGzstjiswaaUsQNQTIKk1U5JRzrDbPLCzCO1isvB5YGaEQieie', (SELECT id FROM facilities WHERE code = 'F10'), 'John', 'Doe', 'Joh_Doe@openlmis.com', true);
+  (400, 'MedicalOfficer', 'Agismyf1Whs0fxr1FFfK8cs3qisVJ1qMs3yuMLDTeEcZEGzstjiswaaUsQNQTIKk1U5JRzrDbPLCzCO1isvB5YGaEQieie', (SELECT id FROM facilities WHERE code = 'F10'), 'John', 'Doe', 'Joh_Doe@openlmis.com', true),
+  (500, 'lmu', 'Agismyf1Whs0fxr1FFfK8cs3qisVJ1qMs3yuMLDTeEcZEGzstjiswaaUsQNQTIKk1U5JRzrDbPLCzCO1isvB5YGaEQieie', (SELECT id FROM facilities WHERE code = 'F10'), 'Frank', 'Doe', 'Frank_Doe@openlmis.com', true);
 
 INSERT INTO supervisory_nodes
   (parentId, facilityId, name, code) VALUES
@@ -35,9 +39,10 @@ INSERT INTO role_assignments
   (userId, roleId, programId, supervisoryNodeId) VALUES
   (200, (SELECT id FROM roles WHERE name = 'Store In-Charge'), 1, null),
   (200, (SELECT id FROM roles WHERE name = 'Store In-Charge'), 2, null),
-  (200, (SELECT id FROM roles WHERE name = 'Store In-Charge'), 1, (SELECT id from supervisory_nodes WHERE code = 'N1')),
-  (200, (SELECT id FROM roles WHERE name = 'Store In-Charge'), 2, (SELECT id from supervisory_nodes WHERE code = 'N1')),
   (300, (SELECT id FROM roles WHERE name = 'FacilityHead'), 1, (SELECT id from supervisory_nodes WHERE code = 'N2')),
   (300, (SELECT id FROM roles WHERE name = 'FacilityHead'), 2, (SELECT id from supervisory_nodes WHERE code = 'N2')),
-  (400, (SELECT id FROM roles WHERE name = 'Medical-Officer'), 1, (SELECT id from supervisory_nodes WHERE code = 'N1')),
-  (400, (SELECT id FROM roles WHERE name = 'Medical-Officer'), 2, (SELECT id from supervisory_nodes WHERE code = 'N1'));
+  (400, (SELECT id FROM roles WHERE name = 'Medical-Officer'), 1, (SELECT id from supervisory_nodes WHERE code = 'N2')),
+  (400, (SELECT id FROM roles WHERE name = 'Medical-Officer'), 2, (SELECT id from supervisory_nodes WHERE code = 'N2')),
+  (500, (SELECT id FROM roles WHERE name = 'LMU'), 1, (SELECT id from supervisory_nodes WHERE code = 'N1')),
+  (500, (SELECT id FROM roles WHERE name = 'LMU'), 2, (SELECT id from supervisory_nodes WHERE code = 'N1'));
+
