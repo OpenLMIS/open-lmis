6cb00b58fdb564d8c10f1a0d9f640f3457901fbd
manjyot singh <manjyots@thoughtworks.com>
2013-03-06 13:02:53 +0530

    Raman/Manjyot - Added code for zip file management

---
+++ test-modules/test-core/src/main/java/org/openlmis/UiUtils/Unzip.java
@@ -0,0 +1,73 @@
+package org.openlmis.UiUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+public class Unzip
+{
+
+
+  public boolean deleteFile(String file){
+    boolean flag=false;
+    File fileToBeDeleted = new File(file);
+    if(fileToBeDeleted.exists())
+    {
+      fileToBeDeleted.delete();
+      flag=true;
+      System.out.println("file deleted : "+ fileToBeDeleted.getAbsoluteFile());
+    }
+    return flag;
+  }
+
+
+  public void unZipIt(String zipFile, String outputFolder){
+
+    byte[] buffer = new byte[2048];
+
+    try{
+      if(new File(zipFile).exists())
+      {
+
+
+      File folder = new File(outputFolder);
+      if(!folder.exists()){
+        folder.mkdir();
+      }
+
+      ZipInputStream zis =
+        new ZipInputStream(new FileInputStream(zipFile));
+      ZipEntry ze = zis.getNextEntry();
+
+      while(ze!=null){
+
+        String fileName = ze.getName();
+        File newFile = new File(outputFolder + File.separator + fileName);
+
+        System.out.println("file unzip : "+ newFile.getAbsoluteFile());
+
+        new File(newFile.getParent()).mkdirs();
+
+        FileOutputStream fos = new FileOutputStream(newFile);
+
+        int len;
+        while ((len = zis.read(buffer)) > 0) {
+          fos.write(buffer, 0, len);
+        }
+
+        fos.close();
+        ze = zis.getNextEntry();
+      }
+
+      zis.closeEntry();
+      zis.close();
+
+      }
+    }catch(IOException ex){
+      ex.printStackTrace();
+    }
+  }
+}
