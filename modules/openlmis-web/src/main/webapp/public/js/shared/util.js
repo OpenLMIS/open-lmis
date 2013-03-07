419d95584a3ee3defc4c5862f085c86bec2f737f
Anshul Rastogi <anshulr@thoughtworks.com>
2013-03-07 17:11:11 +0530

    |#537|+Mridul| fixing bug, changing precision of longitude, latitude to (3,5) while creating facility.

--- modules/openlmis-web/src/main/webapp/public/js/shared/util.js
+++ modules/openlmis-web/src/main/webapp/public/js/shared/util.js
@@ -1,33 +1,37 @@
 var utils = {
-  getFormattedDate:function (date) {
+  getFormattedDate: function (date) {
     return ('0' + date.getDate()).slice(-2) + '/'
       + ('0' + (date.getMonth() + 1)).slice(-2) + '/'
       + date.getFullYear();
   },
 
-  isNumber:function (number) {
-    return !isNaN(utils.parseIntWithBaseTen(number));
+  isNullOrUndefined: function (obj) {
+    return obj == undefined || obj == null;
   },
 
-  parseIntWithBaseTen:function (number) {
+  isNumber: function (numberValue) {
+    if (this.isNullOrUndefined(numberValue)) return false;
+    var number = numberValue.toString();
+
+    if (number.trim() == '') return false;
+    return !isNaN(number);
+  },
+
+  parseIntWithBaseTen: function (number) {
     return parseInt(number, 10);
   },
 
-  getValueFor:function (number, defaultValue) {
+  getValueFor: function (number, defaultValue) {
     if (!utils.isNumber(number)) return defaultValue ? defaultValue : null;
     return utils.parseIntWithBaseTen(number);
   },
 
-  isNullOrUndefined:function (obj) {
-    return obj == undefined || obj == null;
-  },
-
-  isPositiveNumber:function (value) {
+  isPositiveNumber: function (value) {
     var INTEGER_REGEXP = /^\d*$/;
     return INTEGER_REGEXP.test(value);
   },
 
-  isValidPage:function (pageNumber, totalPages) {
+  isValidPage: function (pageNumber, totalPages) {
     pageNumber = parseInt(pageNumber, 10);
     return !!pageNumber && pageNumber > 0 && pageNumber <= totalPages;
   }
