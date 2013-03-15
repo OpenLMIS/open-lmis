983798d13ced082c711f1458a84a682bc5175ed6
Manjyot <manjyots@thoughtworks.com>
2013-03-15 15:39:33 +0530

    Altered test data for showcase purpose

--- test-data/sql/requisition-groups.sql
+++ test-data/sql/requisition-groups.sql
@@ -11,14 +11,14 @@ insert into requisition_group_program_schedules ( requisitionGroupId , programId
 ((select id from requisition_groups where code='RG1'),(select id from programs where code='ESS_MEDS'),(select id from processing_schedules where code='M'),TRUE),
 ((select id from requisition_groups where code='RG1'),(select id from programs where code='MALARIA'),(select id from processing_schedules where code='Q1stM'),TRUE),
 ((select id from requisition_groups where code='RG1'),(select id from programs where code='HIV'),(select id from processing_schedules where code='M'),TRUE),
-((select id from requisition_groups where code='RG1'),(select id from programs where code='ARV'),(select id from processing_schedules where code='M'),TRUE),
+((select id from requisition_groups where code='RG1'),(select id from programs where code='TB'),(select id from processing_schedules where code='M'),TRUE),
 ((select id from requisition_groups where code='RG2'),(select id from programs where code='ESS_MEDS'),(select id from processing_schedules where code='M'),TRUE),
 ((select id from requisition_groups where code='RG2'),(select id from programs where code='MALARIA'),(select id from processing_schedules where code='Q1stM'),TRUE),
 ((select id from requisition_groups where code='RG2'),(select id from programs where code='HIV'),(select id from processing_schedules where code='M'),TRUE),
-((select id from requisition_groups where code='RG2'),(select id from programs where code='ARV'),(select id from processing_schedules where code='M'),TRUE);
+((select id from requisition_groups where code='RG2'),(select id from programs where code='TB'),(select id from processing_schedules where code='M'),TRUE);
 
 
 insert into supply_lines (description, supervisoryNodeId, programId, supplyingFacilityId) values
 ('supplying node for ESS_MEDS', (select id from supervisory_nodes where code = 'N1'), (select id from programs where code='ESS_MEDS'),(select id from facilities where code = 'F11')),
-('supplying node for ARV', (select id from supervisory_nodes where code = 'N1'), (select id from programs where code='ARV'),(select id from facilities where code = 'F10'));
+('supplying node for TB', (select id from supervisory_nodes where code = 'N1'), (select id from programs where code='TB'),(select id from facilities where code = 'F10'));
 
