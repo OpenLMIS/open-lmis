022e25547609c50892d20be218ecb4339608b3f2
Wolde Fikadu <memiwo@gmail.com>
2013-03-21 15:44:58 +0300

    Initial report module implementation

--- modules/report/src/main/java/org/openlmis/report/model/ReportData.java
+++ modules/report/src/main/java/org/openlmis/report/model/ReportData.java
@@ -1,11 +1,6 @@
 package org.openlmis.report.model;
 
 /**
- * Created with IntelliJ IDEA.
- * User: AKE
- * Date: 3/20/13
- * Time: 5:13 PM
- * To change this template use File | Settings | File Templates.
  */
 public interface ReportData {
 }
