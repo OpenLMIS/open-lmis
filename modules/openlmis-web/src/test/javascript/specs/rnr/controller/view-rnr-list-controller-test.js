65b4f58e427f5e65ae47075bc8982a43bf2c5992
manan <manan.bharara@gmail.com>
2013-03-30 15:23:54 +0530

    Fixing failing jasmine spec.

--- modules/openlmis-web/src/test/javascript/specs/rnr/controller/view-rnr-list-controller-test.js
+++ modules/openlmis-web/src/test/javascript/specs/rnr/controller/view-rnr-list-controller-test.js
@@ -140,7 +140,7 @@ describe('ViewRnrListController', function () {
       {'id':1, 'programId':2}
     ];
     scope.openRequisition();
-    expect(location.url()).toEqual("/requisition/1/2?supplyType=full-supply");
+    expect(location.url()).toEqual("/requisition/1/2?supplyType=full-supply&page=1");
   });
 
   function loadRequisitions(expectedUrl, respondWith) {
