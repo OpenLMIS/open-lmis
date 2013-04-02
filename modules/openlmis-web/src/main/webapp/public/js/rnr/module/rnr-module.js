e681f20f84295b790cceed204476a1779c546c44
Rahul Joshi <rahulj@thoughtworks.com>
2013-04-02 16:25:04 +0530

    |#651| renaming rnr partials

--- modules/openlmis-web/src/main/webapp/public/js/rnr/module/rnr-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/rnr/module/rnr-module.js
@@ -6,13 +6,13 @@
 
 var rnrModule = angular.module('rnr', ['openlmis', 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstrap.dropdownToggle']).config(['$routeProvider', function ($routeProvider) {
   $routeProvider.
-    when('/init-rnr', {controller:InitiateRnrController, templateUrl:'partials/init.html', resolve: InitiateRnrController.resolve}).
-    when('/create-rnr/:rnr/:facility/:program', {controller:CreateRequisitionController, templateUrl:'partials/create.html', resolve:CreateRequisitionController.resolve, reloadOnSearch:false}).
-    when('/rnr-for-approval', {controller:ApproveRnrListController, templateUrl:'partials/list.html', resolve:ApproveRnrListController.resolve}).
+    when('/init-rnr', {controller:InitiateRnrController, templateUrl:'partials/create/init.html', resolve: InitiateRnrController.resolve}).
+    when('/create-rnr/:rnr/:facility/:program', {controller:CreateRequisitionController, templateUrl:'partials/create/index.html', resolve:CreateRequisitionController.resolve, reloadOnSearch:false}).
+    when('/rnr-for-approval', {controller:ApproveRnrListController, templateUrl:'partials/view/list.html', resolve:ApproveRnrListController.resolve}).
     when('/requisitions-for-convert-to-order', {controller:ConvertToOrderListController, templateUrl:'partials/convert-to-order-list.html', resolve:ConvertToOrderListController.resolve}).
-    when('/view-requisitions', {controller:ViewRnrListController, templateUrl:'partials/view-rnr.html', resolve:ViewRnrListController.resolve}).
-    when('/rnr-for-approval/:rnr/:program', {controller:ApproveRnrController, templateUrl:'partials/approve.html', resolve:ApproveRnrController.resolve, reloadOnSearch:false}).
-    when('/requisition/:rnr/:program', {controller:ViewRnrController, templateUrl:'partials/view.html', resolve: ViewRnrController.resolve, reloadOnSearch:false}).
+    when('/view-requisitions', {controller:ViewRnrListController, templateUrl:'partials/view/index.html', resolve:ViewRnrListController.resolve}).
+    when('/rnr-for-approval/:rnr/:program', {controller:ApproveRnrController, templateUrl:'partials/approve/approve.html', resolve:ApproveRnrController.resolve, reloadOnSearch:false}).
+    when('/requisition/:rnr/:program', {controller:ViewRnrController, templateUrl:'partials/view/view.html', resolve: ViewRnrController.resolve, reloadOnSearch:false}).
     otherwise({redirectTo:'/init-rnr'});
 }]).directive('rnrValidator',function () {
     return {
