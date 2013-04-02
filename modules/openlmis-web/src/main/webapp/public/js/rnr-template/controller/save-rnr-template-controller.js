bbc2bf71842c5267ecc270be63d81b1f5fc0b345
Manan <manan.bharara@gmail.com>
2013-04-02 16:22:12 +0530

    |#663|+Balvinder| Fixed bug. Giving meaningful error message on template form invalidation

--- modules/openlmis-web/src/main/webapp/public/js/rnr-template/controller/save-rnr-template-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/rnr-template/controller/save-rnr-template-controller.js
@@ -29,7 +29,7 @@ function SaveRnrTemplateController($scope, rnrTemplateForm, program, messageServ
       if (data != null) {
         $scope.errorMap = data.data;
       }
-      updateErrorMessage("Save Failed!");
+      updateErrorMessage("form.error");
     });
   };
 
