c09be28788ec62d5802622a34765d52bff47b3d8
Balvinder Khurana <balvindk@thoughtworks.com>
2013-04-03 17:23:17 +0530

    |#670 | +Jaideep | Closing previous select2 dropdown on clicking new new select2 box

--- modules/openlmis-web/src/main/webapp/public/js/user/module/user-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/user/module/user-module.js
@@ -18,7 +18,19 @@ angular.module('user', ['openlmis', 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstra
           scope.$apply(attrs.onKeyup);
         });
       };
-    }).run(function($rootScope, AuthorizationService) {
+    })
+  .directive('select2Blur', function() {
+    return function (scope, elm, attrs) {
+      angular.element("body").on('mousedown', function(e) {
+        $('.select2-dropdown-open').each(function() {
+          if(!$(this).hasClass('select2-container-active')) {
+            $(this).data("select2").blur();
+          }
+        })
+      });
+    };
+  })
+  .run(function($rootScope, AuthorizationService) {
     $rootScope.userSelected = "selected";
     AuthorizationService.preAuthorize('MANAGE_USERS');
   });
