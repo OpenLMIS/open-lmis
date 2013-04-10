aa2eba7f987fdb2671b527d084ae168a8eb018ee
Elias <eliasmu@gmail.com>
2013-04-10 14:35:53 +0300

    refactored right name on the rights class for the newly added reports added consumption report and reporting rate report to the rights

--- modules/core/src/main/java/org/openlmis/core/domain/Right.java
+++ modules/core/src/main/java/org/openlmis/core/domain/Right.java
@@ -25,9 +25,14 @@ public enum Right {
   APPROVE_REQUISITION("Requisition - Approve", "requisition", "Permission to approve requisitions"),
   CONVERT_TO_ORDER("Requisition - Convert to Order", "requisition", "Permission to convert requisitions to order"),
   VIEW_ORDER("Requisition - View Orders", "requisition", "Permission to view orders"),
-  VIEW_REQUISITION("Requisition - View", "requisition", "Permission to view requisitions");
+  VIEW_REQUISITION("Requisition - View", "requisition", "Permission to view requisitions"),
+  VIEW_FACILITY_REPORT("Report - Facility List", "report", "Permission to view Facility List Report"),
+  VIEW_MAILING_LABEL_REPORT("Report Facility Mailing Label", "report", "Permission to view Mailing labels for Facilities"),
+  VIEW_CONSUMPTION_REPORT("Report - Consumption Report", "report", "Permission to view Consumption Report"),
+  VIEW_REPORTING_RATE_REPORT("Report - Reporting Rate Report", "report", "Permission to view Reporting Rate Report");
 
-  @Getter
+
+    @Getter
   private final String rightName;
 
   @Getter
