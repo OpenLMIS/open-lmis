51ad21148103c36e5862e43e7741dbce88652300
Elias <eliasmu@gmail.com>
2013-04-11 17:00:33 +0300

    move mailing list angular module and controller to reports folder move facility angular module and controller to reports folder added a blank reports (css override) progress on the consumption query.

--- modules/report/src/main/java/org/openlmis/report/builder/ConsumptionQueryBuilder.java
+++ modules/report/src/main/java/org/openlmis/report/builder/ConsumptionQueryBuilder.java
@@ -10,6 +10,19 @@ import java.util.Map;
 public class ConsumptionQueryBuilder {
 
     public static String getQuery(Map params){
-        return "select * from facilities";
+        return "select " +
+                    "sum(quantitydispensed) consumption " +
+                    ", product,productcategory category " +
+                    ", ft.name facilityType " +
+                    ", f.name facility " +
+                "from " +
+                    "requisition_line_items li " +
+                    "join requisitions r on r.id = li.rnrid " +
+                    "join facilities f on r.facilityid = f.id " +
+                    "join facility_types ft on ft.id = f.typeid " +
+                "group by " +
+                    "li.product, li.productcategory, f.name, ft.name " +
+                "order by " +
+                    "li.productCategory, li.product";
     }
 }
