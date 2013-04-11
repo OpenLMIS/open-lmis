cf4ea8921c31f6176084b832426f43e04e85bb55
Wolde Fikadu <memiwo@gmail.com>
2013-04-11 06:51:48 +0300

    updated to work for the new schema design

--- modules/db/src/main/resources/db/migration/V34__insert_rights_for_reports.sql
+++ modules/db/src/main/resources/db/migration/V34__insert_rights_for_reports.sql
@@ -1,6 +1,6 @@
-INSERT INTO rights(name,adminright, description) VALUES
- ('VIEW_FACILITY_REPORT',false,'Permission to view Facility List Report'),
- ('VIEW_MAILING_LABEL_REPORT',false,'Permission to view Mailing labels for Facilities'),
- ('VIEW_CONSUMPTION_REPORT',false,'Permission to view Consumption Report'),
- ('VIEW_REPORTING_RATE_REPORT',false,'Permission to view Reporting Rate Report');
+INSERT INTO rights(name,description) VALUES
+ ('VIEW_FACILITY_REPORT','Permission to view Facility List Report'),
+ ('VIEW_MAILING_LABEL_REPORT','Permission to view Mailing labels for Facilities'),
+ ('VIEW_CONSUMPTION_REPORT','Permission to view Consumption Report'),
+ ('VIEW_REPORTING_RATE_REPORT','Permission to view Reporting Rate Report');
 
