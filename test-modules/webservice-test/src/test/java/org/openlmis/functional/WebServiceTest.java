fe0f5de7658a1ff8ede8261e87ccc69a3b673a7d
Manjyot <manjyots@thoughtworks.com>
2013-04-16 10:45:21 +0530

    +Raman|Gradle folder structure for web service tests

---
+++ test-modules/webservice-test/src/test/java/org/openlmis/functional/WebServiceTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright © 2013 VillageReach.  All Rights Reserved.  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ *
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.openlmis.functional;
+
+
+import org.testng.annotations.*;
+
+
+
+
+public class WebServiceTest  {
+
+  @BeforeMethod(groups = {"smoke"})
+  public void setUp() throws Exception {
+  }
+
+  @DataProvider(name = "envData")
+  public Object[][] getEnvData() {
+    return new Object[][]{};
+  }
+
+  @Test(groups = {"smoke"}, dataProvider = "Data-Provider-Function-Positive")
+  public void testWebServiceTest() throws Exception {
+
+  }
+
+}
+
