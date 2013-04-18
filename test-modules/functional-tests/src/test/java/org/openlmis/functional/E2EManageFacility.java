e1715f4799d96baa366255a2e5db02eb151f8766
Manjyot <manjyots@thoughtworks.com>
2013-04-18 12:39:03 +0530

    Fixing smoke

--- test-modules/functional-tests/src/test/java/org/openlmis/functional/E2EManageFacility.java
+++ test-modules/functional-tests/src/test/java/org/openlmis/functional/E2EManageFacility.java
@@ -27,65 +27,65 @@ import java.util.ArrayList;
 public class E2EManageFacility extends TestCaseHelper {
 
 
-    @BeforeMethod(groups = {"functional"})
-    public void setUp() throws Exception {
-        super.setup();
-    }
+  @BeforeMethod(groups = {"functional"})
+  public void setUp() throws Exception {
+    super.setup();
+  }
 
-    @Test(groups = {"functional"}, dataProvider = "Data-Provider-Function-Positive")
-    public void testE2EManageFacility(String user, String program, String[] credentials) throws Exception {
+  @Test(groups = {"functional"}, dataProvider = "Data-Provider-Function-Positive")
+  public void testE2EManageFacility(String user, String program, String[] credentials) throws Exception {
 
-        LoginPage loginPage = new LoginPage(testWebDriver, baseUrlGlobal);
+    LoginPage loginPage = new LoginPage(testWebDriver, baseUrlGlobal);
 
-        dbWrapper.insertUser("200", user, "Ag/myf1Whs0fxr1FFfK8cs3q/VJ1qMs3yuMLDTeEcZEGzstj/waaUsQNQTIKk1U5JRzrDbPLCzCO1/vB5YGaEQ==", "F10", "Jane_Doe@openlmis.com", "openLmis");
+    dbWrapper.insertUser("200", user, "Ag/myf1Whs0fxr1FFfK8cs3q/VJ1qMs3yuMLDTeEcZEGzstj/waaUsQNQTIKk1U5JRzrDbPLCzCO1/vB5YGaEQ==", "F10", "Jane_Doe@openlmis.com", "openLmis");
 
-        HomePage homePage = loginPage.loginAs(credentials[0], credentials[1]);
+    HomePage homePage = loginPage.loginAs(credentials[0], credentials[1]);
 
-        CreateFacilityPage createFacilityPage = homePage.navigateCreateFacility();
-        String geoZone = "Ngorongoro";
-        String facilityType = "Lvl3 Hospital";
-        String operatedBy = "MoH";
-        String facilityCodePrefix = "FCcode";
-        String facilityNamePrefix = "FCname";
+    CreateFacilityPage createFacilityPage = homePage.navigateCreateFacility();
+    String geoZone = "Ngorongoro";
+    String facilityType = "Lvl3 Hospital";
+    String operatedBy = "MoH";
+    String facilityCodePrefix = "FCcode";
+    String facilityNamePrefix = "FCname";
 
-        String date_time = createFacilityPage.enterValuesInFacility(facilityCodePrefix, facilityNamePrefix, program, geoZone, facilityType, operatedBy);
-        createFacilityPage.verifyMessageOnFacilityScreen(facilityNamePrefix + date_time, "created");
+    String date_time = createFacilityPage.enterValuesInFacility(facilityCodePrefix, facilityNamePrefix, program, geoZone, facilityType, operatedBy);
+    createFacilityPage.verifyMessageOnFacilityScreen(facilityNamePrefix + date_time, "created");
 
-        DeleteFacilityPage deleteFacilityPage = homePage.navigateSearchFacility();
-        deleteFacilityPage.searchFacility(date_time);
-        deleteFacilityPage.clickFacilityList();
-        deleteFacilityPage.deleteFacility(facilityCodePrefix + date_time, facilityNamePrefix + date_time);
-        deleteFacilityPage.verifyDeletedFacility(facilityCodePrefix + date_time, facilityNamePrefix + date_time);
-        HomePage homePageRestore = deleteFacilityPage.restoreFacility();
+    DeleteFacilityPage deleteFacilityPage = homePage.navigateSearchFacility();
+    deleteFacilityPage.searchFacility(date_time);
+    deleteFacilityPage.clickFacilityList();
+    deleteFacilityPage.deleteFacility(facilityCodePrefix + date_time, facilityNamePrefix + date_time);
+    deleteFacilityPage.verifyDeletedFacility(facilityCodePrefix + date_time, facilityNamePrefix + date_time);
+    HomePage homePageRestore = deleteFacilityPage.restoreFacility();
 
-        DeleteFacilityPage deleteFacilityPageRestore = homePageRestore.navigateSearchFacility();
-        deleteFacilityPageRestore.searchFacility(date_time);
-        deleteFacilityPageRestore.clickFacilityList();
-        HomePage homePageEdit = deleteFacilityPageRestore.editAndVerifyFacility("ESSENTIAL MEDICINES", facilityNamePrefix + date_time);
+    DeleteFacilityPage deleteFacilityPageRestore = homePageRestore.navigateSearchFacility();
+    deleteFacilityPageRestore.searchFacility(date_time);
+    deleteFacilityPageRestore.clickFacilityList();
+    HomePage homePageEdit = deleteFacilityPageRestore.editAndVerifyFacility("ESSENTIAL MEDICINES", facilityNamePrefix + date_time);
 
-        DeleteFacilityPage deleteFacilityPageEdit = homePageEdit.navigateSearchFacility();
-        deleteFacilityPageEdit.searchFacility(date_time);
+    DeleteFacilityPage deleteFacilityPageEdit = homePageEdit.navigateSearchFacility();
+    deleteFacilityPageEdit.searchFacility(date_time);
 
-        ArrayList<String> programsSupported = new ArrayList<String>();
-        programsSupported.add("HIV");
-        programsSupported.add("ESSENTIAL MEDICINES");
-        deleteFacilityPageEdit.verifyProgramSupported(programsSupported);
+    ArrayList<String> programsSupported = new ArrayList<String>();
+    programsSupported.add("HIV");
+    programsSupported.add("ESSENTIAL MEDICINES");
+    deleteFacilityPageEdit.verifyProgramSupported(programsSupported);
 
 
-    }
+  }
 
-    @AfterMethod(groups = {"functional"})
-    public void tearDown() throws Exception {
-        HomePage homePage = new HomePage(testWebDriver);
-        homePage.logout(baseUrlGlobal);
-        dbWrapper.deleteData();
-        dbWrapper.closeConnection();
-    }
+  @AfterMethod(groups = {"functional"})
+  public void tearDown() throws Exception {
+    HomePage homePage = new HomePage(testWebDriver);
+    homePage.logout(baseUrlGlobal);
+    dbWrapper.deleteData();
+    dbWrapper.closeConnection();
+  }
 
-    @DataProvider(name = "Data-Provider-Function-Positive")
-    public Object[][] parameterIntTestProviderPositive() {
-        return new Object[][]{
-                {"User123", "HIV", new String[]{"Admin123", "Admin123"}}
-        };
-    }
+  @DataProvider(name = "Data-Provider-Function-Positive")
+  public Object[][] parameterIntTestProviderPositive() {
+    return new Object[][]{
+      {"User123", "HIV", new String[]{"Admin123", "Admin123"}}
+    };
+  }
 }
