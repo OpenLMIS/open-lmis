130bfa80926e0a2406d3a78ab511e30f12da26e4
Wolde Fikadu <memiwo@gmail.com>
2013-04-24 00:57:06 +0300

    fix to report access rights

--- modules/db/src/main/resources/db/migration/V34_1__insert_rights_for_reports.sql
+++ modules/db/src/main/resources/db/migration/V34_1__insert_rights_for_reports.sql
@@ -1,8 +1,8 @@
 INSERT INTO rights(name, adminRight,description) VALUES
- ('VIEW_FACILITY_REPORT',FALSE,'Permission to view Facility List Report'),
- ('VIEW_MAILING_LABEL_REPORT',FALSE,'Permission to view Mailing labels for Facilities'),
- ('VIEW_CONSUMPTION_REPORT',FALSE,'Permission to view Consumption Report'),
- ('VIEW_REPORTING_RATE_REPORT',FALSE,'Permission to view Reporting Rate Report');
+ ('VIEW_FACILITY_REPORT',TRUE ,'Permission to view Facility List Report'),
+ ('VIEW_MAILING_LABEL_REPORT',TRUE,'Permission to view Mailing labels for Facilities'),
+ ('VIEW_CONSUMPTION_REPORT',TRUE,'Permission to view Consumption Report'),
+ ('VIEW_REPORTING_RATE_REPORT',TRUE,'Permission to view Reporting Rate Report');
 
 
 
