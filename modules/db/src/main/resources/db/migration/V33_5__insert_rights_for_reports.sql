9bf7a4d147275ceaaf06d4bfaf9a17ea7efc1f70
jspeshu <jspeshu@gmail.com>
2013-04-24 19:14:05 +0300

    -- fixed the migration problem by renaming the files (note :
    -- dump the current db (i.e. take a buckup)
    
    -- modify the schema version info to male 33.2  current version to true and delete all after that, in alphabetical order delete version number 34 and 35.1 and change the current version to 33.2 (set true the current version col)
    
    -- run gradle migrateDB )

--- modules/db/src/main/resources/db/migration/V33_5__insert_rights_for_reports.sql
+++ modules/db/src/main/resources/db/migration/V33_5__insert_rights_for_reports.sql
@@ -1,3 +1,8 @@
+delete from rights where name = 'VIEW_FACILITY_REPORT';
+delete from rights where name = 'VIEW_MAILING_LABEL_REPORT';
+delete from rights where name = 'VIEW_CONSUMPTION_REPORT';
+delete from rights where name = 'VIEW_REPORTING_RATE_REPORT';
+
 INSERT INTO rights(name, adminRight,description) VALUES
  ('VIEW_FACILITY_REPORT',TRUE,'Permission to view Facility List Report'),
  ('VIEW_MAILING_LABEL_REPORT',TRUE,'Permission to view Mailing labels for Facilities'),
--- modules/db/src/main/resources/db/migration/V33_6__insert_summary_report.sql
+++ modules/db/src/main/resources/db/migration/V33_6__insert_summary_report.sql
@@ -1,2 +1,4 @@
+delete from rights where name = 'VIEW_SUMMARY_REPORT';
+
 INSERT INTO rights(name, adminRight,description) VALUES
  ('VIEW_SUMMARY_REPORT',TRUE,'Permission to view Summary Report')
