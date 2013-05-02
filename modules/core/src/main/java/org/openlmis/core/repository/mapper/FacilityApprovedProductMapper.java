d9ce9b9e8246a2cb5f43daa5a8904052098c6337
Rahul Joshi <rahulj@thoughtworks.com>
2013-05-02 15:45:46 +0530

    |#000|+Sandeep| Product search query refactored

--- modules/core/src/main/java/org/openlmis/core/repository/mapper/FacilityApprovedProductMapper.java
+++ modules/core/src/main/java/org/openlmis/core/repository/mapper/FacilityApprovedProductMapper.java
@@ -8,8 +8,6 @@ package org.openlmis.core.repository.mapper;
 
 import org.apache.ibatis.annotations.*;
 import org.openlmis.core.domain.FacilityApprovedProduct;
-import org.openlmis.core.domain.FacilityType;
-import org.openlmis.core.domain.Product;
 import org.openlmis.core.domain.ProgramProduct;
 import org.springframework.stereotype.Repository;
 
@@ -19,43 +17,42 @@ import java.util.List;
 public interface FacilityApprovedProductMapper {
 
   @Insert("INSERT INTO facility_approved_products(" +
-    "facilityTypeId, programProductId, maxMonthsOfStock, modifiedBy, modifiedDate) values " +
-    "(#{facilityType.id}, #{programProduct.id}, #{maxMonthsOfStock}, #{modifiedBy}, #{modifiedDate})")
+      "facilityTypeId, programProductId, maxMonthsOfStock, modifiedBy, modifiedDate) values " +
+      "(#{facilityType.id}, #{programProduct.id}, #{maxMonthsOfStock}, #{modifiedBy}, #{modifiedDate})")
   @Options(useGeneratedKeys = true)
   Integer insert(FacilityApprovedProduct facilityApprovedProduct);
 
-  @Select("SELECT fap.id, fap.facilityTypeId, fap.programProductId, fap.maxMonthsOfStock " +
-    "FROM products p, facility_approved_products fap, program_products pp, facilities f, " +
-    "product_forms pf , dosage_units du, product_categories pc where " +
-    "pp.programId = #{programId} " +
-    "AND f.id = #{facilityId} AND f.typeId = fap.facilityTypeId " +
-    "AND fap.programProductId = pp.id " +
-    "AND pc.id = p.categoryId " +
-    "AND p.id = pp.productId " +
-    "AND pf.id = p.formId " +
-    "AND du.id = p.dosageUnitId " +
-    "AND p.fullSupply = #{fullSupply} " +
-    "AND p.active = true " +
-    "AND pp.active = true " +
-    "ORDER BY pc.displayOrder, pc.name, p.displayOrder NULLS LAST, p.code")
+  @Select({"SELECT fap.*",
+      "FROM facility_approved_products fap ",
+      "INNER JOIN facilities f ON f.typeId = fap.facilityTypeId",
+      "INNER JOIN program_products pp ON pp.id = fap.programProductId",
+      "INNER JOIN products p ON p.id = pp.productId ",
+      "INNER JOIN product_categories pc ON pc.id = p.categoryId ",
+      "WHERE",
+      "pp.programId = #{programId}",
+      "AND f.id = #{facilityId}",
+      "AND p.fullSupply = #{fullSupply}",
+      "AND p.active = TRUE",
+      "AND pp.active = TRUE",
+      "ORDER BY pc.displayOrder, pc.name, p.displayOrder NULLS LAST, p.code"})
   @Results(value = {
-    @Result(property = "programProduct", column = "programProductID", javaType = ProgramProduct.class,
-      one = @One(select = "org.openlmis.core.repository.mapper.ProgramProductMapper.getById")),
-    @Result(property = "facilityType.id", column = "facilityTypeId")})
+      @Result(property = "programProduct", column = "programProductID", javaType = ProgramProduct.class,
+          one = @One(select = "org.openlmis.core.repository.mapper.ProgramProductMapper.getById")),
+      @Result(property = "facilityType.id", column = "facilityTypeId")})
   List<FacilityApprovedProduct> getProductsByFacilityProgramAndFullSupply(@Param("facilityId") Integer facilityId,
                                                                           @Param("programId") Integer programId,
                                                                           @Param("fullSupply") Boolean fullSupply);
 
   @Select({"SELECT fap.id, fap.facilityTypeId, fap.programProductId, fap.maxMonthsOfStock, fap.modifiedDate, fap.modifiedBy",
-    "FROM facility_approved_products fap, facility_types ft",
-    "where fap.programProductId = #{programProductId} and",
-    "ft.code = #{facilityTypeCode} and ft.id = fap.facilityTypeId"})
+      "FROM facility_approved_products fap, facility_types ft",
+      "where fap.programProductId = #{programProductId} and",
+      "ft.code = #{facilityTypeCode} and ft.id = fap.facilityTypeId"})
   FacilityApprovedProduct getFacilityApprovedProductIdByProgramProductAndFacilityTypeCode(@Param("programProductId") Integer programProductId,
-                                                                          @Param("facilityTypeCode") String facilityTypeCode);
+                                                                                          @Param("facilityTypeCode") String facilityTypeCode);
 
   @Update("UPDATE facility_approved_products set " +
-    "facilityTypeId=#{facilityType.id}, programProductId=#{programProduct.id}, maxMonthsOfStock=#{maxMonthsOfStock}, modifiedBy=#{modifiedBy}, modifiedDate=#{modifiedDate} " +
-    "where id=#{id}")
+      "facilityTypeId=#{facilityType.id}, programProductId=#{programProduct.id}, maxMonthsOfStock=#{maxMonthsOfStock}, modifiedBy=#{modifiedBy}, modifiedDate=#{modifiedDate} " +
+      "where id=#{id}")
   void updateFacilityApprovedProduct(FacilityApprovedProduct facilityApprovedProduct);
 
 }
