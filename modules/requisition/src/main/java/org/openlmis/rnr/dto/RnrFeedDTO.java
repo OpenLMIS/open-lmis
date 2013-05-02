0ea39b82602415c237d1d20246b55408cb6a7dd6
Manan <manan.bharara@gmail.com>
2013-05-02 18:02:54 +0530

    |#684|+Balvinder| Adding ICT4H atom feed dependency. Modifying scripts to create atom feed schema.

---
+++ modules/requisition/src/main/java/org/openlmis/rnr/dto/RnrFeedDTO.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2013 VillageReach. All Rights Reserved. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ *
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.openlmis.rnr.dto;
+
+import org.openlmis.rnr.domain.Rnr;
+
+public class RnrFeedDTO {
+  public static RnrFeedDTO populate(Rnr rnr) {
+    return null;  //To change body of created methods use File | Settings | File Templates.
+  }
+}
