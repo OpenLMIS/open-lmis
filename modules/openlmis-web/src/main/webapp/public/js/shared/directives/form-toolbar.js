de265b1c8d6bc8e0941dab6173b66896632cacff
Jaideep Singh <jaideep@thoughtworks.com>
2013-05-07 16:23:17 +0530

    #275 | UI design for add reports screen

--- modules/openlmis-web/src/main/webapp/public/js/shared/directives/form-toolbar.js
+++ modules/openlmis-web/src/main/webapp/public/js/shared/directives/form-toolbar.js
@@ -12,6 +12,13 @@ app.directive('formToolbar',function () {
       function fixToolbarWidth() {
         var toolbarWidth = $(document).width() - 26;
         angular.element("#action_buttons").css("width", toolbarWidth + "px");
+
+        var headerHeight = $(".header").outerHeight(true);
+        var navHeight = $(".navigation").outerHeight(true);
+        var content = $(".content");
+        var contentGutterHeight = content.outerHeight(true) - content.height();
+
+        content.css('min-height', $(window).height() - (headerHeight + navHeight + contentGutterHeight));
       }
 
       fixToolbarWidth();
