59f8c5a37f8891fa775645311db8e9f9f48b8652
Rahul Joshi <rahulj@thoughtworks.com>
2013-05-09 14:40:41 +0530

    |#000|reformatting mapper queries

--- modules/core/src/main/java/org/openlmis/core/repository/mapper/ProgramMapper.java
+++ modules/core/src/main/java/org/openlmis/core/repository/mapper/ProgramMapper.java
@@ -15,31 +15,31 @@ import java.util.List;
 @Repository
 public interface ProgramMapper {
 
-  @Insert("INSERT INTO programs(code, name, description, active)" +
-    " VALUES (#{code}, #{name}, #{description}, #{active})")
+  @Insert({"INSERT INTO programs(code, name, description, active)",
+      "VALUES (#{code}, #{name}, #{description}, #{active})"})
   @Options(useGeneratedKeys = true)
   Integer insert(Program program);
 
-  @Select("SELECT P.* " +
-    "FROM programs P, programs_supported PS " +
-    "WHERE P.id = PS.programId AND " +
-    "PS.facilityId = #{facilityId} AND " +
-    "PS.active = true AND " +
-    "P.active = true")
+  @Select({"SELECT P.*",
+      "FROM programs P, programs_supported PS",
+      "WHERE P.id = PS.programId AND",
+      "PS.facilityId = #{facilityId} AND",
+      "PS.active = true AND",
+      "P.active = true"})
   List<Program> getActiveByFacility(Long facilityId);
 
   @Select("SELECT * FROM programs ORDER BY templateConfigured DESC, name")
   List<Program> getAll();
 
-  @Select("SELECT " +
-    "p.id AS id, " +
-    "p.code AS code, " +
-    "p.name AS name, " +
-    "p.description AS description, " +
-    "ps.active AS active " +
-    "FROM programs p, programs_supported ps WHERE " +
-    "p.id = ps.programId AND " +
-    "ps.facilityId = #{facilityId}")
+  @Select({"SELECT",
+      "p.id AS id,",
+      "p.code AS code,",
+      "p.name AS name,",
+      "p.description AS description,",
+      "ps.active AS active",
+      "FROM programs p, programs_supported ps WHERE",
+      "p.id = ps.programId AND",
+      "ps.facilityId = #{facilityId}"})
   List<Program> getByFacilityId(Long facilityId);
 
 
@@ -49,31 +49,39 @@ public interface ProgramMapper {
   @Select("SELECT * FROM programs WHERE id = #{id}")
   Program getById(Long id);
 
-  @Select("SELECT DISTINCT p.* " +
-    "FROM programs p " +
-    "INNER JOIN role_assignments ra ON p.id = ra.programId " +
-    "INNER JOIN role_rights rr ON ra.roleId = rr.roleId " +
-    "WHERE ra.userId = #{userId} " +
-    "AND rr.rightName = ANY (#{commaSeparatedRights}::VARCHAR[]) " +
-    "AND ra.supervisoryNodeId IS NOT NULL " +
-    "AND p.active = true")
-  List<Program> getUserSupervisedActivePrograms(@Param(value = "userId") Long userId, @Param(value = "commaSeparatedRights") String commaSeparatedRights);
+  @Select({"SELECT DISTINCT p.*",
+      "FROM programs p",
+      "INNER JOIN role_assignments ra ON p.id = ra.programId",
+      "INNER JOIN role_rights rr ON ra.roleId = rr.roleId",
+      "WHERE ra.userId = #{userId}",
+      "AND rr.rightName = ANY (#{commaSeparatedRights}::VARCHAR[])",
+      "AND ra.supervisoryNodeId IS NOT NULL",
+      "AND p.active = TRUE"})
+  List<Program> getUserSupervisedActivePrograms(@Param(value = "userId") Long userId,
+                                                @Param(value = "commaSeparatedRights") String commaSeparatedRights);
 
   @Select({"SELECT DISTINCT p.* FROM programs p INNER JOIN programs_supported ps ON p.id = ps.programId",
-    "INNER JOIN role_assignments ra ON ra.programId = p.id",
-    "INNER JOIN role_rights rr ON rr.roleId = ra.roleId",
-    "WHERE ra.supervisoryNodeId IS NULL and p.active = true and  ps.active=true and ra.userId = #{userId} and ps.facilityId = #{facilityId} and rr.rightName = ANY(#{commaSeparatedRights}::VARCHAR[])",
-    ""})
-  List<Program> getProgramsSupportedByFacilityForUserWithRights(@Param("facilityId") Long facilityId, @Param("userId") Long userId, @Param("commaSeparatedRights") String commaSeparatedRights);
+      "INNER JOIN role_assignments ra ON ra.programId = p.id",
+      "INNER JOIN role_rights rr ON rr.roleId = ra.roleId",
+      "WHERE ra.supervisoryNodeId IS NULL",
+      "AND p.active = TRUE",
+      "AND ps.active= TRUE",
+      "AND ra.userId = #{userId}",
+      "AND ps.facilityId = #{facilityId}",
+      "AND rr.rightName = ANY(#{commaSeparatedRights}::VARCHAR[])"})
+  List<Program> getProgramsSupportedByFacilityForUserWithRights(@Param("facilityId") Long facilityId,
+                                                                @Param("userId") Long userId,
+                                                                @Param("commaSeparatedRights") String commaSeparatedRights);
 
-  @Select("SELECT DISTINCT p.* " +
-    "FROM programs p " +
-    "INNER JOIN role_assignments ra ON p.id = ra.programId " +
-    "INNER JOIN role_rights rr ON ra.roleId = rr.roleId " +
-    "WHERE ra.userId = #{userId} " +
-    "AND rr.rightName = ANY (#{commaSeparatedRights}::VARCHAR[]) " +
-    "AND p.active = true")
-  List<Program> getActiveProgramsForUserWithRights(@Param(value = "userId") Long userId, @Param(value = "commaSeparatedRights") String commaSeparatedRights);
+  @Select({"SELECT DISTINCT p.*",
+      "FROM programs p",
+      "INNER JOIN role_assignments ra ON p.id = ra.programId",
+      "INNER JOIN role_rights rr ON ra.roleId = rr.roleId",
+      "WHERE ra.userId = #{userId}",
+      "AND rr.rightName = ANY (#{commaSeparatedRights}::VARCHAR[])",
+      "AND p.active = true"})
+  List<Program> getActiveProgramsForUserWithRights(@Param(value = "userId") Long userId,
+                                                   @Param(value = "commaSeparatedRights") String commaSeparatedRights);
 
 
   @Update("UPDATE programs SET templateConfigured = true WHERE id = #{id}")
