828f2bf3759893b6e1e80854089a95b1cebdac2b
Elias <eliasmu@gmail.com>
2013-05-20 10:58:04 +0300

    Add Query builder as well as report mapper for stocked out report

---
+++ modules/report/src/main/java/org/openlmis/report/builder/StockedOutReportQueryBuilder.java
@@ -0,0 +1,133 @@
+package org.openlmis.report.builder;
+
+import java.util.Map;
+
+import static org.apache.ibatis.jdbc.SqlBuilder.*;
+
+/* Date: 4/11/13
+* Time: 11:34 AM
+*/
+public class StockedOutReportQueryBuilder {
+
+   public static String getQuery(Map params){
+
+       String period           = ((String[])params.get("period"))[0];
+       String reportingGroup   = ((String[])params.get("rgroup"))[0] ;
+       String facilityType     = ((String[])params.get("ftype"))[0] ;
+       String program          = ((String[])params.get("program"))[0];
+       String schedule         = ((String[])params.get("schedule"))[0];
+       return getQueryString(params, program , period , reportingGroup, facilityType, schedule);
+
+   }
+
+    private static String getQueryString(Map params, String program , String period, String reportingGroup, String facilityType, String schedule) {
+        BEGIN();
+        SELECT_DISTINCT("facilities.code, facilities.name");
+        SELECT_DISTINCT("gz.name as location");
+        SELECT_DISTINCT("ft.name as facilityType");
+
+        FROM("facilities");
+        INNER_JOIN("requisition_group_members rgm on rgm.facilityid = facilities.id");
+        INNER_JOIN("geographic_zones gz on gz.id = facilities.geographiczoneid");
+        INNER_JOIN("facility_types ft on ft.id = facilities.typeid");
+        INNER_JOIN("programs_supported ps on ps.facilityid = facilities.id");
+        INNER_JOIN("requisition_group_program_schedules rgps on rgps.requisitiongroupid = rgm.requisitiongroupid and ps.programid = rgps.programid");
+        WHERE("facilities.id not in (select r.facilityid from requisitions r where r.periodid = cast (" + period + " as int4) and r.programid = cast(" + program + " as int4) )");
+        writePredicates(program, period, reportingGroup, facilityType, schedule);
+        ORDER_BY(QueryHelpers.getSortOrder(params, "name"));
+        // cache the string query for debugging purposes
+        String strQuery = SQL();
+        return strQuery;
+    }
+
+    private static void writePredicates(String program, String period, String reportingGroup, String facilityType, String schedule) {
+
+        if(reportingGroup != "" && !reportingGroup.endsWith( "undefined")){
+            WHERE("rgm.requisitiongroupid = cast (" + reportingGroup + " as int4)");
+        }
+
+        if(facilityType != "" && !facilityType.endsWith( "undefined")){
+            WHERE("facilities.typeid = cast(" + facilityType+ " as int4)");
+        }
+
+        if(program != "" && !program.endsWith("undefined")){
+           WHERE("ps.programid = cast(" + program+ " as int4)");
+        }
+
+        if(schedule != "" && !schedule.endsWith("undefined")){
+            WHERE("rgps.scheduleid = cast(" + schedule + " as int4)");
+        }
+    }
+
+
+
+
+   public static String getTotalFacilities(Map params){
+       String period           = ((String[])params.get("period"))[0];
+       String reportingGroup   = ((String[])params.get("rgroup"))[0];
+       String facilityType     = ((String[])params.get("ftype"))[0];
+       String program          = ((String[])params.get("program"))[0];
+       String schedule         = ((String[])params.get("schedule"))[0];
+       BEGIN();
+       SELECT("COUNT (*)");
+       FROM("facilities");
+       INNER_JOIN("programs_supported ps on ps.facilityid = facilities.id") ;
+       INNER_JOIN("requisition_group_members rgm on rgm.facilityid = facilities.id");
+       INNER_JOIN("requisition_group_program_schedules rgps on rgps.requisitiongroupid = rgm.requisitiongroupid and ps.programid = rgps.programid");
+       writePredicates(program, period, reportingGroup, facilityType, schedule);
+       return SQL();
+   }
+
+    public static String getStockedOutFacilitiesCount(Map params){
+        String period           = ((String[])params.get("period"))[0];
+        String reportingGroup   = ((String[])params.get("rgroup"))[0];
+        String facilityType     = ((String[])params.get("ftype"))[0];
+        String program          = ((String[])params.get("program"))[0];
+        String schedule         = ((String[])params.get("schedule"))[0];
+
+        BEGIN();
+        SELECT("COUNT (*)");
+        FROM("facilities");
+        INNER_JOIN("programs_supported ps on ps.facilityid = facilities.id") ;
+        INNER_JOIN("requisition_group_members rgm on rgm.facilityid = facilities.id");
+        INNER_JOIN("requisition_group_program_schedules rgps on rgps.requisitiongroupid = rgm.requisitiongroupid and ps.programid = rgps.programid");
+        WHERE("facilities.id not in (select r.facilityid from requisitions r where r.periodid = cast(" + period + " as int4) and r.programid = cast(" + program + " as int4) )");
+        writePredicates(program, period, reportingGroup, facilityType, schedule);
+        return SQL();
+    }
+
+
+
+   public static String getSummaryQuery(Map params){
+
+       String period           = ((String[])params.get("period"))[0];
+       String reportingGroup   = ((String[])params.get("rgroup"))[0];
+       String facilityType     = ((String[])params.get("ftype"))[0];
+       String program          = ((String[])params.get("program"))[0];
+       String schedule         = ((String[])params.get("schedule"))[0];
+
+       BEGIN();
+       SELECT("'Non Reporting Facilities' AS name");
+       SELECT("COUNT (*)");
+       FROM("facilities");
+       INNER_JOIN("programs_supported ps on ps.facilityid = facilities.id") ;
+       INNER_JOIN("requisition_group_members rgm on rgm.facilityid = facilities.id") ;
+       INNER_JOIN("requisition_group_program_schedules rgps on rgps.requisitiongroupid = rgm.requisitiongroupid and ps.programid = rgps.programid");
+       WHERE("facilities.id not in (select r.facilityid from requisitions r where r.periodid = cast(" + period + " as int4) and r.programid = cast(" + program + " as int4) )");
+       writePredicates(program, period, reportingGroup, facilityType,schedule);
+
+       String query = SQL();
+       RESET();
+       BEGIN();
+       SELECT("'Reporting for this Program' AS name");
+       SELECT("COUNT (*)");
+       FROM("facilities");
+       INNER_JOIN("programs_supported ps on ps.facilityid = facilities.id") ;
+       INNER_JOIN("requisition_group_members rgm on rgm.facilityid = facilities.id");
+       INNER_JOIN("requisition_group_program_schedules rgps on rgps.requisitiongroupid = rgm.requisitiongroupid and ps.programid = rgps.programid");
+       writePredicates(program, period, reportingGroup, facilityType, schedule);
+       query += " UNION " + SQL();
+       return query;
+
+   }
+}
---
+++ modules/report/src/main/java/org/openlmis/report/mapper/StockedOutReportMapper.java
@@ -0,0 +1,66 @@
+package org.openlmis.report.mapper;
+
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.mapping.ResultSetType;
+import org.apache.ibatis.session.RowBounds;
+import org.openlmis.report.builder.NonReportingFacilityQueryBuilder;
+import org.openlmis.report.builder.StockedOutReportQueryBuilder;
+import org.openlmis.report.model.dto.NameCount;
+import org.openlmis.report.model.dto.RequisitionGroup;
+import org.openlmis.report.model.report.NonReportingFacilityDetail;
+import org.openlmis.report.model.report.StockedOutFacility;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * User: Elias
+ * Date: 4/11/13
+ * Time: 11:25 AM
+ */
+
+@Repository
+public interface StockedOutReportMapper {
+
+
+    @SelectProvider(type=StockedOutReportQueryBuilder.class, method="getQuery")
+    @Options(resultSetType = ResultSetType.SCROLL_SENSITIVE, fetchSize=10,timeout=0,useCache=true,flushCache=true)
+    public List<StockedOutFacility> getReport(Map params, @Param("RowBounds") RowBounds rowBounds);
+
+    @SelectProvider(type=StockedOutReportQueryBuilder.class, method="getSummaryQuery")
+    @Options(resultSetType = ResultSetType.SCROLL_SENSITIVE, fetchSize=10,timeout=0,useCache=true,flushCache=true)
+    public List<NameCount> getReportSummary(Map params);
+
+    // Gets the count of the total facility count under the selection criteria
+    @SelectProvider(type=StockedOutReportQueryBuilder.class, method="getTotalFacilities")
+    @Options(resultSetType = ResultSetType.SCROLL_SENSITIVE, fetchSize=10,timeout=0,useCache=true,flushCache=true)
+    public List<Integer> getTotalFacilities(Map params);
+
+    // Gets the count of the total facility count that did not report under the selection criteria
+    @SelectProvider(type=StockedOutReportQueryBuilder.class, method="getStockedOutFacilitiesCount")
+    @Options(resultSetType = ResultSetType.SCROLL_SENSITIVE, fetchSize=10,timeout=0,useCache=true,flushCache=true)
+    public List<Integer> getStockedOutTotalFacilities(Map params);
+
+    //TODO: refactor this out to an appropriate class
+    @Select("SELECT id, name " +
+            "   FROM " +
+            "       facility_types where id = #{param1}")
+    List<RequisitionGroup> getFacilityType(int id);
+
+    //TODO: refactor this out to an appropriate class
+    @Select("SELECT id, name " +
+            "   FROM " +
+            "       processing_periods where id = #{param1}")
+    List<RequisitionGroup> getPeriodId(int id);
+
+    //TODO: refactor this out to an appropriate class
+    @Select("SELECT id, name " +
+            "   FROM " +
+            "       programs where id = #{param1}")
+    List<RequisitionGroup> getProgram(int id);
+
+}
