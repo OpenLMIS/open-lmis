044f6654e2674e750e7bf7dc27633d32f5868d6f
Elias <eliasmu@gmail.com>
2013-05-20 10:28:46 +0300

    Add the stocked out facility model

---
+++ modules/report/src/main/java/org/openlmis/report/model/report/StockedOutFacility.java
@@ -0,0 +1,18 @@
+package org.openlmis.report.model.report;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.openlmis.report.model.ReportData;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StockedOutFacility implements ReportData {
+
+    private String id;
+    private String code;
+    private String name;
+    private String facilityType;
+    private String location;
+}
