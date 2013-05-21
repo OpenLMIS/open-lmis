d0452993b9027e6e59fa869d608e5469930daef8
Elias <eliasmu@gmail.com>
2013-05-21 21:31:36 +0300

    Order requisition group lookup by name

--- modules/report/src/main/java/org/openlmis/report/mapper/RequisitionGroupReportMapper.java
+++ modules/report/src/main/java/org/openlmis/report/mapper/RequisitionGroupReportMapper.java
@@ -17,12 +17,12 @@ public interface RequisitionGroupReportMapper {
 
     @Select("SELECT id, name, code " +
             "   FROM " +
-            "       requisition_groups")
+            "       requisition_groups order by name")
     List<RequisitionGroup> getAll();
 
     @Select("SELECT id, name, code " +
             "   FROM " +
-            "       requisition_groups where id = #{param1}")
+            "       requisition_groups where id = #{param1} order by name")
     List<RequisitionGroup> getById(int id);
 
     @Select("SELECT g.id, g.name, g.code " +
@@ -31,7 +31,8 @@ public interface RequisitionGroupReportMapper {
             "       join requisition_group_program_schedules ps on ps.requisitiongroupid = g.id " +
             " where " +
             " ps.programid = cast( #{param1} as int4) and ps.scheduleid = cast( #{param2} as int4) " +
-            " and  g.id in (select rgm.requisitiongroupid from requisition_group_members rgm join programs_supported ps on rgm.facilityid = ps.facilityid where ps.programid = cast(#{param1} as int4) ) ")
+            " and  g.id in (select rgm.requisitiongroupid from requisition_group_members rgm join programs_supported ps on rgm.facilityid = ps.facilityid where ps.programid = cast(#{param1} as int4) ) " +
+            " order by g.name")
     List<RequisitionGroup> getByProgramAndSchedule(int program, int schedule);
 
 }
