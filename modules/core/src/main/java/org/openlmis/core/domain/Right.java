eb5b759a5acf8f47321f7d75b0e0daf58f97e0ef
Wolde Fikadu <memiwo@gmail.com>
2013-05-27 18:49:15 +0300

    ELMIS-150: District Consumption Jasper Report Initial impl

--- modules/core/src/main/java/org/openlmis/core/domain/Right.java
+++ modules/core/src/main/java/org/openlmis/core/domain/Right.java
@@ -39,8 +39,8 @@ public enum Right {
   VIEW_NON_REPORTING_FACILITIES("Report - Non Reporting Facility Report", TRUE, "Permission to view Non reporting facilities report"),
   VIEW_ADJUSTMENT_SUMMARY_REPORT("Report - Adjustment Summary Report", TRUE, "Permission to view adjustment summary Report"),
   VIEW_SUPPLY_STATUS_REPORT("Report - Supply Status by Facility",TRUE, "Permission to view Supply Status by Facility Report"),
-  VIEW_STOCKED_OUT_REPORT("Report - Stocked Out Report", TRUE, "Permission to view stocked out product report");
-
+  VIEW_STOCKED_OUT_REPORT("Report - Stocked Out Report", TRUE, "Permission to view stocked out product report"),
+  VIEW_DISTRICT_CONSUMPTION_REPORT("Report - District Consumption Comparison", TRUE, "Permission to view district consumption comparison report");
     @Getter
   private final String rightName;
 
