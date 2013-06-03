c3e07830ae2a806f168ad7893af37156fcf44ab4
Sriram Viswanathan <sriramv@thoughtworks.com>
2013-06-03 18:01:55 +0530

    |#753|+Manan| Changing minify task.

--- modules/openlmis-web/src/main/webapp/public/js/facility/module/facility-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/facility/module/facility-module.js
@@ -4,10 +4,10 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+'use strict';
 require(['../../shared/app', '../controller/facility-controller', '../controller/facility-search-controller'],
   function (app) {
     app.loadApp();
-    'use strict';
     angular.module('facility', ['openlmis', 'ui.bootstrap.modal', 'ui.bootstrap.dialog']).
       config(['$routeProvider', function ($routeProvider) {
         $routeProvider.
