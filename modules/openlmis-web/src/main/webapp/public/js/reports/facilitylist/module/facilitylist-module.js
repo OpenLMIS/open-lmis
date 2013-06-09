45db020d87cf2ebf0d3e57a40ec2384a63da1f1e
Elias <eliasmu@gmail.com>
2013-06-09 17:56:56 +0200

    Use require js for facility list report

--- modules/openlmis-web/src/main/webapp/public/js/reports/facilitylist/module/facilitylist-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/facilitylist/module/facilitylist-module.js
@@ -1,9 +1,15 @@
 'use strict';
-angular.module('facilitylist', ['openlmis', 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstrap.dropdownToggle'])
-.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.
-    when('/list', {controller:ListFacilitiesController, templateUrl:'partials/list.html',reloadOnSearch:false}).
-    otherwise({redirectTo:'/list'});
-}]).run(function($rootScope) {
-   // $rootScope.Selected = "selected";
-  });
+require(['../../../shared/app' , '../controller/list-facilitylist-controller'], function (app) {
+
+    app.loadApp();
+    angular.module('facilitylist', ['openlmis' , 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstrap.dropdownToggle'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+        when('/list', {controller:ListFacilitiesController, templateUrl:'partials/list.html',reloadOnSearch:false}).
+        otherwise({redirectTo:'/list'});
+    }]).run(function ($rootScope, AuthorizationService) {
+            AuthorizationService.preAuthorize('VIEW_REPORTS');
+        });
+
+    angular.bootstrap(document, ['facilitylist']);
+});
