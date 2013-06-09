19011b1f9c6f7b9575f0e1f89db91e9aad647dee
Elias <eliasmu@gmail.com>
2013-06-09 22:40:15 +0200

    Implement require.js on stocked_out report

--- modules/openlmis-web/src/main/webapp/public/js/reports/stocked_out/module/stocked_out-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/stocked_out/module/stocked_out-module.js
@@ -1,9 +1,17 @@
 'use strict';
-angular.module('stocked_out', ['openlmis', 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstrap.dropdownToggle'])
-.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.
-    when('/list', {controller:StockedOutController, templateUrl:'partials/list.html',reloadOnSearch:false}).
-    otherwise({redirectTo:'/list'});
-}]).run(function($rootScope) {
-   // $rootScope.Selected = "selected";
-  });
+require(['../../../shared/app' , '../controller/stocked_out-controller'], function (app) {
+    app.loadApp();
+
+    angular.module('stocked_out', ['openlmis', 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstrap.dropdownToggle'])
+    .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.
+        when('/list', {controller:StockedOutController, templateUrl:'partials/list.html',reloadOnSearch:false}).
+        otherwise({redirectTo:'/list'});
+    }]).run(
+    function ($rootScope, AuthorizationService) {
+        AuthorizationService.preAuthorize('VIEW_REPORTS');
+    }
+);
+
+    angular.bootstrap(document, ['stocked_out']);
+});
