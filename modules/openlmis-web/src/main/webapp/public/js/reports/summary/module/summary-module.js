a1c54244a421c4f707c4c72e61f774bc1bef454f
Elias <eliasmu@gmail.com>
2013-06-09 22:18:07 +0200

    Implement require js on summary report controller.

--- modules/openlmis-web/src/main/webapp/public/js/reports/summary/module/summary-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/summary/module/summary-module.js
@@ -1,9 +1,16 @@
 'use strict';
-angular.module('summary', ['openlmis', 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstrap.dropdownToggle'])
-.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.
-    when('/list', {controller:SummaryReportController, templateUrl:'partials/list.html',reloadOnSearch:false}).
-    otherwise({redirectTo:'/list'});
-}]).run(function($rootScope) {
-   // $rootScope.Selected = "selected";
-  });
+require(['../../../shared/app' , '../controller/summary-controller'], function (app) {
+    app.loadApp();
+    angular.module('summary', ['openlmis', 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstrap.dropdownToggle'])
+        .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.
+            when('/list', {controller:SummaryReportController, templateUrl:'partials/list.html',reloadOnSearch:false}).
+            otherwise({redirectTo:'/list'});
+        }]).run(
+            function ($rootScope, AuthorizationService) {
+                AuthorizationService.preAuthorize('VIEW_REPORTS');
+            }
+    );
+
+    angular.bootstrap(document, ['summary']);
+});
