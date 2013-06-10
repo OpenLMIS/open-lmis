0a12216c0c7769434bd2ce8a1083e90cd4e4d2ae
Elias <eliasmu@gmail.com>
2013-06-10 07:11:19 +0200

    Implement require.js on the consumption report

--- modules/openlmis-web/src/main/webapp/public/js/reports/consumption/module/consumption-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/consumption/module/consumption-module.js
@@ -1,9 +1,16 @@
 'use strict';
-angular.module('consumption', ['openlmis', 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstrap.dropdownToggle'])
-.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.
-    when('/list', {controller:ConsumptionReportController, templateUrl:'partials/list.html',reloadOnSearch:false}).
-    otherwise({redirectTo:'/list'});
-}]).run(function($rootScope) {
-   // $rootScope.Selected = "selected";
-  });
+require(['../../../shared/app' , '../controller/consumption-controller'], function (app) {
+    app.loadApp();
+    angular.module('consumption', ['openlmis', 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstrap.dropdownToggle'])
+    .config(['$routeProvider', function ($routeProvider) {
+      $routeProvider.
+        when('/list', {controller:ConsumptionReportController, templateUrl:'partials/list.html',reloadOnSearch:false}).
+        otherwise({redirectTo:'/list'});
+    }]).run(
+        function ($rootScope, AuthorizationService) {
+            AuthorizationService.preAuthorize('VIEW_REPORTS');
+        }
+    );
+
+angular.bootstrap(document, ['consumption']);
+});
