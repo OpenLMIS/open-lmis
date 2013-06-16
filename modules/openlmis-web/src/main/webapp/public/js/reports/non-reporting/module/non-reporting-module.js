fdaa12d604df54e1fac1780f4a26db20a374b245
Elias <eliasmu@gmail.com>
2013-06-16 17:03:36 +0300

    Refactor non reporting facility report for a consistent path with "-" separator

--- modules/openlmis-web/src/main/webapp/public/js/reports/non-reporting/module/non-reporting-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/non-reporting/module/non-reporting-module.js
@@ -1,5 +1,5 @@
 'use strict';
-require(['../../../shared/app' , '../controller/non_reporting-controller'], function (app) {
+require(['../../../shared/app' , '../controller/non-reporting-controller'], function (app) {
     app.loadApp();
     angular.module('non_reporting', ['openlmis', 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstrap.dropdownToggle'])
         .config(['$routeProvider', function ($routeProvider) {
