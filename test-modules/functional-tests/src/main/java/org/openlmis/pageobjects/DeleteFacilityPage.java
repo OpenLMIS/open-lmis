2e35d4c8b6bddb8d2cabb01d581e8adaa55aa60a
Anshul Rastogi <anshulr@thoughtworks.com>
2013-06-18 21:46:44 +0530

    |#743| fixing functional tests

--- test-modules/functional-tests/src/main/java/org/openlmis/pageobjects/DeleteFacilityPage.java
+++ test-modules/functional-tests/src/main/java/org/openlmis/pageobjects/DeleteFacilityPage.java
@@ -30,7 +30,7 @@ public class DeleteFacilityPage extends Page {
   @FindBy(how = How.LINK_TEXT, using = "Delete")
   private static WebElement deleteButton;
 
-  @FindBy(how = How.LINK_TEXT, using = "OK")
+  @FindBy(how = How.LINK_TEXT, using = "Ok")
   private static WebElement okButton;
 
   @FindBy(how = How.XPATH, using = "//div[@id='deleteFacilityDialog']/div[@class='modal-body']/p")
@@ -64,7 +64,7 @@ public class DeleteFacilityPage extends Page {
   @FindBy(how = How.XPATH, using = "//div[@id='restoreConfirmModal']/div[@class='modal-body']/p")
   private static WebElement restoreMessageOnAlert;
 
-  @FindBy(how = How.LINK_TEXT, using = "OK")
+  @FindBy(how = How.LINK_TEXT, using = "Ok")
   private static WebElement okLink;
 
   @FindBy(how = How.XPATH, using = " //div[@id='activeConfirmModel']/div[@class='modal-body']/p")
