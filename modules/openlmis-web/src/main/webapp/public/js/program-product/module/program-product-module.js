26b414e8b89a4416f42ee1371f08e9e9024f62c5
Manan <manan.bharara@gmail.com>
2013-06-21 15:32:59 +0530

    |#819|Removing require js from program product isa

--- modules/openlmis-web/src/main/webapp/public/js/program-product/module/program-product-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/program-product/module/program-product-module.js
@@ -5,74 +5,70 @@
  */
 
 'use strict';
-require(['../../shared/app', '../controller/program-product-controller'], function (app) {
-  app.loadApp();
-  var programProductModule = angular.module('programProductModule', ['openlmis', 'ui.bootstrap.modal']).config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.
-            when('/select-push-program', {
-              controller: ProgramProductController,
-              templateUrl: 'partials/list.html',
-              resolve: ProgramProductController.resolve }).
+var programProductModule = angular.module('programProductModule', ['openlmis', 'ui.bootstrap.modal']).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+      when('/select-push-program', {
+        controller: ProgramProductController,
+        templateUrl: 'partials/list.html',
+        resolve: ProgramProductController.resolve }).
 
-            otherwise({redirectTo: '/select-push-program'});
-      }]).directive('numericValidator', function () {
-        return {
-          require: '?ngModel',
-          link: function (scope, element, attrs, ctrl) {
-            var validationFunction = programProductModule[attrs.numericValidator];
+      otherwise({redirectTo: '/select-push-program'});
+  }]).directive('numericValidator', function () {
+    return {
+      require: '?ngModel',
+      link: function (scope, element, attrs, ctrl) {
+        var validationFunction = programProductModule[attrs.numericValidator];
 
-            element.bind('blur', function () {
-              validationFunction(ctrl.$modelValue, element.attr('name'));
-            });
-            ctrl.$parsers.unshift(function (viewValue) {
-              if (validationFunction(viewValue, element.attr('name'))) {
-                if (viewValue == "")  viewValue = undefined;
-                return viewValue;
-              } else {
-                ctrl.$viewValue = ctrl.$modelValue;
-                ctrl.$render();
-                return ctrl.$modelValue;
-              }
-            });
+        element.bind('blur', function () {
+          validationFunction(ctrl.$modelValue, element.attr('name'));
+        });
+        ctrl.$parsers.unshift(function (viewValue) {
+          if (validationFunction(viewValue, element.attr('name'))) {
+            if (viewValue == "")  viewValue = undefined;
+            return viewValue;
+          } else {
+            ctrl.$viewValue = ctrl.$modelValue;
+            ctrl.$render();
+            return ctrl.$modelValue;
           }
-        };
-      });
+        });
+      }
+    };
+  });
 
-  angular.bootstrap(document, ['programProductModule']);
+angular.bootstrap(document, ['programProductModule']);
 
-  programProductModule.numericValue = function (value, errorHolder) {
-    var NUMBERIC_REGEXP_FIXED_PRECISION = /^(\d{0,3}\.\d{0,3}|\d{0,3})$/;
-    var NUMBER_REGEXP = /^\d*\.?\d*$/;
-    var valid = (value == undefined) ? true : NUMBERIC_REGEXP_FIXED_PRECISION.test(value);
+programProductModule.numericValue = function (value, errorHolder) {
+  var NUMBERIC_REGEXP_FIXED_PRECISION = /^(\d{0,3}\.\d{0,3}|\d{0,3})$/;
+  var NUMBER_REGEXP = /^\d*\.?\d*$/;
+  var valid = (value == undefined) ? true : NUMBERIC_REGEXP_FIXED_PRECISION.test(value);
 
-    if (errorHolder != undefined) {
-      document.getElementById(errorHolder).style.display = (NUMBER_REGEXP.test(value)) ? 'none' : 'block';
-    }
+  if (errorHolder != undefined) {
+    document.getElementById(errorHolder).style.display = (NUMBER_REGEXP.test(value)) ? 'none' : 'block';
+  }
 
-    return valid;
-  };
+  return valid;
+};
 
-  programProductModule.positiveInteger = function (value, errorHolder) {
-    var INTEGER_REGEXP = /^\d*$/;
-    var valid = (value == undefined) ? true : INTEGER_REGEXP.test(value);
+programProductModule.positiveInteger = function (value, errorHolder) {
+  var INTEGER_REGEXP = /^\d*$/;
+  var valid = (value == undefined) ? true : INTEGER_REGEXP.test(value);
 
-    if (errorHolder != undefined) {
-      document.getElementById(errorHolder).style.display = (valid) ? 'none' : 'block';
-    }
+  if (errorHolder != undefined) {
+    document.getElementById(errorHolder).style.display = (valid) ? 'none' : 'block';
+  }
 
-    return valid;
-  };
+  return valid;
+};
 
-  programProductModule.integer = function (value, errorHolder) {
-    var INTEGER_REGEXP_FIXED_LENGTH = /^[-]?\d{0,6}$/;
-    var INTEGER_REGEXP = /^[-]?\d*$/;
-    var valid = (value == undefined) ? true : INTEGER_REGEXP_FIXED_LENGTH.test(value);
+programProductModule.integer = function (value, errorHolder) {
+  var INTEGER_REGEXP_FIXED_LENGTH = /^[-]?\d{0,6}$/;
+  var INTEGER_REGEXP = /^[-]?\d*$/;
+  var valid = (value == undefined) ? true : INTEGER_REGEXP_FIXED_LENGTH.test(value);
 
-    if (errorHolder != undefined) {
-      document.getElementById(errorHolder).style.display = (INTEGER_REGEXP.test(value)) ? 'none' : 'block';
-    }
+  if (errorHolder != undefined) {
+    document.getElementById(errorHolder).style.display = (INTEGER_REGEXP.test(value)) ? 'none' : 'block';
+  }
 
-    return valid;
-  };
-
-});
+  return valid;
+};
