fad49c8f70a440eacd6c4d2d46a1d11dae75da20
Manjyot <manjyots@thoughtworks.com>
2013-06-21 16:33:37 +0530

    +Bijesh|Created test seed data for VRMIS

--- modules/db/src/main/resources/seed/geographic_zones.sql
+++ modules/db/src/main/resources/seed/geographic_zones.sql
@@ -4,7 +4,31 @@
 DELETE FROM geographic_zones;
 INSERT INTO geographic_zones
 (code, name, levelId, parent) values
-('Root', 'Root', (SELECT id FROM geographic_levels WHERE code = 'country'), (SELECT id FROM geographic_levels WHERE code = 'country')),
-('Arusha', 'Arusha',(SELECT id FROM geographic_levels WHERE code = 'state'), (SELECT id FROM geographic_levels WHERE code = 'country')),
-('Dodoma', 'Dodoma', (SELECT id FROM geographic_levels WHERE code = 'district'), (SELECT id FROM geographic_levels WHERE code = 'state')),
-('Ngorongoro', 'Ngorongoro', (SELECT id FROM geographic_levels WHERE code = 'city'), (SELECT id FROM geographic_levels WHERE code = 'district'));
+('Root', 'Root', (SELECT id FROM geographic_levels WHERE code = 'country'), (SELECT id FROM geographic_zones WHERE code = 'Root')),
+('Arusha', 'Arusha',(SELECT id FROM geographic_levels WHERE code = 'state'), (SELECT id FROM geographic_zones WHERE code = 'Root')),
+('Dodoma', 'Dodoma', (SELECT id FROM geographic_levels WHERE code = 'district'), (SELECT id FROM geographic_zones WHERE code = 'Arusha')),
+('Ngorongoro', 'Ngorongoro', (SELECT id FROM geographic_levels WHERE code = 'city'), (SELECT id FROM geographic_zones WHERE code = 'Dodoma')),
+
+('Mozambique', 'Mozambique', (SELECT id FROM geographic_levels WHERE code = 'country'), (SELECT id FROM geographic_zones WHERE code = 'Mozambique')),
+('Cabo Delgado Province', 'Cabo Delgado Province', (SELECT id FROM geographic_levels WHERE code = 'state'), (SELECT id FROM geographic_zones WHERE code = 'Mozambique')),
+('Gaza Province', 'Gaza Province', (SELECT id FROM geographic_levels WHERE code = 'state'), (SELECT id FROM geographic_zones WHERE code = 'Mozambique')),
+('Inhambane Province', 'Inhambane Province', (SELECT id FROM geographic_levels WHERE code = 'state'), (SELECT id FROM geographic_zones WHERE code = 'Mozambique')),
+
+('Norte', 'Norte', (SELECT id FROM geographic_levels WHERE code = 'district'), (SELECT id FROM geographic_zones WHERE code = 'Cabo Delgado Province')),
+('Centro', 'Centro', (SELECT id FROM geographic_levels WHERE code = 'district'), (SELECT id FROM geographic_zones WHERE code = 'Gaza Province')),
+('Sul', 'Sul', (SELECT id FROM geographic_levels WHERE code = 'district'), (SELECT id FROM geographic_zones WHERE code = 'Inhambane Province')),
+
+('District1', 'District1', (SELECT id FROM geographic_levels WHERE code = 'city'), (SELECT id FROM geographic_zones WHERE code = 'Norte')),
+('District2', 'District2', (SELECT id FROM geographic_levels WHERE code = 'city'), (SELECT id FROM geographic_zones WHERE code = 'Norte')),
+('District3', 'District3', (SELECT id FROM geographic_levels WHERE code = 'city'), (SELECT id FROM geographic_zones WHERE code = 'Norte')),
+
+('District4', 'District4', (SELECT id FROM geographic_levels WHERE code = 'city'), (SELECT id FROM geographic_zones WHERE code = 'Centro')),
+('District5', 'District5', (SELECT id FROM geographic_levels WHERE code = 'city'), (SELECT id FROM geographic_zones WHERE code = 'Centro')),
+('District6', 'District6', (SELECT id FROM geographic_levels WHERE code = 'city'), (SELECT id FROM geographic_zones WHERE code = 'Centro')),
+
+('District7', 'District7', (SELECT id FROM geographic_levels WHERE code = 'city'), (SELECT id FROM geographic_zones WHERE code = 'Sul')),
+('District8', 'District8', (SELECT id FROM geographic_levels WHERE code = 'city'), (SELECT id FROM geographic_zones WHERE code = 'Sul')),
+('District9', 'District9', (SELECT id FROM geographic_levels WHERE code = 'city'), (SELECT id FROM geographic_zones WHERE code = 'Sul'));
+
+
+
