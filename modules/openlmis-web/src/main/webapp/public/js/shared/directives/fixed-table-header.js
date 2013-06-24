c90a408896f8a930b8f469f674a065c58b89904c
Vishal Sharma <vishalsh@thoughtworks.com>
2013-06-24 11:44:27 +0530

    |#000| Added header-fixed directive in index.html

--- modules/openlmis-web/src/main/webapp/public/js/shared/directives/fixed-table-header.js
+++ modules/openlmis-web/src/main/webapp/public/js/shared/directives/fixed-table-header.js
@@ -4,7 +4,7 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-directives.directive('fixedTableHeader', function() {
+app.directive('fixedTableHeader', function() {
   return {
     restrict: 'EA',
     link: function (scope, element, attr, ctrl) {
