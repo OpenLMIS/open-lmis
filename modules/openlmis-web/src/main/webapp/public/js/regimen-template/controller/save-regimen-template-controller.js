4189b080ac95abe5525d23cf6dabe5cd7289ae94
Sriram Viswanathan <sriramv@thoughtworks.com>
2013-06-26 10:44:29 +0530

    |+760| Removing showForm flag.

--- modules/openlmis-web/src/main/webapp/public/js/regimen-template/controller/save-regimen-template-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/regimen-template/controller/save-regimen-template-controller.js
@@ -12,7 +12,6 @@ function SaveRegimenTemplateController($scope, program, regimens, regimenCategor
   $scope.selectProgramUrl = "/public/pages/admin/regimen-template/index.html#/select-program";
   $scope.regimensByCategory = {};
   $scope.$parent.message = "";
-  $scope.showForm = false;
   function addRegimenByCategory(regimen) {
     var regimenCategoryId = regimen.category.id;
     var regimenList = $scope.regimensByCategory[regimenCategoryId];
@@ -37,7 +36,6 @@ function SaveRegimenTemplateController($scope, program, regimens, regimenCategor
       $scope.inputClass = true;
       $scope.newRegimenError = messageService.get('label.missing.values');
     } else {
-      $scope.showForm = true;
       $scope.newRegimen.programId = $scope.program.id;
       $scope.newRegimen.displayOrder = 1;
       $scope.newRegimen.disable = true;
