f7e0eb6e3faf584d1bdc3cf8fb5f05b065251840
Jaideep Singh <jaideep@thoughtworks.com>
2013-06-28 12:31:31 +0530

    Jaideep | Adding description for directives' behavior

--- modules/openlmis-web/src/main/webapp/public/js/shared/directives/openlmis-message.js
+++ modules/openlmis-web/src/main/webapp/public/js/shared/directives/openlmis-message.js
@@ -4,6 +4,9 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+//  Description:
+//  Including the text from the messages service
+
 app.directive('openlmisMessage', function (messageService) {
   return {
     restrict: 'A',
--- modules/openlmis-web/src/main/webapp/public/js/shared/directives/placeholder.js
+++ modules/openlmis-web/src/main/webapp/public/js/shared/directives/placeholder.js
@@ -4,6 +4,9 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
+//  Description:
+//  Emulating the placeholder attribute for older browsers
+
 app.directive('placeholder',function () {
   return {
     restrict:'A',
