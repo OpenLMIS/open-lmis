95817fbcfdf58bc4690c0051a7c8168bab87d648
Elias <eliasmu@gmail.com>
2013-06-30 18:58:16 +0300

    Created Rest-API for list of products

---
+++ modules/rest-api/src/main/java/org/openlmis/restapi/controller/LookupController.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2013 VillageReach. All Rights Reserved. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ *
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.openlmis.restapi.controller;
+
+import lombok.NoArgsConstructor;
+import org.openlmis.report.service.ReportLookupService;
+import org.openlmis.restapi.response.RestResponse;
+import org.openlmis.restapi.service.RestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.security.Principal;
+
+@Controller
+@NoArgsConstructor
+public class LookupController {
+
+  public static final String ACCEPT_JSON = "Accept=application/json";
+  public static final String UNEXPECTED_EXCEPTION = "unexpected.exception";
+  public static final String FORBIDDEN_EXCEPTION = "forbidden.exception";
+
+
+  @Autowired
+  private RestService restService;
+
+  @Autowired
+  private ReportLookupService lookupService;
+
+  @RequestMapping(value = "/rest-api/lookup/products", method = RequestMethod.GET, headers = ACCEPT_JSON)
+  public ResponseEntity getProducts( Principal principal) {
+    return RestResponse.response("products", lookupService.getAllProducts());
+  }
+
+}
