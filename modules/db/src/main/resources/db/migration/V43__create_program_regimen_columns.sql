816d7570d67f48bb9e9545a385da4a5466c85752
Sriram Viswanathan <sriramv@thoughtworks.com>
2013-07-01 14:07:19 +0530

    |#840| +Vishal| Fix tab focus on regimen screen.  Limit max length of regimen column label and name to 100 characters (on UI and db side).

--- modules/db/src/main/resources/db/migration/V43__create_program_regimen_columns.sql
+++ modules/db/src/main/resources/db/migration/V43__create_program_regimen_columns.sql
@@ -5,8 +5,8 @@ Drop TABLE IF EXISTS program_regimen_columns;
 CREATE TABLE program_regimen_columns (
     id SERIAL PRIMARY KEY,
     programId INTEGER NOT NULL REFERENCES programs(id),
-    name varchar(200) NOT NULL,
-    label varchar(200) NOT NULL,
+    name varchar(100) NOT NULL,
+    label varchar(100) NOT NULL,
     visible boolean NOT NULL,
     dataType varchar(50) NOT NULL,
     createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
