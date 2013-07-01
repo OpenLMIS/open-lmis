d994ad53b666ca60eaf674c8df864c0459617ea8
Anshul Rastogi <anshulr@thoughtworks.com>
2013-07-01 12:38:36 +0530

    |#851| setting test data according to new role and rights type

--- test-data/sql/allocation_users.sql
+++ test-data/sql/allocation_users.sql
@@ -2,8 +2,8 @@
 -- If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 INSERT INTO roles
- (name, adminRole) VALUES
- ('FieldCoordinator', 'f');
+ (name, type) VALUES
+ ('FieldCoordinator', 'ALLOCATION');
 
  INSERT INTO role_rights
  (roleId, rightName) VALUES
