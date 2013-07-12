65fc523ae7304970cba6fde81120b57074b70a4d
Wolde Fikadu <memiwo@gmail.com>
2013-07-12 07:44:17 +0300

    ELMIS-249 : write report query for MSL / MSD Requisition / Issue Voucher for Medical Supplies

---
+++ modules/report/src/main/java/org/openlmis/report/model/report/OrderSummaryReport.java
@@ -0,0 +1,20 @@
+package org.openlmis.report.model.report;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.openlmis.report.model.ReportData;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderSummaryReport implements ReportData {
+
+    private String productCode;
+    private String description;
+    private Integer unitSize;
+    private Integer unitQuantity;
+    private Integer packQuantity;
+    private String descrepancy;
+
+}
---
+++ modules/report/src/main/java/org/openlmis/report/service/OrderSummaryReportDataProvider.java
@@ -0,0 +1,54 @@
+package org.openlmis.report.service;
+
+import lombok.NoArgsConstructor;
+import org.apache.ibatis.session.RowBounds;
+import org.openlmis.report.mapper.OrderSummaryReportMapper;
+import org.openlmis.report.model.ReportData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * User: Wolde
+ * Date: 6/02/13
+ * Time: 2:37 PM
+ */
+@Component
+@NoArgsConstructor
+public class OrderSummaryReportDataProvider extends ReportDataProvider {
+
+
+    private OrderSummaryReportMapper reportMapper;
+
+
+    @Autowired
+    public OrderSummaryReportDataProvider(OrderSummaryReportMapper mapper) {
+        this.reportMapper = mapper;
+    }
+
+    @Override
+    protected List<? extends ReportData> getBeanCollectionReportData(Map<String, String[]> filterCriteria) {
+
+        return getReportDataByFilterCriteriaAndPagingAndSorting(filterCriteria,null,RowBounds.NO_ROW_OFFSET, RowBounds.NO_ROW_LIMIT);
+       // return reportMapper.getReportData(filterCriteria);
+    }
+
+    @Override
+    protected List<? extends ReportData> getResultSetReportData(Map<String, String[]> filterCriteria) {
+        return getReportDataByFilterCriteriaAndPagingAndSorting(filterCriteria,null,RowBounds.NO_ROW_OFFSET, RowBounds.NO_ROW_LIMIT);
+    }
+
+    @Override
+    public List<? extends ReportData> getReportDataByFilterCriteriaAndPagingAndSorting(Map<String, String[]> filterCriteria, Map<String, String[]> SortCriteria, int page, int pageSize) {
+        RowBounds rowBounds = new RowBounds((page-1)*pageSize,pageSize);
+        return reportMapper.getFilteredSortedPagedOrderSummaryReport(filterCriteria, rowBounds);
+    }
+
+    @Override
+    public int getReportDataCountByFilterCriteria(Map<String, String[]> filterCriteria) {
+        return reportMapper.SelectFilteredSummaryReportCount(filterCriteria);
+    }
+
+}
