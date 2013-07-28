b1417042707a141176c2b9f7460f27ca5a2fa7f3
Wolde Fikadu <memiwo@gmail.com>
2013-07-28 22:42:37 +0300

    ELMIS-310 : stock imbalance report sql query update

--- modules/report/src/main/java/org/openlmis/report/builder/StockImbalanceQueryBuilder.java
+++ modules/report/src/main/java/org/openlmis/report/builder/StockImbalanceQueryBuilder.java
@@ -10,35 +10,40 @@ import java.util.Map;
 public class StockImbalanceQueryBuilder {
     public static String getQuery(Map params){
 
-        String query = "select MAX(facilities.name) facility,MAX(facility_types.name) facilityType,li.productcode code,li.productcategory as category, li.product,SUM(li.beginningBalance) openingBalance, SUM(li.quantityreceived) receipts, SUM(li.quantitydispensed) issues, SUM(li.totallossesandadjustments) adjustments,  \n" +
-                "    (((SUM(li.beginningBalance) + SUM(li.quantityreceived)) -  SUM(li.quantitydispensed)) + SUM(li.totallossesandadjustments)) closingBalance," +
-                " round(cast((((SUM(li.beginningBalance) + SUM(li.quantityreceived)) -  SUM(li.quantitydispensed)) + SUM(li.totallossesandadjustments))/SUM(li.amc)::float as numeric),1) monthsOfStock, \n" +
-                " SUM(li.amc) averageMonthlyConsumption       \n" +
-                "    ,(SUM(li.amc) * SUM(facility_approved_products.maxmonthsofstock)) maximumStock,\n" +
-                "  case when(SUM(li.amc) * SUM(facility_approved_products.maxmonthsofstock)) - (((SUM(li.beginningBalance) + SUM(li.quantityreceived)) -  SUM(li.quantitydispensed)) + SUM(li.totallossesandadjustments)) > 0 then \n" +
-                "   (SUM(li.amc) * SUM(facility_approved_products.maxmonthsofstock)) - (((SUM(li.beginningBalance) + SUM(li.quantityreceived)) -  SUM(li.quantitydispensed)) + SUM(li.totallossesandadjustments)) ELSE 0 end  reorderAmount       \n" +
-                "   ,MAX(fs.facility_name) supplyingFacility,round(cast(MAX(li.maxmonthsofstock) as numeric),1) MaxMOS, round(cast(MAX(li.maxmonthsofstock) as numeric),1) minMOS  \n" +
-                "    from facilities        \n" +
-                "    inner join facility_types ON facilities.typeid = facility_types.id       \n" +
-                "    inner join geographic_zones on geographic_zones.id = facilities.geographiczoneid       \n" +
-                "    inner join requisitions r ON  r.facilityid = facilities.id         \n" +
-                "    inner join requisition_line_items li ON li.rnrid = r.id         \n" +
-                "    inner join products ON products.code  ::text =   li.productcode  ::text       \n" +
-                "    inner join program_products ON program_products.productid = products.id \n" +
-                "    inner join facility_approved_products ON facility_approved_products.facilitytypeid = facility_types.id AND facility_approved_products.programproductid = program_products.id \n" +
-                "    inner join programs ON  program_products.programid = programs.id   AND  programs.id = r.programid       \n" +
-                "    inner join programs_supported ON  programs.id = programs_supported.programid   AND   facilities.id = programs_supported.facilityid         \n" +
-                "    inner join requisition_group_members ON facilities.id = requisition_group_members.facilityid         \n" +
-                "    inner join requisition_groups ON requisition_groups.id = requisition_group_members.requisitiongroupid         \n" +
-                "    inner join requisition_group_program_schedules ON requisition_group_program_schedules.programid = programs.id   AND requisition_group_program_schedules.requisitiongroupid = requisition_groups.id         \n" +
-                "    inner join processing_schedules ON processing_schedules.id = requisition_group_program_schedules.programid         \n" +
-                "    inner join processing_periods ON processing_periods.scheduleid = processing_schedules.id  \n" +
-                "    left outer join vw_program_facility_supplier fs ON fs.supervisory_node_id = requisition_groups.supervisorynodeid AND fs.program_id = programs.id \n" +
+        String query = "SELECT distinct\n" +
+                " facilities.name AS Facility,\n" +
+                "facilities.name AS SupplyingFacility,\n" +
+                " requisition_line_items.product AS Product,\n" +
+                " requisition_line_items.stockinhand physicalCount,\n" +
+                " requisition_line_items.amc,\n" +
+                " CASE WHEN coalesce(requisition_line_items.AMC,0) = 0 THEN 0 ELSE round(requisition_line_items.stockinhand/requisition_line_items.AMC,1) END AS months,\n" +
+                " coalesce(CASE WHEN (requisition_line_items.stockinhand - coalesce(amc,0) * fn_get_max_mos (programs.id, facilities.id, products.code)) < 0 THEN 0 ELSE (requisition_line_items.stockinhand - coalesce(amc,0) * fn_get_max_mos (programs.id, facilities.id, products.code)) END ,0) AS orderQuantity,\n" +
+                " CASE WHEN requisition_line_items.stockinhand = 0 THEN 'Stockedout' ELSE\n" +
+                "  CASE WHEN requisition_line_items.stockinhand > 0 AND requisition_line_items.stockinhand  < coalesce(amc,0) * fn_get_max_mos (programs.id, facilities.id, products.code) THEN 'Understocked' ELSE\n" +
+                "\t\tCASE WHEN requisition_line_items.stockinhand  > coalesce(amc,0) * fn_get_max_mos (programs.id, facilities.id, products.code) THEN 'Understocked' ELSE '' END\n" +
+                "  END\n" +
+                " END AS Status\n" +
+                "\n" +
+                "\n" +
+                "FROM\n" +
+                " facilities\n" +
+                "INNER JOIN  facility_types ON  facilities.typeid =  facility_types.id\n" +
+                "INNER JOIN  requisitions ON  requisitions.facilityid =  facilities.id\n" +
+                "INNER JOIN  requisition_line_items ON  requisition_line_items.rnrid =  requisitions.id\n" +
+                "INNER JOIN  products ON  products.code =  requisition_line_items.productcode\n" +
+                "INNER JOIN  program_products ON  program_products.productid =  products.id\n" +
+                "INNER JOIN  programs ON  program_products.programid =  programs.id AND  programs.id =  requisitions.programid\n" +
+                "INNER JOIN  programs_supported ON  programs.id =  programs_supported.programid AND  facilities.id =  programs_supported.facilityid\n" +
+                "INNER JOIN  requisition_group_members ON  facilities.id =  requisition_group_members.facilityid\n" +
+                "INNER JOIN  requisition_groups ON  requisition_groups.id =  requisition_group_members.requisitiongroupid\n" +
+                "INNER JOIN  requisition_group_program_schedules ON  requisition_group_program_schedules.programid =  programs.id AND  requisition_group_program_schedules.requisitiongroupid =  requisition_groups.id\n" +
+                "INNER JOIN  processing_schedules ON  processing_schedules.id =  requisition_group_program_schedules.programid\n" +
+                "INNER JOIN  processing_periods ON  processing_periods.scheduleid =  processing_schedules.id\n" +
+                "where requisition_line_items.stockinhand is not null" +"\n";//+
+               // writePredicates(params)+ "\n";
 
-                writePredicates(params)+ "\n"+
-
-                "group by facilities.name,li.productcode, li.product, li.productcategory ,requisition_groups.id \n" +
-                " order by " + QueryHelpers.getSortOrder(params, "facilities.name asc,li.productcode asc,  li.product asc, li.productcategory asc , requisition_groups.id asc");
+              //  "group by facilities.name,li.productcode, li.product, li.productcategory ,requisition_groups.id \n" +
+               // " order by " + QueryHelpers.getSortOrder(params, "facilities.name asc,li.productcode asc,  li.product asc, li.productcategory asc , requisition_groups.id asc");
         return query;
     }
     private static String writePredicates(Map params){
