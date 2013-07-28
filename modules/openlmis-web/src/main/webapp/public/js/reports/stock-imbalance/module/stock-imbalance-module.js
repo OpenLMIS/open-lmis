2de6bc6ef09087d0411f14641deb2435c2454a11
Wolde Fikadu <memiwo@gmail.com>
2013-07-29 02:08:04 +0300

    ELMIS-292 : updates to stock imbalance report angular controller

--- modules/openlmis-web/src/main/webapp/public/js/reports/stock-imbalance/module/stock-imbalance-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/stock-imbalance/module/stock-imbalance-module.js
@@ -1,7 +1,7 @@
 angular.module('stock_imbalance', ['openlmis', 'ngGrid', 'ui.bootstrap.modal', 'ui.bootstrap.dropdownToggle'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.
-            when('/list', {controller:StockImbalanceController(), templateUrl:'partials/list.html',reloadOnSearch:false}).
+            when('/list', {controller:StockImbalanceController, templateUrl:'partials/list.html',reloadOnSearch:false}).
             otherwise({redirectTo:'/list'});
     }]).run(
     function ($rootScope, AuthorizationService) {
