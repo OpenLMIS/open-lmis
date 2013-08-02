279571dd82ac3b493181526ae25db2ad93775391
Vinay Mor <vinaymor@thoughtworks.com>
2013-08-02 16:35:41 +0530

    |#944| +Anshul| Removing browser jsessionid cookies on explicit logout.

--- modules/openlmis-web/src/main/webapp/public/js/shared/controller/header-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/shared/controller/header-controller.js
@@ -4,12 +4,13 @@
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-function HeaderController($scope, localStorageService) {
+function HeaderController($scope, localStorageService, $cookies) {
   $scope.user = localStorageService.get(localStorageKeys.USERNAME);
 
   $scope.logout = function () {
     localStorageService.remove(localStorageKeys.RIGHT);
     localStorageService.remove(localStorageKeys.USERNAME);
+    delete $cookies['JSESSIONID'];
     window.location = "/j_spring_security_logout";
   };
 }
