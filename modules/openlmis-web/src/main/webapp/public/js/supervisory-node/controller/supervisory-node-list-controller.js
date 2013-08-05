3c86a651f1a0a20ac3f37dae11b8f071657a64bc
Henok Getachew <henygetachew@gmail.com>
2013-08-05 10:44:00 +0300

    ELMIS-353 Show which node is about to be deleted.

--- modules/openlmis-web/src/main/webapp/public/js/supervisory-node/controller/supervisory-node-list-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/supervisory-node/controller/supervisory-node-list-controller.js
@@ -62,7 +62,7 @@ function SupervisoryNodeListController($scope, $location, navigateBackService, S
         var options = {
             id: "removeSupervisoryNodeMemberConfirmDialog",
             header: "Confirmation",
-            body: "Are you sure you want to remove the selected supervisory node"
+            body: "Are you sure you want to remove the supervisory node: " + $scope.selectedSupervisoryNode.name
         };
         OpenLmisDialog.newDialog(options, $scope.removeSupervisoryNodeMemberConfirm, $dialog, messageService);
     };
