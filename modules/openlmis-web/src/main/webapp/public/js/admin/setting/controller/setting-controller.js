87848557b2bf871cdd0c9476e2874170ad2dc671
Elias <eliasmu@gmail.com>
2013-08-07 17:06:03 -0400

    ELMIS-374: implement the update feature for text data typed configuration settings

--- modules/openlmis-web/src/main/webapp/public/js/admin/setting/controller/setting-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/admin/setting/controller/setting-controller.js
@@ -1,10 +1,17 @@
-function ListSettingController($scope, $routeParams, $location, $dialog, Settings , messageService) {
+function ListSettingController($scope, $routeParams, $location, $dialog, Settings, SettingUpdator, messageService) {
 
        Settings.get(function (data){
            $scope.settings = data.settings;
        });
 
     $scope.saveSettings = function(){
-        alert("I have saved it");
+        SettingUpdator.post({}, $scope.settings, function (data){
+            var dialogOpts = {
+                id: "settingsSavedConfirmation",
+                header: messageService.get('confirm.settings.saved'),
+                body: messageService.get('confirm.settings.saved.detail')
+            };
+            OpenLmisDialog.newDialog(dialogOpts, $scope.clearSelectedFacility, $dialog, messageService);
+        });
     }
 }
