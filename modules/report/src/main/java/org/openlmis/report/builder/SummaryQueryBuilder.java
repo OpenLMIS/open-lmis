d76956f74e320bd2f758f2e8186e64f22d4f6b2a
Elias <eliasmu@gmail.com>
2013-08-20 11:58:36 -0400

    added copy right text to java and html files

--- modules/report/src/main/java/org/openlmis/report/builder/SummaryQueryBuilder.java
+++ modules/report/src/main/java/org/openlmis/report/builder/SummaryQueryBuilder.java
@@ -1,9 +1,12 @@
+/*
+ * Copyright © 2013 John Snow, Inc. (JSI). All Rights Reserved.
+ *
+ * The U.S. Agency for International Development (USAID) funded this section of the application development under the terms of the USAID | DELIVER PROJECT contract no. GPO-I-00-06-00007-00.
+ */
+
 package org.openlmis.report.builder;
 
 import java.util.Map;
-
-import static org.apache.ibatis.jdbc.SelectBuilder.*;
-import static org.apache.ibatis.jdbc.SqlBuilder.SELECT;
 
 /**
  * User: Elias
