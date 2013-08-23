1626a61d4805c0d695af7e0bb9c39e1df2082df9
Riya Kataria <riyakata@thoughtworks.com>
2013-08-23 11:08:26 +0530

    |#982| +Anshul | Added all possible date formats to enum.

--- modules/shipment/src/main/java/org/openlmis/order/domain/DateFormat.java
+++ modules/shipment/src/main/java/org/openlmis/order/domain/DateFormat.java
@@ -14,14 +14,36 @@ import org.openlmis.order.serializer.DateFormatSerializer;
 
 public enum DateFormat {
 
-  DATE_1("ddMMyyyy", true),
-  DATE_2("dd/MM/yyyy", true),
-  DATE_3("dd-MM-yyyy", true),
-  DATE_4("dd/MM/yy", true),
-  DATE_5("MMddyy", true),
-
-  DATE_6("MM/yy", false),
-  DATE_7("yyyy/MM/dd", false);
+  DATE_1("MM/yy", false),
+  DATE_2("MM/yyyy", false),
+  DATE_3("yy/MM", false),
+  DATE_4("yyyy/MM", false),
+  DATE_5("dd/MM/yy", true),
+  DATE_6("dd/MM/yyyy", true),
+  DATE_7("MM/dd/yy", true),
+  DATE_8("MM/dd/yyyy", true),
+  DATE_9("yy/MM/dd", true),
+  DATE_10("yyyy/MM/dd", true),
+  DATE_11("MM-yy", false),
+  DATE_12("MM-yyyy", false),
+  DATE_13("yy-MM", false),
+  DATE_14("yyyy-MM", false),
+  DATE_15("dd-MM-yy", true),
+  DATE_16("dd-MM-yyyy", true),
+  DATE_17("MM-dd-yy", true),
+  DATE_18("MM-dd-yyyy", true),
+  DATE_19("yy-MM-dd", true),
+  DATE_20("yyyy-MM-dd", true),
+  DATE_21("MMyy", false),
+  DATE_22("MMyyyy", false),
+  DATE_23("yyMM", false),
+  DATE_24("yyyyMM", false),
+  DATE_25("ddMMyy", true),
+  DATE_26("ddMMyyyy", true),
+  DATE_27("MMddyy", true),
+  DATE_28("MMddyyyy", true),
+  DATE_29("yyMMdd", true),
+  DATE_30("yyyyMMdd", true);
 
   @Getter
   private final String format;
