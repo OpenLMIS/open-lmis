b293da68518584afdc725cb2273d5090b05f93cd
Rahul Joshi <rahulj@thoughtworks.com>
2013-09-12 12:18:33 +0530

    |#1047| moving order file configuration inserts into migration scripts

--- modules/db/src/main/resources/db/migration/V50__create_order_configuration.sql
+++ modules/db/src/main/resources/db/migration/V50__create_order_configuration.sql
@@ -3,11 +3,14 @@
 DROP TABLE IF EXISTS order_configuration;
 
 CREATE TABLE order_configuration (
-  filePrefix        VARCHAR(8),
-  headerInFile      BOOLEAN     NOT NULL,
-  createdBy         INTEGER,
-  createdDate       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  modifiedBy        INTEGER,
-  modifiedDate      TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  filePrefix   VARCHAR(8),
+  headerInFile BOOLEAN NOT NULL,
+  createdBy    INTEGER,
+  createdDate  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  modifiedBy   INTEGER,
+  modifiedDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+INSERT INTO order_configuration
+(filePrefix, headerInFile) VALUES
+('O', FALSE);
--- modules/db/src/main/resources/seed/order_file_template.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO order_configuration VALUES ('O', FALSE);
-
-INSERT INTO order_file_columns (dataFieldLabel, nested, keyPath, columnLabel, format, position, openLmisField) VALUES ('header.order.number', 'order', 'id', 'Order number', '', 1, TRUE);
-INSERT INTO order_file_columns (dataFieldLabel, nested, keyPath, columnLabel, format, position, openLmisField) VALUES ('create.facility.code', 'order', 'rnr/facility/code', 'Facility code', '', 2, TRUE);
-INSERT INTO order_file_columns (dataFieldLabel, nested, keyPath, columnLabel, format, position, openLmisField) VALUES ('header.product.code', 'lineItem', 'productCode', 'Product code', '', 3, TRUE);
-INSERT INTO order_file_columns (dataFieldLabel, nested, keyPath, columnLabel, format, position, openLmisField) VALUES ('header.quantity.approved', 'lineItem', 'quantityApproved', 'Approved quantity', '', 4, TRUE);
-INSERT INTO order_file_columns (dataFieldLabel, nested, keyPath, columnLabel, format, position, openLmisField) VALUES ('label.period', 'order', 'rnr/period/startDate', 'Period', 'MM/yy', 5, TRUE);
-INSERT INTO order_file_columns (dataFieldLabel, nested, keyPath, columnLabel, format, position, openLmisField) VALUES ('header.order.date', 'order', 'createdDate', 'Order date', 'dd/MM/yy', 6, TRUE);
