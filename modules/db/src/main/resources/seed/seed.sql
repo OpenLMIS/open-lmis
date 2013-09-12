ea82ef4b104c8fa2c17d46044a3b285d80aa4b5c
Anshul Rastogi <anshulr@thoughtworks.com>
2013-09-12 14:04:59 +0530

    |#00|Fixing everything on ci

--- modules/db/src/main/resources/seed/seed.sql
+++ modules/db/src/main/resources/seed/seed.sql
@@ -1,12 +1,12 @@
 -- Copyright © 2013 VillageReach.  All Rights Reserved.  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 -- If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-\i src/ main /resources/seed/dosage_units.sql
-\i src/ main /resources/seed/facility_operators.sql
-\i src/ main/resources/seed/facility_types.sql
-\i src/ main /resources/seed/geographic_levels.sql
-\i src/ main /resources/seed/geographic_zones.sql
-\i src/ main /resources/seed/product_forms.sql
-\i src/ main /resources/seed/product_groups.sql
-\i src/ main /resources/seed/programs.sql
-\i src/ main /resources/seed/regimen_categories.sql
+\i src/main/resources/seed/dosage_units.sql
+\i src/main/resources/seed/facility_operators.sql
+\i src/main/resources/seed/facility_types.sql
+\i src/main/resources/seed/geographic_levels.sql
+\i src/main/resources/seed/geographic_zones.sql
+\i src/main/resources/seed/product_forms.sql
+\i src/main/resources/seed/product_groups.sql
+\i src/main/resources/seed/programs.sql
+\i src/main/resources/seed/regimen_categories.sql
