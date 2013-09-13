71b3343b3097ee809dee30292bca4b5a5ec6aeff
Elias <eliasmu@gmail.com>
2013-09-13 12:07:17 -0400

    On Facility allowed products, filter out and show only the products in the program products table that are active

--- modules/core/src/main/java/org/openlmis/core/repository/mapper/ProgramProductMapper.java
+++ modules/core/src/main/java/org/openlmis/core/repository/mapper/ProgramProductMapper.java
@@ -36,7 +36,7 @@ public interface ProgramProductMapper {
   @Update("UPDATE program_products SET  dosesPerMonth=#{dosesPerMonth}, active=#{active}, modifiedBy=#{modifiedBy}, modifiedDate=#{modifiedDate} WHERE programId=#{program.id} AND productId=#{product.id}")
   void update(ProgramProduct programProduct);
 
-  @Select({"SELECT * FROM program_products pp INNER JOIN products p ON pp.productId = p.id WHERE programId = #{id} ",
+  @Select({"SELECT * FROM program_products pp INNER JOIN products p ON pp.productId = p.id WHERE programId = #{id} and pp.active = true",
     "ORDER BY p.displayOrder NULLS LAST, p.code"})
   @Results(value = {
     @Result(property = "id", column = "id"),
