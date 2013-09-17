e4f303af9f8a06ad4ac817f1a4cad69f398d22f5
Rahul Joshi <rahulj@thoughtworks.com>
2013-09-17 13:36:59 +0530

    |#911| Null RequestCache implemented to get rid of cached request

--- modules/authentication/src/main/java/org/openlmis/authentication/web/UserAuthenticationSuccessHandler.java
+++ modules/authentication/src/main/java/org/openlmis/authentication/web/UserAuthenticationSuccessHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013 VillageReach.  All Rights Reserved.  This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * Copyright © 2013 VillageReach. All Rights Reserved. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
  *
  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
@@ -12,21 +12,24 @@ import org.springframework.security.web.authentication.SimpleUrlAuthenticationSu
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.Map;
 
 
 public class UserAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    public static final String USER = "USER";
-    public static final String USER_ID = "USER_ID";
+  public static final String USER = "USER";
+  public static final String USER_ID = "USER_ID";
 
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException {
-        request.getSession().setAttribute(USER_ID, authentication.getPrincipal());
-        Map userDetails = (Map) authentication.getDetails();
-        request.getSession().setAttribute(USER, userDetails.get(USER));
-        super.onAuthenticationSuccess(request, response, authentication);
-    }
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                      Authentication authentication) throws IOException, ServletException {
+    HttpSession session = request.getSession();
+    session.setAttribute(USER_ID, authentication.getPrincipal());
+    Map userDetails = (Map) authentication.getDetails();
+    session.setAttribute(USER, userDetails.get(USER));
+
+    super.onAuthenticationSuccess(request, response, authentication);
+  }
 }
