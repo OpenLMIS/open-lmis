a9439ff982468c051922698d11bb7c714bb59a44
Elias <eliasmu@gmail.com>
2013-09-18 15:42:30 -0400

    change copy right information

--- modules/openlmis-web/src/main/webapp/public/js/ils-gateway/controller/ils-gateway-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/ils-gateway/controller/ils-gateway-controller.js
@@ -1,10 +1,10 @@
-/**
- * Created with IntelliJ IDEA.
- * User: henok
- * Date: 9/5/13
- * Time: 12:44 AM
- * To change this template use File | Settings | File Templates.
+
+/*
+ * Copyright © 2013 John Snow, Inc. (JSI). All Rights Reserved.
+ *
+ * The U.S. Agency for International Development (USAID) funded this section of the application development under the terms of the USAID | DELIVER PROJECT contract no. GPO-I-00-06-00007-00.
  */
+
 function ILSGatewayDashboardController($scope, $routeParams, $location, $dialog, messageService) {
 
     $scope.setHeight = function(){
