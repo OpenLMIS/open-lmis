dbf7c08b7210ee337289bd795962b8cd653bae40
Elias <eliasmu@gmail.com>
2013-09-26 10:13:41 -0400

    ELMIS-649, ELMIS-648, Avoid having an error when opening the different reports

--- modules/openlmis-web/src/main/java/org/openlmis/web/controller/FacilityController.java
+++ modules/openlmis-web/src/main/java/org/openlmis/web/controller/FacilityController.java
@@ -75,7 +75,8 @@ public class FacilityController extends BaseController {
   }
 
   @RequestMapping(value = "/facilities/{id}", method = GET, headers = ACCEPT_JSON)
-  @PreAuthorize("@permissionEvaluator.hasPermission(principal, 'MANAGE_FACILITY')")
+  //@PreAuthorize("@permissionEvaluator.hasPermission(principal, 'MANAGE_FACILITY')")
+  //this method is also being used by the reports
   public ResponseEntity<OpenLmisResponse> getFacility(@PathVariable(value = "id") Long id) {
     return response("facility", facilityService.getById(id));
   }
