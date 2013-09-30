1534839a335c073298f11e7f7e8f19da66a5ac56
Elias <eliasmu@gmail.com>
2013-09-30 17:29:51 -0400

    Add migration script for max stock quantity

--- modules/migration/src/main/resources/db/migration/V71__add_calculation_options_columns.sql
+++ modules/migration/src/main/resources/db/migration/V71__add_calculation_options_columns.sql
@@ -1,6 +1,9 @@
-ALTER TABLE master_rnr_columns
-ADD calculation_options varchar(200) default 'DEFAULT';
+ALTER TABLE master_rnr_columns 
+ADD calculationOption varchar(200) default 'DEFAULT';
 
 ALTER TABLE program_rnr_columns
-ADD calculation_option VARCHAR(200) default 'DEFAULT';
+ADD calculationOption VARCHAR(200) default 'DEFAULT';
 
+UPDATE master_rnr_columns
+set calculationOption = '[{"name":"Default", "id":"DEFAULT"},{"name":"Consumption x 2","id":"CONSUMPTION_X_2"}]'
+where name = 'maxStockQuantity';
