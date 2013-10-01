896a865193efcd4628a0049ed1f2fbf6f0aa57e3
Rahul Joshi <rahulj@thoughtworks.com>
2013-10-01 14:44:50 +0530

    |#000| correcting period dates in test seed

--- test-data/sql/processing_schedules.sql
+++ test-data/sql/processing_schedules.sql
@@ -7,66 +7,66 @@ INSERT INTO processing_schedules (code, name, description) VALUES ('M', 'Monthly
 
 INSERT INTO processing_periods
 (name, description, startDate, endDate, numberOfMonths, scheduleId, modifiedBy) VALUES
-('Period1', 'first period', '2012-12-01', '2012-01-15 23:59:59', 2, (SELECT
+('Period1', 'first period', '2012-12-01', '2013-02-28 23:59:59', 2, (SELECT
                                                                        id
                                                                      FROM processing_schedules
-                                                                     WHERE code = 'Q1stM'), (SELECT
-                                                                                               id
-                                                                                             FROM users
-                                                                                             LIMIT 1)),
-('Period2', 'second period', '2012-02-01', '2012-03-28 23:59:59', 2, (SELECT
+                                                                     WHERE code='Q1stM'), (SELECT
+                                                                                             id
+                                                                                           FROM users
+                                                                                           LIMIT 1)),
+('Period2', 'second period', '2013-03-01', '2012-05-31 23:59:59', 2, (SELECT
                                                                         id
                                                                       FROM processing_schedules
-                                                                      WHERE code = 'Q1stM'), (SELECT
-                                                                                                id
-                                                                                              FROM users
-                                                                                              LIMIT 1)),
+                                                                      WHERE code='Q1stM'), (SELECT
+                                                                                              id
+                                                                                            FROM users
+                                                                                            LIMIT 1)),
 ('Dec2012', 'Dec2012', '2012-12-01', '2012-12-31 23:59:59', 1, (SELECT
                                                                   id
                                                                 FROM processing_schedules
-                                                                WHERE code = 'M'), (SELECT
-                                                                                      id
-                                                                                    FROM users
-                                                                                    LIMIT 1)),
+                                                                WHERE code='M'), (SELECT
+                                                                                    id
+                                                                                  FROM users
+                                                                                  LIMIT 1)),
 ('Jan2013', 'Jan2013', '2013-01-01', '2013-01-31 23:59:59', 1, (SELECT
                                                                   id
                                                                 FROM processing_schedules
-                                                                WHERE code = 'M'), (SELECT
-                                                                                      id
-                                                                                    FROM users
-                                                                                    LIMIT 1)),
+                                                                WHERE code='M'), (SELECT
+                                                                                    id
+                                                                                  FROM users
+                                                                                  LIMIT 1)),
 ('Feb2013', 'Feb2013', '2013-02-01', '2013-02-28 23:59:59', 1, (SELECT
                                                                   id
                                                                 FROM processing_schedules
-                                                                WHERE code = 'M'), (SELECT
-                                                                                      id
-                                                                                    FROM users
-                                                                                    LIMIT 1)),
+                                                                WHERE code='M'), (SELECT
+                                                                                    id
+                                                                                  FROM users
+                                                                                  LIMIT 1)),
 ('Mar2013', 'Mar2013', '2013-03-01', '2013-03-31 23:59:59', 1, (SELECT
                                                                   id
                                                                 FROM processing_schedules
-                                                                WHERE code = 'M'), (SELECT
-                                                                                      id
-                                                                                    FROM users
-                                                                                    LIMIT 1)),
+                                                                WHERE code='M'), (SELECT
+                                                                                    id
+                                                                                  FROM users
+                                                                                  LIMIT 1)),
 ('Apr2013', 'Apr2013', '2013-04-01', '2013-04-30 23:59:59', 1, (SELECT
                                                                   id
                                                                 FROM processing_schedules
-                                                                WHERE code = 'M'), (SELECT
-                                                                                      id
-                                                                                    FROM users
-                                                                                    LIMIT 1)),
+                                                                WHERE code='M'), (SELECT
+                                                                                    id
+                                                                                  FROM users
+                                                                                  LIMIT 1)),
 ('May2013', 'May2013', '2013-05-01', '2013-05-31 23:59:59', 1, (SELECT
                                                                   id
                                                                 FROM processing_schedules
-                                                                WHERE code = 'M'), (SELECT
-                                                                                      id
-                                                                                    FROM users
-                                                                                    LIMIT 1)),
+                                                                WHERE code='M'), (SELECT
+                                                                                    id
+                                                                                  FROM users
+                                                                                  LIMIT 1)),
 ('June2013', 'June2013', '2013-06-01', '2013-06-30 23:59:59', 1, (SELECT
                                                                     id
                                                                   FROM processing_schedules
-                                                                  WHERE code = 'M'), (SELECT
-                                                                                        id
-                                                                                      FROM users
-                                                                                      LIMIT 1));
+                                                                  WHERE code='M'), (SELECT
+                                                                                      id
+                                                                                    FROM users
+                                                                                    LIMIT 1));
