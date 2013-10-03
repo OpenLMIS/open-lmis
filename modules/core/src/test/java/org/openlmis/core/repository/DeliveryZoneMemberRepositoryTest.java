12ee57653473eced1da5b4c68f91d03b9ef8d52f
Rahul Joshi <rahulj@thoughtworks.com>
2013-10-03 11:52:11 +0530

    moving to AGPL 3.0 license

--- modules/core/src/test/java/org/openlmis/core/repository/DeliveryZoneMemberRepositoryTest.java
+++ modules/core/src/test/java/org/openlmis/core/repository/DeliveryZoneMemberRepositoryTest.java
@@ -1,3 +1,13 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2013 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
 package org.openlmis.core.repository;
 
 import org.junit.Test;
@@ -7,7 +17,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.openlmis.core.domain.DeliveryZoneMember;
-import org.openlmis.core.repository.DeliveryZoneMemberRepository;
 import org.openlmis.core.repository.mapper.DeliveryZoneMemberMapper;
 import org.openlmis.db.categories.UnitTests;
 
