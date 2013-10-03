bed083da0e8616eeb8d55b454bb401115d249f39
Elias <eliasmu@gmail.com>
2013-10-03 10:34:07 -0400

    ELMIS-750: Remove the repeated facility type filter

--- test-modules/functional-tests/src/test/java/org/openlmis/functional/reports/OrderReport.java
+++ test-modules/functional-tests/src/test/java/org/openlmis/functional/reports/OrderReport.java
@@ -16,10 +16,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.Select;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Listeners;
+import org.testng.annotations.*;
 
 import java.io.IOException;
 import java.util.HashMap;
