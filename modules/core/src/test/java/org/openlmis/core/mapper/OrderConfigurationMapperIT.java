1d7f5f374c18addee198f2cdece21b75369ace1d
Manan <manan.bharara@gmail.com>
2013-10-04 14:35:00 +0530

    |#000| Moving test files from resources to directory

--- modules/core/src/test/java/org/openlmis/core/mapper/OrderConfigurationMapperIT.java
+++ modules/core/src/test/java/org/openlmis/core/mapper/OrderConfigurationMapperIT.java
@@ -1,20 +1,19 @@
 /*
- * This program is part of the OpenLMIS logistics management information system platform software.
- * Copyright © 2013 VillageReach
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *  
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ *  * Copyright © 2013 VillageReach. All Rights Reserved. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ *  *
+ *  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
  */
 
-package org.openlmis.core.repository.mapper;
+package org.openlmis.core.mapper;
 
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.openlmis.core.domain.OrderConfiguration;
+import org.openlmis.core.repository.mapper.OrderConfigurationMapper;
 import org.openlmis.db.categories.IntegrationTests;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
