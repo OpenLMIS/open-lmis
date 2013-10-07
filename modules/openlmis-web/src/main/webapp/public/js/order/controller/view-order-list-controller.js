6e352dfcc023da5c16eb6cc62e5797d5be6f9259
Manan <manan.bharara@gmail.com>
2013-10-07 13:20:26 +0530

    |#1097| Adding pagination logic to view orders screen.

--- modules/openlmis-web/src/main/webapp/public/js/order/controller/view-order-list-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/order/controller/view-order-list-controller.js
@@ -1,15 +1,32 @@
 /*
- * This program is part of the OpenLMIS logistics management information system platform software.
- * Copyright © 2013 VillageReach
  *
- * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *  
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ *  * Copyright © 2013 VillageReach. All Rights Reserved. This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ *  *
+ *  * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
  */
 
-function ViewOrderListController($scope, orders, messageService) {
-  $scope.orders = orders;
+function ViewOrderListController($scope, Orders, messageService, $location, $routeParams) {
+
+  function refreshGrid() {
+    $scope.currentPage = $routeParams.page ? utils.parseIntWithBaseTen($routeParams.page) : 1;
+
+    Orders.get({page: $scope.currentPage}, function (data) {
+      if ((!data.orders || data.orders.length === 0) && $routeParams.page != 1) {
+        $location.search('page', 1);
+        return;
+      }
+      $scope.orders = data.orders || [];
+      $scope.pageSize = data.pageSize;
+      $scope.numberOfPages = data.numberOfPages || 1;
+    }, function (data) {
+      console.log(data);
+    });
+  }
+
+  $scope.$on('$routeUpdate', refreshGrid);
+
+  refreshGrid();
 
   $scope.gridOptions = { data: 'orders',
     showFooter: false,
@@ -30,7 +47,7 @@ function ViewOrderListController($scope, orders, messageService) {
         cellTemplate: "<div class='ngCellText'><span ng-cell-text><div id=\"ftpComment\" class='ngCellText'> <span ng-show='row.entity.ftpComment' openlmis-message='row.entity.ftpComment'></span></div>"},
       {field: 'emergency', displayName: messageService.get("requisition.type.emergency"),
         cellTemplate: '<div class="ngCellText checked"><i ng-class="{\'icon-ok\': row.entity.rnr.emergency}"></i></div>',
-         width: 90 },
+        width: 90 },
       {cellTemplate: "<div class='ngCellText'><a ng-show=\"row.entity.productsOrdered\" ng-href='/orders/{{row.entity.id}}/download.csv' openlmis-message='link.download.csv'></a>" +
         "<span ng-show=\"!row.entity.productsOrdered\" openlmis-message='msg.no.product.in.order' ng-cell-text></span></div>", width: 180},
       {cellTemplate: "", width: '1%'}
@@ -39,17 +56,9 @@ function ViewOrderListController($scope, orders, messageService) {
 
   $scope.getStatus = function (status) {
     return messageService.get("label.order." + status);
-  }
-}
+  };
 
-ViewOrderListController.resolve = {
-  orders: function ($q, $timeout, Orders) {
-    var deferred = $q.defer();
-    $timeout(function () {
-      Orders.get({}, function (data) {
-        deferred.resolve(data.orders);
-      }, {});
-    }, 100);
-    return deferred.promise;
-  }
+  $scope.$watch('currentPage', function () {
+    $location.search('page', $scope.currentPage);
+  });
 }
