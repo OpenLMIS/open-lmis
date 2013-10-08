19a90c6c452568250ed1ee4d4f8d435b84f2c712
Elias <eliasmu@gmail.com>
2013-10-08 08:51:18 -0400

    apply the ng-table grid for mailing lable report.

--- modules/openlmis-web/src/main/webapp/public/js/reports/mailing-labels/controller/mailing-labels-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mailing-labels/controller/mailing-labels-controller.js
@@ -1,81 +1,19 @@
-function ListMailinglabelsController($scope, MailingLabels, ReportFacilityTypes,RequisitionGroups, $http, $routeParams,$location) {
+function ListMailinglabelsController($scope,$filter, ngTableParams, MailingLabels, ReportFacilityTypes,RequisitionGroups, $http, $routeParams,$location) {
 
-        //to minimize and maximize the filter section
-        var section = 1;
+    $scope.filterGrid = function (){
+       $scope.getPagedDataAsync(0, 0);
+    };
 
-        $scope.section = function (id) {
-            section = id;
-        };
-
-        $scope.show = function (id) {
-            return section == id;
-        };
-
-        $scope.filterGrid = function (){
-           $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
-        };
-
-        //filter form data section    facilityName
-        $scope.filterOptions = {
-            filterText: "",
-            useExternalFilter: false
-        };
-
-        $scope.pagingOptions = {
-            pageSizes: [5, 10, 20, 40, 50, 100],
-            pageSize: 10,
-            totalServerItems: 0,
-            currentPage: 1
-        };
-
-        //filter form data section
-        $scope.filterObject =  {
-             facilityTypeId : $scope.facilityTypeId,
-             facilityType : "",
-             rgroupId : $scope.rgroupId,
-             rgroup : ""
-        };
-        RequisitionGroups.get(function (data) {
-            $scope.requisitionGroups = data.requisitionGroupList;
-            $scope.requisitionGroups.unshift({'name':'-- All Requisition Groups --','id':'0'});
-        });
-
-        ReportFacilityTypes.get(function(data) {
-            $scope.facilityTypes = data.facilityTypes;
-            $scope.facilityTypes.unshift({'name':'-- All Facility Types --','id':'0'});
-        });
-
-        $scope.currentPage = ($routeParams.page) ? parseInt($routeParams.page) || 1 : 1;
-
-    $scope.$watch('rgroupId', function (selection) {
-        if (selection == "All") {
-            $scope.filterObject.rgroupId = -1;
-        } else if (selection != undefined || selection == "") {
-            $scope.filterObject.rgroupId = selection;
-            $.each($scope.requisitionGroups, function (item, idx) {
-                if (idx.id == selection) {
-                    $scope.filterObject.rgroup = idx.name;
-                }
-            });
-        } else {
-            $scope.filterObject.rgroupId = 0;
-        }
-        $scope.filterGrid();
+    RequisitionGroups.get(function (data) {
+        $scope.requisitionGroups = data.requisitionGroupList;
+        $scope.requisitionGroups.unshift({'name':'-- All Requisition Groups --','id':'0'});
     });
 
-    $scope.$watch('facilityTypeId', function(selection){
-            if(selection != undefined || selection == ""){
-                $scope.filterObject.facilityTypeId =  selection;
-                $.each($scope.facilityTypes, function(idx,value){
-                   if(selection == idx.id){
-                       $scope.filterObject.facilityType = value.name;
-                   }
-                });
-            }else{
-                $scope.filterObject.facilityTypeId =  0;
-            }
-            $scope.filterGrid();
-        });
+    ReportFacilityTypes.get(function(data) {
+        $scope.facilityTypes = data.facilityTypes;
+        $scope.facilityTypes.unshift({'name':'-- All Facility Types --','id':'0'});
+    });
+
 
     $scope.exportReport   = function (type){
         var url = '/reports/download/mailinglabels/' + type +'?facilityCodeFilter=' +  $scope.filterObject.facilityCodeFilter + '&facilityNameFilter=' +  $scope.filterObject.facilityNameFilter + '&facilityTypeId=' +  $scope.filterObject.facilityTypeId ;
@@ -84,106 +22,67 @@ function ListMailinglabelsController($scope, MailingLabels, ReportFacilityTypes,
         }
         window.open(url);
     }
-        $scope.goToPage = function (page, event) {
-            angular.element(event.target).parents(".dropdown").click();
-            $location.search('page', page);
-        };
 
-        $scope.$watch("currentPage", function () {  //good watch no problem
 
-            if($scope.currentPage != undefined && $scope.currentPage != 1){
-              //when clicked using the links they have done updated the paging info no problem here
-               //or using the url page param
 
-                $location.search("page", $scope.currentPage);
+    // the grid options
+    $scope.tableParams = new ngTableParams({
+        page: 1,            // show first page
+        total: 0,           // length of data
+        count: 25           // count per page
+    });
+
+    $scope.paramsChanged = function(params) {
+
+        // slice array data on pages
+        if($scope.data == undefined ){
+            $scope.datarows = [];
+            params.total = 0;
+        }else{
+            var data = $scope.data;
+            var orderedData = params.filter ? $filter('filter')(data, params.filter) : data;
+            orderedData = params.sorting ?  $filter('orderBy')(orderedData, params.orderBy()) : data;
+
+            params.total = orderedData.length;
+            $scope.datarows = orderedData.slice( (params.page - 1) * params.count,  params.page * params.count );
+            var i = 0;
+            var baseIndex = params.count * (params.page - 1) + 1;
+            while(i < $scope.datarows.length){
+                $scope.datarows[i].no = baseIndex + i;
+                i++;
             }
-        });
-
-        $scope.$on('$routeUpdate', function () {
-            if (!utils.isValidPage($routeParams.page, $scope.numberOfPages)) {
-                $location.search('page', 1);
-                return;
-            }
-        });
-
-        $scope.sortInfo = { fields:["code","facilityType"], directions: ["ASC"]};
-        $.each($scope.sortInfo.fields, function(index, value) {
-            if(value != undefined) {
-                $scope.filterObject['sort-' + $scope.sortInfo.fields[index]] = $scope.sortInfo.directions[index];
-            }
-        });
-        $scope.$watch('sortInfo', function () {
-            $.each($scope.sortInfo.fields, function (index, value) {
-                if (value != undefined)
-                    $scope.filterObject['sort-'+$scope.sortInfo.fields[index]] = $scope.sortInfo.directions[index];
-            });
-            $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
-        }, true);
-
-        $scope.setPagingData = function(data, page, pageSize, total){
-
-            $scope.myData = data; //pagedData;//
-            $scope.pagingOptions.totalServerItems = total;//data.length;
-            $scope.numberOfPages = ( Math.ceil( total / pageSize))  ? Math.ceil( total / pageSize) : 1 ;
-
-        };
-
-        $scope.getPagedDataAsync = function (pageSize, page) {
-                        var params  = {};
-                        if(pageSize != undefined && page != undefined ){
-                                var params =  {
-                                                "max" : pageSize,
-                                                "page" : page
-                                               };
-                        }
-                        $.each($scope.filterObject, function(index, value) {
-                           // alert('filter object '+ index+' '+value);
-                           params[index] = value;
-                        });
-                        MailingLabels.get(params, function(data) {
-                            $scope.setPagingData(data.pages.rows,page,pageSize,data.pages.total);
-                        });
-
-        };
-
-        $scope.$watch('pagingOptions.currentPage', function () {
-            $scope.currentPage = $scope.pagingOptions.currentPage;
-            $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
-        }, true);
-
-        $scope.$watch('pagingOptions.pageSize', function () {
-            $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
-        }, true);
-
-    $scope.gridOptions = {
-        data: 'myData',
-        columnDefs:
-            [
-                { field: 'code', displayName: 'Facility Code', width: "*", resizable: false},
-                { field: 'facilityName', displayName: 'Facility Name', width: "**" },
-                { field: 'facilityType', displayName: 'Facility Type', width: "*" },
-                { field: 'region', displayName: 'Region', width : "*"},
-                { field: 'address1;', displayName: 'Address 1', width : "**"},
-                { field: 'contact', displayName: 'Contact', width : "*"},
-                { field: 'owner', displayName: 'Operator', width : "*"},
-                { field: 'phoneNumber', displayName: 'Phone', width : "*"},
-                { field: 'active', displayName: 'Active', width : "*"}
-
-            ],
-        enablePaging: true,
-        enableSorting :true,
-        showFooter: true,
-        selectWithCheckboxOnly :false,
-        pagingOptions: $scope.pagingOptions,
-        filterOptions: $scope.filterOptions,
-        useExternalSorting: true,
-        sortInfo: $scope.sortInfo,
-        showColumnMenu: true,
-        enableRowReordering: true,
-        showFilter: true,
-        autoFit :true,
-        plugins: [new ngGridFlexibleHeightPlugin()]
-
+        }
     };
 
+    // watch for changes of parameters
+    $scope.$watch('tableParams', $scope.paramsChanged , true);
+
+    $scope.getPagedDataAsync = function (pageSize, page) {
+        pageSize = 10000;
+        page = 1;
+        var params  = {};
+        if(pageSize != undefined && page != undefined ){
+                var params =  {
+                                "max" : pageSize,
+                                "page" : page
+                               };
+        }
+        //filter form data section
+        $scope.filterObject = {
+            facilityTypeId : $scope.facilityTypeId,
+            rgroupId :  $scope.rgroupId
+        }
+
+        // copy the filters over
+        $.each($scope.filterObject, function(index, value) {
+           params[index] = value;
+        });
+        // go
+        MailingLabels.get(params, function(data) {
+            $scope.data = data.pages.rows ;
+            $scope.paramsChanged($scope.tableParams);
+        });
+    };
+
+    $scope.filterGrid();
 }
--- modules/report/src/main/java/org/openlmis/report/mapper/MailingLabelReportMapper.java
+++ modules/report/src/main/java/org/openlmis/report/mapper/MailingLabelReportMapper.java
@@ -26,8 +26,7 @@ public interface MailingLabelReportMapper {
 
     @SelectProvider(type=MailingLabelReportQueryBuilder.class, method="SelectFilteredSortedPagedMailingLabelsSql")
     @Options(resultSetType = ResultSetType.SCROLL_SENSITIVE, fetchSize=10,timeout=0,useCache=true,flushCache=true)
-    //@Select("SELECT id, code, name FROM facilities")
-    //@RowBounds(getLimit = 10,getOffset = 10)
+
     @Results(value = {
             @Result(column="code", property="code"),
             @Result(column="name", property="facilityName"),
@@ -35,9 +34,6 @@ public interface MailingLabelReportMapper {
             @Result(column="facilityType", property="facilityType"),
             @Result(column="region", property="region"),
             @Result(column="owner", property="owner"),
-/*            @Result(column ="longitude", property = "longitude"),
-            @Result(column ="latitude", property = "latitude"),
-            @Result(column ="altitude", property = "altitude"),*/
             @Result(column = "gpsCoordinates", property = "gpsCoordinates"),
             @Result(column="phoneNumber", property="phoneNumber"),
             @Result(column="fax", property="fax")
