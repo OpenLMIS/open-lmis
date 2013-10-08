b2635f5689e76bbc07b4c0be37a4a1d024209cda
Rahul Joshi <rahulj@thoughtworks.com>
2013-10-08 22:31:39 +0530

    |#000| cleaning DB setup gradle tasks

--- modules/db/src/main/resources/db/migration/V38_1__create_atom_feed_schema.sql
+++ modules/db/src/main/resources/db/migration/V38_1__create_atom_feed_schema.sql
@@ -7,6 +7,7 @@
 -- This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
 -- You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
 --
+create schema  atomfeed;
 
 CREATE TABLE atomfeed.event_records (
   id SERIAL PRIMARY KEY,
