64a553a46a8e18d6cebf317c4b5b705579e04559
Manan <manan.bharara@gmail.com>
2013-10-11 12:29:27 +0530

    |#1101| +Rahul| Refactoring promise resolver for sync of distribution. Adding styling for duplicate icon

--- modules/openlmis-web/src/main/webapp/public/js/distribution/controller/distribution-list-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/distribution/controller/distribution-list-controller.js
@@ -3,13 +3,15 @@
  * Copyright © 2013 VillageReach
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- *  
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
- * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org.
  */
+
 function DistributionListController($scope, SharedDistributions, IndexedDB, SyncFacilityDistributionData, $q, messageService) {
   var COMPLETE = 'is-complete';
   var SYNCED = 'is-synced';
+  var DUPLICATE = 'is-duplicate';
 
   SharedDistributions.update();
 
@@ -38,32 +40,42 @@ function DistributionListController($scope, SharedDistributions, IndexedDB, Sync
         promises.push(defer.promise);
 
         SyncFacilityDistributionData.update({id: distributionId, facilityId: facilityId}, facilityDistributionData,
-          function (data) {
+          function () {
             facilityDistributionData.status = SYNCED;
             facilitySelected = _.findWhere(facilities, {id: facilityId});
             defer.resolve({facility: facilitySelected, facilityDistributionData: facilityDistributionData});
-          }, function (data) {
+          }, function () {
+            facilityDistributionData.status = DUPLICATE;
             facilitySelected = _.findWhere(facilities, {id: facilityId});
-            defer.resolve({facility: facilitySelected, status: 'error'});
+            defer.resolve({facility: facilitySelected, facilityDistributionData: facilityDistributionData});
           });
       });
 
       $q.all(promises).then(function (resolves) {
+        IndexedDB.put('distributions', distributionData, null, null, $scope.sharedDistributions.update);
+
         if (!promises.length) {
-          $scope.message = messageService.get("message.no.facility.synced");
+          $scope.message = 'message.no.facility.synced';
           return;
         }
-        $scope.message = '';
-        $(resolves).each(function (index, resolve) {
-          if (resolve.facilityDistributionData.status != SYNCED) return;
-          $scope.message += resolve.facility.code + ' - ' + resolve.facility.name;
-          if (index < resolves.length - 1) $scope.message += ', ';
+
+        var syncedFacilities = _.filter(resolves, function (resolve) {
+          return resolve.facilityDistributionData.status === SYNCED;
         });
-        $scope.message = messageService.get("message.facility.synced.successfully", $scope.message);
-        IndexedDB.put('distributions', distributionData, null, null, $scope.sharedDistributions.update);
+
+        if (!syncedFacilities.length) {
+          $scope.message = 'error.facility.data.already.synced';
+          return;
+        }
+
+        var facilities = [];
+        $(syncedFacilities).each(function (index, resolve) {
+          facilities.push(resolve.facility.code + ' - ' + resolve.facility.name);
+        });
+
+        $scope.message = messageService.get("message.facility.synced.successfully", facilities.join(', '));
       });
     }
   }
-};
-
+}
 
