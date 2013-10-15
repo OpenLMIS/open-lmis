a3e71ad7172c738648261d42a9ee0592edeb77e3
Elias <eliasmu@gmail.com>
2013-10-15 17:04:39 -0400

    ELMIS-832: database changes to accommodate the missing columns

---
+++ modules/migration/src/main/resources/db/migration/V73__add_shipment_column_options.sql
@@ -0,0 +1,16 @@
+insert into shipment_file_columns (datafieldlabel, name, mandatory, include, position)
+	values ('Facility Code', 'facilityCode', false, true, 8);
+insert into shipment_file_columns (datafieldlabel, name, mandatory, include, position)
+	values ('Quantity Ordered', 'quantityOrdered', false, true, 9);
+insert into shipment_file_columns (datafieldlabel, name, mandatory, include, position)
+	values ('Concatenated Order ID (eg. ARV000001R)', 'concatenatedOrderId', false, true, 10);
+insert into shipment_file_columns (datafieldlabel, name, mandatory, include, position)
+	values ('Program Code', 'programCode', false, true, 11);
+insert into shipment_file_columns (datafieldlabel, name, mandatory, include, position)
+	values ('Substituted Product Code', 'substitutedProductCode', false, true, 12);
+insert into shipment_file_columns (datafieldlabel, name, mandatory, include, position)
+	values ('Substituted Product Name', 'substitutedProductName', false, true, 13);
+insert into shipment_file_columns (datafieldlabel, name, mandatory, include, position)
+	values ('Substituted Product Quantity Shipped', 'substitutedProductQuantityShipped', false, true, 14);
+insert into shipment_file_columns (datafieldlabel, name, mandatory, include, position)
+	values ('Pack Size', 'packSize', false, true, 15);
