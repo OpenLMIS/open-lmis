65733867bbdd58a851e8357cb43ad80fdf3b96da
Jagat Sahoo <jagatsah@thoughtworks.com>
2013-10-25 13:00:03 +0530

    #1105 | Jagat | reverting the changes as not working in ie

--- modules/openlmis-web/src/main/webapp/public/js/user/directives/user-list.js
+++ modules/openlmis-web/src/main/webapp/public/js/user/directives/user-list.js
@@ -5,16 +5,8 @@ userModule.directive('userList', function ($timeout) {
       $timeout(function () {
         var listAnchorElements = $(".user-list a");
         listAnchorElements.live("focus", function () {
-          console.log("inside focus");
           $(".user-actions a").hide();
           $(this).parents("li").find(".user-actions a").css("display", "inline-block");
-        });
-        listAnchorElements.live("blur", function () {
-          console.log("inside blur");
-          $timeout(function () {
-            if (!$('.user-list a:focus').length)
-              $(".user-actions a").hide();
-          });
         });
       });
     }
