ccc8cb0bea1384e436dd7259b454d37eb4789927
Elias <eliasmu@gmail.com>
2013-11-01 11:45:16 -0400

    fix copyright text on files added on core module and others

--- modules/openlmis-web/src/main/java/org/openlmis/web/controller/RequisitionGroupMemberController.java
+++ modules/openlmis-web/src/main/java/org/openlmis/web/controller/RequisitionGroupMemberController.java
@@ -1,3 +1,13 @@
+/*
+ * This program was produced for the U.S. Agency for International Development. It was prepared by the USAID | DELIVER PROJECT, Task Order 4. It is part of a project which utilizes code originally licensed under the terms of the Mozilla Public License (MPL) v2 and therefore is licensed under MPL v2 or later.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the Mozilla Public License as published by the Mozilla Foundation, either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Mozilla Public License for more details.
+ *
+ * You should have received a copy of the Mozilla Public License along with this program. If not, see http://www.mozilla.org/MPL/
+ */
+
 package org.openlmis.web.controller;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -26,13 +36,6 @@ import static org.openlmis.web.response.OpenLmisResponse.error;
 import static org.openlmis.web.response.OpenLmisResponse.success;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
-
-/**
- * Created with IntelliJ IDEA.
- * User: Henok
- * Date: 7/6/13
- * Time: 5:28 PM
- */
 
 @Controller
 @NoArgsConstructor
