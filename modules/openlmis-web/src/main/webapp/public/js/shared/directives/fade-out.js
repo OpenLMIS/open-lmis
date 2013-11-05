c100bc46fc740577d80bf05a762237f7b7dc87b4
Manan <manan.bharara@gmail.com>
2013-11-05 09:34:20 +0530

    |#000|+Rahul| Fixing JsHint error for copyright file

--- modules/openlmis-web/src/main/webapp/public/js/shared/directives/fade-out.js
+++ modules/openlmis-web/src/main/webapp/public/js/shared/directives/fade-out.js
@@ -1,6 +1,6 @@
 /*
  * This program is part of the OpenLMIS logistics management information system platform software.
- * Copyright © 2013 VillageReach
+ * Copyright Â© 2013 VillageReach
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
  *
