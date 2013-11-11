68f095bf42c680cc77ddf91a16073f5780a611b8
Raman Kansal <ramankan@thoughtworks.com>
2013-11-11 11:41:14 +0530

    Raman - Added test for updated parent code

--- test-modules/webservice-test/src/test/java/org/openlmis/functional/CreateUpdateCHW.java
+++ test-modules/webservice-test/src/test/java/org/openlmis/functional/CreateUpdateCHW.java
@@ -337,7 +337,7 @@ public class CreateUpdateCHW extends JsonUtility {
     assertEquals(dbWrapper.getFacilityFieldBYCode("virtualFacility", agentCode), TRUE_FLAG);
     assertEquals(dbWrapper.getRequisitionGroupId(firstParentFacility) , dbWrapper.getRequisitionGroupId(agentCode));
 
-      agentJson.setParentFacilityCode(updateParentFacility);
+    agentJson.setParentFacilityCode(updateParentFacility);
 
     ResponseEntity responseEntityUpdated = client.SendJSON(getJsonStringFor(agentJson),
         UPDATE_URL + agentCode + JSON_EXTENSION,
@@ -345,26 +345,26 @@ public class CreateUpdateCHW extends JsonUtility {
         commTrackUser,
         "Admin123");
     assertTrue("Showing response as : " + responseEntityUpdated.getResponse(),
-        responseEntityUpdated.getResponse().contains("{\"success\":\"CHW updated successfully\"}"));
+    responseEntityUpdated.getResponse().contains("{\"success\":\"CHW updated successfully\"}"));
     assertEquals(dbWrapper.getFacilityFieldBYCode(typeId, updateParentFacility), dbWrapper.getFacilityFieldBYCode(typeId, agentCode));
     assertEquals(dbWrapper.getFacilityFieldBYCode(geographicZoneId, updateParentFacility), dbWrapper.getFacilityFieldBYCode(geographicZoneId, agentCode));
     assertEquals(dbWrapper.getFacilityFieldBYCode(id, updateParentFacility), dbWrapper.getFacilityFieldBYCode(parentFacilityId, agentCode));
-    //assertEquals(dbWrapper.getRequisitionGroupId(updateParentFacility) , dbWrapper.getRequisitionGroupId(agentCode));
+    assertEquals(dbWrapper.getRequisitionGroupId(updateParentFacility) , dbWrapper.getRequisitionGroupId(agentCode));
 
-      List<Integer> listOfProgramsSupportedByParentFacility = new ArrayList();
-      listOfProgramsSupportedByParentFacility = dbWrapper.getAllProgramsOfFacility(updateParentFacility);
-      List<Integer> listOfProgramsSupportedByVirtualFacility = new ArrayList();
-      listOfProgramsSupportedByVirtualFacility = dbWrapper.getAllProgramsOfFacility(agentCode);
-      Set<Integer> setOfProgramsSupportedByParentFacility = new HashSet<>();
-      setOfProgramsSupportedByParentFacility.addAll(listOfProgramsSupportedByParentFacility);
-      Set<Integer> setOfProgramsSupportedByVirtualFacility = new HashSet<>();
-      setOfProgramsSupportedByVirtualFacility.addAll(listOfProgramsSupportedByVirtualFacility);
-      assertTrue(setOfProgramsSupportedByParentFacility.equals(setOfProgramsSupportedByVirtualFacility));
-      assertEquals(listOfProgramsSupportedByParentFacility.size(),listOfProgramsSupportedByVirtualFacility.size());
-      for(Integer programId : listOfProgramsSupportedByParentFacility){
-          assertEquals(dbWrapper.getProgramFieldForProgramIdAndFacilityCode(programId, updateParentFacility, "active"), dbWrapper.getProgramFieldForProgramIdAndFacilityCode(programId, agentCode, "active"));
-          assertEquals(dbWrapper.getProgramStartDateForProgramIdAndFacilityCode(programId,updateParentFacility),dbWrapper.getProgramStartDateForProgramIdAndFacilityCode(programId,agentCode));
-      }
+    List<Integer> listOfProgramsSupportedByParentFacility = new ArrayList();
+    listOfProgramsSupportedByParentFacility = dbWrapper.getAllProgramsOfFacility(updateParentFacility);
+    List<Integer> listOfProgramsSupportedByVirtualFacility = new ArrayList();
+    listOfProgramsSupportedByVirtualFacility = dbWrapper.getAllProgramsOfFacility(agentCode);
+    Set<Integer> setOfProgramsSupportedByParentFacility = new HashSet<>();
+    setOfProgramsSupportedByParentFacility.addAll(listOfProgramsSupportedByParentFacility);
+    Set<Integer> setOfProgramsSupportedByVirtualFacility = new HashSet<>();
+    setOfProgramsSupportedByVirtualFacility.addAll(listOfProgramsSupportedByVirtualFacility);
+    assertTrue(setOfProgramsSupportedByParentFacility.equals(setOfProgramsSupportedByVirtualFacility));
+    assertEquals(listOfProgramsSupportedByParentFacility.size(),listOfProgramsSupportedByVirtualFacility.size());
+    for(Integer programId : listOfProgramsSupportedByParentFacility){
+       assertEquals(dbWrapper.getProgramFieldForProgramIdAndFacilityCode(programId, updateParentFacility, "active"), dbWrapper.getProgramFieldForProgramIdAndFacilityCode(programId, agentCode, "active"));
+       assertEquals(dbWrapper.getProgramStartDateForProgramIdAndFacilityCode(programId,updateParentFacility),dbWrapper.getProgramStartDateForProgramIdAndFacilityCode(programId,agentCode));
+    }
   }
 
   @Test(groups = {"webservice"})
