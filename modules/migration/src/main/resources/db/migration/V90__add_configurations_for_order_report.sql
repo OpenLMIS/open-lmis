72dab0af9f92b333c3df93464dbc22042af00bc2
Elias <eliasmu@gmail.com>
2013-11-30 11:24:41 -0500

    WIP - Support customizable order summary report.

---
+++ modules/migration/src/main/resources/db/migration/V90__add_configurations_for_order_report.sql
@@ -0,0 +1,5 @@
+
+
+INSERT INTO configuration_settings (key, name, groupname, description, value, valueType,displayOrder)
+  values ('ORDER_REPORT_ADDRESS', 'Order Report Address','Order Report Settings','',E'To The Managing Director\r\nMedical Stores Ltd.\r\nLusaka\r\nFax numbers : 01-241193,01-246288', 'TEXT_AREA', 13);
+
