03d9e4fd25161e11ede5b97db512a7f1daad67fd
Elias <eliasmu@gmail.com>
2013-12-13 10:21:48 -0500

    ELMIS-844: fix the programming issues caused when uploading product cost/price

--- modules/core/src/main/java/org/openlmis/core/upload/AbstractModelPersistenceHandler.java
+++ modules/core/src/main/java/org/openlmis/core/upload/AbstractModelPersistenceHandler.java
@@ -71,7 +71,7 @@ public abstract class AbstractModelPersistenceHandler implements RecordHandler<I
 
   private void throwExceptionIfProcessedInCurrentUpload(AuditFields auditFields, BaseModel existing) {
     if (existing != null) {
-      if (existing.getModifiedDate().equals(auditFields.getCurrentTimestamp())) {
+      if (auditFields.getCurrentTimestamp().equals(existing.getModifiedDate())) {
         throw new DataException(getMessageKey());
       }
     }
