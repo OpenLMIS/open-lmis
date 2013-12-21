818a997235057913c8c5d20a1e222753ad37402b
shilpi-mittal <shilpimi@thoughtworks.com>
2013-12-21 22:42:46 +0530

    adding copyright statement to LanguageCheck test

--- test-modules/functional-tests/src/test/java/org/openlmis/functional/LanguageCheck.java
+++ test-modules/functional-tests/src/test/java/org/openlmis/functional/LanguageCheck.java
@@ -1,3 +1,13 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2013 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
 package org.openlmis.functional;
 
 import org.openlmis.UiUtils.TestCaseHelper;
@@ -11,14 +21,6 @@ import java.io.IOException;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-
-/**
- * Created with IntelliJ IDEA.
- * User: shilpi
- * Date: 10/10/13
- * Time: 12:09 AM
- * To change this template use File | Settings | File Templates.
- */
 public class LanguageCheck extends TestCaseHelper {
 
     String langSelected;
