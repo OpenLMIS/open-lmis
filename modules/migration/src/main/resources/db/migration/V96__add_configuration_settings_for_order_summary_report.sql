f098f0d76a552e1276084209b4df539a6aa06c8e
Elias <eliasmu@gmail.com>
2013-12-24 15:14:59 -0500

    ELMIS-994: remove the discrepancies section at the end of the report.

---
+++ modules/migration/src/main/resources/db/migration/V96__add_configuration_settings_for_order_summary_report.sql
@@ -0,0 +1,6 @@
+INSERT INTO configuration_settings (key, name, groupname, description, value, valueType,displayOrder)
+  values ('ORDER_SUMMARY_SHOW_SIGNATURE_SPACE_FOR_CUSTOMER', 'Order Report: Show Signature space for customers','Order Report Settings','',E'True', 'BOOLEAN', 14);
+
+
+INSERT INTO configuration_settings (key, name, groupname, description, value, valueType,displayOrder)
+  values ('ORDER_SUMMARY_SHOW_DISCREPANCY_SECTION', 'Order Report: Show space for explanations of discrepancies','Order Report Settings','',E'True', 'BOOLEAN', 15);
