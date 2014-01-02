5a3ba8f742a69dd1b1fd1648f9513d85f59bcc3d
Rahul Kavale <kavale.rahul@gmail.com>
2014-01-02 17:56:08 +0530

    |#779| +Balvinder | added the page for coverage data, added the coverage controller ,WIP

--- modules/openlmis-web/src/main/webapp/public/js/distribution/module/distribution-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/distribution/module/distribution-module.js
@@ -8,32 +8,34 @@
  * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
 var distributionModule = angular.module('distribution',
-    ['openlmis', 'IndexedDB', 'ui.bootstrap.dialog', 'ui.bootstrap.modal']);
+  ['openlmis', 'IndexedDB', 'ui.bootstrap.dialog', 'ui.bootstrap.modal']);
 
 distributionModule.config(['$routeProvider', function ($routeProvider) {
-      $routeProvider.
-          when('/manage',
-          {controller: DistributionController, templateUrl: 'partials/init.html', resolve: DistributionController.resolve}).
-          when('/list', {controller: DistributionListController, templateUrl: 'partials/list.html'}).
-          when('/view-load-amounts/:deliveryZoneId/:programId/:periodId',
-          {controller: ViewLoadAmountController, templateUrl: 'partials/view-load-amount.html', resolve: ViewLoadAmountController.resolve}).
-          when('/record-facility-data/:distribution',
-          {templateUrl: 'partials/record-facility-data.html', resolve: ResolveDistribution}).
-          when('/record-facility-data/:distribution/:facility/refrigerator-data',
-          {controller: RefrigeratorController, templateUrl: 'partials/refrigerator.html', resolve: ResolveDistribution}).
-          when('/record-facility-data/:distribution/:facility/epi-use',
-          {controller: EPIUseController, templateUrl: 'partials/epi-use.html', resolve: ResolveDistribution}).
-          when('/record-facility-data/:distribution/:facility/epi-inventory',
-          {controller: EPIInventoryController, templateUrl: 'partials/epi-inventory.html', resolve: ResolveDistribution}).
-          when('/record-facility-data/:distribution/:facility/facility-visit',
-          {controller: FacilityVisitController, templateUrl: 'partials/facility-visit.html', resolve: ResolveDistribution}).
-          otherwise({redirectTo: '/manage'});
+    $routeProvider.
+      when('/manage',
+      {controller: DistributionController, templateUrl: 'partials/init.html', resolve: DistributionController.resolve}).
+      when('/list', {controller: DistributionListController, templateUrl: 'partials/list.html'}).
+      when('/view-load-amounts/:deliveryZoneId/:programId/:periodId',
+      {controller: ViewLoadAmountController, templateUrl: 'partials/view-load-amount.html', resolve: ViewLoadAmountController.resolve}).
+      when('/record-facility-data/:distribution',
+      {templateUrl: 'partials/record-facility-data.html', resolve: ResolveDistribution}).
+      when('/record-facility-data/:distribution/:facility/refrigerator-data',
+      {controller: RefrigeratorController, templateUrl: 'partials/refrigerator.html', resolve: ResolveDistribution}).
+      when('/record-facility-data/:distribution/:facility/epi-use',
+      {controller: EPIUseController, templateUrl: 'partials/epi-use.html', resolve: ResolveDistribution}).
+      when('/record-facility-data/:distribution/:facility/epi-inventory',
+      {controller: EPIInventoryController, templateUrl: 'partials/epi-inventory.html', resolve: ResolveDistribution}).
+      when('/record-facility-data/:distribution/:facility/coverage',
+      {controller: CoverageController, templateUrl: 'partials/coverage.html', resolve: ResolveDistribution}).
+      when('/record-facility-data/:distribution/:facility/facility-visit',
+      {controller: FacilityVisitController, templateUrl: 'partials/facility-visit.html', resolve: ResolveDistribution}).
+      otherwise({redirectTo: '/manage'});
 
-    }]).config(function (IndexedDBProvider) {
-      IndexedDBProvider
-          .setDbName("open_lmis")
-          .migration(4, migrationFunc);
-    });
+  }]).config(function (IndexedDBProvider) {
+    IndexedDBProvider
+      .setDbName("open_lmis")
+      .migration(4, migrationFunc);
+  });
 
 distributionModule.directive('notRecorded', function ($timeout) {
   return {
