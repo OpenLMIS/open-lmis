9c32b234829f5e218e04f8b230b9f246f292c63f
Anshul Rastogi <anshulr@thoughtworks.com>
2014-01-29 11:57:21 +0530

    |#00| fixing forgot password FTs

--- test-modules/functional-tests/src/test/java/org/openlmis/functional/ForgotPassword.java
+++ test-modules/functional-tests/src/test/java/org/openlmis/functional/ForgotPassword.java
@@ -11,8 +11,6 @@
 package org.openlmis.functional;
 
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
