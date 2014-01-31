ef1efea52701654e214b13f387832b5fb7c74b81
Elias <eliasmu@gmail.com>
2014-01-31 14:55:10 -0500

    add stored procedure to delete rnr

--- modules/migration/src/main/resources/db/migration/V100__create_sproc_delete_rnr.sql
+++ modules/migration/src/main/resources/db/migration/V100__create_sproc_delete_rnr.sql
@@ -31,41 +31,41 @@ DECLARE v_rli_id integer;
 
 DECLARE msg character varying(2000);
     
-BEGIN
-  li := 0;
-  msg := 'Requisition id ' || in_rnrid || ' not found. No record deleted.';
+  BEGIN
+    li := 0;
+    msg := 'Requisition id ' || in_rnrid || ' not found. No record deleted.';
 
-  select id into v_rnr_id from requisitions where id = in_rnrid;
+    select id into v_rnr_id from requisitions where id = in_rnrid;
 
-  if v_rnr_id > 0 then
-   msg = 'Requisition id ' || in_rnrid || ' deleted successfully.';
+    if v_rnr_id > 0 then
+     msg = 'Requisition id ' || in_rnrid || ' deleted successfully.';
 
 
--- delete losses and adjustments
-DELETE  FROM  requisition_line_item_losses_adjustments where requisitionlineitemid in (select id from requisition_line_items where rnrid in (select id from requisitions where id = v_rnr_id));
+  -- delete losses and adjustments
+  DELETE  FROM  requisition_line_item_losses_adjustments where requisitionlineitemid in (select id from requisition_line_items where rnrid in (select id from requisitions where id = v_rnr_id));
 
-select id into li from requisition_line_items where rnrid = in_rnrid limit 1;
-    
-  -- delete requisition line items 
-  if li > 0 then
-   DELETE FROM requisition_line_items WHERE rnrid= in_rnrid;   
+  select id into li from requisition_line_items where rnrid = in_rnrid limit 1;
+
+    -- delete requisition line items
+    if li > 0 then
+     DELETE FROM requisition_line_items WHERE rnrid= in_rnrid;
+    end if;
+    --
+     DELETE FROM requisition_status_changes where rnrid = v_rnr_id;
+     DELETE FROM regimen_line_items where rnrid = v_rnr_id;
+     DELETE FROM orders where id = v_rnr_id;
+     DELETE FROM comments where rnrid = v_rnr_id;
+     DELETE FROM requisitions WHERE id= in_rnrid;
   end if;
-  -- 
-   DELETE FROM requisition_status_changes where rnrid = v_rnr_id;  
-   DELETE FROM regimen_line_items where rnrid = v_rnr_id;
-   DELETE FROM orders where id = v_rnr_id;  
-   DELETE FROM comments where rnrid = v_rnr_id;
-   DELETE FROM requisitions WHERE id= in_rnrid;
-end if;
 
-  --COMMIT;
-  
-  RETURN msg;
-   EXCEPTION WHEN OTHERS THEN
-   --ROLLBACK;
-    RETURN 'Error in deleting requisition id ' || in_rnrid || '. Please consult database administrtor.';
+    --COMMIT;
 
-END;
+    RETURN msg;
+     EXCEPTION WHEN OTHERS THEN
+     --ROLLBACK;
+      RETURN 'Error in deleting requisition id ' || in_rnrid || '. Please consult database administrtor.';
+
+  END;
 $BODY$
 LANGUAGE plpgsql VOLATILE
 COST 100;
