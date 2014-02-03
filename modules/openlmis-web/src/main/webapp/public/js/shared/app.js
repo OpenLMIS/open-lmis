a76d73ee98c40c3c6523abb829251599ce3fc098
Manan <manan.bharara@gmail.com>
2014-02-03 12:08:11 +0530

    |#1294| Checking on numeric validator regex only if value exists and length greater than zero

--- modules/openlmis-web/src/main/webapp/public/js/shared/app.js
+++ modules/openlmis-web/src/main/webapp/public/js/shared/app.js
@@ -179,10 +179,12 @@ app.integer = function (value, errorHolder) {
 
 
 app.positiveInteger = function (value, errorHolder) {
-  var POSITIVE_INTEGER_REGEXP_FIXED_LENGTH = /^[0-9]*$/;
+  var POSITIVE_INTEGER_REGEXP_FIXED_LENGTH = /^[0-9]*$/, valid;
 
-  var valid = isUndefined(value);
-  if (value.toString().length > 0) valid = POSITIVE_INTEGER_REGEXP_FIXED_LENGTH.test(value);
+  if (isUndefined(value))
+    valid = true;
+  else if (value.toString().length > 0)
+    valid = POSITIVE_INTEGER_REGEXP_FIXED_LENGTH.test(value);
 
   if (errorHolder !== undefined && document.getElementById(errorHolder) !== null) {
     document.getElementById(errorHolder).style.display = (valid) ? 'none' : 'block';
