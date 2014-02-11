973a454ce75881ff2778f7b5718ad93fd5db6d56
Anshul Rastogi <anshulr@thoughtworks.com>
2014-02-11 17:25:35 +0530

    |#1271| +Riya| calculating coverage rate, wastage rate for childCoverageLineitems, adding jasmine specs for the same.

--- modules/openlmis-web/src/main/webapp/public/js/distribution/controller/child-coverage-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/distribution/controller/child-coverage-controller.js
@@ -14,6 +14,26 @@ function ChildCoverageController($scope, $routeParams, distributionService) {
   $scope.selectedFacilityId = $routeParams.facility;
   $scope.childCoverage = $scope.distribution.facilityDistributions[$scope.selectedFacilityId].childCoverage;
 
+  var getValue = function (obj) {
+    return (!isUndefined(obj)) ? ((!isUndefined(obj.value)) ? parseInt(obj.value, 10) : 0) : 0;
+  };
+
+  var syncNR = function (product1, product2) {
+    if (!isUndefined($scope.openedVialMap[product2].openedVial))
+      $scope.openedVialMap[product1].openedVial = {notRecorded: $scope.openedVialMap[product2].openedVial.notRecorded};
+  };
+
+  var convertListToMap = function (list, key) {
+    var map = {};
+    list.forEach(function (lineItem) {
+      map[lineItem[key]] = lineItem;
+    });
+    return map;
+  };
+
+  $scope.childCoverageMap = convertListToMap($scope.childCoverage.childCoverageLineItems, 'vaccination');
+  $scope.openedVialMap = convertListToMap($scope.childCoverage.openedVialLineItems, 'productVialName');
+
   $scope.columns = {
     vaccination: "label.child.vaccination.doses",
     targetGroup: "label.child.coverage.target.Group",
@@ -34,35 +54,72 @@ function ChildCoverageController($scope, $routeParams, distributionService) {
   $scope.productsMap = {
     "BCG": {
       products: ['BCG'],
+      vaccinations: ['BCG'],
       rowSpan: 1
     },
     "Polio (Newborn)": {
       products: ['Polio10', 'Polio20'],
+      vaccinations: ['Polio (Newborn)', 'Polio 1st dose', 'Polio 2nd dose', 'Polio 3rd dose'],
       rowSpan: 4
     },
     "Penta 1st dose": {
       products: ['Penta1', 'Penta10'],
+      vaccinations: ['Penta 1st dose', 'Penta 2nd dose', 'Penta 3rd dose'],
       rowSpan: 3
     },
     "PCV10 1st dose": {
       products: ['PCV'],
+      vaccinations: ['PCV10 1st dose', 'PCV10 1st dose', 'PCV10 1st dose'],
       rowSpan: 3
     },
     "Measles": {
       products: ['Measles'],
+      vaccinations: ['Measles'],
       rowSpan: 1
     }
   };
 
-  $scope.getTotal = function (value1, value2) {
-    return getValue(value1) + getValue(value2);
-  };
-
-  var getValue = function (value) {
-    return (!isUndefined(value)) ? parseInt(value, 10) : 0;
-  };
-
   $scope.hideCell = function (vaccination) {
     return Object.keys($scope.productsMap).indexOf(vaccination) === -1;
+  };
+
+  $scope.$watch("openedVialMap['Polio20'].openedVial.notRecorded", function () {
+    syncNR('Polio10', 'Polio20');
+  });
+
+  $scope.$watch("openedVialMap['Penta10'].openedVial.notRecorded", function () {
+    syncNR('Penta1', 'Penta10');
+  });
+
+  $scope.getTotal = function (obj1, obj2) {
+    return getValue(obj1) + getValue(obj2);
+  };
+
+  $scope.getTotalVaccinations = function (childCoverageLineItem) {
+    return $scope.getTotal(childCoverageLineItem.healthCenter11Months, childCoverageLineItem.outReach11Months) +
+        $scope.getTotal(childCoverageLineItem.healthCenter23Months, childCoverageLineItem.outReach23Months);
+  };
+
+  $scope.calculateCoverageRate = function (total, targetGroup) {
+    return (isUndefined(targetGroup) ? null : (targetGroup === 0 ? null : Math.round((total / targetGroup) * 100)));
+  };
+
+  $scope.calculateWastageRate = function (productsForVaccination) {
+    var totalDosesConsumed = 0;
+    var totalVaccinations = 0;
+    if (!isUndefined(productsForVaccination)) {
+      $.each(productsForVaccination.products, function (index, product) {
+        var openedVialLineItem = $scope.openedVialMap[product];
+        if (!isUndefined(openedVialLineItem.packSize) && !isUndefined(openedVialLineItem.openedVial) && !isUndefined(openedVialLineItem.openedVial.value)) {
+          totalDosesConsumed += openedVialLineItem.packSize * openedVialLineItem.openedVial.value;
+        }
+      });
+
+      $.each(productsForVaccination.vaccinations, function (index, vaccination) {
+        var childCoverageLineItem = $scope.childCoverageMap[vaccination];
+        totalVaccinations += $scope.getTotalVaccinations(childCoverageLineItem);
+      });
+    }
+    return totalDosesConsumed === 0 ? null : Math.round(((totalDosesConsumed - totalVaccinations) / totalDosesConsumed) * 100);
   };
 }
