66351ee0ed48ae69554ed8574ff3a7bae84ed364
Wolde Fikadu <memiwo@gmail.com>
2014-02-18 13:45:41 +0300

    Admin Dashboard Filter Params

--- modules/openlmis-web/src/main/webapp/public/js/dashboard/controller/dashboard-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/dashboard/controller/dashboard-controller.js
@@ -9,7 +9,138 @@
  * You should have received a copy of the Mozilla Public License along with this program. If not, see http://www.mozilla.org/MPL/
  */
 
-function AdminDashboardController($scope) {
+function AdminDashboardController($scope,ReportPrograms, ReportSchedules, ReportPeriods, RequisitionGroupsByProgram, ReportProductsByProgram, OperationYears, ReportPeriodsByScheduleAndYear, RequisitionGroupsByProgramSchedule) {
+
+    $scope.filterObject = {};
+
+    $scope.startYears = [];
+
+    OperationYears.get(function (data) {
+        $scope.startYears = data.years;
+        $scope.startYears.unshift('-- All Years --');
+    });
+
+    ReportPrograms.get(function (data) {
+        $scope.programs = data.programs;
+        $scope.programs.unshift({'name': '-- Select Programs --'});
+    });
+
+    ReportSchedules.get(function(data){
+        $scope.schedules = data.schedules;
+        $scope.schedules.unshift({'name':'-- Select a Schedule --', 'id':'0'}) ;
+
+    });
+
+    $scope.$watch('programId', function(selection){
+        if(selection !== undefined || selection === ""){
+            if (selection === '') {
+                return;
+            }
+
+            ReportProductsByProgram.get({programId: selection}, function(data){
+                $scope.products = data.productList;
+            });
+
+            RequisitionGroupsByProgram.get({program: selection }, function(data){
+                $scope.requisitionGroups = data.requisitionGroupList;
+                $scope.requisitionGroups.unshift({'name':'-- All Requisition Groups --'});
+            });
+        }
+    });
+
+
+
+    $scope.ChangeSchedule = function(scheduleBy){
+        if(scheduleBy == 'byYear'){
+
+            ReportPeriodsByScheduleAndYear.get({scheduleId: $scope.filterObject.scheduleId, year: $scope.filterObject.year}, function(data){
+                $scope.periods = data.periods;
+                $scope.periods.unshift({'name':'-- Select a Period --','id':'0'});
+            });
+
+        }else{
+
+            ReportPeriods.get({ scheduleId : $scope.filterObject.scheduleId },function(data) {
+                $scope.periods = data.periods;
+                $scope.periods.unshift({'name':'-- Select a Period --','id':'0'});
+
+            });
+        }
+    };
+
+    $scope.$watch('rgroupId', function (selection) {
+        if (selection == "All") {
+            $scope.filterObject.rgroupId = -1;
+        } else if (selection !== undefined || selection === "") {
+            $scope.filterObject.rgroupId = selection;
+            $.each($scope.requisitionGroups, function (item, idx) {
+                if (idx.id == selection) {
+                    $scope.filterObject.rgroup = idx.name;
+                }
+            });
+        } else {
+            $scope.filterObject.rgroupId = 0;
+        }
+       // $scope.filterGrid();
+    });
+
+    $scope.$watch('periodId', function (selection) {
+        if (selection == "All") {
+            $scope.filterObject.periodId = -1;
+        } else if (selection !== undefined || selection === "") {
+            $scope.filterObject.periodId = selection;
+            $.each($scope.periods, function (item, idx) {
+                if (idx.id == selection) {
+                    $scope.filterObject.period = idx.name;
+                }
+            });
+
+        } else {
+            $scope.filterObject.periodId = 0;
+        }
+       // $scope.filterGrid();
+    });
+
+
+    $scope.$watch('scheduleId', function (selection) {
+        if (selection == "All") {
+            $scope.filterObject.scheduleId = -1;
+        } else if (selection !== undefined || selection === "") {
+            $scope.filterObject.scheduleId = selection;
+            $.each($scope.schedules , function (item, idx) {
+                if (idx.id == selection) {
+                    $scope.filterObject.schedule = idx.name;
+                }
+            });
+
+        } else {
+            $scope.filterObject.scheduleId = 0;
+        }
+        $scope.ChangeSchedule('');
+
+    });
+
+
+    $scope.$watch('year', function (selection) {
+
+        if (selection == "-- All Years --") {
+            $scope.filterObject.year = -1;
+        } else if (selection !== undefined || selection === "") {
+            $scope.filterObject.year = selection;
+
+        } else {
+            $scope.filterObject.year = 0;
+        }
+
+        if($scope.filterObject.year === -1 || $scope.filterObject.year === 0){
+
+            $scope.ChangeSchedule('bySchedule');
+        }else{
+
+            $scope.ChangeSchedule('byYear');
+        }
+    });
+
 
     /* Bar Chart */
     var barChartTicks = [[1, "Tab1"], [2, "Tab2"], [3, "Tab3"],[4, "Tab4"],[5, "Tab5"]];
