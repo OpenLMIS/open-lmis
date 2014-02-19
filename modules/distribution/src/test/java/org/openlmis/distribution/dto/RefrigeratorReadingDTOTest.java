1448a019d3fe68466c5c03ad71459d37588336be
Riya Kataria <riyakata@thoughtworks.com>
2014-02-19 15:20:37 +0530

    |#1327| Changing functioningCorrectly value to 'N' in case of refrigerator problems in RefrigeratorReadingDTOTest

--- modules/distribution/src/test/java/org/openlmis/distribution/dto/RefrigeratorReadingDTOTest.java
+++ modules/distribution/src/test/java/org/openlmis/distribution/dto/RefrigeratorReadingDTOTest.java
@@ -30,7 +30,7 @@ public class RefrigeratorReadingDTOTest {
   public void shouldTransformRefrigeratorReadingToReading() throws Exception {
     Long facilityVisitId = 5L;
     Reading temperature = new Reading("32.4", false);
-    Reading functioningCorrectly = new Reading("Y", false);
+    Reading functioningCorrectly = new Reading("N", false);
     Reading lowAlarmEvents = new Reading("2", false);
     Reading highAlarmEvents = new Reading("", true);
     Reading problemSinceLastTime = new Reading("Y", false);
@@ -39,7 +39,7 @@ public class RefrigeratorReadingDTOTest {
     Long facilityId = 2L;
 
     RefrigeratorReading expectedReading = new RefrigeratorReading(new Refrigerator("brand", "model", "serial number", facilityId, true), facilityVisitId, 32.4F,
-      "Y", 2, null, "Y", problems, notes);
+      "N", 2, null, "Y", problems, notes);
 
     RefrigeratorReadingDTO refrigeratorReadingDTO = new RefrigeratorReadingDTO(new Refrigerator("brand", "model", "serial number", facilityId, true),
       facilityVisitId,
@@ -60,7 +60,7 @@ public class RefrigeratorReadingDTOTest {
   public void shouldValidateRefrigeratorProblemsIfProblemSinceLastVisitIsTrue() throws Exception {
     Long facilityVisitId = 5L;
     Reading temperature = new Reading("32.4", false);
-    Reading functioningCorrectly = new Reading("Y", false);
+    Reading functioningCorrectly = new Reading("N", false);
     Reading lowAlarmEvents = new Reading("2", false);
     Reading highAlarmEvents = new Reading("", true);
     Reading problemSinceLastTime = new Reading("Y", false);
@@ -69,7 +69,7 @@ public class RefrigeratorReadingDTOTest {
     Long facilityId = 2L;
 
     RefrigeratorReading expectedReading = new RefrigeratorReading(new Refrigerator("brand", "model", "serial number", facilityId, true), facilityVisitId, 32.4F,
-      "Y", 2, null, "Y", problems, notes);
+      "N", 2, null, "Y", problems, notes);
 
     RefrigeratorReadingDTO refrigeratorReadingDTO = new RefrigeratorReadingDTO(new Refrigerator("brand", "model", "serial number", facilityId, true),
       facilityVisitId,
