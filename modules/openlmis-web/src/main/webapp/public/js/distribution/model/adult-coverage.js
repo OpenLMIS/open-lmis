373343bead5bc4910cad8b6c8b315535481eec61
Balvinder Khurana <balvindk@thoughtworks.com>
2014-02-21 15:26:47 +0530

    #0000| checking is line item is undefined

--- modules/openlmis-web/src/main/webapp/public/js/distribution/model/adult-coverage.js
+++ modules/openlmis-web/src/main/webapp/public/js/distribution/model/adult-coverage.js
@@ -19,7 +19,7 @@ function AdultCoverage(facilityVisitId, adultCoverageJSON) {
 
 }
 AdultCoverage.prototype.wastageRate = function (openedVialLineItem) {
-  if (isUndefined(openedVialLineItem.value) || isUndefined(openedVialLineItem.packSize)) return null;
+  if (isUndefined(openedVialLineItem) || isUndefined(openedVialLineItem.value) || isUndefined(openedVialLineItem.packSize)) return null;
   var totalDosesConsumed = openedVialLineItem.value * openedVialLineItem.packSize;
   if (totalDosesConsumed === 0) return null;
   return Math.round((totalDosesConsumed - this.totalTetanus()) / totalDosesConsumed * 100);
