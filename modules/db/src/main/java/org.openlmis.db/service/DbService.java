4668d04510745f36df1f5edc95e06715982edf60
Balvinder Khurana <balvindk@thoughtworks.com>
2014-02-28 12:48:08 +0530

    |#1341 | Documentation for db module

--- modules/db/src/main/java/org.openlmis.db/service/DbService.java
+++ modules/db/src/main/java/org.openlmis.db/service/DbService.java
@@ -17,6 +17,9 @@ import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
+/**
+ * This service exposes methods for basic database utilities
+ */
 @Service
 @NoArgsConstructor
 public class DbService {
