7c0beee06c7defb096e63560ae95b13ca3f60352
Wolde Fikadu <memiwo@gmail.com>
2014-03-01 21:40:14 +0300

    Dashboard controller test spec

--- modules/openlmis-web/src/test/javascript/specs/dashboard/controller/dashboard-controller-test.js
+++ modules/openlmis-web/src/test/javascript/specs/dashboard/controller/dashboard-controller-test.js
@@ -7,20 +7,42 @@
  */
 
 describe("Dashboard Controller",function (){
-    var scope, ctrl, httpBackend, location, facilities, controller;
+    var scope,rootScope, ctrl, httpBackend, location, facilities;
 
     beforeEach(module('openlmis'));
-    beforeEach(inject(function ($httpBackend, $rootScope, $location, $controller, $routeParams) {
+    beforeEach(inject(function ($httpBackend, $rootScope, $location, $controller) {
         scope = $rootScope.$new();
+        rootScope = $rootScope;
+        rootScope.hasPermission = function () {
+            return true;
+        };
         location = $location;
         httpBackend = $httpBackend;
         facilities = [
-            {"id": "10134", "name": "National Warehouse", "description": null}
+            {"id": "10134", "name": "National Warehouse", "description": null, "geographicZone" :{"id": 505}}
         ];
         ctrl = $controller(AdminDashboardController, {$scope: scope, $rootScope: rootScope});
 
     }));
 
-    it('Should')
+  /*  it('should load item fill rate data for selected facility ', function (){
+        scope.filterObject = {};
+        scope.filterObject.facilityId =  10135;
+        scope.filterObject.geographicZoneId = facilities[0].geographicZone.id;
+        scope.filterObject.productIdList = [211,234];
+        scope.filterObject.programId = 1;
+
+        var itemFillRates = [
+            {"product": "product 1", "fillRate": 45},
+            {"product": "product 2", "fillRate": -55}
+        ];
+
+        httpBackend.expectGET('/dashboard/itemFillRate.json?geographicZoneId=505&facilityId=10135&programId=1&periodId=4&productIdList='+scope.filterObject.productIdList).respond({"itemFillRate": itemFillRates});
+        scope.loadFillRates();
+
+        httpBackend.flush();
+
+        expect(scope.itemFills).toEqual(itemFillRates);
+    });*/
 
 });
