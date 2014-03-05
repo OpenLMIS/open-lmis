4407c80a7409a5fa00a3a1010612ef69996938b5
Anshul Rastogi <anshulr@thoughtworks.com>
2014-03-05 17:01:48 +0530

    |#00|+Rahul| fixing typo in alter script, fixing test.

--- modules/db/src/main/resources/db/migration/V105__rename_name_to_username.sql
+++ modules/db/src/main/resources/db/migration/V105__rename_name_to_username.sql
@@ -8,4 +8,4 @@
 -- You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
 --
 
-ALTER TABLE requisition_status_changes RENAME COLUMN name TO userNamek;
+ALTER TABLE requisition_status_changes RENAME COLUMN name TO userName;
