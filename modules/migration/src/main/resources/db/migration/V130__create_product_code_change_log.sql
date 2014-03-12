05af7ada8b84259783f798565ef70442d8a96df6
Muhammad Ahmed <muhammad_ahmed@jsi.com>
2014-03-12 13:44:25 -0400

    scrips to update product codes

---
+++ modules/migration/src/main/resources/db/migration/V130__create_product_code_change_log.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS product_code_change_log;
+
+CREATE TABLE product_code_change_log
+(
+  program character varying(4),
+  old_code character varying(12),
+  new_code character varying(12),
+  product character varying(200),
+  unit character varying(200),
+  changeddate timestamp,
+  migrated boolean DEFAULT 'N'
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE product_code_change_log
+  OWNER TO postgres;
+
+
+
