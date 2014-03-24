14a0af651a1141e0418ec7f74419abd723775ee9
Riya Kataria <riyakata@thoughtworks.com>
2014-03-24 10:35:56 +0530

    |#000 | Moving UserToken entity to domain directory (Authentication module)

--- modules/authentication/src/main/java/org/openlmis/authentication/domain/UserToken.java
+++ modules/authentication/src/main/java/org/openlmis/authentication/domain/UserToken.java
@@ -8,7 +8,7 @@
  * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
 
-package org.openlmis.authentication;
+package org.openlmis.authentication.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
