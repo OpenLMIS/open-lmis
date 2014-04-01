4a1144c7a30e766d07cb5cbc2b89bf7740fac63e
Elias <eliasmu@gmail.com>
2014-04-01 10:39:37 -0400

    added isActive flag on user_preference_master

--- modules/migration/src/main/resources/db/migration/V149__create_user_preferance_tables.sql
+++ modules/migration/src/main/resources/db/migration/V149__create_user_preferance_tables.sql
@@ -1,16 +1,17 @@
 CREATE TABLE user_preference_master
 (
-  id                        INT PRIMARY KEY,
+  id                        SERIAL PRIMARY KEY,
   key                       VARCHAR(50) NOT NULL UNIQUE ,
   name                      VARCHAR(50) NOT NULL,
   groupName                 VARCHAR(50),
-  groupDisplayOrder         INT,
+  groupDisplayOrder         INT DEFAULT 1,
   displayOrder              INT,
   description               VARCHAR(2000),
   entityType                VARCHAR(50),
   inputType                 VARCHAR(50),
   dataType                  VARCHAR(50),
   defaultValue              VARCHAR(2000),
+  isActive                  BOOLEAN DEFAULT TRUE,
 
   createdBy                 INTEGER,
   createdDate               TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
