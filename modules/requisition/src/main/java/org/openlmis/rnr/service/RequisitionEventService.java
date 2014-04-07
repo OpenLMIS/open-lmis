76b1a41ddd2df02861e6e2f9987effd0e92c8b04
Anshul Rastogi <anshulr@thoughtworks.com>
2014-04-07 16:12:10 +0530

    |#1378| Balvinder | adding service to send email to concerned users on requisition status change

--- modules/requisition/src/main/java/org/openlmis/rnr/service/RequisitionEventService.java
+++ modules/requisition/src/main/java/org/openlmis/rnr/service/RequisitionEventService.java
@@ -11,13 +11,22 @@
 package org.openlmis.rnr.service;
 
 import org.ict4h.atomfeed.server.service.EventService;
+import org.openlmis.core.domain.User;
 import org.openlmis.core.exception.DataException;
+import org.openlmis.core.service.MessageService;
+import org.openlmis.email.service.EmailService;
 import org.openlmis.rnr.domain.Rnr;
+import org.openlmis.rnr.domain.RnrStatus;
 import org.openlmis.rnr.event.RequisitionStatusChangeEvent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
 import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Exposes the services for handling event of change in rnr status.
@@ -26,14 +35,45 @@ import java.net.URISyntaxException;
 @Service
 public class RequisitionEventService {
 
+  private static Map<RnrStatus, String> actionUrlMap = new HashMap<>();
+
+  static {
+    actionUrlMap.put(RnrStatus.SUBMITTED, "/public/pages/rnr");
+    actionUrlMap.put(RnrStatus.AUTHORIZED, "");
+    actionUrlMap.put(RnrStatus.IN_APPROVAL, "");
+    actionUrlMap.put(RnrStatus.APPROVED, "");
+  }
+
   @Autowired
-  EventService eventService;
+  private EventService eventService;
+
+  @Autowired
+  private MessageService messageService;
+
+  @Autowired
+  private EmailService emailService;
+
+  @Value("${mail.base.url}")
+  private String baseUrl;
 
   public void notifyForStatusChange(Rnr requisition) {
     try {
       eventService.notify(new RequisitionStatusChangeEvent(requisition));
     } catch (URISyntaxException e) {
       throw new DataException("error.malformed.uri");
+    }
+  }
+
+  public void notifyUsers(Rnr requisition, List<User> users) {
+    SimpleMailMessage mailMessage = new SimpleMailMessage();
+    mailMessage.setSubject(messageService.message("msg.email.notification.subject"));
+    String actionUrl = baseUrl + actionUrlMap.get(requisition.getStatus());
+    for (User user : users) {
+      mailMessage.setTo(user.getEmail());
+      mailMessage.setText(messageService.message("msg.email.notification.body", user.getUserName(),
+        requisition.getFacility().getName(), requisition.getPeriod().getName(), user.getUserName(),
+        actionUrl));
+      emailService.send(mailMessage);
     }
   }
 }
