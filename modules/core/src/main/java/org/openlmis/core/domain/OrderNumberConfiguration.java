a6d9fbe36c4163c3c28969cd3c98f20aa31da49c
Balvinder Khurana <balvindk@thoughtworks.com>
2014-04-15 17:00:38 +0530

    # 1408| Adding table, corresponding mappers and POJO for order configuration

---
+++ modules/core/src/main/java/org/openlmis/core/domain/OrderNumberConfiguration.java
@@ -0,0 +1,29 @@
+/*
+ * This program is part of the OpenLMIS logistics management information system platform software.
+ * Copyright © 2013 VillageReach
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
+ */
+
+package org.openlmis.core.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderNumberConfiguration {
+
+  private String orderNumberPrefix;
+  private Boolean includeOrderNumberPrefix;
+  private Boolean includeProgramCode;
+  private Boolean includeSequenceCode;
+  private Boolean includeRnrTypeSuffix;
+
+}
+
