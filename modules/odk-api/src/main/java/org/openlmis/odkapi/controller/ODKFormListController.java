01eb9f68fe05acc95faa41742b70d766b3c7f1b9
Messay Yohannes <deliasmes@gmail.com>
2014-04-25 03:55:31 +0300

    Removed test code

--- modules/odk-api/src/main/java/org/openlmis/odkapi/controller/ODKFormListController.java
+++ modules/odk-api/src/main/java/org/openlmis/odkapi/controller/ODKFormListController.java
@@ -41,8 +41,6 @@ public class ODKFormListController extends  BaseController
     @ResponseBody
     public ResponseEntity<ODKXFormList> getAvailableForms(HttpServletRequest httpServletRequest)
     {
-        System.out.println("when getting form list "+"================"+httpServletRequest.getParameter("deviceID"));
-
         ODKXFormList odkxFormListXML = odkxFormService.getAvailableXFormDefinitions();
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set(OPEN_ROSA_VERSION_HEADER, OPEN_ROSA_VERSION);
@@ -54,7 +52,6 @@ public class ODKFormListController extends  BaseController
     @ResponseBody
     public ResponseEntity<String> getForm(@PathVariable String formId, HttpServletRequest httpServletRequest)
     {
-        System.out.println("when getting a form "+"================"+httpServletRequest.getParameter("deviceID"));
         odkxForm = odkxFormService.getXFormByFormId(formId);
         HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.set(OPEN_ROSA_VERSION_HEADER, OPEN_ROSA_VERSION);
