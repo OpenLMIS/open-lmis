a2d63f2ffc3547d895e38e376bd3db1da8fe0c1f
hassan <hhasssan@tz.pfscm.org>
2014-05-15 18:30:18 +0300

    ELMIS-1132:Add copy right  in Regimen Summary Report file

--- modules/openlmis-web/src/main/webapp/public/js/reports/regimen-summary/controller/regimen-summary-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/regimen-summary/controller/regimen-summary-controller.js
@@ -1,16 +1,14 @@
-function RegimenSummaryControllers($scope, $filter, ngTableParams,
-                                   RegimenSummaryReport , $window,DistrictFinancialSummaryReport,ReportRegimens,ReportRegimenCategories,RequisitionGroupsByProgram,ReportRegimensByCategory,ReportPeriodsByScheduleAndYear,ReportSchedules,ReportRegimenPrograms,ReportPeriods, OperationYears, SettingsByKey,localStorageService, $http, $routeParams, $location) {
+/*
+ * This program was produced for the U.S. Agency for International Development. It was prepared by the USAID | DELIVER PROJECT, Task Order 4. It is part of a project which utilizes code originally licensed under the terms of the Mozilla Public License (MPL) v2 and therefore is licensed under MPL v2 or later.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the Mozilla Public License as published by the Mozilla Foundation, either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Mozilla Public License for more details.
+ *
+ * You should have received a copy of the Mozilla Public License along with this program. If not, see http://www.mozilla.org/MPL/
+ */
 
-    $scope.OnFilterChanged = function(){
-        // clear old data if there was any
-        $scope.data = $scope.datarows = [];
-        RegimenSummaryReport.get($scope.filter, function (data) {
-            if (data.pages !== undefined && data.pages.rows !== undefined) {
-                $scope.data = data.pages.rows;
-               $scope.paramsChanged($scope.tableParams);
-            }
-        });
-    };
+function RegimenSummaryControllers($scope,$window,RegimenSummaryReport) {
 
     $scope.exportReport = function (type) {
         $scope.filter.pdformat = 1;
@@ -19,6 +17,17 @@ function RegimenSummaryControllers($scope, $filter, ngTableParams,
         $window.open(url);
     };
 
-
+    $scope.OnFilterChanged = function() {
+        // clear old data if there was any
+        $scope.data = $scope.datarows = [];
+        $scope.filter.max = 10000;
+        RegimenSummaryReport.get($scope.filter, function(data) {
+            if (data.pages !== undefined && data.pages.rows !== undefined) {
+                $scope.data = data.pages.rows;
+               // alert(JSON.stringify($scope.data));
+               $scope.paramsChanged($scope.tableParams);
+            }
+        });
+    };
 
 }
