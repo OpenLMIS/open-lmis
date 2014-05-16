05cee770ee5b484836c3fd50d9f5e24e0ac38de8
Wolde Fikadu <memiwo@gmail.com>
2014-05-16 16:24:44 +0300

    Dashboard alerts view Requisition details

--- modules/openlmis-web/src/main/webapp/public/js/dashboard/controller/notifications-detail-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/dashboard/controller/notifications-detail-controller.js
@@ -49,26 +49,6 @@ function NotificationsDetailController($scope,$routeParams,messageService,Settin
 
     }
 
-   /* var newWin = null;
-    $scope.popUp = function popUp(strURL, strType, strHeight, strWidth) {
-        if (newWin != null && !newWin.closed) {
-            newWin.close();
-        }
-        var strOptions="";
-        if (strType=="console") {
-            strOptions="resizable,height="+strHeight+",width="+strWidth;
-        }
-        if (strType=="fixed") {
-            strOptions="status,height="+strHeight+",width="+strWidth;
-        }
-        if (strType=="elastic") {
-            strOptions="toolbar,menubar,scrollbars,"+"resizable,location,height="+strHeight+",width="+strWidth;
-        }
-        newWin = window.open(strURL, 'newWin', strOptions);
-        newWin.focus();
-
-    }*/
-
     $scope.resetNotificationData = function(){
         $scope.notificationDetail.datarows = null;
     };
