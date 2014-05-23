3c641dd5d04445558f72e1b4cb77d344c02b10e3
Wolde Fikadu <memiwo@gmail.com>
2014-05-23 12:12:28 +0300

    fix migration script version number

--- modules/migration/src/main/resources/db/migration/V223__create_fn_get_reporting_status_by_facilityid_programid_and_periodid.sql
+++ modules/migration/src/main/resources/db/migration/V223__create_fn_get_reporting_status_by_facilityid_programid_and_periodid.sql
@@ -12,17 +12,17 @@ BEGIN
 
 select facilityid from requisitions where facilityid = v_facilityid and programid = v_programid and periodid = v_periodid INTO v_req_facilityid;
 
-IF v_req_facilityid IS NULL THEN RETURN ''non_reporting''; END IF;
+IF v_req_facilityid IS NULL THEN RETURN 'non_reporting'; END IF;
 
-SELECT value from configuration_settings where key=''LATE_REPORTING_DAYS'' INTO v_late_days;
+SELECT value from configuration_settings where key='LATE_REPORTING_DAYS' INTO v_late_days;
 
-SELECT date_part(''day'', (select createddate from requisitions r where r.programId = v_programid and r.periodId = v_periodid and facilityid = v_facilityid)- 
+SELECT date_part('day', (select createddate from requisitions r where r.programId = v_programid and r.periodId = v_periodid and facilityid = v_facilityid)- 
 (select startdate from processing_periods where id = v_periodid))::integer INTO v_reporting_date;
 
 SELECT CASE WHEN 
 COALESCE(v_reporting_date,0) > COALESCE(v_late_days,10)
-  THEN ''late_reporting'' 
-  ELSE ''reporting'' END INTO v_ret;
+  THEN 'late_reporting' 
+  ELSE 'reporting' END INTO v_ret;
 
 return v_ret;
 END;
