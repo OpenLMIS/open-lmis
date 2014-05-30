611902701f2f20b114c8833124f8b6de555bb4f4
Wolde Fikadu <memiwo@gmail.com>
2014-05-30 11:46:38 +0300

    DashboardLookupService Unit Test - Get Shipment Lead-time

--- modules/report/src/test/java/org/openlmis/report/service/lookup/DashboardLookupServiceTest.java
+++ modules/report/src/test/java/org/openlmis/report/service/lookup/DashboardLookupServiceTest.java
@@ -10,6 +10,7 @@ import org.openlmis.db.categories.UnitTests;
 import org.openlmis.report.mapper.lookup.DashboardMapper;
 import org.openlmis.report.mapper.lookup.RnRStatusSummaryReportMapper;
 import org.openlmis.report.model.dto.ItemFillRate;
+import org.openlmis.report.model.dto.ShipmentLeadTime;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
@@ -17,9 +18,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.powermock.api.mockito.PowerMockito.*;
-//import static org.mockito.Mockito.*;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
+import static org.openlmis.report.service.lookup.DashboardLookupService.getCommaSeparatedIds;
+
 
 
 /**
@@ -51,9 +53,9 @@ public class DashboardLookupServiceTest {
         List<ItemFillRate> expectedItemFillRate = new ArrayList<>(1);
         expectedItemFillRate.add(new ItemFillRate(50, "Product A"));
 
-        when(mapper.getItemFillRate(1L,1L,1L, DashboardLookupService.getCommaSeparatedIds(productsId))).thenReturn(expectedItemFillRate);
+        when(mapper.getItemFillRate(1L,1L,1L, getCommaSeparatedIds(productsId))).thenReturn(expectedItemFillRate);
         assertThat(lookupService.getItemFillRate(1L,1L,1L,productsId),is(expectedItemFillRate));
-        Mockito.verify(mapper).getItemFillRate(1L,1L,1L,DashboardLookupService.getCommaSeparatedIds(productsId));
+        Mockito.verify(mapper).getItemFillRate(1L,1L,1L,getCommaSeparatedIds(productsId));
 
     }
     @Test
@@ -63,6 +65,13 @@ public class DashboardLookupServiceTest {
         assertThat(lookupService.getOrderFillRate(1L, 1L, 1L), is(nullValue()));
         Mockito.verify(mapper).getOrderFillRate(1L,1L,1L);
 
+    }
+    @Test
+    public void shouldReturnShipmentLeadTime() throws Exception {
+        List<ShipmentLeadTime> expectedShipmentLeadTimeList = new ArrayList<>(1);
+        when(mapper.getShipmentLeadTime(1L,1L,getCommaSeparatedIds(null))).thenReturn(expectedShipmentLeadTimeList);
+        assertThat(lookupService.getShipmentLeadTime(1L,1L, null), is(expectedShipmentLeadTimeList));
+        Mockito.verify(mapper.getShipmentLeadTime(1L,1L,getCommaSeparatedIds(null)));
     }
 
 }
