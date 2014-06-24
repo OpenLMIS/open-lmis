945fa3f94c6b25a4a84b94bede6f86fd3734e6cb
Messay Yohannes <deliasmes@gmail.com>
2014-06-24 16:02:37 +0300

    Finalized ODK stock status survey feature

---
+++ modules/migration/src/main/resources/db/migration/V240__insert_odk_xform_survey_types.sql
@@ -0,0 +1,5 @@
+INSERT INTO odk_xform_survey_type
+            ( survey_name,
+              number_of_questions,
+              active )
+VALUES ('Facility GPS Location and Pictures',10, true ), ('Stock Status', 13, true);
---
+++ modules/migration/src/main/resources/db/migration/V241__add_odk_xform_survey_type_id_column_to_odk_xform_table.sql
@@ -0,0 +1,1 @@
+ALTER TABLE odk_xform ADD COLUMN odk_xform_survey_type_id integer;
