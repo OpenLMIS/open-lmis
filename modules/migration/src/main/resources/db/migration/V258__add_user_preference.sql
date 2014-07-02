9a34edf755b321cdc3e253ee8ff766a8952393fb
Wolde Fikadu <memiwo@gmail.com>
2014-07-02 18:29:47 +0300

    set user preferred value if it exists on drop down list

--- modules/migration/src/main/resources/db/migration/V258__add_user_preference.sql
+++ modules/migration/src/main/resources/db/migration/V258__add_user_preference.sql
@@ -1,10 +1,13 @@
+/*
 delete from user_preferences where userpreferencekey = 'DEFAULT_GEOGRAPHIC_ZONE';
 delete from user_preference_master where key = 'DEFAULT_GEOGRAPHIC_ZONE';
 INSERT INTO "user_preference_master" VALUES (11, 'DEFAULT_GEOGRAPHIC_ZONE', 'Default Geographic Zone', 'DEFAULTS', 1, 1, 'Set default', 'geographiczone', 'single-selected', 'int', '1', 't', NULL, '2014-6-26 10:00:09.213', NULL, '2014-6-26 10:00:09.213');
 INSERT INTO "user_preferences" VALUES (2, 'DEFAULT_GEOGRAPHIC_ZONE', '437', NULL, '2014-4-2 13:42:06.174776', NULL, '2014-4-2 13:42:06.174776');
 
+*/
 
 --update user preference for facility and geozone
+/*
 DELETE FROM user_preferences where userid = 2 and userpreferencekey = 'DEFAULT_FACILITY';
 INSERT INTO user_preferences(userid, userpreferencekey, value, createdby, createddate, modifiedby,
             modifieddate)
@@ -16,4 +19,5 @@ INSERT INTO user_preferences(userid, userpreferencekey, value, createdby, create
             modifieddate)
     VALUES (2,'DEFAULT_GEOGRAPHIC_ZONE','437',NULL,'2014-04-02 13:42:06.174776',NULL,'2014-04-02 13:42:06.174776');
 
+*/
 
