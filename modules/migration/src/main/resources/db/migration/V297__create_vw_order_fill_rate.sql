ea63b413fa7936a7ce7a1053186343d3e27cc8a7
hassan <hhasssan@tz.pfscm.org>
2014-07-18 10:51:51 +0300

    ELMIS-1125-Minor changes on order_fill_rate query

--- modules/migration/src/main/resources/db/migration/V297__create_vw_order_fill_rate.sql
+++ modules/migration/src/main/resources/db/migration/V297__create_vw_order_fill_rate.sql
@@ -1,12 +1,5 @@
-DROP VIEW IF EXISTS vw_order_fill_rate;
-
 CREATE OR REPLACE VIEW vw_order_fill_rate AS 
- SELECT DISTINCT dw_orders.status, dw_orders.facilityid, dw_orders.periodid, 
- dw_orders.productprimaryname AS product, products.code AS productcode, facilities.name AS facilityname,
-  dw_orders.scheduleid, dw_orders.facilitytypeid, dw_orders.productid, dw_orders.productcategoryid, 
-  dw_orders.programid, dw_orders.geographiczoneid AS zoneid, dw_orders.geographiczonename AS zonename, 
-  sum(COALESCE(dw_orders.quantityapproved, 0)::numeric) AS quantityapproved, 
-  sum(COALESCE(dw_orders.quantityreceived, 0)::numeric) AS quantityreceived, sum(
+ SELECT DISTINCT dw_orders.status, dw_orders.facilityid, dw_orders.periodid, dw_orders.productprimaryname AS product, products.code AS productcode, facilities.name AS facilityname, dw_orders.scheduleid, dw_orders.facilitytypeid, dw_orders.productid, dw_orders.productcategoryid, dw_orders.programid, dw_orders.geographiczoneid AS zoneid, dw_orders.geographiczonename AS zonename, sum(COALESCE(dw_orders.quantityapproved, 0)::numeric) AS quantityapproved, sum(COALESCE(dw_orders.quantityreceived, 0)::numeric) AS quantityreceived, sum(
         CASE
             WHEN COALESCE(dw_orders.quantityapproved, 0) = 0 THEN 0::numeric
             ELSE 
@@ -32,3 +25,6 @@ CREATE OR REPLACE VIEW vw_order_fill_rate AS
    JOIN facilities ON facilities.id = dw_orders.facilityid
   WHERE dw_orders.status::text = ANY (ARRAY['RELEASED'::character varying::text])
   GROUP BY dw_orders.scheduleid, dw_orders.facilitytypeid, dw_orders.productid, dw_orders.status, dw_orders.facilityid, dw_orders.periodid, dw_orders.productprimaryname, products.code, facilities.name, dw_orders.productcategoryid, dw_orders.programid, dw_orders.geographiczoneid, dw_orders.geographiczonename;
+
+ALTER TABLE vw_order_fill_rate
+  OWNER TO postgres;
