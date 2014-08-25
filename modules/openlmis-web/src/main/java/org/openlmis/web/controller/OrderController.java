ab476b805ebbc88962b3c9b1eec5e57f5ec5cdc0
Elias Muluneh <eliasmu@gmail.com>
2014-08-25 16:28:52 -0400

    ELMIS-1359: Restore the rnr comfigurations as they were in the production environment

--- modules/openlmis-web/src/main/java/org/openlmis/web/controller/OrderController.java
+++ modules/openlmis-web/src/main/java/org/openlmis/web/controller/OrderController.java
@@ -30,7 +30,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import static java.util.Arrays.asList;
@@ -79,9 +81,12 @@ public class OrderController extends BaseController {
   @PreAuthorize("@permissionEvaluator.hasPermission(principal, 'VIEW_ORDER')")
   public ResponseEntity<OpenLmisResponse> getOrdersForPage(@RequestParam(value = "page",
     required = true,
-    defaultValue = "1") Integer page, HttpServletRequest request) {
-    ResponseEntity<OpenLmisResponse> response = response(ORDERS,
-      getOrdersForView(orderService.getOrdersForPage(page, loggedInUserId(request), Right.VIEW_ORDER)));
+    defaultValue = "1") Integer page,
+    @RequestParam(value="query", defaultValue = "") String query,
+    @RequestParam(value="searchType", defaultValue = "All") String searchType,
+    HttpServletRequest request) {
+
+    ResponseEntity<OpenLmisResponse> response = response(ORDERS, getOrdersForView(orderService.getSearchedOrdersForPage(loggedInUserId(request), page, query, searchType)));
     response.getBody().addData(PAGE_SIZE, orderService.getPageSize());
     response.getBody().addData(NUMBER_OF_PAGES, orderService.getNumberOfPages());
     return response;
