bec3391dd8e7f1aa889c5dd9fc8597491c6f2e0e
adugna <adugna.worku@gmail.com>
2014-08-28 12:18:47 +0300

    ELMIS-1105 removed zero average - Consumption Average by product report

--- modules/report/src/main/java/org/openlmis/report/builder/AverageConsumptionQueryBuilder.java
+++ modules/report/src/main/java/org/openlmis/report/builder/AverageConsumptionQueryBuilder.java
@@ -45,7 +45,7 @@ public class AverageConsumptionQueryBuilder {
         LEFT_OUTER_JOIN("supply_lines sl on sl.supervisorynodeid = r.supervisorynodeid and r.programid = sl.programid");
         LEFT_OUTER_JOIN("facilities s on s.id = sl.supplyingfacilityid");
 
-        WHERE("r.status in ('APPROVED', 'RELEASED')");
+        WHERE("r.status in ('APPROVED', 'RELEASED') AND quantitydispensed > 0");
         if(filter != null){
             if (filter.getFacilityTypeId() != 0) {
                 WHERE("ft.id = #{filterCriteria.facilityTypeId}");
