3b2ed1a3f1c38b7ee7126a4307e9e57a07daf641
adugna <adugna.worku@gmail.com>
2014-09-18 16:20:31 +0300

    -- New configuration settings data correction

--- modules/migration/src/main/resources/db/migration/V322__add_configuration_settings_for_report_header.sql
+++ modules/migration/src/main/resources/db/migration/V322__add_configuration_settings_for_report_header.sql
@@ -1,4 +1,4 @@
-delete from rights where name = 'VIEW_NON_REPORTING_FACILITIES';
+delete from configuration_settings where key = 'OPERATOR_NAME';
 
-INSERT INTO rights (name, rightType, description) VALUES
- ('VIEW_NON_REPORTING_FACILITIES','REPORT','Permission to view Non reporting Facility List Report');
+INSERT INTO configuration_settings (key, name, groupname, description, value, valueType,displayOrder)
+  values ('OPERATOR_NAME', 'Reporting header main title: Name of the organization', 'GENERAL', '','Ministry of Health and Social Welfare',  'TEXT', 1);
