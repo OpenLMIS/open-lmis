6876fdeba6705b9f9e6a7a2cddeea69c33ac3d72
hassan <hhasssan@tz.pfscm.org>
2014-10-08 09:41:47 +0300

    ELMIS-1448:Add font End Code for Order fill Rate Summary report

--- modules/openlmis-web/src/main/webapp/public/js/shared/services/extension-services.js
+++ modules/openlmis-web/src/main/webapp/public/js/shared/services/extension-services.js
@@ -631,3 +631,11 @@ services.factory('GetPushedProductList', function($resource){
 services.factory('GetUserUnassignedSupervisoryNode',function ($resource){
     return $resource('/reports/supervisory-node/user-unassigned-node.json', {}, {});
 });
+
+services.factory('OrderFillRateSummaryReport', function ($resource) {
+    return $resource('/reports/reportdata/orderFillRateReportSummary.json', {}, {});
+});
+
+services.factory('GetOrderFillRateSummary', function($resource){
+    return $resource('/reports/OrderFillRateSummary/program/:programId/period/:periodId/schedule/:scheduleId/facilityTypeId/:facilityTypeId/zone/:zoneId/status/:status/orderFillRateSummary.json',{},{});
+});
