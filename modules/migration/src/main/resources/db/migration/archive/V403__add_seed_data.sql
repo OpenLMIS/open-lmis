541e2e9b88d8422d9f87969273c837ffaec07b26
Elias Muluneh <eliasmu@gmail.com>
2015-02-26 17:13:13 -0500

    Remove script that breaks the vaccine module.

---
+++ modules/migration/src/main/resources/db/migration/archive/V403__add_seed_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO vaccine_diseases (name, description, displayOrder) VALUES
+('Pertusis','Pertusis',4),
+('Yellow Fever','Yellow Fever',5),
+('Meningitis','Meningitis',6),
+('Pneumonia','Pneumonia',7);
