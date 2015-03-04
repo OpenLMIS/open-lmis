401b0c9618a7a8766e5e5f1006383f9c05fb5c14
Elias Muluneh <eliasmu@gmail.com>
2015-03-04 09:04:42 -0500

    Integration test for maintenance log

--- modules/db/src/main/resources/seed/asset_management_lookups.sql
+++ modules/db/src/main/resources/seed/asset_management_lookups.sql
@@ -20,3 +20,9 @@ INSERT INTO equipment_service_vendors
 (name, website,contactperson, primaryPhone, email, description, specialization, geographicCoverage, registrationdate )
 VALUES
 ('The Vendor', 'www.vendor.io', 'John Doe', '1-203-933-2334', 'john@vendor.io', 'Fixing all kinds of stuff', 'Photocopy Machines', 'Tanzania', '2008-01-01');
+
+
+INSERT INTO equipment_service_contracts
+(vendorid, identifier, startdate, enddate, description, terms, coverage, contractdate)
+VALUES
+(1, '12323','2015-02-27', '2019-02-26', 'Servicing all the equipments', 'Pay Per Maintenance', 'Full Tanzania', '2014-09-17');
