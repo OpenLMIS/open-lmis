65d4acb99ccba6fb777574e1f7250232efe174cd
adugna <adugna.worku@gmail.com>
2015-03-23 18:10:51 +0300

    fix build failure

--- modules/core/src/test/java/org/openlmis/core/repository/mapper/RightMapperIT.java
+++ modules/core/src/test/java/org/openlmis/core/repository/mapper/RightMapperIT.java
@@ -96,7 +96,7 @@ public class RightMapperIT {
 
     List<Right> rights = rightMapper.getAll();
 
-    assertThat(rights.size(),is(68));
+    assertThat(rights.size(),is(69));
     assertThat(rights.get(0).getDisplayOrder(),is(1));
   }
 }
