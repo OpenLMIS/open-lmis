ab5a1d53d24245611471ee1cd837e8232dc5b4ef
Elias Muluneh <eliasmu@gmail.com>
2015-04-03 11:26:16 -0400

    VIMS-70 Domain objects for vitamin supplementation, and age group, supplementation line item.

---
+++ modules/vaccine/src/main/java/org/openlmis/vaccine/domain/reports/VitaminSupplementationLineItem.java
@@ -0,0 +1,20 @@
+package org.openlmis.vaccine.domain.reports;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.openlmis.core.domain.BaseModel;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VitaminSupplementationLineItem extends BaseModel {
+
+  Long reportId;
+  Long vaccineVitaminId;
+  Long vitaminAgeGroupId;
+  String vitaminName;
+  Long displayOrder;
+  Long maleValue;
+  Long femaleValue;
+}
