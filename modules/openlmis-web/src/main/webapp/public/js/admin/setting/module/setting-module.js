0f3bec59c545d29fa815fb24b1e95bb4ad39ee5f
Elias Muluneh <eliasmu@gmail.com>
2015-04-08 12:34:24 -0400

    Added feature configuration setting of type HTML, use textAngular for html editing

--- modules/openlmis-web/src/main/webapp/public/js/admin/setting/module/setting-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/admin/setting/module/setting-module.js
@@ -8,51 +8,74 @@
  * You should have received a copy of the Mozilla Public License along with this program. If not, see http://www.mozilla.org/MPL/
  */
 
-angular.module('setting', ['openlmis', 'ui.bootstrap','ui.bootstrap.modal', 'ui.bootstrap.dialog','ui.bootstrap.tabs']).config(['$routeProvider', function ($routeProvider) {
+angular.module('setting', ['openlmis', 'textAngular', 'ui.bootstrap', 'ui.bootstrap.modal', 'ui.bootstrap.dialog', 'ui.bootstrap.tabs']).config(['$routeProvider','$provide', function ($routeProvider, $provide) {
+
+  $provide.decorator('taOptions', ['$delegate', function (taOptions) {
+
+    taOptions.toolbar = [
+      ['h1', 'h2', 'h3'],
+      ['bold', 'italics', 'underline'],['ul', 'ol',  'clear'],
+      ['html', 'insertImage', 'insertLink',],
+      ['justifyLeft', 'justifyCenter', 'justifyRight']
+    ];
+    taOptions.classes = {
+      focussed: 'focussed',
+      toolbar: 'btn-toolbar',
+      toolbarGroup: 'btn-group',
+      toolbarButton: 'btn btn-default',
+      toolbarButtonActive: 'active',
+      disabled: 'disabled',
+      textEditor: 'form-control',
+      htmlEditor: 'form-control'
+    };
+    return taOptions;
+  }]);
+
   $routeProvider.
-    when('/list', {controller:ListSettingController, templateUrl:'partials/list.html'}).
-    otherwise({redirectTo:'/list'});
-}]).run(function($rootScope, AuthorizationService) {
-    $rootScope.roleSelected = "selected";
-    AuthorizationService.preAuthorize('MANAGE_SETTING');
+    when('/list', {controller: ListSettingController, templateUrl: 'partials/list.html'}).
+    otherwise({redirectTo: '/list'});
+}]).run(function ($rootScope, AuthorizationService) {
+  $rootScope.roleSelected = "selected";
+  AuthorizationService.preAuthorize('MANAGE_SETTING');
 });
 
 
 app.directive('setting', function ($compile, $http, $templateCache) {
 
-    var getTemplate = function(contentType) {
-        var templateLoader,
-            baseUrl = '/public/pages/admin/setting/templates/',
-            templateMap = {
-                TEXT: 'text.html',
-                TEXT_AREA: 'text-area.html',
-                NUMBER: 'number.html',
-                OPTIONS: 'options.html',
-                BOOLEAN: 'boolean.html',
-                EMAIL: 'email.html'
-            };
+  var getTemplate = function (contentType) {
+    var templateLoader,
+      baseUrl = '/public/pages/admin/setting/templates/',
+      templateMap = {
+        TEXT: 'text.html',
+        HTML: 'html.html',
+        TEXT_AREA: 'text-area.html',
+        NUMBER: 'number.html',
+        OPTIONS: 'options.html',
+        BOOLEAN: 'boolean.html',
+        EMAIL: 'email.html'
+      };
 
-        var templateUrl = baseUrl + templateMap[contentType];
-        templateLoader = $http.get(templateUrl, {cache: $templateCache});
+    var templateUrl = baseUrl + templateMap[contentType];
+    templateLoader = $http.get(templateUrl, {cache: $templateCache});
 
-        return templateLoader;
-    };
+    return templateLoader;
+  };
 
-    var linker = function(scope, element, attrs) {
-        var loader = getTemplate(scope.content.valueType);
-        var promise = loader.success(function(html) {
-                            element.html(html);
-                        }).then(function (response) {
-                            element.replaceWith($compile(element.html())(scope));
-                        });
-    };
+  var linker = function (scope, element, attrs) {
+    var loader = getTemplate(scope.content.valueType);
+    var promise = loader.success(function (html) {
+      element.html(html);
+    }).then(function (response) {
+      element.replaceWith($compile(element.html())(scope));
+    });
+  };
 
-    return {
-        restrict: "E",
-        rep1ace: true,
-        link: linker,
-        scope: {
-            content:'=ngModel'
-        }
-    };
+  return {
+    restrict: "E",
+    rep1ace: true,
+    link: linker,
+    scope: {
+      content: '=ngModel'
+    }
+  };
 });
