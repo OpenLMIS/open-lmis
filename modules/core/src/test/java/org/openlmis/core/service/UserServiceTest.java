33946a61db6bfa52a213005a56b2292f8fda9b54
joshzamor <josh.zamor@villagereach.org>
2015-04-17 15:16:45 -0700

    BOP-71, changed default reset password functionality to always verify an account.

--- modules/core/src/test/java/org/openlmis/core/service/UserServiceTest.java
+++ modules/core/src/test/java/org/openlmis/core/service/UserServiceTest.java
@@ -322,7 +322,7 @@ public class UserServiceTest {
 
     userService.updateUserPassword(userId, newPassword);
 
-    verify(userRepository).updateUserPassword(userId, hashedPassword);
+    verify(userRepository).updateUserPasswordAndActivate(userId, hashedPassword);
   }
 
   @Test
