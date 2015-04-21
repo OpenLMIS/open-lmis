96300755394e02550e0085f9dd6afd65626093bb
Muhammad Ahmed <muhammad.zulfiqar@gmail.com>
2015-04-21 17:14:22 -0400

    fix script error

--- modules/migration/src/main/resources/db/migration/new_scripts/V467__remove_unused_stored_procedure_in_datawarehouse.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V467__remove_unused_stored_procedure_in_datawarehouse.sql
@@ -1,8 +1,8 @@
 -- delete unused stored procedure in datawarehouse
-DROP FUNCTION fn_populate_alert_facility_stockedout();
-DROP FUNCTION fn_populate_alert_requisition_approved();
-DROP FUNCTION fn_populate_alert_requisition_emergency();
-DROP FUNCTION fn_populate_alert_requisition_pending();
-DROP FUNCTION fn_populate_alert_requisition_rejected();
-DROP FUNCTION fn_populate_dw_orders(integer[]);
-DROP FUNCTION fn_populate_dw_orders_2(integer[]);
+DROP FUNCTION IF EXISTS fn_populate_alert_facility_stockedout();
+DROP FUNCTION IF EXISTS fn_populate_alert_requisition_approved();
+DROP FUNCTION IF EXISTS fn_populate_alert_requisition_emergency();
+DROP FUNCTION IF EXISTS fn_populate_alert_requisition_pending();
+DROP FUNCTION IF EXISTS fn_populate_alert_requisition_rejected();
+DROP FUNCTION IF EXISTS fn_populate_dw_orders(integer[]);
+DROP FUNCTION IF EXISTS fn_populate_dw_orders_2(integer[]);
