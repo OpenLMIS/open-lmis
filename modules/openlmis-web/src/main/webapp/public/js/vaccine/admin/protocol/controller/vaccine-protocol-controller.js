afa43b91c1cf0f6d6af1e511885146b7b68dc2ca
Elias Muluneh <eliasmu@gmail.com>
2015-04-30 12:58:45 -0400

    VIMS-67: revise the admin feature to allow users to configure different doses for program products.

--- modules/openlmis-web/src/main/webapp/public/js/vaccine/admin/protocol/controller/vaccine-protocol-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/vaccine/admin/protocol/controller/vaccine-protocol-controller.js
@@ -19,6 +19,17 @@ function VaccineProtocolController($scope, programs, protocol, SaveVaccineProduc
     });
   };
 
+  $scope.addDosageForProduct = function(product){
+    var dose = $scope.protocol.possibleDoses[product.doses.length];
+    if(dose != undefined){
+      var newEntry = {doseId: dose.id, productId: product.productId, programId: $scope.program, displayName: dose.name, trackMale: true, displayOrder: product.doses.length + 1 , trackFemale: true};
+      product.doses.push(newEntry);
+    }
+  };
+
+  $scope.removeDosageFromProduct = function(product){
+      product.doses.pop();
+  };
 
   $scope.save = function(){
     SaveVaccineProductDose.update($scope.protocol, function(data){
@@ -29,7 +40,7 @@ function VaccineProtocolController($scope, programs, protocol, SaveVaccineProduc
   $scope.addProduct = function(product, scope){
     scope.showAddNewModal = false;
     var dose = $scope.protocol.possibleDoses[0]
-    $scope.protocol.protocols.push({productName: product.primaryName, doses: [{doseId: dose.id, productId: product.id, programId: $scope.program, displayName: dose.displayName, trackMale: true, trackFemale: true}]});
+    $scope.protocol.protocols.push({productName: product.primaryName, productId: product.id , doses: [{doseId: dose.id, productId: product.id, programId: $scope.program, displayOrder: 1, displayName: dose.name, trackMale: true, trackFemale: true}]});
   };
 }
 
