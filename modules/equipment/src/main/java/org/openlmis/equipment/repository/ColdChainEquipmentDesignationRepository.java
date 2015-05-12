c13dc2b0b82d0a1ddc711619e731df9e6fd193ab
Chrispinus Melkiory <chrismelky05@gmail.com>
2015-05-12 22:48:33 +0300

    Add Repository and service call to get CC status data

---
+++ modules/equipment/src/main/java/org/openlmis/equipment/repository/ColdChainEquipmentDesignationRepository.java
@@ -0,0 +1,44 @@
+/*
+ * This program was produced for the U.S. Agency for International Development. It was prepared by the USAID | DELIVER PROJECT, Task Order 4. It is part of a project which utilizes code originally licensed under the terms of the Mozilla Public License (MPL) v2 and therefore is licensed under MPL v2 or later.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the Mozilla Public License as published by the Mozilla Foundation, either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Mozilla Public License for more details.
+ *
+ * You should have received a copy of the Mozilla Public License along with this program. If not, see http://www.mozilla.org/MPL/
+ */
+
+package org.openlmis.equipment.repository;
+
+import org.openlmis.equipment.domain.ColdChainEquipmentDesignation;
+import org.openlmis.equipment.domain.EquipmentType;
+import org.openlmis.equipment.repository.mapper.ColdChainEquipmentDesignationMapper;
+import org.openlmis.equipment.repository.mapper.EquipmentTypeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ColdChainEquipmentDesignationRepository {
+
+  @Autowired
+  ColdChainEquipmentDesignationMapper designationMapper;
+
+  public ColdChainEquipmentDesignation getById(Long id){
+    return designationMapper.getById(id);
+  }
+
+  public List<ColdChainEquipmentDesignation> getAll(){
+    return designationMapper.getAll();
+  }
+
+  public void insert(ColdChainEquipmentDesignation designation){
+    designationMapper.insert(designation);
+  }
+
+  public void update(ColdChainEquipmentDesignation designation){
+    designationMapper.update(designation);
+  }
+
+}
---
+++ modules/equipment/src/main/java/org/openlmis/equipment/service/ColdChainStatusDataService.java
@@ -0,0 +1,56 @@
+/*
+ * This program was produced for the U.S. Agency for International Development. It was prepared by the USAID | DELIVER PROJECT, Task Order 4. It is part of a project which utilizes code originally licensed under the terms of the Mozilla Public License (MPL) v2 and therefore is licensed under MPL v2 or later.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the Mozilla Public License as published by the Mozilla Foundation, either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Mozilla Public License for more details.
+ *
+ * You should have received a copy of the Mozilla Public License along with this program. If not, see http://www.mozilla.org/MPL/
+ */
+
+package org.openlmis.equipment.service;
+
+import org.openlmis.equipment.domain.ColdChainEquipmentDesignation;
+import org.openlmis.equipment.domain.ColdChainEquipmentEnergyType;
+import org.openlmis.equipment.domain.ColdChainEquipmentPqsStatus;
+import org.openlmis.equipment.domain.Donor;
+import org.openlmis.equipment.repository.ColdChainEquipmentDesignationRepository;
+import org.openlmis.equipment.repository.ColdChainEquipmentEnergyTypeRepository;
+import org.openlmis.equipment.repository.ColdChainEquipmentPqsStatusRepository;
+import org.openlmis.equipment.repository.DonorRepository;
+import org.openlmis.equipment.repository.mapper.ColdChainEquipmentPqsStatusMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ColdChainStatusDataService {
+
+  @Autowired
+  private ColdChainEquipmentDesignationRepository designationRepository;
+
+  @Autowired
+  ColdChainEquipmentPqsStatusRepository pqsStatusRepository;
+
+  @Autowired
+  ColdChainEquipmentEnergyTypeRepository energyTypeRepository;
+
+  @Autowired
+  DonorRepository donorRepository;
+
+  public List<ColdChainEquipmentDesignation> getAllDesignations(){
+    return designationRepository.getAll();
+  }
+
+  public List<ColdChainEquipmentEnergyType> getAllEnergyTypes(){
+    return energyTypeRepository.getAll();
+  }
+
+  public List<ColdChainEquipmentPqsStatus> getAllPqsStatus(){
+    return pqsStatusRepository.getAll();
+  }
+  public List<Donor> getAllDonors(){
+    return donorRepository.getAll();
+  }
+}
