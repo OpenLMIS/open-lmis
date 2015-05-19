589b7e4723ab463735f9fdce894911b53a67cdc6
Chrispinus Melkiory <chrismelky05@gmail.com>
2015-05-19 12:02:31 +0300

    Add Controller and Service Call for Energy Types

---
+++ modules/openlmis-web/src/test/java/org/openlmis/web/controller/equipment/EquipmentEnergyTypeControllerTest.java
@@ -0,0 +1,79 @@
+/*
+ * This program was produced for the U.S. Agency for International Development. It was prepared by the USAID | DELIVER PROJECT, Task Order 4. It is part of a project which utilizes code originally licensed under the terms of the Mozilla Public License (MPL) v2 and therefore is licensed under MPL v2 or later.
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the Mozilla Public License as published by the Mozilla Foundation, either version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the Mozilla Public License for more details.
+ *
+ * You should have received a copy of the Mozilla Public License along with this program. If not, see http://www.mozilla.org/MPL/
+ */
+
+package org.openlmis.web.controller.equipment;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.openlmis.db.categories.UnitTests;
+import org.openlmis.equipment.domain.EquipmentEnergyType;
+import org.openlmis.equipment.domain.EquipmentType;
+import org.openlmis.equipment.service.EquipmentEnergyTypeService;
+import org.openlmis.equipment.service.EquipmentTypeService;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.openlmis.authentication.web.UserAuthenticationSuccessHandler.USER;
+import static org.openlmis.authentication.web.UserAuthenticationSuccessHandler.USER_ID;
+
+@Category(UnitTests.class)
+public class EquipmentEnergyTypeControllerTest {
+
+  @Rule
+  public PowerMockRule rule = new PowerMockRule();
+
+  @Mock
+  EquipmentEnergyTypeService service;
+
+  @InjectMocks
+  EquipmentEnergyTypeController controller;
+
+  private MockHttpServletRequest httpServletRequest = new MockHttpServletRequest();
+
+  @Before
+  public void setUp() throws Exception {
+    MockHttpSession mockHttpSession = new MockHttpSession();
+    httpServletRequest.setSession(mockHttpSession);
+    mockHttpSession.setAttribute(USER, USER);
+    mockHttpSession.setAttribute(USER_ID, 1L);
+  }
+
+  @Test
+  public void shouldGetAll() throws Exception {
+    controller.getAll();
+    verify(service).getAll();
+  }
+
+  @Test
+  public void shouldGetById() throws Exception {
+    EquipmentEnergyType type = new EquipmentEnergyType();
+    type.setName("Test");
+    when(service.getById(1L)).thenReturn(type);
+
+    controller.getById(1L);
+    verify(service).getById(1L);
+  }
+
+  @Test
+  public void shouldSave() throws Exception {
+    EquipmentEnergyType type = new EquipmentEnergyType();
+    type.setName("Test");
+
+    controller.save(type);
+    verify(service).save(type);
+  }
+}
