2caa0d230c31cd67128e55ac4344cdeb54cbeddf
Elias Muluneh <eliasmu@gmail.com>
2015-05-20 10:07:18 -0400

    Bootstrap client side view ivd code from create functionality developed so far.

--- modules/openlmis-web/src/main/webapp/public/js/vaccine/report/controller/vaccine-report-view-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/vaccine/report/controller/vaccine-report-view-controller.js
@@ -7,6 +7,81 @@
  *
  * You should have received a copy of the Mozilla Public License along with this program. If not, see http://www.mozilla.org/MPL/
  */
-function VaccineReportViewController($scope) {
+
+function ViewVaccineReportController($scope, programs, VaccineReportFacilities, VaccineReportPeriods, VaccineReportInitiate, messageService , $location) {
+
+  $scope.programs = programs;
+
+  $scope.onProgramChanged = function(){
+    VaccineReportFacilities.get({programId: $scope.filter.program}, function(data){
+      $scope.facilities = data.facilities;
+    });
+  };
+
+
+  $scope.onFacilityChanged = function(){
+    if(isUndefined($scope.filter.facility)){
+      return;
+    }
+    VaccineReportPeriods.get({facilityId: $scope.filter.facility, programId: $scope.filter.program}, function(data){
+      $scope.periodGridData = data.periods;
+      if($scope.periodGridData.length > 0){
+        $scope.periodGridData[0].showButton = true;
+      }
+    });
+  };
+
+  $scope.initiate = function(period){
+    if(!angular.isUndefined(period.id) && (period.id !== null)){
+      // redirect already
+      $location.path('/create/'+ period.id);
+    }else{
+      // initiate
+      VaccineReportInitiate.get({ periodId: period.periodId, facilityId: period.facilityId, programId: period.programId}, function(data){
+        $location.path('/create/'+ data.report.id);
+      });
+    }
+  };
+
+  function getActionButton(showButton){
+    return '<input type="button" ng-click="initiate(row.entity)" openlmis-message="button.proceed" class="btn btn-primary btn-small grid-btn" ng-show="' + showButton + '"/>';
+  }
+
+  $scope.periodGridOptions = { data: 'periodGridData',
+    canSelectRows: false,
+    displayFooter: false,
+    displaySelectionCheckbox: false,
+    enableColumnResize: true,
+    enableColumnReordering: true,
+    enableSorting: false,
+    showColumnMenu: false,
+    showFilter: false,
+    columnDefs: [
+      {field: 'periodName', displayName: messageService.get("label.periods")},
+      {field: 'status', displayName: messageService.get("label.ivd.status") },
+      {field: '', displayName: '', cellTemplate: getActionButton('row.entity.showButton')}
+    ]
+  };
+
+
+  // load facility list for program.
+  if(programs.length == 1){
+    $scope.filter = {program: programs[0].id};
+    $scope.onProgramChanged();
+  }
 
 }
+
+ViewVaccineReportController.resolve = {
+  programs: function($q, $timeout, VaccineSupervisedIvdPrograms){
+    var deferred = $q.defer();
+
+    $timeout(function(){
+      VaccineSupervisedIvdPrograms.get({},function(data){
+        deferred.resolve(data.programs);
+      });
+    },100);
+
+    return deferred.promise;
+  }
+};
