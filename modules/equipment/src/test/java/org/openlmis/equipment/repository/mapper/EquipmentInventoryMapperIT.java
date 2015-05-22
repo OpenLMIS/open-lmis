532c741634d06d607996a6d7e32dccc1b4780ef9
Elias Muluneh <eliasmu@gmail.com>
2015-05-22 15:07:08 -0400

    refactor Equipment Inventory mapper tests to enable previously disabled tests.

--- modules/equipment/src/test/java/org/openlmis/equipment/repository/mapper/EquipmentInventoryMapperIT.java
+++ modules/equipment/src/test/java/org/openlmis/equipment/repository/mapper/EquipmentInventoryMapperIT.java
@@ -11,10 +11,17 @@
 package org.openlmis.equipment.repository.mapper;
 
 import org.joda.time.DateTime;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.openlmis.core.builder.FacilityBuilder;
+import org.openlmis.core.builder.ProgramBuilder;
+import org.openlmis.core.domain.Facility;
+import org.openlmis.core.domain.Program;
 import org.openlmis.core.query.QueryExecutor;
+import org.openlmis.core.repository.mapper.FacilityMapper;
+import org.openlmis.core.repository.mapper.ProgramMapper;
 import org.openlmis.db.categories.IntegrationTests;
 import org.openlmis.equipment.domain.Equipment;
 import org.openlmis.equipment.domain.EquipmentInventory;
@@ -24,6 +31,16 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.ResultSet;
+import java.util.List;
+
+import static com.natpryce.makeiteasy.MakeItEasy.a;
+import static com.natpryce.makeiteasy.MakeItEasy.make;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 
 @Category(IntegrationTests.class)
@@ -43,119 +60,103 @@ public class EquipmentInventoryMapperIT {
   EquipmentMapper equipmentMapper;
 
   @Autowired
+  FacilityMapper facilityMapper;
+
+  @Autowired
+  ProgramMapper programMapper;
+
+  @Autowired
   QueryExecutor queryExecutor;
+
+  Equipment equipment;
+
+  Facility facility;
+
+  Program program;
+
+  @Before
+  public void setup(){
+    EquipmentType type = new EquipmentType();
+    type.setCode("1");
+    type.setCode("Type");
+    typeMapper.insert(type);
+
+    equipment = new Equipment();
+    equipment.setCode("123");
+    equipment.setName("Name");
+    equipment.setEquipmentType(type);
+    equipmentMapper.insert(equipment);
+
+    facility = make(a(FacilityBuilder.defaultFacility));
+    facilityMapper.insert(facility);
+
+    program = make(a(ProgramBuilder.defaultProgram));
+    programMapper.insert(program);
+  }
 
   @Test
   public void shouldGetAllInventoryItemsForFacility() throws Exception{
-    EquipmentInventory inventory = new EquipmentInventory();
+    EquipmentInventory inventory = makeAnEquipmentInventory(equipment);
 
-    inventory.setProgramId(1L);
-    inventory.setFacilityId(1L);
-    inventory.setOperationalStatusId(1L);
-    inventory.setEquipmentId(1L);
-    inventory.setReplacementRecommended(false);
-    inventory.setYearOfInstallation(2012);
-    inventory.setSerialNumber("2323");
-    inventory.setPurchasePrice(0F);
-    inventory.setDateLastAssessed(DateTime.now().toDate());
+    mapper.insert(inventory);
 
-//    mapper.insert(inventory);
-//
-//    List<EquipmentInventory> inventories =  mapper.getInventoryByFacilityAndProgram(1L, 1L);
-//    assertEquals(inventories.size(), 1);
+    List<EquipmentInventory> inventories =  mapper.getInventoryByFacilityAndProgram(facility.getId(),program.getId());
+    assertEquals(inventories.size(), 1);
   }
 
   @Test
   public void shouldGetInventoryById() throws Exception {
-    EquipmentInventory inventory = new EquipmentInventory();
-    inventory.setProgramId(1L);
-    inventory.setFacilityId(1L);
-    inventory.setOperationalStatusId(1L);
-    inventory.setEquipmentId(1L);
-    inventory.setModel("123");
-    inventory.setReplacementRecommended(false);
-    inventory.setYearOfInstallation(2012);
-    inventory.setSerialNumber("2323");
-    inventory.setPurchasePrice(0F);
-    inventory.setDateLastAssessed(DateTime.now().toDate());
+    EquipmentInventory inventory =   makeAnEquipmentInventory(equipment);
+    mapper.insert(inventory);
+    EquipmentInventory result = mapper.getInventoryById(inventory.getId());
 
-//    mapper.insert(inventory);
-//
-//    EquipmentInventory result = mapper.getInventoryById(inventory.getId());
-//
-//    assertEquals(result.getModel(), inventory.getModel());
-//    assertEquals(result.getOperationalStatusId(), inventory.getOperationalStatusId());
+    assertEquals(result.getModel(), inventory.getModel());
+    assertEquals(result.getOperationalStatusId(), inventory.getOperationalStatusId());
   }
 
   @Test
   public void shouldInsert()throws Exception {
-    EquipmentType type = new EquipmentType();
-    type.setCode("1");
-    type.setCode("Type");
-    typeMapper.insert(type);
 
-    Equipment equipment = new Equipment();
-    equipment.setCode("123");
-    equipment.setName("Name");
-    equipment.setEquipmentType(type);
-    equipmentMapper.insert(equipment);
 
-    EquipmentInventory inventory = new EquipmentInventory();
-    inventory.setFacilityId(1L);
-    inventory.setEquipmentId(equipment.getId());
-    inventory.setOperationalStatusId(1L);
-    inventory.setProgramId(1L);
-    inventory.setModel("123");
-    inventory.setReplacementRecommended(false);
-    inventory.setYearOfInstallation(2012);
-    inventory.setSerialNumber("2323");
-    inventory.setPurchasePrice(0F);
-    inventory.setDateLastAssessed(DateTime.now().toDate());
 
-//    mapper.insert(inventory);
-//
-//    assertThat(inventory.getId(), CoreMatchers.is(notNullValue()));
-//
-//    ResultSet rs = queryExecutor.execute("Select * from facility_program_equipments where id = " + inventory.getId());
-//    assertEquals(rs.next(), true);
-//    assertEquals(rs.getString("model"), "123");
+    EquipmentInventory inventory =  makeAnEquipmentInventory(equipment);
+    mapper.insert(inventory);
+
+    assertThat(inventory.getId(), is(notNullValue()));
+
+    ResultSet rs = queryExecutor.execute("Select * from equipment_inventories where id = " + inventory.getId());
+    assertEquals(rs.next(), true);
+    assertEquals(rs.getString("model"), "123");
   }
 
   @Test
   public void shouldUpdate() throws Exception{
-    EquipmentType type = new EquipmentType();
-    type.setCode("1");
-    type.setCode("Type");
-    typeMapper.insert(type);
+    EquipmentInventory inventory = makeAnEquipmentInventory(equipment);
+    mapper.insert(inventory);
 
-    Equipment equipment = new Equipment();
-    equipment.setCode("123");
-    equipment.setName("Name");
-    equipment.setEquipmentType(type);
+    inventory.setModel("3432");
 
-    equipmentMapper.insert(equipment);
+    mapper.update(inventory);
 
+    ResultSet rs = queryExecutor.execute("Select * from equipment_inventories where id = " + inventory.getId());
+    assertEquals(rs.next(), true);
+    assertEquals(rs.getString("model"), "3432");
+  }
+
+  private EquipmentInventory makeAnEquipmentInventory(Equipment equipment) {
     EquipmentInventory inventory = new EquipmentInventory();
-    inventory.setFacilityId(1L);
-
+    inventory.setFacilityId(facility.getId());
     inventory.setEquipmentId(equipment.getId());
     inventory.setOperationalStatusId(1L);
-    inventory.setProgramId(1L);
+    inventory.setIsActive(true);
+    inventory.setHasServiceContract(true);
+    inventory.setProgramId(program.getId());
     inventory.setModel("123");
     inventory.setReplacementRecommended(false);
     inventory.setYearOfInstallation(2012);
     inventory.setSerialNumber("2323");
     inventory.setPurchasePrice(0F);
     inventory.setDateLastAssessed(DateTime.now().toDate());
-
-//    mapper.insert(inventory);
-//
-//    inventory.setModel("3432");
-//
-//    mapper.update(inventory);
-//
-//    ResultSet rs = queryExecutor.execute("Select * from facility_program_equipments where id = " + inventory.getId());
-//    assertEquals(rs.next(), true);
-//    assertEquals(rs.getString("model"), "3432");
+    return inventory;
   }
 }
