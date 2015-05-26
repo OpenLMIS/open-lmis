f86ba150b3fee646f0f1418c30303aaaa97a5a73
Elias Muluneh <eliasmu@gmail.com>
2015-05-26 17:35:12 -0400

    Fix unit tests that were broken when refactoring tab settings out of product dose controller.

--- modules/openlmis-web/src/test/java/org/openlmis/web/controller/vaccine/ProductDoseControllerTest.java
+++ modules/openlmis-web/src/test/java/org/openlmis/web/controller/vaccine/ProductDoseControllerTest.java
@@ -22,6 +22,7 @@ import org.openlmis.core.domain.ConfigurationSetting;
 import org.openlmis.core.service.ConfigurationSettingService;
 import org.openlmis.db.categories.UnitTests;
 import org.openlmis.vaccine.domain.VaccineProductDose;
+import org.openlmis.vaccine.dto.ProductDoseDTO;
 import org.openlmis.vaccine.dto.VaccineServiceConfigDTO;
 import org.openlmis.vaccine.service.VaccineProductDoseService;
 import org.openlmis.web.response.OpenLmisResponse;
@@ -38,18 +39,13 @@ public class ProductDoseControllerTest {
   @Mock
   VaccineProductDoseService service;
 
-  @Mock
-  ConfigurationSettingService settingService;
-
   @InjectMocks
   ProductDoseController controller;
 
   @Test
   public void shouldGetProgramProtocol() throws Exception {
-    List<VaccineProductDose> doses = new ArrayList<>();
-    when(service.getForProgram(1L)).thenReturn(doses);
-    when(settingService.getBoolValue(anyString())).thenReturn(true);
-    when(settingService.getSearchResults(anyString())).thenReturn(asList(new ConfigurationSetting()));
+    VaccineServiceConfigDTO dto = new VaccineServiceConfigDTO();
+    when(service.getProductDoseForProgram(1L)).thenReturn(dto);
     ResponseEntity<OpenLmisResponse> response = controller.getProgramProtocol(1L);
     assertThat(response.getBody().getData().get("protocol"), is(notNullValue()));
   }
@@ -57,10 +53,12 @@ public class ProductDoseControllerTest {
   @Test
   public void shouldSave() throws Exception {
     VaccineServiceConfigDTO dto = new VaccineServiceConfigDTO();
-    dto.setTabVisibilitySettings(new ArrayList<ConfigurationSetting>());
-    doNothing().when(service).save(anyList());
+    dto.setProtocols(new ArrayList<ProductDoseDTO>());
+    doNothing().when(service).save(dto.getProtocols());
+
     ResponseEntity<OpenLmisResponse> response = controller.save(dto);
+
     verify(service).save(anyList());
-    assertThat("success", is(response.getBody().getData().get("status")));
+    assertThat(dto, is(response.getBody().getData().get("protocol")));
   }
 }
