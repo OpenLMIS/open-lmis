5c4ea9f5413baf27dc39388139c21a9f891069d6
Elias Muluneh <eliasmu@gmail.com>
2015-06-16 18:55:49 -0400

    Cleanup - Remove smt poc code

--- modules/vaccine/src/test/java/org/openlmis/vaccine/builders/reports/VaccineReportBuilder.java
+++ modules/vaccine/src/test/java/org/openlmis/vaccine/builders/reports/VaccineReportBuilder.java
@@ -2,7 +2,6 @@ package org.openlmis.vaccine.builders.reports;
 
 import com.natpryce.makeiteasy.Instantiator;
 import com.natpryce.makeiteasy.PropertyLookup;
-import org.openlmis.vaccine.domain.Status;
 import org.openlmis.vaccine.domain.reports.DiseaseLineItem;
 import org.openlmis.vaccine.domain.reports.VaccineReport;
 import org.openlmis.vaccine.dto.ReportStatusDTO;
