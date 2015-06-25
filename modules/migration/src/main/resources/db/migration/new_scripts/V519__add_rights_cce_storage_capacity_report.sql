33df4953c427eef6aaec9f3d0203697d9e4c9e86
Chongsun Ahn <cahn922@yahoo.com>
2015-06-25 15:58:26 -0700

    Make CCE storage capacity migration script more robust
    Make script more robust by checking for right existence.

--- modules/migration/src/main/resources/db/migration/new_scripts/V519__add_rights_cce_storage_capacity_report.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V519__add_rights_cce_storage_capacity_report.sql
@@ -1,4 +1,8 @@
-DELETE FROM rights WHERE name = 'VIEW_CCE_STORAGE_CAPACITY_REPORT';
-
-INSERT INTO rights (name, rightType, description, displaynamekey)
-VALUES ('VIEW_CCE_STORAGE_CAPACITY_REPORT','REPORT','Permission to view CCE Storage Capacity Report','right.report.cce.storage.capacity');
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM rights WHERE name = 'VIEW_CCE_STORAGE_CAPACITY_REPORT') THEN
+    INSERT INTO rights (name, rightType, description, displaynamekey)
+    VALUES ('VIEW_CCE_STORAGE_CAPACITY_REPORT','REPORT','Permission to view CCE Storage Capacity Report','right.report.cce.storage.capacity');
+  END IF;
+END;
+$$
