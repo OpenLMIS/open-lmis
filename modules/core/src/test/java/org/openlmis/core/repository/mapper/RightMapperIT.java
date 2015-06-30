6e6540b436967261e676b139773f45aba7d39ded
Ben Leibert <ben.leibert@villagereach.org>
2015-06-30 16:22:57 -0700

    VIMS 207 - Resolved File Conflicts

--- modules/core/src/test/java/org/openlmis/core/repository/mapper/RightMapperIT.java
+++ modules/core/src/test/java/org/openlmis/core/repository/mapper/RightMapperIT.java
@@ -96,7 +96,7 @@ public class RightMapperIT {
 
     List<Right> rights = rightMapper.getAll();
 
-    assertThat(rights.size(),is(76));
+    assertThat(rights.size(),is(77));
     assertThat(rights.get(0).getDisplayOrder(),is(1));
   }
 }
