c06c4a4f8e3d7c299623bfce30f570179808ac5f
Ben Leibert <ben.leibert@villagereach.org>
2015-07-06 14:35:44 -0700

    VIMS 207 – Revamped SQL that previously relied on postres' LATERAL functionality.Commit fe0cd1b, made on 6/30/2015, relied on LATERAL functionality that isn’t supported by our version (9.2) of postrges. This commit address that problem. Although it’s still necessary to expose this report to user within the “all reports” role, another developer plans to do so as part of a separate check-in.

--- modules/migration/src/main/resources/db/migration/new_scripts/V527__vw_cold_chain_equipment.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V527__vw_cold_chain_equipment.sql
@@ -1,14 +1,9 @@
-
 /* 
    This view is intended to return all information required by a Cold Chain Equipment Inventory Report.
 
    Dependency: fn_get_geozonetree_names.
 */
 
-
---TODO: Reimplement this, sans LATERAL in order to support version 9.2 of postgres.
-
-/*
 CREATE OR REPLACE VIEW vw_cold_chain_equipment AS 
 
 SELECT
@@ -22,7 +17,7 @@ facilities.id AS "facilityId", facilities.name AS "facilityName",
 ft.name AS "facilityTypeName", 
 facilities.address1 AS "facilityAddress1", facilities.address2 AS "facilityAddress2", facilities.haselectricity AS "facilityHasElectricity",
 fo.text AS "facilityOperator",
-gz.id AS geoZoneID, gz.name AS geoZoneName, geo_zone_tree.*
+gz.id AS geoZoneID, gz.name AS geoZoneName, geo_zone_tree.hierarchy AS "geozoneHierarchy"
  
 FROM equipment_inventories ei JOIN
 (
@@ -32,31 +27,36 @@ WHERE UPPER(et.code) = 'CCE1'
 ) AS nested
 
 ON nested.id = ei.equipmentid
-JOIN equipment_cold_chain_equipments AS ecce
+LEFT JOIN equipment_cold_chain_equipments AS ecce
 ON nested.id = ecce.equipmentid
-JOIN equipment_energy_types AS eet
+LEFT JOIN equipment_energy_types AS eet
 ON nested.energytypeid = eet.id 
-JOIN facilities
+LEFT JOIN facilities
 ON ei.facilityid = facilities.id
-JOIN facility_types AS ft
+LEFT JOIN facility_types AS ft
 ON facilities.typeid = ft.id
-JOIN equipment_inventory_statuses AS eis
+LEFT JOIN equipment_inventory_statuses AS eis
 ON ei.id = eis.inventoryid
-JOIN equipment_operational_status AS eos
+LEFT JOIN equipment_operational_status AS eos
 ON eis.statusid = eos.id OR eis.notfunctionalstatusid = eos.id
-JOIN facility_operators as fo
+LEFT JOIN facility_operators as fo
 ON facilities.operatedbyid = fo.id
-JOIN geographic_zones gz
+LEFT JOIN geographic_zones gz
 ON facilities.geographiczoneid = gz.id
 
+--Using LATERAL is a clean approach, but unsupported in the version of postgres we're targeting
+/*
 LEFT OUTER JOIN LATERAL
 (
 	SELECT * FROM fn_get_geozonetree_names(facilities.geographiczoneid) AS "geozoneHierarchy"
+) AS geo_zone_tree 
+ON true; */
+LEFT JOIN
+(
+	SELECT * FROM fn_get_geozonetree_names()
 ) AS geo_zone_tree
-ON true;
+ON gz.id = geo_zone_tree.leafid;
 
 
 ALTER TABLE vw_cold_chain_equipment
   OWNER TO postgres;
-
-*/ 
