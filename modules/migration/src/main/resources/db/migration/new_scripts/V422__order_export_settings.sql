6032b4400d0ef4892cb5503a91ac73da79b92b4b
Danni Yu <dyu@thoughtworks.com>
2015-07-21 14:10:38 +0800

    use /tmp instead of ./ for local ftp files

--- modules/migration/src/main/resources/db/migration/new_scripts/V422__order_export_settings.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V422__order_export_settings.sql
@@ -1,7 +1,7 @@
 
 DELETE FROM configuration_settings WHERE key = 'LOCAL_ORDER_EXPORT_DIRECTORY';
 INSERT INTO configuration_settings(key, value, name, description, groupname, valuetype, displayOrder)
-values('LOCAL_ORDER_EXPORT_DIRECTORY','./local-order-ftp-data','Local Directory To Export Order','Local Directory to Export Order','Order Export','TEXT', 4);
+values('LOCAL_ORDER_EXPORT_DIRECTORY','/tmp/local-order-ftp-data','Local Directory To Export Order','Local Directory to Export Order','Order Export','TEXT', 4);
 
 DELETE FROM configuration_settings WHERE key = 'USE_FTP_TO_SEND_ORDERS';
 INSERT INTO configuration_settings(key, value, name, description, groupname, valuetype, displayOrder)
