ed7affea0590b2ab237ba46075053c379a63bb18
Elias Muluneh <eliasmu@gmail.com>
2015-09-12 20:55:49 -0400

    Implement finalize and undo finalize functionalities on ui of facility demographic estimates.

--- modules/openlmis-web/src/main/webapp/public/js/vaccine/services/demographic-services.js
+++ modules/openlmis-web/src/main/webapp/public/js/vaccine/services/demographic-services.js
@@ -40,3 +40,15 @@ services.factory('SaveDistrictDemographicEstimates', function ($resource) {
 services.factory('DemographicEstimatePrograms', function($resource){
   return $resource('/vaccine/demographics/programs.json', {}, {});
 });
+
+services.factory('UserSupervisoryRights', function($resource){
+  return $resource('/users/supervisory/rights.json', {},{});
+});
+
+services.factory('FinalizeFacilityDemographicEstimates', function($resource){
+  return $resource('/vaccine/demographic/estimate/finalize/facilities.json', {}, update)
+});
+
+services.factory('UndoFinalizeFacilityDemographicEstimates', function($resource){
+  return $resource('/vaccine/demographic/estimate/undo-finalize/facilities.json', {}, update)
+});
