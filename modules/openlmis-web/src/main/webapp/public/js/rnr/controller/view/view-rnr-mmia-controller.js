17215899b793b808e8be7fdfc8c27962f308a41b
Yingli Yuan <ylyuan@thoughtworks.com>
2015-10-28 17:54:13 +0800

    YY&CS - #39 - translation in mmia requsition page

--- modules/openlmis-web/src/main/webapp/public/js/rnr/controller/view/view-rnr-mmia-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/rnr/controller/view/view-rnr-mmia-controller.js
@@ -1,51 +1,78 @@
-function ViewRnrMmiaController($scope, $route, Requisitions) {
+function ViewRnrMmiaController($scope, $route, Requisitions, messageService) {
+    $scope.adult = [];
+    $scope.children = [];
+    $scope.other = [];
+
+    $scope.regimeTotal = 0;
+    $scope.regimeAdult = [];
+    $scope.regimeChildren = [];
+
+    $scope.patient = [];
 
     $scope.$on('$viewContentLoaded', function () {
         $scope.loadMmiaDetail();
     });
+    $scope.$on('messagesPopulated', function () {
+        $scope.initMonth();
+    });
 
     $scope.loadMmiaDetail = function () {
         Requisitions.get({id: $route.current.params.rnr}, function (data) {
-            console.log(data);
-
             $scope.rnr = data.rnr;
 
-            $scope.adult = [];
-            $scope.children = [];
-            $scope.other = [];
+            $scope.year = data.rnr.period.name.substr(3, 4);
 
-            var i = 0;
-            for (i = 0; i < 12; i++) {
-                $scope.adult.push($scope.rnr.nonSkippedLineItems[i]);
-            }
+            $scope.initMonth();
 
-            for (i = 12; i < 22; i++) {
-                $scope.children.push($scope.rnr.nonSkippedLineItems[i]);
-            }
-
-            for (i = 22; i < 24; i++) {
-                $scope.other.push($scope.rnr.nonSkippedLineItems[i]);
-            }
-
-            $scope.regimen = data.rnr.regimenLineItems;
-
-            $scope.regimenTotal = 0;
-            $scope.regimenAdult = [];
-            $scope.regimenChildren = [];
-            for (i = 0; i < 8; i++) {
-                $scope.regimenTotal += $scope.regimen[i].patientsOnTreatment;
-                $scope.regimenAdult.push($scope.regimen[i]);
-            }
-            for (i = 8; i < 18; i++) {
-                $scope.regimenTotal += $scope.regimen[i].patientsOnTreatment;
-                $scope.regimenChildren.push($scope.regimen[i]);
-            }
-
-            $scope.patient = [];
-            for (i = 0; i < 7; i++) {
-                $scope.patient.push($scope.rnr.patientQuantifications[i]);
-            }
+            $scope.initProduct();
+            $scope.initRegime();
+            $scope.initPatient();
         });
 
+    };
+
+    $scope.initMonth = function () {
+        var month = "month." + $scope.rnr.period.name.substr(0, 3);
+        $scope.month = messageService.get(month);
+
+    };
+
+    $scope.initProduct = function () {
+        var i = 0;
+
+        for (i = 0; i < 12; i++) {
+            $scope.adult.push($scope.rnr.nonSkippedLineItems[i]);
+        }
+
+        for (i = 12; i < 22; i++) {
+            $scope.children.push($scope.rnr.nonSkippedLineItems[i]);
+        }
+
+        for (i = 22; i < 24; i++) {
+            $scope.other.push($scope.rnr.nonSkippedLineItems[i]);
+        }
+    };
+
+    $scope.initRegime = function () {
+        var regime = $scope.rnr.regimenLineItems;
+        var i = 0;
+
+        for (i = 0; i < 8; i++) {
+            $scope.regimeTotal += regime[i].patientsOnTreatment;
+            $scope.regimeAdult.push(regime[i]);
+        }
+
+        for (i = 8; i < 18; i++) {
+            $scope.regimeTotal += regime[i].patientsOnTreatment;
+            $scope.regimeChildren.push(regime[i]);
+        }
+    };
+
+    $scope.initPatient = function () {
+        var i = 0;
+
+        for (i = 0; i < 7; i++) {
+            $scope.patient.push($scope.rnr.patientQuantifications[i]);
+        }
     };
 }
