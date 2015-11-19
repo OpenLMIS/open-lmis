196f7d83ca414560d77b0b10a98bd2b4cb99a403
Ben Leibert <ben.leibert@villagereach.org>
2015-11-18 21:45:21 -0800

    Remove redundant Karma configuration - take two.
    Now that karma.config.js' singleRun value is being used, set it appropriately (to true).

--- modules/openlmis-web/karma.config.js
+++ modules/openlmis-web/karma.config.js
@@ -99,6 +99,6 @@ module.exports = function (config) {
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
-    singleRun: false
+    singleRun: true
   });
 };
