8415982ef0f0ce02f6f3fea7445b273fe971a9b7
Yingli Yuan <ylyuan@thoughtworks.com>
2015-12-08 16:39:47 +0800

    YY - #124 - add App Version right

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V649_1__add_tablet_info_rights.sql
@@ -0,0 +1,2 @@
+INSERT INTO rights (name, rightType, description, displaynamekey) VALUES
+('VIEW_TABLET_INFO_REPORT','REPORT','Permission to View Tablet Information Reports', 'right.report.tablet.info');
