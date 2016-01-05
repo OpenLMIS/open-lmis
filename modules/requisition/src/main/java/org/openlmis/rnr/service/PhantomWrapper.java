6a98714db956bda92a36ede25d747c686b7867c5
cuipengfei <cuipengfei2008@gmail.com>
2016-01-05 16:13:27 +0800

    CPF&HK - 483- use phantom wrapper to generate pdf

---
+++ modules/requisition/src/main/java/org/openlmis/rnr/service/PhantomWrapper.java
@@ -0,0 +1,10 @@
+package org.openlmis.rnr.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PhantomWrapper {
+    public void generatePDF(String url, String sessionid, String pdfPath) {
+
+    }
+}
