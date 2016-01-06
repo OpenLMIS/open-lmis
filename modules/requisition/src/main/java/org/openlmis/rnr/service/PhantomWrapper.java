b41ec6995a002cf20ea18d678efd9a8587cac69a
cuipengfei <cuipengfei2008@gmail.com>
2016-01-06 16:21:52 +0800

    CPF&HK - 483- remove phantom js log

--- modules/requisition/src/main/java/org/openlmis/rnr/service/PhantomWrapper.java
+++ modules/requisition/src/main/java/org/openlmis/rnr/service/PhantomWrapper.java
@@ -10,11 +10,11 @@ import java.nio.file.Paths;
 
 @Component
 public class PhantomWrapper {
+
     public void generatePDF(String url, String pdfPath, String sessionid) throws IOException, URISyntaxException, InterruptedException {
         URL pdfScriptUrl = this.getClass().getClassLoader().getResource("pdf/rasterize.js");
         String pdfScriptpath = Paths.get(pdfScriptUrl.toURI()).toFile().getAbsolutePath();
         ProcessBuilder phantomjs = new ProcessBuilder("phantomjs", pdfScriptpath, url, pdfPath, sessionid);
-        phantomjs.redirectOutput(new File("/app/tomcat/open-lmis/logs/pdf.log"));
         phantomjs.start().waitFor();
     }
 }
