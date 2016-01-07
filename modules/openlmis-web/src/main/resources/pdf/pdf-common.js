cef1caec1d3a91df93b69d4be734ad3e1204ea1c
cuipengfei <cuipengfei2008@gmail.com>
2016-01-07 16:48:46 +0800

    CPF - 484- extract common function for via and mmia pdf generation

---
+++ modules/openlmis-web/src/main/resources/pdf/pdf-common.js
@@ -0,0 +1,74 @@
+"use strict";
+
+function extractDomain(url) {
+    var domain;
+    if (url.indexOf("://") > -1) {
+        domain = url.split('/')[2];
+    } else {
+        domain = url.split('/')[0];
+    }
+    domain = domain.split(':')[0];
+    return domain;
+}
+
+function hasSwitchedToPtLanguage(response) {
+    //this request is the pt language json, we can only render pdf after it's loaded
+    return responseUrlContains(response, 'messages.json');
+}
+
+function isXhrFinished(response) {
+    //the following 3 requests are critical for the page to render, we need to wait
+    //for them to finish loading then we can click pt button
+    return responseUrlContains(response, 'reports.html') ||
+        responseUrlContains(response, 'skipped.json') ||
+        responseUrlContains(response, 'locales.json');
+}
+
+function responseUrlContains(response, str) {
+    return response.url.indexOf(str) > -1;
+}
+
+function tryToSwitchToPtLanguage(page) {
+    window.setTimeout(function () {
+        page.evaluate(function () {
+            console.log("trying to switch language to pt");
+            var ptButton = $("#locale_pt");
+            if ($(ptButton[0]).attr("id") !== undefined) {
+                ptButton.click();
+                console.log("found pt button, clicked");
+            } else {
+                console.log("can not find pt button");
+            }
+            $(".toggleFullScreen").hide()
+        });
+    }, 100);
+}
+
+function monitorResponses(page, address, onLoaded) {
+    page.onConsoleMessage = function (msg, lineNum, sourceId) {
+        console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
+    };
+
+    page.onResourceReceived = function (response) {
+        if (response.stage === 'end') {
+            console.log(response.url + "   " + response.stage);
+            if (hasSwitchedToPtLanguage(response)) {
+                onLoaded();
+            }
+            if (isXhrFinished(response)) {
+                tryToSwitchToPtLanguage(page);
+            }
+        }
+    };
+
+    page.open(address, function (status) {
+        console.log(status);
+        if (status !== 'success') {
+            console.log('Unable to load the address!');
+            phantom.exit(1);
+        }
+    });
+}
+
+exports.extractDomain = extractDomain;
+exports.monitorResponses = monitorResponses;
--- modules/openlmis-web/src/main/resources/pdf/via-pdf-renderer.js
+++ modules/openlmis-web/src/main/resources/pdf/via-pdf-renderer.js
@@ -1,11 +1,12 @@
 "use strict";
 var page = require('webpage').create(),
-    system = require('system');
+    system = require('system'),
+    common = require('./pdf-common');
 
 var address = system.args[1];
 var output = system.args[2];
 var sessionId = system.args[3];
-var domain = extractDomain(address);
+var domain = common.extractDomain(address);
 
 phantom.addCookie({
     'name': 'JSESSIONID',
@@ -13,33 +14,7 @@ phantom.addCookie({
     'domain': domain
 });
 
-page.onResourceReceived = function (response) {
-    if (response.stage === 'end') {
-        console.log(response.url + "   " + response.stage);
-        if (hasSwitchedToPtLanguage(response)) {
-            renderPDF();
-        }
-        if (isXhrFinished(response)) {
-            tryToSwitchToPtLanguage();
-        }
-    }
-};
-
-page.onConsoleMessage = function (msg, lineNum, sourceId) {
-    console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
-};
-
-page.open(address, function (status) {
-    console.log(status);
-    if (status !== 'success') {
-        console.log('Unable to load the address!');
-        phantom.exit(1);
-    }
-});
-
-function responseUrlContains(response, str) {
-    return response.url.indexOf(str) > -1;
-}
+common.monitorResponses(page, address, onLoaded);
 
 function adjustPageForRender() {
     page.evaluate(function () {
@@ -76,7 +51,7 @@ function adjustPageForRender() {
     });
 }
 
-function renderPDF() {
+function onLoaded() {
     console.log("message loaded, prepare to render");
 
     window.setTimeout(function () {
@@ -84,43 +59,4 @@ function renderPDF() {
         page.render(output);
         phantom.exit();
     }, 200);
-}
-
-function extractDomain(url) {
-    var domain;
-    if (url.indexOf("://") > -1) {
-        domain = url.split('/')[2];
-    } else {
-        domain = url.split('/')[0];
-    }
-    domain = domain.split(':')[0];
-    return domain;
-}
-
-function tryToSwitchToPtLanguage() {
-    window.setTimeout(function () {
-        page.evaluate(function () {
-            console.log("trying to switch language to pt");
-            var ptButton = $("#locale_pt");
-            if ($(ptButton[0]).attr("id") !== undefined) {
-                ptButton.click();
-                console.log("found pt button, clicked");
-            } else {
-                console.log("can not find pt button");
-            }
-        });
-    }, 100);
-}
-
-function hasSwitchedToPtLanguage(response) {
-    //this request is the pt language json, we can only render pdf after it's loaded
-    return responseUrlContains(response, 'messages.json');
-}
-
-function isXhrFinished(response) {
-    //the following 3 requests are critical for the page to render, we need to wait
-    //for them to finish loading then we can click pt button
-    return responseUrlContains(response, 'reports.html') ||
-        responseUrlContains(response, 'skipped.json') ||
-        responseUrlContains(response, 'locales.json');
 }
