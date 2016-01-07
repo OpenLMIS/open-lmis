69bd5bf2d35a265defabf0d824fc82b937faff87
cuipengfei <cuipengfei2008@gmail.com>
2016-01-07 17:06:23 +0800

    CPF - 484- ignore ssl error, because we use self signed https

--- modules/requisition/src/main/java/org/openlmis/rnr/service/PhantomWrapper.java
+++ modules/requisition/src/main/java/org/openlmis/rnr/service/PhantomWrapper.java
@@ -2,7 +2,6 @@ package org.openlmis.rnr.service;
 
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -14,7 +13,7 @@ public class PhantomWrapper {
     public void generatePDF(String url, String pdfPath, String sessionid) throws IOException, URISyntaxException, InterruptedException {
         URL pdfScriptUrl = this.getClass().getClassLoader().getResource("pdf/rasterize.js");
         String pdfScriptpath = Paths.get(pdfScriptUrl.toURI()).toFile().getAbsolutePath();
-        ProcessBuilder phantomjs = new ProcessBuilder("phantomjs", pdfScriptpath, url, pdfPath, sessionid);
+        ProcessBuilder phantomjs = new ProcessBuilder("phantomjs", "--ignore-ssl-errors=yes", pdfScriptpath, url, pdfPath, sessionid);
         phantomjs.start().waitFor();
     }
 }
