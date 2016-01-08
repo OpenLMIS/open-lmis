40252a5c336f0c3a9fa4c469c594483787c25a88
cuipengfei <cuipengfei2008@gmail.com>
2016-01-08 17:53:45 +0800

    CPF - 484- let phantom timeout when tomcat timeout

--- modules/requisition/src/main/java/org/openlmis/rnr/service/PhantomWrapper.java
+++ modules/requisition/src/main/java/org/openlmis/rnr/service/PhantomWrapper.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
 
 @Component
 public class PhantomWrapper {
@@ -15,6 +14,6 @@ public class PhantomWrapper {
         URL pdfScriptUrl = this.getClass().getClassLoader().getResource("pdf/" + programCode + "-pdf-renderer.js");
         String pdfScriptpath = Paths.get(pdfScriptUrl.toURI()).toFile().getAbsolutePath();
         ProcessBuilder phantomjs = new ProcessBuilder("phantomjs", "--ignore-ssl-errors=yes", pdfScriptpath, url, pdfPath, sessionid);
-        phantomjs.start().waitFor(60, TimeUnit.SECONDS);
+        phantomjs.start().waitFor();//if phantom takes too long, it'll timeout when tomcat says so
     }
 }
