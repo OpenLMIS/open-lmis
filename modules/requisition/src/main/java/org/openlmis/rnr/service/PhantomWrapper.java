11857fc83225a9460f44e357ce37a6a6693b2d24
cuipengfei <cuipengfei2008@gmail.com>
2016-01-08 17:13:11 +0800

    CPF - 484- give phantom one minute to generate pdf, otherwise timeout

--- modules/requisition/src/main/java/org/openlmis/rnr/service/PhantomWrapper.java
+++ modules/requisition/src/main/java/org/openlmis/rnr/service/PhantomWrapper.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class PhantomWrapper {
@@ -14,6 +15,6 @@ public class PhantomWrapper {
         URL pdfScriptUrl = this.getClass().getClassLoader().getResource("pdf/" + programCode + "-pdf-renderer.js");
         String pdfScriptpath = Paths.get(pdfScriptUrl.toURI()).toFile().getAbsolutePath();
         ProcessBuilder phantomjs = new ProcessBuilder("phantomjs", "--ignore-ssl-errors=yes", pdfScriptpath, url, pdfPath, sessionid);
-        phantomjs.start().waitFor();
+        phantomjs.start().waitFor(60, TimeUnit.SECONDS);
     }
 }
