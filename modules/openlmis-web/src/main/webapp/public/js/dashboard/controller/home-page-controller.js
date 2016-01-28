c35aac162589cfec72fbccc5762c24c46f186dbc
Cedric Song <tcsong@thoughtworks.com>
2016-01-28 10:48:38 +0800

    HK - #553 - remove useless js code

--- modules/openlmis-web/src/main/webapp/public/js/dashboard/controller/home-page-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/dashboard/controller/home-page-controller.js
@@ -1,10 +1,8 @@
 function homePageController($scope, FeatureToggleService) {
-    $scope.h1 = "";
     $scope.showDashboard = false;
     $scope.showHomepage = false;
     var viewToggleKey = {key: "homepage.view"};
     FeatureToggleService.get(viewToggleKey, function (result) {
-        console.log(result);
         if (!result.key) {
             $scope.showDashboard = true;
         } else {
