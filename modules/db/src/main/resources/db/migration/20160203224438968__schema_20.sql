27dfa43b4b3bb33944ac234f2a64bae906b20cee
Chongsun Ahn <cahn922@yahoo.com>
2016-02-04 15:29:23 -0800

    Remove lock_timeout statement, breaking the build

--- modules/db/src/main/resources/db/migration/20160203224438968__schema_20.sql
+++ modules/db/src/main/resources/db/migration/20160203224438968__schema_20.sql
@@ -2,13 +2,6 @@
 -- PostgreSQL database dump
 --
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
 --
 -- Name: atomfeed; Type: SCHEMA; Schema: -; Owner: postgres
 --
