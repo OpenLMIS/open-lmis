5faa2655ddfc1ea3a2d89d1bd9dfa223a5c32295
cuipengfei <cuipengfei2008@gmail.com>
2016-02-17 11:46:16 +0800

    CPF -591- smaller methods in the code that is about to be changed

--- modules/rest-api/src/main/java/org/openlmis/restapi/service/RestStockCardService.java
+++ modules/rest-api/src/main/java/org/openlmis/restapi/service/RestStockCardService.java
@@ -23,125 +23,132 @@ import java.util.*;
 @NoArgsConstructor
 public class RestStockCardService {
 
-  @Autowired
-  private FacilityRepository facilityRepository;
+    @Autowired
+    private FacilityRepository facilityRepository;
 
-  @Autowired
-  private ProductService productService;
+    @Autowired
+    private ProductService productService;
 
-  @Autowired
-  private StockAdjustmentReasonRepository stockAdjustmentReasonRepository;
+    @Autowired
+    private StockAdjustmentReasonRepository stockAdjustmentReasonRepository;
 
-  @Autowired
-  private StockCardService stockCardService;
+    @Autowired
+    private StockCardService stockCardService;
 
-  @Transactional
-  public List<StockCardEntry> adjustStock(Long facilityId, List<StockEvent> stockEventList, Long userId) {
-    if (!validFacility(facilityId)) {
-      throw new DataException("error.facility.unknown");
+    @Transactional
+    public List<StockCardEntry> adjustStock(Long facilityId, List<StockEvent> stockEvents, Long userId) {
+        if (!validFacility(facilityId)) {
+            throw new DataException("error.facility.unknown");
+        }
+
+        List<StockCardEntry> entries = createStockCardEntries(stockEvents, facilityId, userId);
+        stockCardService.addStockCardEntries(entries);
+        return entries;
     }
 
-    Map<String, StockCard> stockCardMap = new HashMap<>();
-    List<StockCardEntry> entries = new ArrayList<>();
+    @Transactional
+    public void updateStockCardSyncTime(Long facilityId, List<String> stockCardProductCodeList) {
+        if (facilityId == null || stockCardProductCodeList == null) {
+            throw new DataException("");
+        }
 
-    for (StockEvent stockEvent : stockEventList) {
-      String errorInStockEvent = validateStockEvent(stockEvent);
-      if (errorInStockEvent != null) {
-        throw new DataException(errorInStockEvent);
-      }
-
-      String productCode = stockEvent.getProductCode();
-      StockCard stockCard;
-
-      if (stockCardMap.get(productCode) == null) {
-        stockCard = stockCardService.getOrCreateStockCard(facilityId, productCode);
-        stockCardMap.put(productCode, stockCard);
-      } else {
-        stockCard = stockCardMap.get(productCode);
-      }
-
-      StockCardEntry entry = createStockCardEntry(stockEvent, stockCard, userId);
-      entries.add(entry);
-    }
-    stockCardService.addStockCardEntries(entries);
-    return entries;
-  }
-
-
-  private StockCardEntry createStockCardEntry(StockEvent stockEvent, StockCard stockCard, Long userId) {
-    StockAdjustmentReason stockAdjustmentReason = stockAdjustmentReasonRepository.getAdjustmentReasonByName(stockEvent.getReasonName());
-
-    long quantity = stockEvent.getQuantity();
-    quantity = stockAdjustmentReason.getAdditive() ? quantity : quantity * -1;
-
-    StockCardEntry entry = new StockCardEntry(stockCard, StockCardEntryType.ADJUSTMENT, quantity, stockEvent.getOccurred(), stockEvent.getReferenceNumber());
-    entry.setAdjustmentReason(stockAdjustmentReason);
-    entry.setCreatedBy(userId);
-    entry.setModifiedBy(userId);
-    entry.setCreatedDate(stockEvent.getCreatedTime());
-
-    Map<String, String> customProps = stockEvent.getCustomProps();
-    if (null != customProps) {
-      for (String k : customProps.keySet()) {
-        entry.addKeyValue(k, customProps.get(k));
-      }
-    }
-    return entry;
-  }
-
-  private boolean validFacility(Long facilityId) {
-    return facilityRepository.getById(facilityId) != null;
-  }
-
-  private String validateStockEvent(StockEvent stockEvent) {
-    if (!stockEvent.isValidAdjustment()) return "error.stockmanagement.invalidadjustment";
-    if (!validProduct(stockEvent)) return "error.product.unknown";
-    if (!validAdjustmentReason(stockEvent)) return "error.stockadjustmentreason.unknown";
-    return null;
-  }
-
-  private boolean validProduct(StockEvent stockEvent) {
-    return productService.getByCode(stockEvent.getProductCode()) != null;
-  }
-
-  private boolean validAdjustmentReason(StockEvent stockEvent) {
-    return stockAdjustmentReasonRepository.getAdjustmentReasonByName(stockEvent.getReasonName()) != null;
-  }
-
-  public List<StockCardDTO> queryStockCardByOccurred(long facilityId, Date startTime, Date endTime) {
-    List<StockCard> stockCards = stockCardService.queryStockCardByOccurred(facilityId, startTime, endTime);
-
-    return transformStockCardsToDTOs(stockCards);
-  }
-
-  private List<StockCardDTO> transformStockCardsToDTOs(List<StockCard> stockCards) {
-    List<StockCardDTO> stockCardDTOs = new ArrayList<>();
-    for (StockCard stockCard : stockCards) {
-      StockCardDTO stockCardDTO = new StockCardDTO(stockCard);
-      stockCardDTOs.add(stockCardDTO);
-      stockCardDTO.setStockMovementItems(transformStockCardEntries(stockCard.getEntries()));
-    }
-    return stockCardDTOs;
-  }
-
-  private List<StockCardMovementDTO> transformStockCardEntries(List<StockCardEntry> stockCardEntries) {
-    ArrayList<StockCardMovementDTO> stockCardMovementDTOs = new ArrayList<>();
-    for (StockCardEntry stockCardEntry : stockCardEntries) {
-      stockCardMovementDTOs.add(new StockCardMovementDTO(stockCardEntry));
-    }
-    return stockCardMovementDTOs;
-  }
-
-  @Transactional
-  public void updateStockCardSyncTime(Long facilityId, List<String> stockCardProductCodeList) {
-    if (facilityId == null || stockCardProductCodeList == null) {
-      throw new DataException("");
+        if (stockCardProductCodeList.isEmpty()) {
+            stockCardService.updateAllStockCardSyncTimeForFacilityToNow(facilityId);
+        } else {
+            stockCardService.updateStockCardSyncTimeToNow(facilityId, stockCardProductCodeList);
+        }
     }
 
-    if (stockCardProductCodeList.isEmpty()) {
-      stockCardService.updateAllStockCardSyncTimeForFacilityToNow(facilityId);
-    } else {
-      stockCardService.updateStockCardSyncTimeToNow(facilityId, stockCardProductCodeList);
+    public List<StockCardDTO> queryStockCardByOccurred(long facilityId, Date startTime, Date endTime) {
+        List<StockCard> stockCards = stockCardService.queryStockCardByOccurred(facilityId, startTime, endTime);
+
+        return transformStockCardsToDTOs(stockCards);
     }
-  }
+
+    private List<StockCardEntry> createStockCardEntries(List<StockEvent> stockEvents, Long facilityId, Long userId) {
+        Map<String, StockCard> stockCardMap = new HashMap<>();
+        List<StockCardEntry> entries = new ArrayList<>();
+
+        for (StockEvent stockEvent : stockEvents) {
+            String errorInStockEvent = validateStockEvent(stockEvent);
+            if (errorInStockEvent != null) {
+                throw new DataException(errorInStockEvent);
+            }
+
+            StockCard stockCard = getOrCreateStockCard(facilityId, stockEvent.getProductCode(), stockCardMap);
+            StockCardEntry entry = createStockCardEntry(stockEvent, stockCard, userId);
+            entries.add(entry);
+        }
+        return entries;
+    }
+
+    private StockCard getOrCreateStockCard(Long facilityId, String productCode, Map<String, StockCard> stockCardMap) {
+        StockCard stockCard;
+
+        if (stockCardMap.get(productCode) == null) {
+            stockCard = stockCardService.getOrCreateStockCard(facilityId, productCode);
+            stockCardMap.put(productCode, stockCard);
+        } else {
+            stockCard = stockCardMap.get(productCode);
+        }
+        return stockCard;
+    }
+
+    private StockCardEntry createStockCardEntry(StockEvent stockEvent, StockCard stockCard, Long userId) {
+        StockAdjustmentReason stockAdjustmentReason = stockAdjustmentReasonRepository.getAdjustmentReasonByName(stockEvent.getReasonName());
+
+        long quantity = stockEvent.getQuantity();
+        quantity = stockAdjustmentReason.getAdditive() ? quantity : quantity * -1;
+
+        StockCardEntry entry = new StockCardEntry(stockCard, StockCardEntryType.ADJUSTMENT, quantity, stockEvent.getOccurred(), stockEvent.getReferenceNumber());
+        entry.setAdjustmentReason(stockAdjustmentReason);
+        entry.setCreatedBy(userId);
+        entry.setModifiedBy(userId);
+        entry.setCreatedDate(stockEvent.getCreatedTime());
+
+        Map<String, String> customProps = stockEvent.getCustomProps();
+        if (null != customProps) {
+            for (String k : customProps.keySet()) {
+                entry.addKeyValue(k, customProps.get(k));
+            }
+        }
+        return entry;
+    }
+
+    private boolean validFacility(Long facilityId) {
+        return facilityRepository.getById(facilityId) != null;
+    }
+
+    private String validateStockEvent(StockEvent stockEvent) {
+        if (!stockEvent.isValidAdjustment()) return "error.stockmanagement.invalidadjustment";
+        if (!validProduct(stockEvent)) return "error.product.unknown";
+        if (!validAdjustmentReason(stockEvent)) return "error.stockadjustmentreason.unknown";
+        return null;
+    }
+
+    private boolean validProduct(StockEvent stockEvent) {
+        return productService.getByCode(stockEvent.getProductCode()) != null;
+    }
+
+    private boolean validAdjustmentReason(StockEvent stockEvent) {
+        return stockAdjustmentReasonRepository.getAdjustmentReasonByName(stockEvent.getReasonName()) != null;
+    }
+
+    private List<StockCardDTO> transformStockCardsToDTOs(List<StockCard> stockCards) {
+        List<StockCardDTO> stockCardDTOs = new ArrayList<>();
+        for (StockCard stockCard : stockCards) {
+            StockCardDTO stockCardDTO = new StockCardDTO(stockCard);
+            stockCardDTOs.add(stockCardDTO);
+            stockCardDTO.setStockMovementItems(transformStockCardEntries(stockCard.getEntries()));
+        }
+        return stockCardDTOs;
+    }
+
+    private List<StockCardMovementDTO> transformStockCardEntries(List<StockCardEntry> stockCardEntries) {
+        ArrayList<StockCardMovementDTO> stockCardMovementDTOs = new ArrayList<>();
+        for (StockCardEntry stockCardEntry : stockCardEntries) {
+            stockCardMovementDTOs.add(new StockCardMovementDTO(stockCardEntry));
+        }
+        return stockCardMovementDTOs;
+    }
 }
