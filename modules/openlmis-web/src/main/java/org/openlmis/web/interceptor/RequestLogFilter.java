256d42d0b263158d5641d7ff30c59f2ee1d84c5b
cuipengfei <cuipengfei2008@gmail.com>
2016-03-01 17:17:00 +0800

    CPF -591- use the spring provided api to log instead of using an interceptor

---
+++ modules/openlmis-web/src/main/java/org/openlmis/web/interceptor/RequestLogFilter.java
@@ -0,0 +1,28 @@
+package org.openlmis.web.interceptor;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.filter.AbstractRequestLoggingFilter;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class RequestLogFilter extends AbstractRequestLoggingFilter {
+
+    Logger logger = Logger.getLogger(getClass());
+
+    @Override
+    protected void beforeRequest(HttpServletRequest request, String message) {
+        //the request body is only available after request, so don't log anything here
+    }
+
+    @Override
+    protected void afterRequest(HttpServletRequest request, String message) {
+        logger.info("User: " + request.getHeader("UserName") +
+                " Facility: " + request.getHeader("FacilityName") +
+                " Device:" + request.getHeader("DeviceInfo"));
+        if (!message.contains("password")) {
+            logger.info(message);
+        } else {
+            logger.info("request body contains sensitive data, skipping");
+        }
+    }
+}
