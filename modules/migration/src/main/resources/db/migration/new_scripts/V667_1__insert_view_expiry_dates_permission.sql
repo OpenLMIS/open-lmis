e075897c147edc59fd44ab502ffeb86a69064b0e
mingong <gongmingqm10@foxmail.com>
2016-03-29 15:43:48 +0800

    DY&MG - #605 - Add view expiry dates permission

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V667_1__insert_view_expiry_dates_permission.sql
@@ -0,0 +1,2 @@
+INSERT INTO rights (name, rightType, description, displaynamekey) VALUES
+('VIEW_EXPIRY_DATES_REPORT','REPORT','Permission to View expiry dates Reports', 'right.report.expiry.dates');
