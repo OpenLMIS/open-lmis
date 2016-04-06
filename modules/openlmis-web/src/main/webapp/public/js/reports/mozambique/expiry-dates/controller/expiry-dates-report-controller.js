6347093b02f3e6ece3d959e3f2c4b4065ee6c78e
Danni Yu <dyu@thoughtworks.com>
2016-04-06 18:15:39 +0800

    DY - #000 - delete console log

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/expiry-dates/controller/expiry-dates-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/expiry-dates/controller/expiry-dates-report-controller.js
@@ -86,7 +86,6 @@ function ExpiryDatesReportController($scope, $filter, $controller, $http, CubesG
                 };
             }
         });
-        console.log(drugOccurredHash);
         return drugOccurredHash;
     }
 
