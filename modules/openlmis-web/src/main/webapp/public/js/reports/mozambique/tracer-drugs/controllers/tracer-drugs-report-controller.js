7dc3b5daac1197301a96f353d117f372c1579cbf
sherlockHK <576717731@qq.com>
2016-04-21 10:20:59 +0800

    HK -#706 - Add button to download tracer drugs csv file.

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/tracer-drugs/controllers/tracer-drugs-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/tracer-drugs/controllers/tracer-drugs-report-controller.js
@@ -1,4 +1,4 @@
-function TracerDrugsReportController($scope, $controller, DateFormatService, TracerDrugsChartService) {
+function TracerDrugsReportController($scope, $controller, $filter,DateFormatService, TracerDrugsChartService, CubesGenerateUrlService) {
     $controller('BaseProductReportController', {$scope: $scope});
 
     $scope.getTimeRange = function (dateRange) {
@@ -6,16 +6,44 @@ function TracerDrugsReportController($scope, $controller, DateFormatService, Tra
         $scope.reportParams.endTime = dateRange.endTime;
     };
 
+    var selectedProvinceCode = $scope.reportParams.provinceId ? $scope.getGeographicZoneById($scope.provinces, $scope.reportParams.provinceId).code : undefined;
+    var selectedDistrictCode = $scope.reportParams.districtId ? $scope.getGeographicZoneById($scope.districts, $scope.reportParams.districtId).code : undefined;
+
     $scope.$on('$viewContentLoaded', function () {
         renderDefaultTracerDrugsReport();
     });
 
     $scope.loadReport = function () {
-        var selectedProvinceCode = $scope.reportParams.provinceId ? $scope.getGeographicZoneById($scope.provinces, $scope.reportParams.provinceId).code : undefined;
-        var selectedDistrictCode = $scope.reportParams.districtId ? $scope.getGeographicZoneById($scope.districts, $scope.reportParams.districtId).code : undefined;
-
         TracerDrugsChartService.makeTracerDrugsChart('tracer-report', new Date($scope.reportParams.startTime), new Date($scope.reportParams.endTime), selectedProvinceCode, selectedDistrictCode);
     };
+
+    $scope.downLoadRawData = function () {
+        var fields = ["facility.facility_name", "drug.drug_name,date,soh"];
+        window.open(CubesGenerateUrlService.generateFactsUrlForCsvDownloading('vw_weekly_tracer_soh', fields, generateCutParams()));
+    };
+
+    function generateCutParams() {
+        var cutsParams = [{
+            dimension: "cutDate",
+            values: [$filter('date')($scope.reportParams.startTime, "yyyy,MM,dd") + "-" + $filter('date')($scope.reportParams.endTime, "yyyy,MM,dd")]
+        }];
+        var zoneConfig = getUserSelectedZoneConfig(selectedProvinceCode, selectedDistrictCode);
+        if (zoneConfig.isOneDistrict) {
+            cutsParams.push({
+                dimension: "location",
+                values: [[selectedProvinceCode, selectedDistrictCode]]
+            });
+        } else if (zoneConfig.isOneProvince) {
+            cutsParams.push({dimension: "location", values: [selectedProvinceCode]});
+        }
+        return cutsParams;
+    }
+
+    function getUserSelectedZoneConfig(provinceCode, districtCode) {
+        var isOneDistrict = provinceCode !== undefined && districtCode !== undefined;
+        var isOneProvince = provinceCode !== undefined && districtCode === undefined;
+        return {isOneDistrict: isOneDistrict, isOneProvince: isOneProvince};
+    }
 
     function renderDefaultTracerDrugsReport() {
         var defaultStartTime = DateFormatService.formatDateWithFirstDayOfMonth(new Date());
