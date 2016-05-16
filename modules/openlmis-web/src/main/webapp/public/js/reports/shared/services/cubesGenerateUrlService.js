2e81ffcce075f2618bf57a9034664a7ed53dd77c
Yingli Yuan <ylyuan@thoughtworks.com>
2016-05-16 15:26:36 +0800

    YL - #702 - refactor: make cuts Consistent in cubesGenerateUrlService

--- modules/openlmis-web/src/main/webapp/public/js/reports/shared/services/cubesGenerateUrlService.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/shared/services/cubesGenerateUrlService.js
@@ -2,24 +2,24 @@ services.factory('CubesGenerateUrlService', function () {
     var baseUrl = "/cubesreports/cube/";
 
     var generateAggregateUrl = function (cubesName, drillDowns, cuts) {
-        return baseUrl + cubesName + "/aggregate" + "?drilldown=" + drillDowns.join("|") + "&cut=" + generateCuts(cuts);
+        return baseUrl + cubesName + "/aggregate" + "?drilldown=" + drillDowns.join("|") + "&cut=" + generateCutsWithDivider(cuts, ';');
     };
 
     var generateFactsUrl = function (cubesName, cuts) {
-        return baseUrl + cubesName + "/facts" + "?cut=" + generateCuts(cuts);
+        return baseUrl + cubesName + "/facts" + "?cut=" + generateCutsWithDivider(cuts, ';');
     };
 
     var generateMembersUrl = function (cubesName, cut) {
-        return baseUrl + cubesName + "/members/" + cut.dimension + "?cut=" + cut.dimension + ":" + cut.value;
+        return baseUrl + cubesName + "/members/" + cut.dimension + "?cut=" + generateCutsWithDivider([cut], ',');
     };
 
     var generateFactsUrlWithParams = function (cubesName, cuts, params) {
         return generateFactsUrl(cubesName, cuts) + "&" + jQuery.param(params);
     };
 
-    function generateCuts(cuts) {
+    function generateCutsWithDivider(cuts, divider) {
         return _.map(cuts, function (cut) {
-            return cut.dimension + ":" + cut.values.join(";");
+            return cut.dimension + ":" + cut.values.join(divider);
         }).join("|");
     }
 
