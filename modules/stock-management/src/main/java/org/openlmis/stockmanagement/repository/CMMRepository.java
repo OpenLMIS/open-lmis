5b1dfba624aeb033aaa65bacff4a33828614172b
cuipengfei <cuipengfei2008@gmail.com>
2016-05-30 12:04:14 +0800

    CPF -752- give cmm repository the ability to query cmm value by date

--- modules/stock-management/src/main/java/org/openlmis/stockmanagement/repository/CMMRepository.java
+++ modules/stock-management/src/main/java/org/openlmis/stockmanagement/repository/CMMRepository.java
@@ -5,19 +5,25 @@ import org.openlmis.stockmanagement.repository.mapper.CMMMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public class CMMRepository {
 
-  @Autowired
-  private CMMMapper mapper;
+    @Autowired
+    private CMMMapper mapper;
 
-  public void createOrUpdate(CMMEntry entry) {
-    CMMEntry existingEntry = mapper.getCMMEntryByFacilityAndPeriodAndProductCode(entry.getFacilityId(), entry.getProductCode(), entry.getPeriodBegin(), entry.getPeriodEnd());
-    if (existingEntry == null) {
-      mapper.insert(entry);
-    } else {
-      entry.setId(existingEntry.getId());
-      mapper.update(entry);
+    public void createOrUpdate(CMMEntry entry) {
+        CMMEntry existingEntry = mapper.getCMMEntryByFacilityAndPeriodAndProductCode(entry.getFacilityId(), entry.getProductCode(), entry.getPeriodBegin(), entry.getPeriodEnd());
+        if (existingEntry == null) {
+            mapper.insert(entry);
+        } else {
+            entry.setId(existingEntry.getId());
+            mapper.update(entry);
+        }
     }
-  }
+
+    public Float getCmmValue(long facilityId, String productCode, Date day) {
+        return mapper.getCMMEntryByFacilityAndDayAndProductCode(facilityId, productCode, day).getCmmValue();
+    }
 }
--- modules/stock-management/src/test/java/org/openlmis/stockmanagement/repository/CMMRepositoryTest.java
+++ modules/stock-management/src/test/java/org/openlmis/stockmanagement/repository/CMMRepositoryTest.java
@@ -14,7 +14,9 @@ import org.openlmis.stockmanagement.repository.mapper.CMMMapper;
 import java.util.Date;
 import java.util.List;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.when;
 
@@ -22,46 +24,57 @@ import static org.powermock.api.mockito.PowerMockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class CMMRepositoryTest {
 
-  @Mock
-  private CMMMapper mapper;
+    @Mock
+    private CMMMapper mapper;
 
-  @InjectMocks
-  private CMMRepository repository;
+    @InjectMocks
+    private CMMRepository repository;
 
-  @Test
-  public void shouldUpdateCMMIfItAlreadyExists() {
-    Date beginDate = new Date();
-    Date endDate = new Date();
-    CMMEntry cmm = createCMMEntry(1.0F, "P1", beginDate, endDate, 9L);
-    cmm.setId(999L);
-    when(mapper.getCMMEntryByFacilityAndPeriodAndProductCode(9L, "P1", beginDate, endDate)).thenReturn(cmm);
+    @Test
+    public void shouldUpdateCMMIfItAlreadyExists() {
+        Date beginDate = new Date();
+        Date endDate = new Date();
+        CMMEntry cmm = createCMMEntry(1.0F, "P1", beginDate, endDate, 9L);
+        cmm.setId(999L);
+        when(mapper.getCMMEntryByFacilityAndPeriodAndProductCode(9L, "P1", beginDate, endDate)).thenReturn(cmm);
 
-    repository.createOrUpdate(cmm);
+        repository.createOrUpdate(cmm);
 
-    ArgumentCaptor<CMMEntry> captor = ArgumentCaptor.forClass(CMMEntry.class);
-    verify(mapper).update(captor.capture());
-    List<CMMEntry> captorAllValues = captor.getAllValues();
-    assertEquals(999L, captorAllValues.get(0).getId().longValue());
-  }
+        ArgumentCaptor<CMMEntry> captor = ArgumentCaptor.forClass(CMMEntry.class);
+        verify(mapper).update(captor.capture());
+        List<CMMEntry> captorAllValues = captor.getAllValues();
+        assertEquals(999L, captorAllValues.get(0).getId().longValue());
+    }
 
-  @Test
-  public void shouldInsertCMMIfItDoesNotExist() {
-    Date beginDate = new Date();
-    Date endDate = new Date();
-    CMMEntry cmm = createCMMEntry(1.0F, "P1", beginDate, endDate, 9L);
-    when(mapper.getCMMEntryByFacilityAndPeriodAndProductCode(9L, "P1", beginDate, endDate)).thenReturn(null);
+    @Test
+    public void shouldInsertCMMIfItDoesNotExist() {
+        Date beginDate = new Date();
+        Date endDate = new Date();
+        CMMEntry cmm = createCMMEntry(1.0F, "P1", beginDate, endDate, 9L);
+        when(mapper.getCMMEntryByFacilityAndPeriodAndProductCode(9L, "P1", beginDate, endDate)).thenReturn(null);
 
-    repository.createOrUpdate(cmm);
-    verify(mapper).insert(cmm);
-  }
+        repository.createOrUpdate(cmm);
+        verify(mapper).insert(cmm);
+    }
 
-  private CMMEntry createCMMEntry(Float cmmValue, String productCode, Date beginDate, Date endDate, Long facilityId) {
-    CMMEntry entry = new CMMEntry();
-    entry.setProductCode(productCode);
-    entry.setCmmValue(cmmValue);
-    entry.setPeriodBegin(beginDate);
-    entry.setPeriodEnd(endDate);
-    entry.setFacilityId(facilityId);
-    return entry;
-  }
+    @Test
+    public void shouldGetCmmValueByDay() throws Exception {
+        Date beginDate = new Date();
+        Date endDate = new Date();
+        CMMEntry cmm = createCMMEntry(1.0F, "P1", beginDate, endDate, 9L);
+        when(mapper.getCMMEntryByFacilityAndDayAndProductCode(9L, "P1", beginDate)).thenReturn(cmm);
+
+        Float cmmValue = repository.getCmmValue(9L, "P1", beginDate);
+        assertThat(cmmValue, is(1.0F));
+    }
+
+    private CMMEntry createCMMEntry(Float cmmValue, String productCode, Date beginDate, Date endDate, Long facilityId) {
+        CMMEntry entry = new CMMEntry();
+        entry.setProductCode(productCode);
+        entry.setCmmValue(cmmValue);
+        entry.setPeriodBegin(beginDate);
+        entry.setPeriodEnd(endDate);
+        entry.setFacilityId(facilityId);
+        return entry;
+    }
 }
