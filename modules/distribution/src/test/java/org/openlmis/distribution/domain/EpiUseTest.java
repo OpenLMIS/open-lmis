7d874b9dcaa08c4201e8b19847484cf46a599679
Łukasz Lewczyński <llewczynski@soldevelo.com>
2016-05-31 09:11:51 +0200

    Changed the way how Epi Use Line Item are sorted

--- modules/distribution/src/test/java/org/openlmis/distribution/domain/EpiUseTest.java
+++ modules/distribution/src/test/java/org/openlmis/distribution/domain/EpiUseTest.java
@@ -85,4 +85,78 @@ public class EpiUseTest {
     assertThat(lineItems.get(1).getProductGroup().getName(), is(productGroup1.getName()));
   }
 
+  @Test
+  public void shouldSortLineItemByProductDisplayOrder() throws Exception {
+    Facility facility = new Facility(2L);
+    ProgramSupported programSupported = new ProgramSupported(1L, true, new Date());
+
+    FacilityProgramProduct facilityProgramProduct1 = mock(FacilityProgramProduct.class);
+    FacilityProgramProduct facilityProgramProduct2 = mock(FacilityProgramProduct.class);
+    FacilityProgramProduct facilityProgramProduct3 = mock(FacilityProgramProduct.class);
+
+    ProductGroup productGroup1 = new ProductGroup("PG3", "PG3", 3L);
+    ProductGroup productGroup2 = new ProductGroup("PG2", "PG2", 1L);
+    ProductGroup productGroup3 = new ProductGroup("PG1", "PG1", 2L);
+
+    when(facilityProgramProduct1.getActiveProductGroup()).thenReturn(productGroup1);
+    when(facilityProgramProduct2.getActiveProductGroup()).thenReturn(productGroup2);
+    when(facilityProgramProduct3.getActiveProductGroup()).thenReturn(productGroup3);
+
+    List<FacilityProgramProduct> programProducts = asList(facilityProgramProduct1, facilityProgramProduct2, facilityProgramProduct3);
+    programSupported.setProgramProducts(programProducts);
+    facility.setSupportedPrograms(asList(programSupported));
+
+    mockStatic(FacilityProgramProduct.class);
+    when(FacilityProgramProduct.filterActiveProducts(programProducts)).thenReturn(programProducts);
+    FacilityVisit facilityVisit = new FacilityVisit();
+
+    EpiUse epiUse = new EpiUse(facility, facilityVisit);
+
+    List<EpiUseLineItem> lineItems = epiUse.getLineItems();
+    assertThat(lineItems.size(), is(3));
+    assertThat(lineItems.get(0).getProductGroup().getCode(), is(productGroup2.getCode()));
+    assertThat(lineItems.get(0).getProductGroup().getName(), is(productGroup2.getName()));
+    assertThat(lineItems.get(1).getProductGroup().getCode(), is(productGroup3.getCode()));
+    assertThat(lineItems.get(1).getProductGroup().getName(), is(productGroup3.getName()));
+    assertThat(lineItems.get(2).getProductGroup().getCode(), is(productGroup1.getCode()));
+    assertThat(lineItems.get(2).getProductGroup().getName(), is(productGroup1.getName()));
+  }
+
+  @Test
+  public void shouldSortLineItemByProductDisplayOrderAndProductGroupCode() throws Exception {
+    Facility facility = new Facility(2L);
+    ProgramSupported programSupported = new ProgramSupported(1L, true, new Date());
+
+    FacilityProgramProduct facilityProgramProduct1 = mock(FacilityProgramProduct.class);
+    FacilityProgramProduct facilityProgramProduct2 = mock(FacilityProgramProduct.class);
+    FacilityProgramProduct facilityProgramProduct3 = mock(FacilityProgramProduct.class);
+
+    ProductGroup productGroup1 = new ProductGroup("PG3", "PG3", 2L);
+    ProductGroup productGroup2 = new ProductGroup("PG2", "PG2", 1L);
+    ProductGroup productGroup3 = new ProductGroup("PG1", "PG1", null);
+
+    when(facilityProgramProduct1.getActiveProductGroup()).thenReturn(productGroup1);
+    when(facilityProgramProduct2.getActiveProductGroup()).thenReturn(productGroup2);
+    when(facilityProgramProduct3.getActiveProductGroup()).thenReturn(productGroup3);
+
+    List<FacilityProgramProduct> programProducts = asList(facilityProgramProduct1, facilityProgramProduct2, facilityProgramProduct3);
+    programSupported.setProgramProducts(programProducts);
+    facility.setSupportedPrograms(asList(programSupported));
+
+    mockStatic(FacilityProgramProduct.class);
+    when(FacilityProgramProduct.filterActiveProducts(programProducts)).thenReturn(programProducts);
+    FacilityVisit facilityVisit = new FacilityVisit();
+
+    EpiUse epiUse = new EpiUse(facility, facilityVisit);
+
+    List<EpiUseLineItem> lineItems = epiUse.getLineItems();
+    assertThat(lineItems.size(), is(3));
+    assertThat(lineItems.get(0).getProductGroup().getCode(), is(productGroup2.getCode()));
+    assertThat(lineItems.get(0).getProductGroup().getName(), is(productGroup2.getName()));
+    assertThat(lineItems.get(1).getProductGroup().getCode(), is(productGroup1.getCode()));
+    assertThat(lineItems.get(1).getProductGroup().getName(), is(productGroup1.getName()));
+    assertThat(lineItems.get(2).getProductGroup().getCode(), is(productGroup3.getCode()));
+    assertThat(lineItems.get(2).getProductGroup().getName(), is(productGroup3.getName()));
+  }
+
 }
