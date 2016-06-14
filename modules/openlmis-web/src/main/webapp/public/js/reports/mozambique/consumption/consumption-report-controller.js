a64fdd9ab0eb94b2af1cca2177e28d469b4bfafc
Hanxian Lin <saitoplam@hotmail.com>
2016-06-14 16:38:20 +0800

    LHX - 68 - move splitPeriods method to base controller in order to reuse

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/consumption/consumption-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/consumption/consumption-report-controller.js
@@ -72,48 +72,4 @@ function ConsumptionReportController($scope, $controller, $filter, $http, $q, Cu
         return !$scope.noProductSelected;
     }
 
-    $scope.splitPeriods = function (start, end) {
-        var previousMonth = -1, thisMonth = 0, nextMonth = 1, periodStartDay = 21, periodEndDay = 20;
-
-        function shiftMonthAtDay(date, shiftMonth, atDay) {
-            var resultDate = new Date(date);
-            resultDate.setMonth(date.getMonth() + shiftMonth);
-            resultDate.setDate(atDay);
-            return resultDate;
-        }
-
-        function periodOf(date) {
-            var coveredDate = new Date(date);
-            var periodStart, periodEnd;
-            if (coveredDate.getDate() < periodStartDay) {
-                periodStart = shiftMonthAtDay(coveredDate, previousMonth, periodStartDay);
-                periodEnd = shiftMonthAtDay(coveredDate, thisMonth, periodEndDay);
-            } else {
-                periodStart = shiftMonthAtDay(coveredDate, thisMonth, periodStartDay);
-                periodEnd = shiftMonthAtDay(coveredDate, nextMonth, periodEndDay);
-            }
-            return {periodStart: periodStart, periodEnd: periodEnd};
-        }
-
-        function nextPeriod(period) {
-            return {
-                periodStart: shiftMonthAtDay(period.periodStart, nextMonth, periodStartDay),
-                periodEnd: shiftMonthAtDay(period.periodEnd, nextMonth, periodEndDay)
-            };
-        }
-
-        function periodsInBetween(first, last) {
-            var periods = [first];
-
-            var next = nextPeriod(first);
-            while (next.periodStart.getTime() <= last.periodStart.getTime()) {
-                periods.push(next);
-                next = nextPeriod(next);
-            }
-
-            return periods;
-        }
-
-        return periodsInBetween(periodOf(start), periodOf(end));
-    };
 }
--- modules/openlmis-web/src/main/webapp/public/js/reports/shared/controller/base-product-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/shared/controller/base-product-report-controller.js
@@ -210,4 +210,49 @@ function BaseProductReportController($scope, $filter, ProductReportService, $cac
         }, $dialog);
     }
 
+    $scope.splitPeriods = function (start, end) {
+        var previousMonth = -1, thisMonth = 0, nextMonth = 1, periodStartDay = 21, periodEndDay = 20;
+
+        function shiftMonthAtDay(date, shiftMonth, atDay) {
+            var resultDate = new Date(date);
+            resultDate.setMonth(date.getMonth() + shiftMonth);
+            resultDate.setDate(atDay);
+            return resultDate;
+        }
+
+        function periodOf(date) {
+            var coveredDate = new Date(date);
+            var periodStart, periodEnd;
+            if (coveredDate.getDay() < periodStartDay) {
+                periodStart = shiftMonthAtDay(coveredDate, previousMonth, periodStartDay);
+                periodEnd = shiftMonthAtDay(coveredDate, thisMonth, periodEndDay);
+            } else {
+                periodStart = shiftMonthAtDay(coveredDate, thisMonth, periodStartDay);
+                periodEnd = shiftMonthAtDay(coveredDate, nextMonth, periodEndDay);
+            }
+            return {periodStart: periodStart, periodEnd: periodEnd};
+        }
+
+        function nextPeriod(period) {
+            return {
+                periodStart: shiftMonthAtDay(period.periodStart, nextMonth, periodStartDay),
+                periodEnd: shiftMonthAtDay(period.periodEnd, nextMonth, periodEndDay)
+            };
+        }
+
+        function periodsInBetween(first, last) {
+            var periods = [first];
+
+            var next = nextPeriod(first);
+            while (next.periodStart.getTime() <= last.periodStart.getTime()) {
+                periods.push(next);
+                next = nextPeriod(next);
+            }
+
+            return periods;
+        }
+
+        return periodsInBetween(periodOf(start), periodOf(end));
+    };
+
 }
