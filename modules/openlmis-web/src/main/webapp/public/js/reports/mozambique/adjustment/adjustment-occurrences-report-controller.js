9b915647475cdaaccca29551f8e28a4cd0dcc3b0
Hanxian Lin <saitoplam@hotmail.com>
2016-06-14 15:11:20 +0800

    LHX - 68 -create adjustment type selection field and set default value to negative;

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/adjustment/adjustment-occurrences-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/adjustment/adjustment-occurrences-report-controller.js
@@ -1,6 +1,13 @@
 function AdjustmentOccurrencesReportController($scope, $controller) {
   $controller('BaseProductReportController', {$scope: $scope});
 
+  $scope.adjustmentTypes = [
+    {value: 'negative', name: 'Negative Adjustment'},
+    {value: 'positive', name:'Positive Adjustment'}
+  ];
+
+  $scope.selectedAdjustmentType= '';
+
   $scope.$on('$viewContentLoaded', function () {
     $scope.loadProducts();
     $scope.loadHealthFacilities();
