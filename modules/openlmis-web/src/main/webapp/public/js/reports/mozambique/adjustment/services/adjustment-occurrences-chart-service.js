10612ef7bfa61a23387918966239237630104707
Hanxian Lin <saitoplam@hotmail.com>
2016-06-16 15:39:53 +0800

    LHX - 68 - change legend sequence of adjustment chart; make period not start on 0 point of x axis

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/adjustment/services/adjustment-occurrences-chart-service.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/adjustment/services/adjustment-occurrences-chart-service.js
@@ -4,6 +4,13 @@ services.factory('AdjustmentOccurrencesChartService', function (messageService) 
     var adjustmentGraphConfigs = {
       "negative": [
         {
+          "balloonText": messageService.get("adjustment.chart.inventory.negative") + ": [[value]]",
+          "fillAlphas": 0.6,
+          "lineAlpha": 0.4,
+          "title": messageService.get("adjustment.chart.inventory.negative"),
+          "valueField": "INVENTORY_NEGATIVE"
+        },
+        {
           "balloonText": messageService.get("adjustment.chart.expired.return.to.supplier") + ": [[value]]",
           "fillAlphas": 0.6,
           "lineAlpha": 0.4,
@@ -23,13 +30,6 @@ services.factory('AdjustmentOccurrencesChartService', function (messageService) 
           "lineAlpha": 0.4,
           "title": messageService.get("adjustment.chart.loans.deposit"),
           "valueField": "LOANS_DEPOSIT"
-        },
-        {
-          "balloonText": messageService.get("adjustment.chart.inventory.negative") + ": [[value]]",
-          "fillAlphas": 0.6,
-          "lineAlpha": 0.4,
-          "title": messageService.get("adjustment.chart.inventory.negative"),
-          "valueField": "INVENTORY_NEGATIVE"
         },
         {
           "balloonText": messageService.get("adjustment.chart.prod.defective") + ": [[value]]",
@@ -107,10 +107,7 @@ services.factory('AdjustmentOccurrencesChartService', function (messageService) 
         "offset": 30
       },
       "chartCursor": {},
-      "categoryField": "period",
-      "categoryAxis": {
-        "startOnAxis": true
-      }
+      "categoryField": "period"
     });
   }
 
