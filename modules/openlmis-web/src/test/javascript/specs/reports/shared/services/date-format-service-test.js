e664bada401716953a0d2c5b1d95a6a93793af73
Hanxian Lin <saitoplam@hotmail.com>
2016-06-21 10:12:22 +0800

    LHX  && WSZ - 775 - add test for format period start day and end day

--- modules/openlmis-web/src/test/javascript/specs/reports/shared/services/date-format-service-test.js
+++ modules/openlmis-web/src/test/javascript/specs/reports/shared/services/date-format-service-test.js
@@ -24,4 +24,14 @@ describe("date format service test", function () {
         var formattedDate = dateFormatService.formatDateWithLocale(new Date("2011-11-11"));
         expect(formattedDate).toEqual("11 " + messageService.get("month.abbr.11") + " 2011");
     });
+
+    it("should format start date of period", function () {
+        var formattedDate = dateFormatService.formatDateWithStartDayOfPeriod(new Date("2011-11-11"));
+        expect(formattedDate).toEqual("2011-11-21");
+    });
+
+    it("should format end date of period", function () {
+        var formattedDate = dateFormatService.formatDateWithEndDayOfPeriod(new Date("2011-11-11"));
+        expect(formattedDate).toEqual("2011-11-20");
+    });
 });
