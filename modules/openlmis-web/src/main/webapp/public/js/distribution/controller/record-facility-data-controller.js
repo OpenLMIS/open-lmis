6079a123d0fc355bb004ebeebc494d754dcd06fd
Łukasz Lewczyński <llewczynski@soldevelo.com>
2016-06-21 11:53:06 +0200

    Added missing $http dependency

--- modules/openlmis-web/src/main/webapp/public/js/distribution/controller/record-facility-data-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/distribution/controller/record-facility-data-controller.js
@@ -8,7 +8,7 @@
  * You should have received a copy of the GNU Affero General Public License along with this program.  If not, see http://www.gnu.org/licenses.  For additional information contact info@OpenLMIS.org. 
  */
 
-function RecordFacilityDataController($scope, $location, $routeParams, distributionService, AuthorizationService, $dialog) {
+function RecordFacilityDataController($scope, $location, $routeParams, distributionService, AuthorizationService, $dialog, $http) {
   $scope.label = $routeParams.facility ? 'label.change.facility' : "label.select.facility";
 
   $scope.distribution = distributionService.distribution;
