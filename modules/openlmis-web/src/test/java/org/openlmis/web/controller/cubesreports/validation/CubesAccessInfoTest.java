b01baf14c98d49bbd5b571f0685cbd5c12c63f85
cuipengfei <cuipengfei2008@gmail.com>
2016-06-27 16:50:46 +0800

    CPF -794- parse cubes query string when part of the location is missing

--- modules/openlmis-web/src/test/java/org/openlmis/web/controller/cubesreports/validation/CubesAccessInfoTest.java
+++ modules/openlmis-web/src/test/java/org/openlmis/web/controller/cubesreports/validation/CubesAccessInfoTest.java
@@ -3,8 +3,9 @@ package org.openlmis.web.controller.cubesreports.validation;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
 import static org.junit.Assert.assertThat;
-import static org.openlmis.core.domain.moz.MozFacilityTypes.CSRUR_I;
+import static org.openlmis.core.domain.moz.MozFacilityTypes.*;
 
 public class CubesAccessInfoTest {
     @Test
@@ -15,5 +16,35 @@ public class CubesAccessInfoTest {
         assertThat(cubesAccessInfo.getFacility(), is("HF8"));
         assertThat(cubesAccessInfo.getDistrict(), is("MATOLA"));
         assertThat(cubesAccessInfo.getProvince(), is("MAPUTO_PROVINCIA"));
+    }
+
+    @Test
+    public void shouldParseQueryStringWhenFacilityIsMissing() throws Exception {
+        CubesAccessInfo cubesAccessInfo = CubesAccessInfo.createInstance(CSRUR_II, "?cut=drug:08S01Z|location:MAPUTO_PROVINCIA,MATOLA");
+
+        assertThat(cubesAccessInfo.getCurrentUserFacilityType(), is(CSRUR_II));
+        assertThat(cubesAccessInfo.getFacility(), isEmptyOrNullString());
+        assertThat(cubesAccessInfo.getDistrict(), is("MATOLA"));
+        assertThat(cubesAccessInfo.getProvince(), is("MAPUTO_PROVINCIA"));
+    }
+
+    @Test
+    public void shouldParseQueryStringWhenDistrictIsMissing() throws Exception {
+        CubesAccessInfo cubesAccessInfo = CubesAccessInfo.createInstance(DDM, "?cut=drug:08S01Z|location:MAPUTO_PROVINCIA");
+
+        assertThat(cubesAccessInfo.getCurrentUserFacilityType(), is(DDM));
+        assertThat(cubesAccessInfo.getFacility(), isEmptyOrNullString());
+        assertThat(cubesAccessInfo.getDistrict(), isEmptyOrNullString());
+        assertThat(cubesAccessInfo.getProvince(), is("MAPUTO_PROVINCIA"));
+    }
+
+    @Test
+    public void shouldParseQueryStringWhenAllLocationsAreMissing() throws Exception {
+        CubesAccessInfo cubesAccessInfo = CubesAccessInfo.createInstance(DPM, "?cut=drug:08S01Z");
+
+        assertThat(cubesAccessInfo.getCurrentUserFacilityType(), is(DPM));
+        assertThat(cubesAccessInfo.getFacility(), isEmptyOrNullString());
+        assertThat(cubesAccessInfo.getDistrict(), isEmptyOrNullString());
+        assertThat(cubesAccessInfo.getProvince(), isEmptyOrNullString());
     }
 }
