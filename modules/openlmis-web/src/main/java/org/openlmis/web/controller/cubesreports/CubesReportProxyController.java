194b7107a9962056075032718a027d8a821e100c
cuipengfei <cuipengfei2008@gmail.com>
2016-06-29 10:40:04 +0800

    CPF -794- integrate cubes validation service into the proxy controller

--- modules/openlmis-web/src/main/java/org/openlmis/web/controller/cubesreports/CubesReportProxyController.java
+++ modules/openlmis-web/src/main/java/org/openlmis/web/controller/cubesreports/CubesReportProxyController.java
@@ -1,8 +1,9 @@
 package org.openlmis.web.controller.cubesreports;
 
 import lombok.NoArgsConstructor;
+import org.openlmis.web.controller.cubesreports.validation.CubesAccessInfo;
+import org.openlmis.web.controller.cubesreports.validation.CubesReportValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,8 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 
 import static java.net.URLDecoder.decode;
+import static org.springframework.http.HttpEntity.EMPTY;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
@@ -20,16 +23,25 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 public class CubesReportProxyController {
 
     private static final String CUBES_ADDRESS = "http://localhost:5555";
+    private static final String CUBES_REQUEST_PREFIX = "\\/cubesreports";
 
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private CubesReportValidationService cubesReportValidationService;
+
     @RequestMapping(method = GET, value = "/cubesreports/**")
     public ResponseEntity redirect(HttpServletRequest request) throws UnsupportedEncodingException {
+        String queryUri = request.getRequestURI().replaceFirst(CUBES_REQUEST_PREFIX, "");
         String queryString = request.getQueryString() == null ? "" : "?" + decode(request.getQueryString(), "UTF-8");
-        String cubesRequestUrl = CUBES_ADDRESS +
-                request.getRequestURI().replaceFirst("\\/cubesreports", "")
-                + queryString;
-        return restTemplate.exchange(cubesRequestUrl, HttpMethod.GET, HttpEntity.EMPTY, String.class);
+
+        CubesAccessInfo cubesAccessInfo = cubesReportValidationService.validate(queryUri, queryString);
+        if (cubesAccessInfo.isValid()) {
+            String cubesRequestUrl = CUBES_ADDRESS + queryUri + cubesAccessInfo.getCubesQueryString();
+            return restTemplate.exchange(cubesRequestUrl, HttpMethod.GET, EMPTY, String.class);
+        } else {
+            return new ResponseEntity(FORBIDDEN);
+        }
     }
 }
--- modules/openlmis-web/src/test/java/org/openlmis/web/controller/cubesreports/CubesReportProxyControllerTest.java
+++ modules/openlmis-web/src/test/java/org/openlmis/web/controller/cubesreports/CubesReportProxyControllerTest.java
@@ -1,10 +1,15 @@
 package org.openlmis.web.controller.cubesreports;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.openlmis.core.domain.moz.MozFacilityTypes;
 import org.openlmis.db.categories.UnitTests;
+import org.openlmis.web.controller.cubesreports.validation.CubesAccessInfo;
+import org.openlmis.web.controller.cubesreports.validation.CubesReportValidationService;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
@@ -15,14 +20,18 @@ import org.springframework.web.client.RestTemplate;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
 @Category(UnitTests.class)
 @RunWith(PowerMockRunner.class)
 public class CubesReportProxyControllerTest {
 
-    private RestTemplate restTemplate = mock(RestTemplate.class);
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Mock
+    private CubesReportValidationService cubesReportValidationService;
 
     @InjectMocks
     private CubesReportProxyController controller;
@@ -33,6 +42,10 @@ public class CubesReportProxyControllerTest {
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setRequestURI("/cubesreports/abc");
         request.setQueryString("cut%3ddrug%40default%3a08R01Z");
+
+        CubesAccessInfo cubesAccessInfo = CubesAccessInfo.createInstance(MozFacilityTypes.CSRUR_I, "?cut=drug@default:08R01Z");
+        cubesAccessInfo.setValid(true);
+        when(cubesReportValidationService.validate(anyString(), anyString())).thenReturn(cubesAccessInfo);
 
         when(restTemplate.exchange("http://localhost:5555/abc?cut=drug@default:08R01Z", HttpMethod.GET, HttpEntity.EMPTY, String.class))
                 .thenReturn(new ResponseEntity<String>("hello from cubes", HttpStatus.OK));
@@ -50,6 +63,10 @@ public class CubesReportProxyControllerTest {
         MockHttpServletRequest request = new MockHttpServletRequest();
         request.setRequestURI("/cubesreports/abc");
         request.setQueryString(null);
+
+        CubesAccessInfo cubesAccessInfo = CubesAccessInfo.createInstance(MozFacilityTypes.CSRUR_I, "");
+        cubesAccessInfo.setValid(true);
+        when(cubesReportValidationService.validate(anyString(), anyString())).thenReturn(cubesAccessInfo);
 
         when(restTemplate.exchange("http://localhost:5555/abc", HttpMethod.GET, HttpEntity.EMPTY, String.class))
                 .thenReturn(new ResponseEntity<String>("hello from cubes", HttpStatus.OK));
