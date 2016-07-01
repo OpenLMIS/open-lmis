198678239a9ca8b4918c2d38ff4081a1ae9d115b
cuipengfei <cuipengfei2008@gmail.com>
2016-07-01 12:17:18 +0800

    CPF -472- change soh single report to use the new cubes data instead of old self crafted java api

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/stock-on-hand-product/controller/single-product-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/stock-on-hand-product/controller/single-product-report-controller.js
@@ -1,24 +1,37 @@
-function SingleProductReportController($scope, $filter, $controller, ProductReportService) {
+function SingleProductReportController($scope, $filter, $controller, $http, CubesGenerateCutParamsService, CubesGenerateUrlService) {
     $controller('BaseProductReportController', {$scope: $scope});
 
     $scope.$on('$viewContentLoaded', function () {
         $scope.loadProducts();
     });
 
-    $scope.loadReport = function(){
-        var params = {endTime: $filter('date')($scope.reportParams.endTime, "yyyy-MM-dd HH:mm:ss")};
-        params.productId = $scope.reportParams.productId;
-        params.geographicZoneId = $scope.reportParams.districtId || $scope.reportParams.provinceId;
-
+    $scope.loadReport = function () {
         if (validateProduct()) {
-            ProductReportService.loadProductReport().get(params, function (data) {
-                $scope.reportData = data.products;
+            var params = $scope.reportParams;
+            $scope.locationIdToCode(params);
+            var selectedProduct = [{'drug.drug_code': $scope.reportParams.productCode}];
+            var cutsParams = CubesGenerateCutParamsService.generateCutsParams("occurred", undefined, $filter('date')(params.endTime, "yyyy,MM,dd"),
+                params.selectedFacility, selectedProduct, params.selectedProvince, params.selectedDistrict);
+
+            $http.get(CubesGenerateUrlService.generateFactsUrl('vw_daily_full_soh', cutsParams)).success(function (sohEntries) {
+                $scope.reportData = _.chain(sohEntries)
+                    .groupBy(function (sohEntry) {
+                        return sohEntry['drug.drug_code'] + sohEntry['facility.facility_code'];
+                    })
+                    .map(function (sameFacilitySameDrugEntries) {
+                        var maxOccurredDateEntry = _.max(sameFacilitySameDrugEntries, function (entry) {
+                            return new Date(entry.occurred_date);
+                        });
+                        maxOccurredDateEntry.soh = parseInt(maxOccurredDateEntry.soh);
+                        return maxOccurredDateEntry;
+                    })
+                    .value();
             });
         }
     };
 
     function validateProduct() {
-        $scope.invalid = !$scope.reportParams.productId;
+        $scope.invalid = !$scope.reportParams.productCode;
         return !$scope.invalid;
     }
 
--- modules/openlmis-web/src/test/javascript/specs/reports/mozambique/stock-out/controllers/single-product-report-controller-test.js
+++ modules/openlmis-web/src/test/javascript/specs/reports/mozambique/stock-out/controllers/single-product-report-controller-test.js
@@ -1,15 +1,45 @@
 describe("Single product Report Controller", function () {
     var scope, productData, httpBackend;
 
-    productData = {
-        "products": [{
-            "productName": "Lamivudina 150mg/Zidovudina 300mg/Nevirapina 200mg Embalagem 10mg ",
-            "facilityName": "Health Facility 1",
-            "productQuantity": 210,
-            "soonestExpiryDate": 1446539380922,
-            "lastSyncDate": 1446538464758
-        }]
-    };
+    productData = [
+        {
+            "drug.drug_code": "07A03",
+            "location.province_code": "MAPUTO_PROVINCIA",
+            "cmm": -1.0,
+            "occurred.month": 12.0,
+            "drug.drug_name": "Paracetamol500mgComprimidos",
+            "location.province_name": "Maputo Prov\u00edncia",
+            "facility.facility_name": "Marracuene",
+            "occurred.day": 8.0,
+            "location.district_code": "MARRACUENE",
+            "facility.facility_code": "HF1",
+            "last_sync_date": "2016-06-21T08:55:53.908508+08:00",
+            "expiry_date": "2017-04-08",
+            "soh": "1000",
+            "occurred_date": "2015-12-08",
+            "vw_daily_full_soh_facility_name": "Marracuene",
+            "location.district_name": "Marracuene",
+            "occurred.year": 2015.0
+        },
+        {
+            "drug.drug_code": "07A03",
+            "location.province_code": "MAPUTO_PROVINCIA",
+            "cmm": -1.0,
+            "occurred.month": 12.0,
+            "drug.drug_name": "Paracetamol500mgComprimidos",
+            "location.province_name": "Maputo Prov\u00edncia",
+            "facility.facility_name": "Marracuene",
+            "occurred.day": 17.0,
+            "location.district_code": "MARRACUENE",
+            "facility.facility_code": "HF1",
+            "last_sync_date": "2016-06-21T08:55:53.908508+08:00",
+            "expiry_date": "2017-04-08",
+            "soh": "1000",
+            "occurred_date": "2015-12-17",
+            "vw_daily_full_soh_facility_name": "Marracuene",
+            "location.district_name": "Marracuene",
+            "occurred.year": 2015.0
+        }];
 
     beforeEach(module('openlmis'));
     beforeEach(module('ui.bootstrap.dialog'));
@@ -21,16 +51,16 @@ describe("Single product Report Controller", function () {
     }));
 
     it('should load single product report successfully', function () {
-        scope.reportParams = {productId : undefined};
+        scope.reportParams = {};
         scope.loadReport();
         expect(scope.invalid).toBe(true);
-        scope.reportParams.productId = 199;
+        scope.reportParams.productCode = '01A01';
 
-        httpBackend.expectGET('/reports/single-product-report?productId=199').respond(200, productData);
+        httpBackend.expectGET('/cubesreports/cube/vw_daily_full_soh/facts?cut=occurred:-|drug:01A01').respond(200, productData);
         scope.loadReport();
         httpBackend.flush();
 
         expect(scope.reportData.length).toBe(1);
-        expect(scope.reportData[0].productName).toEqual("Lamivudina 150mg/Zidovudina 300mg/Nevirapina 200mg Embalagem 10mg ");
+        expect(scope.reportData[0]['drug.drug_name']).toEqual("Paracetamol500mgComprimidos");
     });
 });
