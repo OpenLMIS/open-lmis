31739101e220da22ae61a16f71d9a080c08f35bb
Hanxian Lin <saitoplam@hotmail.com>
2016-07-08 14:25:29 +0800

    LHX && WSZ - 769 - ues filter block the request over 2MB

---
+++ modules/openlmis-web/src/main/java/org/openlmis/web/interceptor/LargeRequestFilter.java
@@ -0,0 +1,26 @@
+package org.openlmis.web.interceptor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class LargeRequestFilter extends OncePerRequestFilter {
+
+    public static final int MAX_REQUEST_SIZE = 2097152;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        if (request.getContentLength() > MAX_REQUEST_SIZE) {
+            response.reset();
+            response.sendError(HttpStatus.FORBIDDEN.value(), "Request size too larger!");
+        }
+    }
+
+}
