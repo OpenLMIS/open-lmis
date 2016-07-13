dead35153fb8091881be08771fe9e171a8b5ac7c
Danni Yu <dyu@thoughtworks.com>
2016-07-13 11:29:22 +0800

    DY - #818 - inject messageservice

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/stock-out/controller/stock-out-single-product-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/stock-out/controller/stock-out-single-product-report-controller.js
@@ -1,4 +1,4 @@
-function StockOutSingleProductReportController($scope, $filter, $q, $controller, $http, $timeout, CubesGenerateUrlService, $routeParams, ProductReportService, StockoutSingleProductFacilityChartService, StockoutSingleProductZoneChartService, StockoutSingleProductTreeDataBuilder, CubesGenerateCutParamsService, FeatureToggleService,$cacheFactory) {
+function StockOutSingleProductReportController($scope, $filter, $q, $controller, $http, $timeout, CubesGenerateUrlService, $routeParams, ProductReportService, StockoutSingleProductFacilityChartService, StockoutSingleProductZoneChartService, StockoutSingleProductTreeDataBuilder, CubesGenerateCutParamsService, FeatureToggleService, messageService, $cacheFactory) {
     $controller('BaseProductReportController', {$scope: $scope});
 
     var stockOuts;
