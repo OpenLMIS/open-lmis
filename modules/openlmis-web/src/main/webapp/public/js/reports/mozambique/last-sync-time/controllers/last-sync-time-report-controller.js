3635e7c257e04030719550663f3955ad46c9fd9f
Hanxian Lin <saitoplam@hotmail.com>
2016-07-22 15:29:57 +0800

    DY && LHX - 167 - generate donut char of hf counts for each period; generate table report for last sync time;

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/last-sync-time/controllers/last-sync-time-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/last-sync-time/controllers/last-sync-time-report-controller.js
@@ -1,8 +1,11 @@
-function LastSyncTimeReportController($scope, $http, GeographicZoneService, CubesGenerateUrlService, CubesGenerateCutParamsService, $filter) {
+function LastSyncTimeReportController($scope, $http, GeographicZoneService, CubesGenerateUrlService, CubesGenerateCutParamsService) {
 
   $scope.provinces = [];
   $scope.districts = [];
   $scope.tree_data = [];
+  $scope.col_defs = [{
+    field: 'LastSyncTime'
+  }];
 
   $scope.$on('$viewContentLoaded', function () {
     loadGeographicZonesBasedOnUserProfile();
@@ -34,9 +37,86 @@ function LastSyncTimeReportController($scope, $http, GeographicZoneService, Cube
       cutsParams = CubesGenerateCutParamsService.generateCutsParams(undefined, undefined, undefined,
           undefined, undefined, $scope.provinces[0], $scope.districts[0]);
     }
+
     $http.get(CubesGenerateUrlService.generateFactsUrl('vw_sync_time', cutsParams)).success(function(data){
-      console.log(data);
+      buildTreeData(data);
+      var lessThan1DayCount =0;
+      var lessThan3DaysCount =0;
+      var moreThan3DaysCount =0;
+      var currentTime = new Date();
+      var oneDay = 86400000;
+      var threeDay = 259200000;
+
+      _.forEach(data,function(){
+        var interval = currentTime - new Date(data['last_sync_time']);
+        if( interval < oneDay){
+          lessThan1DayCount++;
+        } else if (interval > threeDay ){
+          moreThan3DaysCount++;
+        } else {
+          lessThan3DaysCount++;
+        }
+      });
+      drawDonutChart(lessThan1DayCount, lessThan3DaysCount, moreThan3DaysCount);
     });
   }
 
+  function buildTreeData(syncTimeData) {
+    _.forEach($scope.provinces, function(province) {
+      var provinceItem = {
+        Name: province['name'],
+        LastSyncTime: '',
+        children: []
+      };
+      _.forEach($scope.districts, function(district) {
+        if (province['id'] === district['parentId']) {
+          var districtItem = {
+            Name: district['name'],
+            LastSyncTime: '',
+            children: []
+          };
+          _.forEach(syncTimeData, function(facilityData) {
+            if (facilityData['location.district_code'] === district.code
+                && facilityData['location.province_code'] === province.code) {
+              var facilityItem = {
+                Name: facilityData['facility.facility_name'],
+                LastSyncTime: facilityData['last_sync_time']
+              };
+              districtItem.children.push(facilityItem);
+            }
+          });
+          provinceItem.children.push(districtItem);
+        }
+      });
+      $scope.tree_data.push(provinceItem);
+    });
+  }
+
+  function drawDonutChart(lessThan1DayCount, lessThan3DaysCount, moreThan3DaysCount) {
+    AmCharts.makeChart( "chart-div", {
+      "type": "pie",
+      "theme": "light",
+      "dataProvider": [ {
+        "title": lessThan1DayCount + "Health facilities last updated in the last 24 hours",
+        "value": lessThan1DayCount
+      },
+        {
+          "title": lessThan3DaysCount + "Health facilities last updated in the last 3 days",
+          "value": lessThan3DaysCount
+        } ,
+        {
+          "title": moreThan3DaysCount + "Health facilities last updated more than 3 days ago",
+          "value": moreThan3DaysCount
+        }],
+      "titleField": "title",
+      "valueField": "value",
+      "labelRadius": 5,
+      "radius": "42%",
+      "innerRadius": "60%",
+      "labelText": "[[title]]",
+      "export": {
+        "enabled": true
+      }
+    } );
+  }
 }
