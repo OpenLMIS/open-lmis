e6015c540e76712765ba92fc274f9ca42616d3a5
Hanxian Lin <saitoplam@hotmail.com>
2016-09-26 14:25:13 +0800

    LHX && KTX - fix - delete duplicate creation of materialized view

--- modules/migration/src/main/resources/db/migration/new_scripts/V653_1__create_stockout_view.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V653_1__create_stockout_view.sql
@@ -36,24 +36,24 @@ BEGIN
 END;
 $event$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE VIEW vw_stockouts AS
-  SELECT
-    facilities.name             AS facility_name,
-    zone.name                   AS district,
-    parent_zone.name            AS province,
-    products.code               AS drug_code,
-    products.primaryname        AS drug_name,
-    programs.name               AS program,
-    stock_card_entries.occurred AS stockout_date,
-    (stockout_event(stock_cards.id, stock_card_entries.occurred)).*
-  FROM facilities
-    JOIN geographic_zones AS zone ON facilities.geographiczoneid = zone.id
-    JOIN geographic_zones AS parent_zone ON zone.parentid = parent_zone.id
-    JOIN stock_cards ON facilities.id = stock_cards.facilityid
-    JOIN products ON stock_cards.productid = products.id
-    JOIN program_products ON products.id = program_products.productid
-    JOIN programs ON program_products.programid = programs.id
-    JOIN stock_card_entries ON stock_cards.id = stock_card_entries.stockcardid
-    JOIN stock_card_entry_key_values ON stock_card_entries.id = stock_card_entry_key_values.stockcardentryid
-  WHERE keycolumn = 'soh' AND valuecolumn = '0' AND stock_card_entries.quantity != 0
-  ORDER BY facility_name, drug_name, stockout_date;
+-- CREATE OR REPLACE VIEW vw_stockouts AS
+--   SELECT
+--     facilities.name             AS facility_name,
+--     zone.name                   AS district,
+--     parent_zone.name            AS province,
+--     products.code               AS drug_code,
+--     products.primaryname        AS drug_name,
+--     programs.name               AS program,
+--     stock_card_entries.occurred AS stockout_date,
+--     (stockout_event(stock_cards.id, stock_card_entries.occurred)).*
+--   FROM facilities
+--     JOIN geographic_zones AS zone ON facilities.geographiczoneid = zone.id
+--     JOIN geographic_zones AS parent_zone ON zone.parentid = parent_zone.id
+--     JOIN stock_cards ON facilities.id = stock_cards.facilityid
+--     JOIN products ON stock_cards.productid = products.id
+--     JOIN program_products ON products.id = program_products.productid
+--     JOIN programs ON program_products.programid = programs.id
+--     JOIN stock_card_entries ON stock_cards.id = stock_card_entries.stockcardid
+--     JOIN stock_card_entry_key_values ON stock_card_entries.id = stock_card_entry_key_values.stockcardentryid
+--   WHERE keycolumn = 'soh' AND valuecolumn = '0' AND stock_card_entries.quantity != 0
+--   ORDER BY facility_name, drug_name, stockout_date;
--- modules/migration/src/main/resources/db/migration/new_scripts/V687__add_full_soh_view.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V687__add_full_soh_view.sql
@@ -30,34 +30,34 @@ END
 $$
 LANGUAGE 'plpgsql';
 
-CREATE MATERIALIZED VIEW vw_daily_full_soh AS
-  (SELECT DISTINCT ON (facility_code, drug_code, occurred)
-     facilities.name                                                          AS facility_name,
-     facilities.code                                                          AS facility_code,
-
-     ZONE.name                                                                AS district_name,
-     ZONE.code                                                                AS district_code,
-
-     parent_zone.name                                                         AS province_name,
-     parent_zone.code                                                         AS province_code,
-
-     products.code                                                            AS drug_code,
-     products.primaryname                                                     AS drug_name,
-
-     set_value(stock_card_entries.id, 'soh')                                  AS soh,
-     soonest_expiry_date(set_value(stock_card_entries.id, 'expirationdates')) AS soonest_expiry_date,
-
-     occurred,
-     stock_cards.modifieddate                                                 AS last_sync_date,
-     cmm_at_day(facilities.id, products.code, occurred)                       AS cmm
-   FROM stock_card_entries
-     JOIN stock_cards ON stock_card_entries.stockcardid = stock_cards.id
-     JOIN products ON stock_cards.productid = products.id
-     JOIN facilities ON stock_cards.facilityid = facilities.id
-     JOIN geographic_zones AS ZONE ON facilities.geographiczoneid = ZONE.id
-     JOIN geographic_zones AS parent_zone ON ZONE.parentid = parent_zone.id
-     LEFT JOIN cmm_entries ON facilities.id = cmm_entries.facilityid
-   ORDER BY facility_code, drug_code, occurred, stock_card_entries.createddate DESC);
+-- CREATE MATERIALIZED VIEW vw_daily_full_soh AS
+--   (SELECT DISTINCT ON (facility_code, drug_code, occurred)
+--      facilities.name                                                          AS facility_name,
+--      facilities.code                                                          AS facility_code,
+--
+--      ZONE.name                                                                AS district_name,
+--      ZONE.code                                                                AS district_code,
+--
+--      parent_zone.name                                                         AS province_name,
+--      parent_zone.code                                                         AS province_code,
+--
+--      products.code                                                            AS drug_code,
+--      products.primaryname                                                     AS drug_name,
+--
+--      set_value(stock_card_entries.id, 'soh')                                  AS soh,
+--      soonest_expiry_date(set_value(stock_card_entries.id, 'expirationdates')) AS soonest_expiry_date,
+--
+--      occurred,
+--      stock_cards.modifieddate                                                 AS last_sync_date,
+--      cmm_at_day(facilities.id, products.code, occurred)                       AS cmm
+--    FROM stock_card_entries
+--      JOIN stock_cards ON stock_card_entries.stockcardid = stock_cards.id
+--      JOIN products ON stock_cards.productid = products.id
+--      JOIN facilities ON stock_cards.facilityid = facilities.id
+--      JOIN geographic_zones AS ZONE ON facilities.geographiczoneid = ZONE.id
+--      JOIN geographic_zones AS parent_zone ON ZONE.parentid = parent_zone.id
+--      LEFT JOIN cmm_entries ON facilities.id = cmm_entries.facilityid
+--    ORDER BY facility_code, drug_code, occurred, stock_card_entries.createddate DESC);
 
 CREATE OR REPLACE FUNCTION refresh_daily_full_soh()
   RETURNS INT LANGUAGE plpgsql
