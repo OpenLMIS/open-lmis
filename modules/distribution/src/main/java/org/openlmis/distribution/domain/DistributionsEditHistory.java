a1e7dd87b304020ba42f3e25658d0e9ff0d2bb1b
Sebastian Kostuch <skostuch@soldevelo.com>
2017-01-11 16:23:37 +0100

    Improved PDF and CSV history files

--- modules/distribution/src/main/java/org/openlmis/distribution/domain/DistributionsEditHistory.java
+++ modules/distribution/src/main/java/org/openlmis/distribution/domain/DistributionsEditHistory.java
@@ -1,7 +1,9 @@
 package org.openlmis.distribution.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.openlmis.core.domain.Facility;
@@ -11,23 +13,25 @@ import java.util.Date;
 
 import static org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion.NON_EMPTY;
 
-@EqualsAndHashCode(callSuper = false)
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 @JsonSerialize(include = NON_EMPTY)
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DistributionsEditHistory {
-    private Distribution distribution;
+  private Distribution distribution;
 
-    private String district;
-    private Facility facility;
+  private String district;
+  private Facility facility;
 
-    private String dataScreen;
-    private String editedItem;
+  private String dataScreen;
+  private String editedItem;
 
-    private String originalValue;
-    private String newValue;
+  private String originalValue;
+  private String newValue;
 
-    private Date editedDatetime;
-    private User editedBy;
+  private Date editedDatetime;
+  private User editedBy;
 
 }
