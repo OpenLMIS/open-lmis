7026f12cce2e121391d9745c35c51501a502fdf2
Sebastian Kostuch <skostuch@soldevelo.com>
2017-01-17 11:09:08 +0100

    Added missing annotations into FacilitatorDTO

--- modules/distribution/src/main/java/org/openlmis/distribution/dto/FacilitatorDTO.java
+++ modules/distribution/src/main/java/org/openlmis/distribution/dto/FacilitatorDTO.java
@@ -2,8 +2,13 @@ package org.openlmis.distribution.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
 import org.openlmis.distribution.domain.Facilitator;
+
+import static org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion.NON_EMPTY;
 
 /**
  *  Facilitator entity represents the name and role of a person.
@@ -12,6 +17,9 @@ import org.openlmis.distribution.domain.Facilitator;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonSerialize(include = NON_EMPTY)
+@EqualsAndHashCode(callSuper = false)
 public class FacilitatorDTO {
 
   private Reading name;
