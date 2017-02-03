2120c26eb7bd7d4c776f3d15abf757fb7baf5770
Tianxi Ku <kutianxi@outlook.com>
2017-02-03 13:12:33 +0800

    TK - 1082 - do not load default tracer report

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/tracer-drugs/controllers/tracer-drugs-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/tracer-drugs/controllers/tracer-drugs-report-controller.js
@@ -1,10 +1,6 @@
 function TracerDrugsReportController($scope, $controller, DateFormatService, TracerDrugsChartService, $timeout, $q, $http) {
     $controller('BaseProductReportController', {$scope: $scope});
 
-    $scope.$on('$viewContentLoaded', function () {
-        renderDefaultTracerDrugsReport();
-    });
-
     $scope.loadReport = function () {
         TracerDrugsChartService.makeTracerDrugsChart('tracer-report', 'legend-div', new Date($scope.reportParams.startTime), new Date($scope.reportParams.endTime), getSelectedProvince(), getSelectedDistrict());
     };
@@ -20,42 +16,4 @@ function TracerDrugsReportController($scope, $controller, DateFormatService, Tra
     function getSelectedDistrict() {
         return $scope.getGeographicZoneById($scope.districts, $scope.reportParams.districtId);
     }
-
-    function renderDefaultTracerDrugsReport() {
-        var defaultStartTime = DateFormatService.formatDateWithFirstDayOfMonth(new Date());
-        var defaultEndTime = $scope.reportParams.endTime;
-
-        var getHomeFacility = $http.get('/rest-api/lookup/home-facility');
-        $q.when(getHomeFacility).then(function (result) {
-            var homeFacility = result.data['home-facility'];
-            $timeout(function () {
-                setDefaultLocation(homeFacility);
-                TracerDrugsChartService.makeTracerDrugsChart('tracer-report', 'legend-div', new Date(defaultStartTime), new Date(defaultEndTime), getSelectedProvince(), getSelectedDistrict());
-            }, 1000);
-        });
-    }
-
-    function setDefaultLocation(homeFacility) {
-        var facilityType = homeFacility.facilityType.code;
-        if(facilityType === 'Central'){
-            if($scope.provinces.length === 1) {
-                $scope.reportParams.provinceId = $scope.provinces[0].id;
-            } else {
-                $scope.reportParams.provinceId = ' ';
-            }
-            $scope.reportParams.districtId = ' ';
-        }
-
-        if (facilityType === 'DPM') {
-            $scope.reportParams.provinceId = $scope.provinces[0].id;
-            $scope.reportParams.districtId = ' ';
-        }
-
-        if (facilityType === 'DDM') {
-            $scope.reportParams.districtId = $scope.districts[0].id;
-        }
-
-    }
-
-
 }
