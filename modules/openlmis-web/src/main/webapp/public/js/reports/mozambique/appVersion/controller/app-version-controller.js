54f24b2ced11a84f8009ec9ddd092cc438707537
Tianxi Ku <kutianxi@outlook.com>
2017-02-04 18:05:33 +0800

    TK - 1083 - add export function to app version report

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/appVersion/controller/app-version-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/appVersion/controller/app-version-controller.js
@@ -1,30 +1,52 @@
-function VersionReportController($scope, VersionReportService, $cacheFactory) {
-    $scope.$on('$viewContentLoaded', function () {
-        $scope.loadUserSummary();
+function VersionReportController($scope, VersionReportService, $cacheFactory, ReportExportExcelService, messageService) {
+  $scope.$on('$viewContentLoaded', function () {
+    $scope.loadUserSummary();
+  });
+
+  $scope.loadUserSummary = function () {
+    VersionReportService.get(function (data) {
+      $scope.appVersions = data.app_versions;
+      $scope.sortType = 'userName';
+    });
+  };
+
+  if ($cacheFactory.get('keepHistoryInStockOnHandPage') !== undefined) {
+    $cacheFactory.get('keepHistoryInStockOnHandPage').put('saveDataOfStockOnHand', "no");
+  }
+  if ($cacheFactory.get('keepHistoryInStockOutReportPage') !== undefined) {
+    $cacheFactory.get('keepHistoryInStockOutReportPage').put('saveDataOfStockOutReport', "no");
+    $cacheFactory.get('keepHistoryInStockOutReportPage').put('saveDataOfStockOutReportForSingleProduct', "no");
+  }
+
+  $scope.partialPropertiesFilter = function (searchValue) {
+    return function (entry) {
+      var regex = new RegExp(searchValue, "gi");
+
+      return regex.test(entry.userName) ||
+        regex.test(entry.facilityName) ||
+        regex.test(entry.appVersion);
+    };
+  };
+
+  $scope.exportXLSX = function () {
+    var data = {
+      reportHeaders: {
+        user: messageService.get('label.app.version.user'),
+        facilityName: messageService.get('label.app.version.facilityname'),
+        appVersion: messageService.get('label.app.version.appversion')
+      },
+      reportContent: []
+    };
+
+    $scope.appVersions.forEach(function (appVersion) {
+      var appVersionReportEntry = {};
+      appVersionReportEntry.user = appVersion.userName;
+      appVersionReportEntry.facilityName = appVersion.facilityName;
+      appVersionReportEntry.appVersion = appVersion.appVersion;
+
+      data.reportContent.push(appVersionReportEntry);
     });
 
-    $scope.loadUserSummary = function () {
-        VersionReportService.get(function (data) {
-            $scope.appVersions = data.app_versions;
-            $scope.sortType = 'userName';
-        });
-    };
-
-    if ($cacheFactory.get('keepHistoryInStockOnHandPage') !== undefined) {
-        $cacheFactory.get('keepHistoryInStockOnHandPage').put('saveDataOfStockOnHand', "no");
-    }
-    if ($cacheFactory.get('keepHistoryInStockOutReportPage') !== undefined) {
-        $cacheFactory.get('keepHistoryInStockOutReportPage').put('saveDataOfStockOutReport', "no");
-        $cacheFactory.get('keepHistoryInStockOutReportPage').put('saveDataOfStockOutReportForSingleProduct', "no");
-    }
-
-    $scope.partialPropertiesFilter = function(searchValue) {
-        return function(entry) {
-            var regex = new RegExp(searchValue, "gi");
-
-            return regex.test(entry.userName) ||
-                regex.test(entry.facilityName) ||
-                regex.test(entry.appVersion);
-        };
-    };
+    ReportExportExcelService.exportAsXlsx(data, messageService.get('report.file.app.version'));
+  };
 }
