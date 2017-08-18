5d6646a0dc6c10069fdc015b02d2eebca2eff52c
Schubert Rodriguez <scrodrig@thoughtworks.com>
2017-08-18 16:17:59 -0500

    US45 - GR/SR - Add methods in order to transform dates strings and timestamps to dateString dd-MM-yyyy

--- modules/openlmis-web/src/main/webapp/public/js/reports/shared/services/date-format-service.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/shared/services/date-format-service.js
@@ -1,94 +1,105 @@
 services.factory('DateFormatService', function ($filter, messageService) {
 
-    var formatDateWithFirstDayOfMonth = function (date) {
-        return $filter('date')(new Date(date.getFullYear(), date.getMonth(), 1), "yyyy-MM-dd");
-    };
+  var formatDateWithFirstDayOfMonth = function (date) {
+    return $filter('date')(new Date(date.getFullYear(), date.getMonth(), 1), "yyyy-MM-dd");
+  };
 
-    var formatDateWithLastDayOfMonth = function (date) {
-        return $filter('date')(new Date(date.getFullYear(), date.getMonth() + 1, 0), "yyyy-MM-dd");
-    };
+  var formatDateWithLastDayOfMonth = function (date) {
+    return $filter('date')(new Date(date.getFullYear(), date.getMonth() + 1, 0), "yyyy-MM-dd");
+  };
 
-    var formatDateWithStartDayOfPeriod = function (date) {
-        if (date.getDate() < 21) {
-            return $filter('date')(new Date(date.getFullYear(), date.getMonth() - 1, 21), "yyyy-MM-dd");
-        } else {
-            return $filter('date')(new Date(date.getFullYear(), date.getMonth(), 21), "yyyy-MM-dd");
-        }
-    };
-
-    var formatDateWithEndDayOfPeriod = function (date) {
-        if (date.getDate() > 20) {
-            return $filter('date')(new Date(date.getFullYear(), date.getMonth() + 1, 20), "yyyy-MM-dd");
-        } else {
-            return $filter('date')(new Date(date.getFullYear(), date.getMonth(), 20), "yyyy-MM-dd");
-        }
-    };
-
-    var formatDateWithLocale = function (dateString) {
-        return formatDate(dateString, true);
-    };
-
-    var formatDateWithLocaleNoDay = function (dateString) {
-        return formatDate(dateString, false);
-    };
-
-    function formatDate(dateString, withDay) {
-        var date = new Date(dateString);
-        var monthYear = messageService.get('month.abbr.' + (date.getMonth() + 1)) + " " + date.getFullYear();
-
-        if (withDay) {
-            return date.getDate() + " " + monthYear;
-        } else {
-            return monthYear;
-        }
+  var formatDateWithStartDayOfPeriod = function (date) {
+    if (date.getDate() < 21) {
+      return $filter('date')(new Date(date.getFullYear(), date.getMonth() - 1, 21), "yyyy-MM-dd");
+    } else {
+      return $filter('date')(new Date(date.getFullYear(), date.getMonth(), 21), "yyyy-MM-dd");
     }
+  };
 
-    function formatDateWithTimeAndLocale(dateString) {
-        var dateWithLocale = formatDateWithLocale(dateString);
-        var date = new Date(dateString);
-        var time = $filter('date')(date, "hh:mm a");
-        return time + ' ' + dateWithLocale;
+  var formatDateWithEndDayOfPeriod = function (date) {
+    if (date.getDate() > 20) {
+      return $filter('date')(new Date(date.getFullYear(), date.getMonth() + 1, 20), "yyyy-MM-dd");
+    } else {
+      return $filter('date')(new Date(date.getFullYear(), date.getMonth(), 20), "yyyy-MM-dd");
     }
+  };
 
-    function formatDateWith24HoursTime(dateString) {
-        var date = Date.parse(dateString);
-        return $filter('date')(date, 'dd/MM/yyyy HH:mm');
+  var formatDateWithLocale = function (dateString) {
+    return formatDate(dateString, true);
+  };
+
+  var formatDateWithLocaleNoDay = function (dateString) {
+    return formatDate(dateString, false);
+  };
+
+  function formatDate(dateString, withDay) {
+    var date = new Date(dateString);
+    var monthYear = messageService.get('month.abbr.' + (date.getMonth() + 1)) + " " + date.getFullYear();
+
+    if (withDay) {
+      return date.getDate() + " " + monthYear;
+    } else {
+      return monthYear;
     }
+  }
 
+  function formatDateWithTimeAndLocale(dateString) {
+    var dateWithLocale = formatDateWithLocale(dateString);
+    var date = new Date(dateString);
+    var time = $filter('date')(date, "hh:mm a");
+    return time + ' ' + dateWithLocale;
+  }
 
-    function formatDateWithUnderscore(date) {
-        return formatDateElementsTwoCharacters(date.getFullYear()) + "_" + formatDateElementsTwoCharacters((date.getMonth() + 1)) + "_" + formatDateElementsTwoCharacters(date.getDate()) + "_at_" + formatDateElementsTwoCharacters(date.getHours()) + "." + formatDateElementsTwoCharacters(date.getMinutes()) + "." + formatDateElementsTwoCharacters(date.getSeconds());
+  function formatDateWith24HoursTime(dateString) {
+    var date = Date.parse(dateString);
+    return $filter('date')(date, 'dd/MM/yyyy HH:mm');
+  }
+
+  function formatDateWithDateMonthYear(timestamp) {
+    var date = new Date(timestamp);
+    return $filter('date')(date, 'dd-MM-yyyy');
+  }
+
+  function formatDateWithDateMonthYearForString(stringDate) {
+    var date = new Date(stringDate);
+    return $filter('date')(date, 'dd-MM-yyyy');
+  }
+
+  function formatDateWithUnderscore(date) {
+    return formatDateElementsTwoCharacters(date.getFullYear()) + "_" + formatDateElementsTwoCharacters((date.getMonth() + 1)) + "_" + formatDateElementsTwoCharacters(date.getDate()) + "_at_" + formatDateElementsTwoCharacters(date.getHours()) + "." + formatDateElementsTwoCharacters(date.getMinutes()) + "." + formatDateElementsTwoCharacters(date.getSeconds());
+  }
+
+  function formatDateElementsTwoCharacters(element) {
+    if (element < 10) {
+      return "0" + element;
     }
+    return element;
+  }
 
-    function formatDateElementsTwoCharacters(element) {
-        if (element < 10) {
-            return "0" + element;
-        }
-        return element;
+  function getFridaysBetween(start, end) {
+    var dates = [];
+    for (var day = new Date(start); day <= end; day.setDate(day.getDate() + 1)) {
+      var isFriday = day.getDay() == 5;
+      if (isFriday) {
+        dates.push(new Date(day));
+      }
     }
+    return dates;
+  }
 
-    function getFridaysBetween(start, end) {
-        var dates = [];
-        for (var day = new Date(start); day <= end; day.setDate(day.getDate() + 1)) {
-            var isFriday = day.getDay() == 5;
-            if (isFriday) {
-                dates.push(new Date(day));
-            }
-        }
-        return dates;
-    }
-
-    return {
-        formatDateWithFirstDayOfMonth: formatDateWithFirstDayOfMonth,
-        formatDateWithLastDayOfMonth: formatDateWithLastDayOfMonth,
-        formatDateWithLocale: formatDateWithLocale,
-        formatDateWithLocaleNoDay: formatDateWithLocaleNoDay,
-        formatDateWithStartDayOfPeriod: formatDateWithStartDayOfPeriod,
-        formatDateWithEndDayOfPeriod: formatDateWithEndDayOfPeriod,
-        formatDateWithTimeAndLocale: formatDateWithTimeAndLocale,
-        formatDateWith24HoursTime: formatDateWith24HoursTime,
-        formatDateWithUnderscore: formatDateWithUnderscore,
-        formatDateElementsTwoCharacters: formatDateElementsTwoCharacters,
-        getFridaysBetween: getFridaysBetween
-    };
+  return {
+    formatDateWithFirstDayOfMonth: formatDateWithFirstDayOfMonth,
+    formatDateWithLastDayOfMonth: formatDateWithLastDayOfMonth,
+    formatDateWithLocale: formatDateWithLocale,
+    formatDateWithLocaleNoDay: formatDateWithLocaleNoDay,
+    formatDateWithStartDayOfPeriod: formatDateWithStartDayOfPeriod,
+    formatDateWithEndDayOfPeriod: formatDateWithEndDayOfPeriod,
+    formatDateWithTimeAndLocale: formatDateWithTimeAndLocale,
+    formatDateWith24HoursTime: formatDateWith24HoursTime,
+    formatDateWithDateMonthYear: formatDateWithDateMonthYear,
+    formatDateWithDateMonthYearForString: formatDateWithDateMonthYearForString,
+    formatDateWithUnderscore: formatDateWithUnderscore,
+    formatDateElementsTwoCharacters: formatDateElementsTwoCharacters,
+    getFridaysBetween: getFridaysBetween
+  };
 });
--- modules/openlmis-web/src/test/javascript/specs/reports/shared/services/date-format-service-test.js
+++ modules/openlmis-web/src/test/javascript/specs/reports/shared/services/date-format-service-test.js
@@ -1,63 +1,73 @@
 describe("date format service test", function () {
 
-    var dateFormatService, messageService;
-    beforeEach(module('openlmis'));
+  var dateFormatService, messageService;
+  beforeEach(module('openlmis'));
 
-    beforeEach(function () {
-        inject(function (DateFormatService, _messageService_) {
-            dateFormatService = DateFormatService;
-            messageService = _messageService_;
-        })
-    });
+  beforeEach(function () {
+    inject(function (DateFormatService, _messageService_) {
+      dateFormatService = DateFormatService;
+      messageService = _messageService_;
+    })
+  });
 
-    it("should format first day of month", function () {
-        var formattedDate = dateFormatService.formatDateWithFirstDayOfMonth(new Date("2011-11-11"));
-        expect(formattedDate).toEqual("2011-11-01");
-    });
+  it("should format first day of month", function () {
+    var formattedDate = dateFormatService.formatDateWithFirstDayOfMonth(new Date("2011-11-11"));
+    expect(formattedDate).toEqual("2011-11-01");
+  });
 
-    it("should format last day of month", function () {
-        var formattedDate = dateFormatService.formatDateWithLastDayOfMonth(new Date("2011-11-11"));
-        expect(formattedDate).toEqual("2011-11-30");
-    });
+  it("should format last day of month", function () {
+    var formattedDate = dateFormatService.formatDateWithLastDayOfMonth(new Date("2011-11-11"));
+    expect(formattedDate).toEqual("2011-11-30");
+  });
 
-    it("should format date with local", function () {
-        var formattedDate = dateFormatService.formatDateWithLocale(new Date("2011-11-11T00:00:00"));
-        expect(formattedDate).toEqual("11 " + messageService.get("month.abbr.11") + " 2011");
-    });
+  it("should format date with local", function () {
+    var formattedDate = dateFormatService.formatDateWithLocale(new Date("2011-11-11T00:00:00"));
+    expect(formattedDate).toEqual("11 " + messageService.get("month.abbr.11") + " 2011");
+  });
 
-    it("should format start date of period", function () {
-        var formattedDate = dateFormatService.formatDateWithStartDayOfPeriod(new Date("2011-11-11"));
-        expect(formattedDate).toEqual("2011-10-21");
-    });
+  it("should format start date of period", function () {
+    var formattedDate = dateFormatService.formatDateWithStartDayOfPeriod(new Date("2011-11-11"));
+    expect(formattedDate).toEqual("2011-10-21");
+  });
 
-    it("should format end date of period", function () {
-        var formattedDate = dateFormatService.formatDateWithEndDayOfPeriod(new Date("2011-11-11"));
-        expect(formattedDate).toEqual("2011-11-20");
-    });
+  it("should format end date of period", function () {
+    var formattedDate = dateFormatService.formatDateWithEndDayOfPeriod(new Date("2011-11-11"));
+    expect(formattedDate).toEqual("2011-11-20");
+  });
 
-    it('should format date with full time', function () {
-        var dateString = '2016-10-25T19:54:00.998865';
-        var formattedDate = dateFormatService.formatDateWith24HoursTime(dateString);
+  it('should format date with full time', function () {
+    var formattedDate = dateFormatService.formatDateWithDateMonthYear(1502945999999);
+    expect(formattedDate).toEqual('16-08-2017');
+  });
 
-        expect(formattedDate).toEqual('25/10/2016 19:54');
-    });
+  it('should format date with dd-MM-yyyy', function () {
+    var dateString = '2016-10-25T19:54:00.998865';
+    var formattedDate = dateFormatService.formatDateWith24HoursTime(dateString);
+    expect(formattedDate).toEqual('25/10/2016 19:54');
+  });
 
-    it("should format splited '_' ", function () {
-        var formattedDate = dateFormatService.formatDateWithUnderscore(new Date("2011-11-11T00:00:00"));
-        expect(formattedDate).toEqual("2011_11_11_at_00.00.00");
-    });
+  it('should format date with dd-MM-yyyy from string date', function () {
+    var dateString = '2016-10-25T00:00:00.000000';
+    var formattedDate = dateFormatService.formatDateWithDateMonthYearForString(dateString);
+    expect(formattedDate).toEqual('25-10-2016');
+  });
 
-    it("should add a character 0 when the value is less than 10", function () {
-        var value = Math.floor((Math.random() * 9));
-        var expectedValue = '0' + value;
-        var valueStringFormatted = dateFormatService.formatDateElementsTwoCharacters(value);
-        expect(expectedValue).toEqual(valueStringFormatted);
-    });
+  it("should format splited '_' ", function () {
+    var formattedDate = dateFormatService.formatDateWithUnderscore(new Date("2011-11-11T00:00:00"));
+    expect(formattedDate).toEqual("2011_11_11_at_00.00.00");
+  });
 
-    it("should not add a character 0 when the value is greater than 9", function () {
-        var value = Math.floor((Math.random() * 31) + 10);
-        var expectedValue = value;
-        var valueStringFormatted = dateFormatService.formatDateElementsTwoCharacters(value);
-        expect(expectedValue).toEqual(valueStringFormatted);
-    });
+  it("should add a character 0 when the value is less than 10", function () {
+    var value = Math.floor((Math.random() * 9));
+    var expectedValue = '0' + value;
+    var valueStringFormatted = dateFormatService.formatDateElementsTwoCharacters(value);
+    expect(expectedValue).toEqual(valueStringFormatted);
+  });
+
+  it("should not add a character 0 when the value is greater than 9", function () {
+    var value = Math.floor((Math.random() * 31) + 10);
+    var expectedValue = value;
+    var valueStringFormatted = dateFormatService.formatDateElementsTwoCharacters(value);
+    expect(expectedValue).toEqual(valueStringFormatted);
+  });
 });
