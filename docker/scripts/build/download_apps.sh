1fee678e9163bc591cc3da8d5ddbad868f7f6cab
Ernesto Medina <ernezto@gmail.com>
2017-08-25 10:09:57 -0500

    US47 - EM/MR - clean up docker scripts

--- docker/scripts/build/download_apps.sh
+++ docker/scripts/build/download_apps.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 #use openlmis web app as root war
 rm -fr /usr/local/tomcat/webapps/ROOT
 wget -O /usr/local/tomcat/webapps/ROOT.war --auth-no-challenge --http-user=user_ansible --no-check-certificate --http-password=b5c5990592a49e0df211b0e704d3bdd0 \
@@ -17,14 +18,6 @@ unzip /usr/local/tomcat/webapps/db/db.jar -d /opt/flyway/sql/db
 unzip /usr/local/tomcat/webapps/db/migration.jar -d /opt/flyway/sql/migration
 echo "CREATE SCHEMA atomfeed;" > /opt/flyway/sql/db/V1__create_atomfeed_schema.sql
 
-#put cubes package in /app/cubes
-# mkdir -p /app/cubes && wget -O /app/cubes/cubes.zip --auth-no-challenge --http-user=user_ansible --no-check-certificate --http-password=b5c5990592a49e0df211b0e704d3bdd0 \
-# https://52.69.16.156:8080/job/Lmis-cubes/lastSuccessfulBuild/artifact/cubes.zip
-#
-# cd /app/cubes && unzip ./cubes.zip && rm cubes.zip
-# #install cubes app dependencies
-# pip install -r /app/cubes/bin/requirements.txt
-
 #add properties file that will contain db credentials
 mkdir -p /usr/local/tomcat/extra_properties
 wget -O /usr/local/tomcat/extra_properties/app.properties https://raw.githubusercontent.com/SIGLUS/openlmis-devops/master/deployment/configuration/files/env/$ENV/app.properties
@@ -32,11 +25,3 @@ wget -O /usr/local/tomcat/extra_properties/atomfeed.properties https://raw.githu
 wget -O /usr/local/tomcat/extra_properties/default.properties https://raw.githubusercontent.com/SIGLUS/openlmis-devops/master/deployment/configuration/files/env/$ENV/default.properties
 wget -O /usr/local/tomcat/extra_properties/mailing.properties https://raw.githubusercontent.com/SIGLUS/openlmis-devops/master/deployment/configuration/files/env/$ENV/mailing.properties
 
-#add SIMAM template files
-# mkdir -p /app/tomcat/openlmis/emailattachment/template
-# mkdir -p /app/tomcat/openlmis/emailattachment/cache
-# mkdir -p /app/tomcat/openlmis/tmp
-#
-# wget -O /app/tomcat/openlmis/emailattachment/template/template_Simam_import_Regimen.xlsx https://github.com/SIGLUS/open-lmis/raw/2.0-moz/modules/export/src/main/resources/templates/template_Simam_import_Regimen.xlsx
-# wget -O /app/tomcat/openlmis/emailattachment/template/template_Simam_import_Regimen_EMPTY.xlsx https://github.com/SIGLUS/open-lmis/raw/2.0-moz/modules/export/src/main/resources/templates/template_Simam_import_Regimen_EMPTY.xlsx
-# wget -O /app/tomcat/openlmis/emailattachment/template/template_Simam_import_Requi.xlsx https://github.com/SIGLUS/open-lmis/raw/2.0-moz/modules/export/src/main/resources/templates/template_Simam_import_Requi.xlsx
