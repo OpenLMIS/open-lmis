3ff48f4ae617a7dbdc053daf0b930f76a78cd8dd
Ernesto Medina <ernezto@gmail.com>
2017-09-11 14:06:08 -0500

    US47 - EM - adds relocated files that were missing

--- docker/test-runner-portal/build.sh
+++ docker/test-runner-portal/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+FILE=./installers/jdk-7u80-linux-x64.tar.gz
 
-docker build -t test-runner-portal --rm . 
-#--no-cache
+if [ ! -f "$FILE" ]; then
+	wget -O installers/jdk-7u80-linux-x64.tar.gz http://52.69.16.156:8000/installers/jdk-7u80-linux-x64.tar.gz
+fi
+
+docker build -t test-runner-portal --rm . --no-cache
