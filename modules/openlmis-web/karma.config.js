337eb308ba8654451523c6719f7a5d938fbeb9a9
jrodri <jrodri@thoughtworks.com>
2017-09-20 16:20:04 -0500

    TT67 FC/JR Change firefox to phantomjs for js tests

--- modules/openlmis-web/karma.config.js
+++ modules/openlmis-web/karma.config.js
@@ -56,7 +56,7 @@ module.exports = function (config) {
     plugins: [
       'karma-jasmine',
       'karma-coverage',
-      'karma-firefox-launcher'
+      'karma-phantomjs-launcher'
     ],
 
     // test results reporter to use
@@ -93,7 +93,7 @@ module.exports = function (config) {
     // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
     // - PhantomJS
     // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
-    browsers: ['Firefox'],
+    browsers: ['PhantomJS'],
 
     // If browser does not capture in given timeout [ms], kill it
     captureTimeout: 30000,
