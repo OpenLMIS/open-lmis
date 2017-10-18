c0e008e6c0db144ec154ee5f0dfc4a0bcdb00ec9
faustocv <fausto.castaneda.v@gmail.com>
2017-10-17 19:27:14 -0500

    TT67 - Improves java_unit_test shell script

--- java_unit_test.sh
+++ java_unit_test.sh
@@ -3,8 +3,9 @@ taskrunner="${TASKRUNNER:-siglus/taskrunner:0.0.2}"
 pipelinename="${PIPELINE_NAME:-openlmis_portal}"
 dockernet="${DOCKER_NET:-gocd_default}"
 dbserver="${DB_SERVER:-db}"
-LOCAL_DOCKER_SCRIPT="docker run --rm -v $(pwd):/openlmis -w /openlmis $taskrunner ./gradlew :modules:db:setupDB :modules:migration:setupExtensions -PdatabaseHostName=$dbserver"
-CI_DOCKER_SCRIPT="docker run --rm --net $dockernet --volumes-from $HOSTNAME -w /godata/pipelines/$pipelinename $taskrunner ./gradlew :modules:db:setupDB :modules:migration:setupExtensions -PdatabaseHostName=$dbserver"
-LOCAL_SCRIPT="./gradlew :modules:db:setupDB :modules:migration:setupExtensions -PdatabaseHostName=$dbserver"
+gradleexec="./gradlew clean :modules:db:setupDB :modules:migration:setupExtensions test -x :modules:openlmis-web:less -PdatabaseHostName=$dbserver"
+LOCAL_DOCKER_SCRIPT="docker run --rm -v $(pwd):/openlmis -w /openlmis $taskrunner $gradleexec"
+CI_DOCKER_SCRIPT="docker run --rm --net $dockernet --volumes-from $HOSTNAME -w /godata/pipelines/$pipelinename $taskrunner $gradleexec"
+LOCAL_SCRIPT="$gradleexec"
 
 . ./task_executor.sh
