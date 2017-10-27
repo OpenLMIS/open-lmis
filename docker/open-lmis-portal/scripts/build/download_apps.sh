3891bc753b9430608512734865bf2d93d1f8ddf9
faustocv <fausto.castaneda.v@gmail.com>
2017-10-27 10:42:37 -0500

    TT69 - MR/FC Changes directories in Dockerfile

--- docker/open-lmis-portal/scripts/build/download_apps.sh
+++ docker/open-lmis-portal/scripts/build/download_apps.sh
@@ -2,12 +2,12 @@
 
 #use openlmis web app as root war
 rm -fr /usr/local/tomcat/webapps/ROOT
-cp /tmp/builds/openlmis-web.war /usr/local/tomcat/webapps/ROOT.war
+cp /libs/openlmis-web.war /usr/local/tomcat/webapps/ROOT.war
 
 #download migration jars
 mkdir -p /usr/local/tomcat/webapps/db/
-cp /tmp/builds/db.jar /usr/local/tomcat/webapps/db/
-cp /tmp/builds/migration.jar /usr/local/tomcat/webapps/db/
+cp /libs/db.jar /usr/local/tomcat/webapps/db/
+cp /libs/migration.jar /usr/local/tomcat/webapps/db/
 
 #unzip migration jars and add one extra migration file that creates atomfeed schema
 unzip /usr/local/tomcat/webapps/db/db.jar -d /opt/flyway/sql/db
@@ -16,4 +16,4 @@ echo "CREATE SCHEMA atomfeed;" > /opt/flyway/sql/db/V1__create_atomfeed_schema.s
 
 #add properties file that will contain db credentials
 mkdir -p /usr/local/tomcat/extra_properties
-cp -R /tmp/configuration/*.properties /usr/tomcat/extra_properties/
+cp -R /configuration/*.properties /usr/tomcat/extra_properties/
