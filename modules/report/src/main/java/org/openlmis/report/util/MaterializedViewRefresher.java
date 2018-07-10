7fd70f786460f74cca2ac51a3076c0f53e658158
yingguo <793832732@qq.com>
2018-07-10 11:04:57 +0800

    add more log to track scheduled task

--- modules/report/src/main/java/org/openlmis/report/util/MaterializedViewRefresher.java
+++ modules/report/src/main/java/org/openlmis/report/util/MaterializedViewRefresher.java
@@ -1,20 +1,25 @@
 package org.openlmis.report.util;
 
 import org.openlmis.core.repository.mapper.MaterializedViewMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MaterializedViewRefresher {
+    private static final Logger log = LoggerFactory.getLogger(MaterializedViewRefresher.class);
 
     @Autowired
     private MaterializedViewMapper materializedViewMapper;
 
     public void refreshDailyMaterializedViews() {
+        log.info("start to refresh daily materialized view");
         materializedViewMapper.refreshPeriodMovements();
     }
 
     public void refreshHourlyMaterializedViews() {
+        log.info("start to refresh hourly materialized view");
         materializedViewMapper.refreshLotExpiryDate();
         materializedViewMapper.refreshDailyFullSOH();
         materializedViewMapper.refreshCMMEntries();
