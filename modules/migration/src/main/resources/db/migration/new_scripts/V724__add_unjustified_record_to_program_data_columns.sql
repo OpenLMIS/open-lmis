fb20d6d3ba9320803cc604cf3363762410185466
Yunchao Du <ycdu@thoughtworks.com>
2018-10-09 09:53:34 +0800

    [SMS-187]add unjustified record to program_data_columns

--- modules/migration/src/main/resources/db/migration/new_scripts/V724__add_unjustified_record_to_program_data_columns.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V724__add_unjustified_record_to_program_data_columns.sql
@@ -1,3 +1,4 @@
+DELETE FROM program_data_items WHERE programdatacolumnid IN (SELECT id FROM program_data_columns WHERE code IN ('UNJUSTIFIED_HIVDETERMINE','UNJUSTIFIED_HIVUNIGOLD','UNJUSTIFIED_SYPHILLIS','UNJUSTIFIED_MALARIA'));
 DELETE FROM program_data_columns WHERE code IN ('UNJUSTIFIED_HIVDETERMINE','UNJUSTIFIED_HIVUNIGOLD','UNJUSTIFIED_SYPHILLIS','UNJUSTIFIED_MALARIA');
 INSERT INTO program_data_columns (code, supplementalProgramId) VALUES ('UNJUSTIFIED_HIVDETERMINE',(SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
 INSERT INTO program_data_columns (code, supplementalProgramId) VALUES ('UNJUSTIFIED_HIVUNIGOLD',(SELECT id FROM supplemental_programs WHERE code = 'RAPID_TEST'));
