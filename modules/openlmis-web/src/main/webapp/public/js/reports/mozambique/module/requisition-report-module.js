4687b31c1f3c7526f4404d9413bea9b922482c5f
rongliu <rongliu@thoughtworks.com>
2018-10-11 15:41:47 +0800

    [SMS-90] add over stock product report page.

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/module/requisition-report-module.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/module/requisition-report-module.js
@@ -1,94 +1,82 @@
 angular.module('requisition-report', ['openlmis', 'angularCombine', 'ngTable', 'ui.bootstrap.dialog', 'treeGrid', 'ngGrid', 'syncTimeTreeGrid', 'nsPopover']).config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider.
-                when('/requisition', {
-                    controller: RequisitionReportController,
-                    templateUrl: 'requisition/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/stock-on-hand-single-product', {
-                    controller: SingleProductReportController,
-                    templateUrl: 'stock-on-hand/single-product/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/stock-on-hand-all-products', {
-                    controller: SingleFacilityReportController,
-                    templateUrl: 'stock-on-hand/all-products/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/app-version', {
-                    controller: VersionReportController,
-                    templateUrl: 'appVersion/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/expiry-dates', {
-                    controller: ExpiryDatesReportController,
-                    templateUrl: 'expiry-dates/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/lot-expiry-dates', {
-                    controller: LotExpiryDatesReportController,
-                    templateUrl: 'expiry-dates/partials/lot-list.html',
-                    reloadOnSearch: false
-                }).
-                when('/stock-out-all-products', {
-                    controller: StockOutAllProductsReportController,
-                    templateUrl: 'stockout/all-products/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/stock-out-single-product', {
-                    controller: StockOutSingleProductReportController,
-                    templateUrl: 'stockout/single-product/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/stock-out-single-product/:code', {
-                    controller: StockOutSingleProductReportController,
-                    templateUrl: 'stockout/single-product/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/stock-movements/product/:productCode/facility/:facilityCode', {
-                    controller: StockMovementReportController,
-                    templateUrl: 'stock-movements/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/tracer-drugs', {
-                    controller: TracerDrugsReportController,
-                    templateUrl: 'tracer-drugs/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/nos-drugs', {
-                  controller: NosDrugsReportController,
-                  templateUrl: 'nos/partials/list.html',
-                  reloadOnSearch: false
-                }).
-                when('/consumption', {
-                    controller: ConsumptionReportController,
-                    templateUrl: 'consumption/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/adjustment', {
-                    controller: AdjustmentOccurrencesReportController,
-                    templateUrl: 'adjustment/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/last-sync-time', {
-                    controller: LastSyncTimeReportController,
-                    templateUrl: 'last-sync-time/partials/list.html',
-                    reloadOnSearch: false
-                }).
-                when('/rapid-test', {
-                  controller: RapidTestReportController,
-                  templateUrl: 'rapid-test/partials/list.html',
-                  reloadOnSearch: false
-                }).otherwise({redirectTo: '/'});
-        }]).run(
-    function ($rootScope, AuthorizationService) {
+  function ($routeProvider) {
+    $routeProvider.when('/requisition', {
+      controller: RequisitionReportController,
+      templateUrl: 'requisition/partials/list.html',
+      reloadOnSearch: false
+    }).when('/stock-on-hand-single-product', {
+      controller: SingleProductReportController,
+      templateUrl: 'stock-on-hand/single-product/partials/list.html',
+      reloadOnSearch: false
+    }).when('/stock-on-hand-all-products', {
+      controller: SingleFacilityReportController,
+      templateUrl: 'stock-on-hand/all-products/partials/list.html',
+      reloadOnSearch: false
+    }).when('/app-version', {
+      controller: VersionReportController,
+      templateUrl: 'appVersion/partials/list.html',
+      reloadOnSearch: false
+    }).when('/expiry-dates', {
+      controller: ExpiryDatesReportController,
+      templateUrl: 'expiry-dates/partials/list.html',
+      reloadOnSearch: false
+    }).when('/lot-expiry-dates', {
+      controller: LotExpiryDatesReportController,
+      templateUrl: 'expiry-dates/partials/lot-list.html',
+      reloadOnSearch: false
+    }).when('/stock-out-all-products', {
+      controller: StockOutAllProductsReportController,
+      templateUrl: 'stockout/all-products/partials/list.html',
+      reloadOnSearch: false
+    }).when('/stock-out-single-product', {
+      controller: StockOutSingleProductReportController,
+      templateUrl: 'stockout/single-product/partials/list.html',
+      reloadOnSearch: false
+    }).when('/stock-out-single-product/:code', {
+      controller: StockOutSingleProductReportController,
+      templateUrl: 'stockout/single-product/partials/list.html',
+      reloadOnSearch: false
+    }).when('/stock-movements/product/:productCode/facility/:facilityCode', {
+      controller: StockMovementReportController,
+      templateUrl: 'stock-movements/partials/list.html',
+      reloadOnSearch: false
+    }).when('/tracer-drugs', {
+      controller: TracerDrugsReportController,
+      templateUrl: 'tracer-drugs/partials/list.html',
+      reloadOnSearch: false
+    }).when('/nos-drugs', {
+      controller: NosDrugsReportController,
+      templateUrl: 'nos/partials/list.html',
+      reloadOnSearch: false
+    }).when('/over-stock', {
+      controller: OverStockReportController,
+      templateUrl: 'over-stock/partials/list.html',
+      reloadOnSearch: false
+    }).when('/consumption', {
+        controller: ConsumptionReportController,
+        templateUrl: 'consumption/partials/list.html',
+        reloadOnSearch: false
+      }).when('/adjustment', {
+      controller: AdjustmentOccurrencesReportController,
+      templateUrl: 'adjustment/partials/list.html',
+      reloadOnSearch: false
+    }).when('/last-sync-time', {
+      controller: LastSyncTimeReportController,
+      templateUrl: 'last-sync-time/partials/list.html',
+      reloadOnSearch: false
+    }).when('/rapid-test', {
+      controller: RapidTestReportController,
+      templateUrl: 'rapid-test/partials/list.html',
+      reloadOnSearch: false
+    }).otherwise({redirectTo: '/'});
+  }]).run(
+  function ($rootScope, AuthorizationService) {
 
-    }
+  }
 ).config(function (angularCombineConfigProvider) {
-        angularCombineConfigProvider.addConf(/filter-/, '/public/pages/reports/shared/filters.html');
-    }).filter("districtFilter", DistrictFilter)
-    .filter("provinceFilter", ProvinceFilter)
-    .filter("facilityFilter", FacilityFilter)
-    .directive('datePickerContainer', datePickerContainer)
-    .directive('onFinishRender',onFinishRender);
+  angularCombineConfigProvider.addConf(/filter-/, '/public/pages/reports/shared/filters.html');
+}).filter("districtFilter", DistrictFilter)
+  .filter("provinceFilter", ProvinceFilter)
+  .filter("facilityFilter", FacilityFilter)
+  .directive('datePickerContainer', datePickerContainer)
+  .directive('onFinishRender', onFinishRender);
