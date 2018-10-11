81440506ed5298020d4324d5507356cceb63b8b5
rongliu <rongliu@thoughtworks.com>
2018-10-11 17:25:18 +0800

    [SMS-90] update facility selection html file structure.

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/over-stock/controllers/over-stock-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/over-stock/controllers/over-stock-report-controller.js
@@ -2,6 +2,10 @@ function OverStockReportController($scope, $controller, TracerDrugsChartService)
   $controller('BaseProductReportController', {$scope: $scope});
   $scope.reportLoaded = false;
 
+  $scope.$on('$viewContentLoaded', function () {
+    $scope.loadHealthFacilities();
+  });
+
   $scope.loadReport = function () {
     if ($scope.validateProvince() && $scope.validateDistrict()) {
       $scope.reportLoaded = TracerDrugsChartService.makeTracerDrugsChart('tracer-report', 'legend-div', new Date($scope.reportParams.startTime), new Date($scope.reportParams.endTime), getSelectedProvince(), getSelectedDistrict());
