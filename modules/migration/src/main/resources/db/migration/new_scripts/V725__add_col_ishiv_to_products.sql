d7f8ea5a1be51dc1c33875a488dd4393cf4827c7
Zhe Bao <175539358@qq.com>
2018-10-18 13:55:27 +0800

    [SMS-91] update ishiv value

--- modules/migration/src/main/resources/db/migration/new_scripts/V725__add_col_ishiv_to_products.sql
+++ modules/migration/src/main/resources/db/migration/new_scripts/V725__add_col_ishiv_to_products.sql
@@ -1,6 +1,6 @@
 ALTER TABLE products ADD ishiv boolean default false;
 
-UPDATE products as p SET ishiv = CASE WHEN(tmp.code = 'MMIA' or tmp.parentid=1) THEN true END
+UPDATE products as p SET ishiv = CASE WHEN(tmp.code = 'MMIA' or tmp.parentid=1) THEN true ELSE false END
 FROM (select pt.id,pg.code,pg.parentid from products pt
   left join program_products pp on pt.id = pp.productid
   left join programs pg on pp.programid = pg.id ) as tmp WHERE p.id = tmp.id;
