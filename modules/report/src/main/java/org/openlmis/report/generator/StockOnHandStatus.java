81b0a03ec44b3b6112c1c7c9eb339d69a1c7883e
Zhe Bao <175539358@qq.com>
2018-10-24 10:31:44 +0800

    [SMS-86] code refactor: del useless code

--- modules/report/src/main/java/org/openlmis/report/generator/StockOnHandStatus.java
+++ modules/report/src/main/java/org/openlmis/report/generator/StockOnHandStatus.java
@@ -5,21 +5,19 @@ import org.apache.poi.xssf.usermodel.XSSFColor;
 
 public enum StockOnHandStatus {
 
-    REGULAR_STOCK(IndexedColors.GREEN.getIndex(), "stock.cmm.regular.stock", "regularStock",
+    REGULAR_STOCK("stock.cmm.regular.stock", "regularStock",
             new XSSFColor(new java.awt.Color(140,211,104))),
 
-    LOW_STOCK(IndexedColors.YELLOW.getIndex(), "stock.cmm.low.stock", "lowStock",
+    LOW_STOCK("stock.cmm.low.stock", "lowStock",
             new XSSFColor(new java.awt.Color(250,215,77))),
 
-    STOCK_OUT(IndexedColors.RED.getIndex(), "stock.cmm.stock.out", "stockOut",
+    STOCK_OUT("stock.cmm.stock.out", "stockOut",
             new XSSFColor(new java.awt.Color(245,33,45))),
 
-    OVER_STOCK(IndexedColors.VIOLET.getIndex(), "stock.cmm.over.stock", "overStock",
+    OVER_STOCK("stock.cmm.over.stock", "overStock",
             new XSSFColor(new java.awt.Color(187,149,227))),
 
-    NOT_EXIST((short)-1, null, null, null);
-
-    private short colorIndex;
+    NOT_EXIST(null, null, null);
 
     private String messageKey;
 
@@ -27,15 +25,10 @@ public enum StockOnHandStatus {
 
     private XSSFColor color;
 
-    StockOnHandStatus(short index, String key, String desc, XSSFColor color) {
-        this.colorIndex = index;
+    StockOnHandStatus(String key, String desc, XSSFColor color) {
         this.messageKey = key;
         this.description = desc;
         this.color = color;
-    }
-
-    public short getColorIndex() {
-        return colorIndex;
     }
 
     public String getMessageKey() {
