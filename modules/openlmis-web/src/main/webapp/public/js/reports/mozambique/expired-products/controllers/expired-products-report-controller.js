bac10aa14b0a4f5b807d3cce3076bb264b28a9d9
rongliu <rongliu@thoughtworks.com>
2018-10-25 17:11:17 +0800

    [SMS-96] refactor expired products report controller.

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/expired-products/controllers/expired-products-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/expired-products/controllers/expired-products-report-controller.js
@@ -1,85 +1,86 @@
-function ExpiredProductsReportController($scope, $controller, $filter, OverStockProductsService, DateFormatService, ReportGroupSortAndFilterService) {
+function ExpiredProductsReportController($scope, $controller, $filter, ExpiredProductsService, DateFormatService,
+                                         ReportGroupSortAndFilterService) {
   $controller('BaseProductReportController', {$scope: $scope});
-  $scope.formattedOverStockList = [];
-  $scope.showOverStockProductsTable = false;
-  $scope.overStockList = null;
+  $scope.formattedExpiredProductList = [];
+  $scope.showExpiredProductsTable = false;
+  $scope.expiredProductList = null;
   $scope.filterList = [];
   $scope.filterText = "";
-  $scope.isDistrictOverStock = false;
-
+  $scope.isDistrictExpiredProduct = false;
+  
   $scope.$on('$viewContentLoaded', function () {
     $scope.loadHealthFacilities();
     onLoadScrollEvent();
   });
-
+  
   $scope.loadReport = function () {
     if ($scope.validateProvince() &&
       $scope.validateDistrict() &&
       $scope.validateFacility()) {
       var reportParams = $scope.reportParams;
-
+      
       var overStockParams = {
         endTime: $filter('date')(reportParams.endTime, "yyyy-MM-dd") + " 23:59:59",
         provinceId: reportParams.provinceId.toString(),
         districtId: reportParams.districtId.toString(),
         facilityId: reportParams.facilityId.toString()
       };
-
+      
       $scope.isDistrictOverStock = utils.isEmpty(reportParams.districtId);
       $scope.formattedOverStockList = [];
-
-      OverStockProductsService
-        .getOverStockProductList()
+      
+      ExpiredProductsService
+        .getExpiredProductList()
         .get(_.pick(overStockParams, function (param) {
           if (!utils.isEmpty(param)) {
             return param;
           }
         }), {}, function (overStockResponse) {
-        $scope.showOverStockProductsTable = true;
-        $scope.formattedOverStockList = formatOverStockList(overStockResponse.rnr_list);
-        $scope.showOverStockProductsTable = overStockResponse.rnr_list.length;
-        $scope.overStockList = formatOverStockProductListTime(overStockResponse.rnr_list);
-        $scope.filterAndSort();
-      });
+          $scope.showOverStockProductsTable = true;
+          $scope.formattedOverStockList = formatOverStockList(overStockResponse.rnr_list);
+          $scope.showOverStockProductsTable = overStockResponse.rnr_list.length;
+          $scope.overStockList = formatOverStockProductListTime(overStockResponse.rnr_list);
+          $scope.filterAndSort();
+        });
     }
   };
-
+  
   $scope.exportXLSX = function () {
     var reportParams = $scope.reportParams;
-
-    OverStockProductsService.getDataForExport(
+    
+    ExpiredProductsService.getDataForExport(
       reportParams.provinceId.toString(),
       reportParams.districtId.toString(),
       reportParams.facilityId.toString(),
       $filter('date')(reportParams.endTime, "yyyy-MM-dd") + " 23:59:59"
     );
   };
-
+  
   var sortList = ['lotNumber', 'expiryDate', 'stockOnHandOfLot'];
   $scope.filterAndSort = function () {
-    $scope.filterList = ReportGroupSortAndFilterService.search($scope.overStockList, $scope.filterText, "lotList", "expiryDate");
+    $scope.filterList = ReportGroupSortAndFilterService.search($scope.expiredProductList, $scope.filterText, "lotList", "expiryDate");
     $scope.filterList = ReportGroupSortAndFilterService.groupSort($scope.filterList, $scope.sortType, $scope.sortReverse, sortList);
     $scope.formattedOverStockList = formatOverStockList($scope.filterList);
-
+    
   };
-
+  
   function formatOverStockProductListTime(overStockList) {
     return _.map(overStockList, function (overStockItem) {
       overStockItem.cmm = toFixedNumber(overStockItem.cmm);
       overStockItem.mos = toFixedNumber(overStockItem.mos);
       return overStockItem;
     });
-
+    
   }
+  
   function onLoadScrollEvent() {
     var fixedBodyDom = document.getElementById("fixed-body");
     fixedBodyDom.onscroll = function () {
       var fixedBodyDomLeft = this.scrollLeft;
       document.getElementById("fixed-header").scrollLeft = fixedBodyDomLeft;
     };
-
   }
-
+  
   function formatOverStockList(overStockList) {
     var formattedOverStockList = [];
     _.forEach(overStockList, function (overStock) {
@@ -94,8 +95,8 @@ function ExpiredProductsReportController($scope, $controller, $filter, OverStock
           expiryDate: DateFormatService.formatDateWithLocale(lot.expiryDate),
           stockOnHandOfLot: lot.stockOnHandOfLot
         };
-
-        if (!$scope.isDistrictOverStock) {
+        
+        if (!$scope.isDistrictExpiredProduct) {
           _.assign(formatItem, {
             cmm: toFixedNumber(overStock.cmm),
             mos: toFixedNumber(overStock.mos),
@@ -103,28 +104,28 @@ function ExpiredProductsReportController($scope, $controller, $filter, OverStock
             isFirst: index === 0
           });
         }
-
+        
         formattedOverStockList.push(formatItem);
       });
     });
-
+    
     return formattedOverStockList;
   }
-
+  
   function toFixedNumber(originNumber) {
     if (_.isNull(originNumber)) {
       return null;
     }
-
+    
     return parseFloat(originNumber.toFixed(2));
   }
 }
 
-services.factory('OverStockProductsService', function ($resource, $filter, ReportExportExcelService, messageService) {
-  function getOverStockProductList() {
+services.factory('ExpiredProductsService', function ($resource, $filter, ReportExportExcelService, messageService) {
+  function getExpiredProductList() {
     return $resource('/reports/overstock-report', {}, {});
   }
-
+  
   function getDataForExport(provinceId, districtId, facilityId, endTime) {
     var data = {
       provinceId: provinceId,
@@ -133,7 +134,7 @@ services.factory('OverStockProductsService', function ($resource, $filter, Repor
       endTime: endTime,
       reportType: 'overStockProductReport'
     };
-
+    
     ReportExportExcelService.exportAsXlsxBackend(
       _.pick(data, function (param) {
         if (!utils.isEmpty(param)) {
@@ -141,9 +142,9 @@ services.factory('OverStockProductsService', function ($resource, $filter, Repor
         }
       }), messageService.get('report.file.over.stock.products.report'));
   }
-
+  
   return {
-    getOverStockProductList: getOverStockProductList,
+    getExpiredProductList: getExpiredProductList,
     getDataForExport: getDataForExport
   };
 });
