215da2b87a60f2b967eba6dfb1812a2e271ed585
yingguo <793832732@qq.com>
2018-10-30 15:52:16 +0800

    [SMS-98] export the product excel

--- modules/report/src/main/java/org/openlmis/report/generator/impl/ExpiredProductsReportGenerator.java
+++ modules/report/src/main/java/org/openlmis/report/generator/impl/ExpiredProductsReportGenerator.java
@@ -1,13 +1,18 @@
 package org.openlmis.report.generator.impl;
 
+import org.apache.commons.lang.StringUtils;
+import org.openlmis.core.utils.DateUtil;
 import org.openlmis.report.generator.AbstractReportModelGenerator;
+import org.openlmis.report.model.dto.LotInfo;
 import org.openlmis.report.model.dto.StockProductDto;
 import org.openlmis.report.model.params.StockReportParam;
 import org.openlmis.report.service.SimpleTableService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,22 +21,108 @@ public class ExpiredProductsReportGenerator extends AbstractReportModelGenerator
 
     private final static String KEY_QUERY_RESULT = "KEY_QUERY_RESULT";
 
+    private final static String FACILITY_PRICE_COLUMN = "8";
+
+    private final static String DISTRICT_PRICE_COLUMN = "7";
+
+    private final static String CMM_COLUMN = "9";
+
+    private final static String MOS_COLUMN = "10";
+
     @Autowired
     private SimpleTableService simpleTableService;
 
     @Override
     protected Object getReportHeaders(Map<Object, Object> paraMap, Map<String, Object> queryResult) {
-        return null;
+        Map<String, String> headers = new LinkedHashMap<>();
+        headers.put("province", getMessage("report.header.province"));
+        headers.put("district", getMessage("report.header.district"));
+        if(null != paraMap.get("districtId")){
+            headers.put("facility", getMessage("report.header.facility"));
+        }
+        headers.put("drugCode", getMessage("report.header.drug.code"));
+        headers.put("drugName", getMessage("report.header.drug.name"));
+        headers.put("lot", getMessage("report.header.lot"));
+        headers.put("expiryDate", getMessage("report.header.expiry.date"));
+        headers.put("soh", getMessage("report.header.stock.on.hand"));
+        headers.put("price", getMessage("report.header.price"));
+        if(null != paraMap.get("districtId")){
+            headers.put("cmm", getMessage("report.header.cmm"));
+            headers.put("MoS", getMessage("report.header.MoS"));
+        }
+        return headers;
     }
 
     @Override
     protected Object getReportContent(Map<Object, Object> paraMap, Map<String, Object> queryResult) {
-        return null;
+        List<StockProductDto> stockProductDtoList = (List<StockProductDto>) queryResult.get(KEY_QUERY_RESULT);
+        List<Map<String, Object>> content = new ArrayList<>();
+        for (StockProductDto dto : stockProductDtoList) {
+            for (LotInfo lotinfo : dto.getLotList()) {
+                Map<String, Object> rowMap = new HashMap<>();
+                rowMap.put("province", dto.getProvinceName());
+                rowMap.put("district", dto.getDistrictName());
+                rowMap.put("drugCode", dto.getProductCode());
+                rowMap.put("drugName", dto.getProductName());
+                rowMap.put("lot", lotinfo.getLotNumber());
+
+                Map<String, Object> tmpValue = new HashMap<>();
+                tmpValue.put("value", DateUtil.formatDate(lotinfo.getExpiryDate()));
+                Map<String, Object> styleMap = new HashMap<>();
+                styleMap.put("dataPattern", DateUtil.FORMAT_DATE_TIME);
+                styleMap.put("excelDataPattern", "m/d/yy");
+                tmpValue.put("style", styleMap);
+                tmpValue.put("dataType", "date");
+                rowMap.put("expiryDate", tmpValue);
+
+                rowMap.put("soh", lotinfo.getStockOnHandOfLot().toString());
+                rowMap.put("price", dto.getPrice());
+                if(null != paraMap.get("districtId")){
+                    rowMap.put("facility", dto.getFacilityName());
+                    rowMap.put("cmm", getFormatDoubleValue(dto.getCmm()));
+                    rowMap.put("MoS", getFormatDoubleValue(dto.getMos()));
+                }
+                content.add(rowMap);
+            }
+        }
+
+        return content;
     }
 
     @Override
     protected List<Map<String, String>> getReportMergedRegions(Map<Object, Object> paraMap, Map<String, Object> queryResult) {
-        return null;
+        List<StockProductDto> stockProductDtoList = (List<StockProductDto>) queryResult.get(KEY_QUERY_RESULT);
+        Map<String, String> mergedColumns = getMergedColumns(paraMap);
+
+        return getMergedRegions(stockProductDtoList, mergedColumns);
+    }
+
+    private Map<String, String> getMergedColumns(Map<Object, Object> paraMap) {
+        Map<String, String> mergedColumns = new HashMap<>();
+        if(null == paraMap.get("districtId") || StringUtils.isBlank(paraMap.get("districtId").toString())) {
+            mergedColumns.put("DISTRICT_PRICE_COLUMN", DISTRICT_PRICE_COLUMN);
+        } else {
+            mergedColumns.put("FACILITY_PRICE_COLUMN", FACILITY_PRICE_COLUMN);
+            mergedColumns.put("CMM_COLUMN", CMM_COLUMN);
+            mergedColumns.put("MOS_COLUMN", MOS_COLUMN);
+        }
+
+        return mergedColumns;
+    }
+
+    private List<Map<String, String>> getMergedRegions(List<StockProductDto> stockProductDtoList, Map<String, String> mergedColumns) {
+        List<Map<String, String>> mergedRegions = new ArrayList<>();
+        int index = 0;
+
+        for (StockProductDto dto : stockProductDtoList) {
+            int cmmSpan = dto.getLotList().size();
+            for(Map.Entry<String, String> entry : mergedColumns.entrySet()) {
+                mergedRegions.add(createMergedRegion(String.valueOf(index + 1), String.valueOf(index + cmmSpan), entry.getValue(), entry.getValue(), getFormatDoubleValue(dto.getCmm())));
+            }
+            index = index + cmmSpan;
+        }
+
+        return mergedRegions;
     }
 
     @Override
