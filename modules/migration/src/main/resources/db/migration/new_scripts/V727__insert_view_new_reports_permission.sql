cb1741108bb0733fa5dac5be2011b1c2e00eeed2
jhwang <332409662@qq.com>
2018-11-05 17:49:03 +0800

    [SMS-194] ADD rights for new report(over stock, expired, expiring, al).

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V727__insert_view_new_reports_permission.sql
@@ -0,0 +1,11 @@
+INSERT INTO rights (name, rightType, description, displaynamekey) VALUES
+('VIEW_OVER_STOCK_PRODUCTS_REPORT','REPORT','Permission to View over stock products Report', 'right.report.over.stock.products.report');
+
+INSERT INTO rights (name, rightType, description, displaynamekey) VALUES
+('VIEW_EXPIRED_PRODUCTS_REPORT','REPORT','Permission to View expired products Report', 'right.report.expired.products.report');
+
+INSERT INTO rights (name, rightType, description, displaynamekey) VALUES
+('VIEW_EXPIRING_PRODUCTS_REPORT','REPORT','Permission to View expiring products Report', 'right.report.expiring.products.report');
+
+INSERT INTO rights (name, rightType, description, displaynamekey) VALUES
+('VIEW_AL_REPORT','REPORT','Permission to View AL Report', 'right.report.al.report');
