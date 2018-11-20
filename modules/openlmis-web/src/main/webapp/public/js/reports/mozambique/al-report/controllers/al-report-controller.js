fcbfe8a63b264d34d9d485006aa56c43cf85b58f
rongliu <rongliu@thoughtworks.com>
2018-11-20 15:30:46 +0800

    [SMS-105] add Portuguese version for al report.

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/al-report/controllers/al-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/al-report/controllers/al-report-controller.js
@@ -2,5 +2,10 @@ function ALReportController($scope, $controller) {
   $controller("BaseProductReportController", {$scope: $scope});
 
   $scope.$on('$viewContentLoaded', function () {
+    $scope.loadHealthFacilities();
   });
+  
+  $scope.loadReport = function () {
+    console.log('load al report controller');
+  }
 }
