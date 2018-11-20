26639d64c9fef1370a15ca46cdde7a9c474d8fe2
rongliu <rongliu@thoughtworks.com>
2018-11-20 17:54:56 +0800

    [SMS-105] show the new al report on web port.

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/al-report/controllers/al-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/al-report/controllers/al-report-controller.js
@@ -1,11 +1,43 @@
-function ALReportController($scope, $controller) {
+function ALReportController($scope, $controller, $filter, ReportDataServices) {
   $controller("BaseProductReportController", {$scope: $scope});
-
+  
   $scope.$on('$viewContentLoaded', function () {
     $scope.loadHealthFacilities();
   });
   
   $scope.loadReport = function () {
-    console.log('load al report controller');
+    if ($scope.validateProvince() &&
+      $scope.validateDistrict() &&
+      $scope.validateFacility()) {
+      var reportParams = $scope.reportParams;
+      
+      var alReportParams = {
+        startTime: $filter('date')(reportParams.startTime, "yyyy-MM-dd") + " 23:59:59",
+        endTime: $filter('date')(reportParams.endTime, "yyyy-MM-dd") + " 23:59:59",
+        provinceId: reportParams.provinceId.toString(),
+        districtId: reportParams.districtId.toString(),
+        facilityId: reportParams.facilityId.toString(),
+        reportType: 'alReport'
+      };
+      
+      ReportDataServices
+        .getProductList()
+        .post(utils.pickEmptyObject(alReportParams), function (alReportResponse) {
+          $scope.alReportObject = formatALResponseData(alReportResponse.data);
+          $scope.responseNoData = _.isEmpty($scope.alReportObject);
+        });
+    }
+  };
+  
+  function formatALResponseData(responseData) {
+    if (responseData.length >= 3) {
+      return {
+        hf: responseData[0].splice(1, 8),
+        chw: responseData[1].splice(1, 8),
+        total: responseData[2].splice(1, 8)
+      };
+    }
+    
+    return {};
   }
 }
