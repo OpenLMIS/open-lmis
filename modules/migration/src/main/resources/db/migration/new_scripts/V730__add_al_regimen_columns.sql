3f148d85424bf99cbc847f95ddcb0a8ab7aa9ef9
Zhe Bao <175539358@qq.com>
2018-11-21 14:43:28 +0800

    [SMS-105] Web: Create an new AL report on Web Portal

--- modules/migration/src/main/resources/db/migration/new_scripts/V730__add_al_regimen_columns.sql
+++
@@ -1,6 +0,0 @@
-alter table program_regimen_columns drop constraint program_regimen_columns_programid_fkey;
-DELETE FROM program_regimen_columns WHERE programId = 5;
-INSERT INTO program_regimen_columns (programId, name, label, visible, dataType, displayOrder) VALUES (5, 'patientsOnTreatment', 'Number of patients on treatment', true, 'regimen.reporting.dataType.numeric', 3);
-INSERT INTO program_regimen_columns (programId, name, label, visible, dataType, displayOrder) VALUES (5, 'hf', 'hf', true, 'regimen.reporting.dataType.numeric', 4);
-INSERT INTO program_regimen_columns (programId, name, label, visible, dataType, displayOrder) VALUES (5, 'chw', 'chw', true, 'regimen.reporting.dataType.numeric', 5);
-alter table program_regimen_columns add constraint program_regimen_columns_programid_fkey foreign key(programid) references programs(id);
