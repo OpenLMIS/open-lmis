f41c44fcd0795c740043a942055e2390f8ea5e9e
rongliu <rongliu@thoughtworks.com>
2018-11-27 09:50:12 +0800

    [SMS-106] fixed date formatted on al reports.

--- modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/al-report/controllers/al-report-controller.js
+++ modules/openlmis-web/src/main/webapp/public/js/reports/mozambique/al-report/controllers/al-report-controller.js
@@ -13,8 +13,8 @@ function ALReportController($scope, $controller, $filter, ReportDataServices, me
       var reportParams = $scope.reportParams;
       
       var alReportParams = {
-        startTime: $filter('date')(reportParams.startTime, "yyyy-MM-dd") + " 23:59:59",
-        endTime: $filter('date')(reportParams.endTime, "yyyy-MM-dd") + " 23:59:59",
+        startTime: $filter('date')(reportParams.startTime, 'yyyy-MM-dd') + ' 00:00:00',
+        endTime: $filter('date')(reportParams.endTime, 'yyyy-MM-dd') + ' 23:59:59',
         provinceId: reportParams.provinceId.toString(),
         districtId: reportParams.districtId.toString(),
         facilityId: reportParams.facilityId.toString(),
@@ -38,8 +38,8 @@ function ALReportController($scope, $controller, $filter, ReportDataServices, me
       provinceId: reportParams.provinceId.toString(),
       districtId: reportParams.districtId.toString(),
       facilityId: reportParams.facilityId.toString(),
-      startTime: $filter('date')(reportParams.startTime, "yyyy-MM-dd") + " 23:59:59",
-      endTime: $filter('date')(reportParams.endTime, "yyyy-MM-dd") + " 23:59:59",
+      startTime: $filter('date')(reportParams.startTime, 'yyyy-MM-dd') + ' 00:00:00',
+      endTime: $filter('date')(reportParams.endTime, 'yyyy-MM-dd') + ' 23:59:59',
       reportType: 'alReport',
       facility: reportParams.facilityName,
       district: reportParams.districtName,
