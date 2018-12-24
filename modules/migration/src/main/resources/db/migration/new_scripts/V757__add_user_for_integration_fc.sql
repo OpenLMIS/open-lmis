165dc3ac624d72dd02f00901046f018c4253e838
anyi <superwingcool@gmail.com>
2018-12-24 11:18:13 +0800

    [SMS-113] add Integration account

---
+++ modules/migration/src/main/resources/db/migration/new_scripts/V757__add_user_for_integration_fc.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(username, password, firstname, lastname, restrictlogin, email, verified, active, createdby, modifiedby, ismobileuser)
+    VALUES ('integration', 'qjjDbjxwpCPyISfgW8qI5ZiitRsOJkLd142srFkPPDqvblXHPbo1DYhNdpgDnNSipI1aHX5up4Ns8YqbsdWnipVqgieie',
+    'integration', 'FC', true, 'integration@email.com', true, true, 1, 1, true);
