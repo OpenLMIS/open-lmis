30b04b1b68311f22f626d0569258aa04a8b302b7
yingguo <793832732@qq.com>
2019-01-09 10:45:02 +0800

    [SMS-341] get chw and hf for al report

--- modules/rest-api/src/main/java/org/openlmis/restapi/domain/RegimenLineItemForRest.java
+++ modules/rest-api/src/main/java/org/openlmis/restapi/domain/RegimenLineItemForRest.java
@@ -19,14 +19,18 @@ import static com.fasterxml.jackson.databind.annotation.JsonSerialize.Inclusion.
 public class RegimenLineItemForRest extends RegimenLineItem {
     private String categoryName;
 
-    public RegimenLineItemForRest(String code, String name, Integer patientsOnTreatment, String categoryName) {
+    public RegimenLineItemForRest(String code, String name, Integer patientsOnTreatment, String categoryName, Integer hf, Integer chw) {
         this.setCode(code);
         this.setName(name);
         this.setPatientsOnTreatment(patientsOnTreatment);
         this.categoryName = categoryName;
+        this.setHf(hf);
+        this.setChw(chw);
     }
 
     public static RegimenLineItemForRest convertFromRegimenLineItem(RegimenLineItem regimenLineItem) {
-        return new RegimenLineItemForRest(regimenLineItem.getCode(), regimenLineItem.getName(), regimenLineItem.getPatientsOnTreatment(), regimenLineItem.getCategory().getName());
+        return new RegimenLineItemForRest(regimenLineItem.getCode(), regimenLineItem.getName(),
+                regimenLineItem.getPatientsOnTreatment(), regimenLineItem.getCategory().getName(),
+                regimenLineItem.getHf(), regimenLineItem.getChw());
     }
 }
