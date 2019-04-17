1f909390ad721040320e35f00ea776ffc2c0700c
jinghui.zhang <jinghui.zhang@thoughtworks.com>
2019-04-17 17:28:48 +0800

    [update]

--- modules/rest-api/src/test/java/org/openlmis/restapi/controller/RestStockCardControllerTest.java
+++ modules/rest-api/src/test/java/org/openlmis/restapi/controller/RestStockCardControllerTest.java
@@ -70,7 +70,7 @@ public class RestStockCardControllerTest {
     ResponseEntity<RestResponse> expectedResponse = new ResponseEntity<>(new RestResponse(SUCCESS, successMsg), HttpStatus.OK);
     when(RestResponse.success(successMsg)).thenReturn(expectedResponse);
 
-    ResponseEntity<RestResponse> response = restStockCardController.adjustStock(facilityId, stockEventList, principal);
+    ResponseEntity<RestResponse> response = restStockCardController.adjustStock(facilityId,"86", stockEventList, principal);
 
     verify(restStockCardService).adjustStock(facilityId, stockEventList, userId);
     assertThat(response.getStatusCode(), is(HttpStatus.OK));
@@ -88,7 +88,7 @@ public class RestStockCardControllerTest {
     ResponseEntity<RestResponse> expectedResponse = new ResponseEntity<>(new RestResponse(ERROR, "invalid data"), HttpStatus.BAD_REQUEST);
     when(error(dataException.getOpenLmisMessage(), HttpStatus.BAD_REQUEST)).thenReturn(expectedResponse);
 
-    ResponseEntity<RestResponse> response = restStockCardController.adjustStock(facilityId, stockEventList, principal);
+    ResponseEntity<RestResponse> response = restStockCardController.adjustStock(facilityId,"86", stockEventList, principal);
 
     assertThat(response.getStatusCode(), is(HttpStatus.BAD_REQUEST));
     assertThat((String) response.getBody().getData().get(ERROR), is("invalid data"));
